630d69614a9067068d7f7a202111bdb49248c38e
ywsing <stupidsing@gmail.com>
2010-06-26 22:43:57 +0800

    remove log4j init from test case

--- src/test/java/org/fp/InterpreterTest.java
+++ src/test/java/org/fp/InterpreterTest.java
@@ -7,7 +7,6 @@ import org.suite.doer.Parser;
 import org.suite.node.Atom;
 import org.suite.node.Int;
 import org.suite.node.Node;
-import org.util.LogUtil;
 
 public class InterpreterTest {
 
@@ -22,8 +21,6 @@ public class InterpreterTest {
 
 	@Test
 	public void testFibonacci() {
-		LogUtil.initLog4j();
-
 		addFunction("fib",
 				"n => if (n = 0; n = 1) then 1 else (fib / (n - 1) + fib / (n - 2))");
 		assertEquals(Int.create(89), evaluate("fib / 10"));
