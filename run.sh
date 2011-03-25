f1ee596c9d2d9e1a82fb430ace7010f7c63f66ef
stupidsing <stupidsing@gmail.com>
2011-03-26 00:21:04 +0800

    allow history in run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd "`dirname ${0}`" &&
 LIBS=$(cat .classpath | grep var | cut -d\" -f4 | tr \\n : | sed s/M2_REPO/\\/home\\/ywsing\\/.m2\\/repository/g) &&
-rlwrap -H /dev/null java -cp ${LIBS}:target/suite-1.0.jar org.suite.Main
+rlwrap -H ~/.suite_history java -cp ${LIBS}:target/suite-1.0.jar org.suite.Main
