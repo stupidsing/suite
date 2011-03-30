f8f4436cd208ba4a937e7d6df59d0ad64e14fcab
stupidsing <stupidsing@gmail.com>
2011-03-30 11:36:36 +0800

    enables jvm debug port

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+
 cd "`dirname ${0}`" &&
 LIBS=$(cat .classpath | grep var | cut -d\" -f4 | tr \\n : | sed s/M2_REPO/\\/home\\/ywsing\\/.m2\\/repository/g) &&
-rlwrap -H ~/.suite_history java -cp ${LIBS}:target/suite-1.0.jar org.suite.Main
+rlwrap -H ~/.suite_history java -cp ${LIBS}:target/suite-1.0.jar ${DEBUGOPTS} org.suite.Main
