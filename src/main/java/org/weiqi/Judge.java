d1e7c0192234824e6db3aa9a3fa4d085c2a319ba
stupidsing <stupidsing@gmail.com>
2011-05-02 18:58:50 +0800

    spacings

--- src/main/java/org/weiqi/Judge.java
+++ src/main/java/org/weiqi/Judge.java
@@ -13,12 +13,15 @@ public class Judge {
 
 	public static Occupation checkByOccupationExistence(Board board) {
 		Set<Occupation> players = new HashSet<Occupation>();
+
 		for (Coordinate c : Coordinate.getAll())
 			players.add(board.get(c));
+
 		for (Occupation player : Weiqi.players)
 			if (players.contains(player)
 					&& !players.contains(player.opponent()))
 				return player;
+
 		return null;
 	}
 
