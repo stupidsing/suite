838651f0bc2cf0b1053cb8b22e49bfff36eef0aa
stupidsing <stupidsing@gmail.com>
2011-05-02 20:45:45 +0800

    display board in O/X

--- src/main/java/org/weiqi/UserInterface.java
+++ src/main/java/org/weiqi/UserInterface.java
@@ -1,12 +1,28 @@
 package org.weiqi;
 
+import org.weiqi.Weiqi.Occupation;
+
 public class UserInterface {
 
 	public static void display(Board board) {
 		for (int x = 0; x < Weiqi.SIZE; x++) {
-			for (int y = 0; y < Weiqi.SIZE; y++)
-				System.out.print(board.get(new Coordinate(x, y)) + " ");
+			for (int y = 0; y < Weiqi.SIZE; y++) {
+				Coordinate c = new Coordinate(x, y);
+				System.out.print(display(board.get(c)) + " ");
+			}
+
 			System.out.println();
+		}
+	}
+
+	public static String display(Occupation color) {
+		switch (color) {
+		case BLACK:
+			return "X";
+		case WHITE:
+			return "O";
+		default:
+			return " ";
 		}
 	}
 }
