c366af512307e1fc65c0fbe79f6537ef822013cd
stupidsing <stupidsing@gmail.com>
2011-05-08 10:23:58 +0800

    add evaluator test

---
+++ src/test/java/org/weiqi/EvaluatorTest.java
@@ -0,0 +1,41 @@
+package org.weiqi;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.weiqi.Weiqi.Occupation;
+
+public class EvaluatorTest {
+
+	@Test
+	public void testOneWhite() {
+		Board board = new Board();
+		board.set(Coordinate.c(9, 9), Occupation.WHITE);
+		assertEquals(36110, Evaluator.evaluate(Occupation.WHITE, board));
+		assertEquals(-36110, Evaluator.evaluate(Occupation.BLACK, board));
+	}
+
+	@Test
+	public void testEqualPower() {
+		Board board = new Board();
+		board.set(Coordinate.c(3, 3), Occupation.WHITE);
+		board.set(Coordinate.c(15, 15), Occupation.BLACK);
+		assertEquals(0, Evaluator.evaluate(Occupation.WHITE, board));
+		assertEquals(0, Evaluator.evaluate(Occupation.BLACK, board));
+	}
+
+	@Test
+	public void testTerritory() {
+		Board board = new Board();
+		board.set(Coordinate.c(0, 3), Occupation.WHITE);
+		board.set(Coordinate.c(1, 3), Occupation.WHITE);
+		board.set(Coordinate.c(2, 3), Occupation.WHITE);
+		board.set(Coordinate.c(3, 2), Occupation.WHITE);
+		board.set(Coordinate.c(3, 1), Occupation.WHITE);
+		board.set(Coordinate.c(3, 0), Occupation.WHITE);
+		board.set(Coordinate.c(15, 15), Occupation.BLACK);
+		assertEquals(1450, Evaluator.evaluate(Occupation.WHITE, board));
+		assertEquals(-1450, Evaluator.evaluate(Occupation.BLACK, board));
+	}
+
+}
