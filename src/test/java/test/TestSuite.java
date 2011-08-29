727c5a91832f6aff4a7111d70f44a00b4f007722
stupidsing <stupidsing@gmail.com>
2011-08-29 21:04:58 +0800

    reduce suite sample

--- src/test/java/test/TestSuite.java
+++ src/test/java/test/TestSuite.java
@@ -1,21 +1,14 @@
 package test;
 
 import org.apache.log4j.Level;
-import org.btree.B_TreeTest;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.suite.ComparerTest;
-import org.suite.FormatterTest;
-import org.suite.ImportTest;
-import org.suite.ParserTest;
 import org.suite.ProverTest;
 import org.util.LogUtil;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { B_TreeTest.class, ComparerTest.class,
-		FormatterTest.class, ImportTest.class, ParserTest.class,
-		ProverTest.class })
+@Suite.SuiteClasses({ ProverTest.class })
 public class TestSuite {
 
 	@Before
