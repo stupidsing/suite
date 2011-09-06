247f0f81986238af6787caec0841e8a7005c9a00
U-903-CLUB-BOX\903club <903club@903-club-box.(none)>
2011-09-06 21:41:00 +0800

    refactor test package name also

--- src/test/java/org/instructionexecutor/FunctionCompilerTest.java
+++ src/test/java/org/instructionexecutor/FunctionCompilerTest.java
@@ -1,4 +1,4 @@
-package org.instructioncode;
+package org.instructionexecutor;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
--- src/test/java/org/instructionexecutor/LogicCompilerTest.java
+++ src/test/java/org/instructionexecutor/LogicCompilerTest.java
@@ -1,4 +1,4 @@
-package org.instructioncode;
+package org.instructionexecutor;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
