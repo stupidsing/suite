05df7449067579fd0573cde251b1e9c35585e2ba
stupidsing <stupidsing@gmail.com>
2012-01-20 17:51:11 +0800

    fix alphabeta for tictactoe

--- src/main/java/org/ai/AlphabetaSearch.java
+++ src/main/java/org/ai/AlphabetaSearch.java
@@ -1,8 +1,11 @@
 package org.ai;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class AlphabetaSearch {
+import org.util.Util.Pair;
+
+public class AlphabetaSearch<State> {
 
 	public interface Game<State> {
 		public List<State> generate(State state);
@@ -10,27 +13,50 @@ public class AlphabetaSearch {
 		public int evaluate(State state);
 	}
 
-	public static <State> int search(Game<State> game //
-			, State state, int depth //
+	private Game<State> game;
+	private List<State> moves = new ArrayList<State>();
+
+	public AlphabetaSearch(Game<State> game) {
+		this.game = game;
+	}
+
+	public List<State> search(State state, int depth) {
+		moves.clear();
+		return search0(state, depth, 1 + Integer.MIN_VALUE, Integer.MAX_VALUE).t2;
+	}
+
+	public Pair<Integer, List<State>> search0(State state, int depth //
 			, int alpha, int beta) {
 		if (depth > 0) {
 			List<State> states = game.generate(state);
 
 			if (!states.isEmpty()) {
-				for (State state1 : states) {
-					int score = -search(game, state1, depth - 1, -beta, -alpha);
+				List<State> principalVariation = null;
 
-					if (score <= beta)
-						beta = Math.max(score, beta);
-					else
-						return score;
+				for (State state1 : states) {
+					moves.add(state1);
+
+					Pair<Integer, List<State>> result = search0(state1,
+							depth - 1, -beta, -alpha);
+					int score = -result.t1;
+
+					if (score > alpha) {
+						alpha = score;
+						principalVariation = result.t2;
+					}
+
+					moves.remove(moves.size() - 1);
+
+					if (score > beta)
+						break;
 				}
 
-				return beta;
+				return Pair.create(alpha, principalVariation);
 			}
 		}
 
-		return game.evaluate(state);
+		List<State> moves1 = new ArrayList<State>(moves);
+		return Pair.create(game.evaluate(state), moves1);
 	}
 
 }
