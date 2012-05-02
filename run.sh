da416d5bd90851cb64ffb8e301002683bf1876ce
stupidsing <stupidsing@gmail.com>
2012-05-02 10:17:04 +0800

    change stack size

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # Builds by mvn -Dmaven.test.skip=true assembly:single
 
 DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+OPTS="-Xss1m ${DEBUGOPTS}"
 
 cd "`dirname ${0}`" &&
-rlwrap -H ~/.suite_history java ${DEBUGOPTS} -jar target/suite-1.0-jar-with-dependencies.jar $*
+rlwrap -H ~/.suite_history java ${OPTS} -jar target/suite-1.0-jar-with-dependencies.jar $*
