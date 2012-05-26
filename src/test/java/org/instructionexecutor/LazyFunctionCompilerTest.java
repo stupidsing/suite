429920f7909fd2447bcb31e4125f5de77dc38912
Sing Yiu Wah <ywsing@lenovo.(none)>
2012-05-26 10:13:33 +0800

    refactor lines

--- src/test/java/org/instructionexecutor/LazyFunctionCompilerTest.java
+++ src/test/java/org/instructionexecutor/LazyFunctionCompilerTest.java
@@ -57,9 +57,10 @@ public class LazyFunctionCompilerTest {
 
 	@Test
 	public void testString() {
-		assertEquals(Int.create(-34253924), eval("str-to-int {\"-34253924\"}"));
-		assertEquals(Atom.create("true"),
-				eval("equals {\"-34253924\"} {int-to-str {-34253924}}"));
+		assertEquals(Int.create(-34253924), eval("" //
+				+ "str-to-int {\"-34253924\"}"));
+		assertEquals(Atom.create("true"), eval("" //
+				+ "equals {\"-34253924\"} {int-to-str {-34253924}}"));
 	}
 
 	@Test
