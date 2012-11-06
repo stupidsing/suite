180763a951cd999b38052a168bcd4888b2c07bf4
ywsing <stupidsing@gmail.com>
2012-11-06 10:44:33 +0800

    avoid changing directory in run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@
 DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 OPTS="-Xss1m ${DEBUGOPTS}"
 
-cd "`dirname ${0}`" &&
-rlwrap -H ~/.suite_history java ${OPTS} -jar target/suite-1.0-jar-with-dependencies.jar $*
+rlwrap -H ${HOME}/.suite_history java ${OPTS} -jar "`dirname ${0}`/target/suite-1.0-jar-with-dependencies.jar" $*
