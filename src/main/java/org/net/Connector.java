4fc79c8819990b3ae64e2d9d042af80816684cfd
ywsing <stupidsing@gmail.com>
2012-11-09 16:14:01 +0800

    use java 7 try syntax

--- src/main/java/org/net/Connector.java
+++ src/main/java/org/net/Connector.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -38,23 +39,15 @@ public class Connector {
 
 				executor.execute(new Runnable() {
 					public void run() {
-						BufferedReader reader = null;
-						PrintWriter writer = null;
-
-						try {
-							OutputStream os = socket.getOutputStream();
-							InputStream is = socket.getInputStream();
-							InputStreamReader isr = new InputStreamReader(is);
-							reader = new BufferedReader(isr);
-							writer = new PrintWriter(os);
-
+						try (OutputStream os = socket.getOutputStream();
+								InputStream is = socket.getInputStream();
+								InputStreamReader isr = new InputStreamReader(
+										is);
+								Reader reader = new BufferedReader(isr);
+								PrintWriter writer = new PrintWriter(os);) {
 							writer.println("Hello World");
 						} catch (IOException ex) {
 							LogUtil.error(Connector.this.getClass(), ex);
-						} finally {
-							Util.closeQuietly(reader);
-							Util.closeQuietly(writer);
-							Util.closeQuietly(socket);
 						}
 					}
 				});
