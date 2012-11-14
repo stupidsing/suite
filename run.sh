71ee2365d70fd12845fc43ccea59c9d24cbee974
Sing Yiu Wah <ywsing@lenovo.(none)>
2012-11-14 09:55:06 +0800

    refactor run script

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@
 
 DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 OPTS="-Xss1m ${DEBUGOPTS}"
+JAR="`dirname ${0}`/target/suite-1.0-jar-with-dependencies.jar"
 
-rlwrap -H ${HOME}/.suite_history java ${OPTS} -jar "`dirname ${0}`/target/suite-1.0-jar-with-dependencies.jar" $*
+rlwrap -H ${HOME}/.suite_history java ${OPTS} -jar "${JAR}" $*
