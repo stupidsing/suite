8a803907576bd2290abcbb0ac916bca4a3904ba5
ywsing <stupidsing@gmail.com>
2013-01-10 11:49:24 +0800

    more pretty printing

--- src/main/java/org/suite/doer/PrettyPrinter.java
+++ src/main/java/org/suite/doer/PrettyPrinter.java
@@ -1,7 +1,10 @@
 package org.suite.doer;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.parser.Operator;
 import org.parser.Operator.Assoc;
@@ -20,6 +23,11 @@ public class PrettyPrinter {
 
 	private static final int LINELENGTH = 80; // Estimated
 	private static final String INDENTSPACES = "    ";
+	private static final Set<Node> LINEBREAKERKEYWORDS = new HashSet<Node>(
+			Arrays.asList(Atom.create("else-if")));
+
+	private static final Set<Operator> LINEBREAKAFTEROPERATORS = new HashSet<Operator>(
+			Arrays.asList(TermOp.CONTD_, TermOp.FUN___));
 
 	public String prettyPrint(Node node) {
 		estimateLengths(node);
@@ -28,7 +36,8 @@ public class PrettyPrinter {
 	}
 
 	private void prettyPrint0(Node node, int prec0) {
-		int x = getX(), y = nLines;
+		node = node.finalNode();
+		int x = getX(), y = getY();
 		int length = getEstimatedLength(node);
 
 		// Line too long?
@@ -48,8 +57,11 @@ public class PrettyPrinter {
 					Node left = tree.getLeft();
 					Node right = tree.getRight();
 					Assoc assoc = op.getAssoc();
+
 					int leftPrec = prec - (assoc == Assoc.LEFT ? 1 : 0);
 					int rightPrec = prec - (assoc == Assoc.RIGHT ? 1 : 0);
+					if (op == TermOp.BRACES)
+						leftPrec = rightPrec = 0;
 
 					Tree tree1 = Tree.decompose(right, op);
 					Node r0 = tree1 != null ? tree1.getLeft() : null;
@@ -62,9 +74,9 @@ public class PrettyPrinter {
 							&& es1 != null //
 							&& x + es0 + es1 + opLength < LINELENGTH) {
 						prettyPrint0(left, leftPrec);
-						append(op.getName());
+						int yOp = appendOperator(op);
 						prettyPrint0(right, rightPrec);
-						closeBraces(op);
+						closeBraces(op, yOp);
 					} else { // Breaks after the operator
 						boolean incRightIndent = Tree.decompose(right, op) == null;
 
@@ -73,9 +85,9 @@ public class PrettyPrinter {
 						if (incRightIndent)
 							incrementIndent();
 
-						appendOperator(op);
+						int yOp = appendOperatorLineFeed(op);
 						prettyPrint0(right, rightPrec);
-						closeBraces(op);
+						closeBraces(op, yOp);
 
 						if (incRightIndent)
 							decrementIndent();
@@ -85,12 +97,16 @@ public class PrettyPrinter {
 				append(Formatter.dump(node));
 
 			if (needPars) {
-				if (y != nLines)
+				if (y != getY())
 					nl();
 				append(")");
 			}
-		} else
+		} else {
+			if (LINEBREAKERKEYWORDS.contains(node) && !isLineBegin())
+				nl();
+
 			append(Formatter.dump(node)); // Space sufficient
+		}
 	}
 
 	private void prettyPrintList(Operator op, Node node) {
@@ -107,14 +123,14 @@ public class PrettyPrinter {
 				else
 					prettyPrint0(tree.getLeft(), prec1);
 
-				appendOperator(op);
+				int yOp = appendOperatorLineFeed(op);
 
 				if (!isLeftAssoc)
 					prettyPrintList(op, tree.getRight());
 				else
 					prettyPrint0(tree.getRight(), prec1);
 
-				closeBraces(op);
+				closeBraces(op, yOp);
 				return;
 			}
 		}
@@ -122,9 +138,12 @@ public class PrettyPrinter {
 		prettyPrint0(node, prec);
 	}
 
-	private void closeBraces(Operator op) {
-		if (op == TermOp.BRACES)
+	private void closeBraces(Operator op, int y) {
+		if (op == TermOp.BRACES) {
+			if (y != getY())
+				nl();
 			append("}");
+		}
 	}
 
 	private int estimateLengths(Node node) {
@@ -132,6 +151,7 @@ public class PrettyPrinter {
 	}
 
 	private int estimateLengths(Node node, int prec0) {
+		node = node.finalNode();
 		int key = getKey(node);
 		Integer length = lengthByIds.get(key);
 
@@ -140,11 +160,15 @@ public class PrettyPrinter {
 
 			if (node instanceof Tree) {
 				Tree tree = (Tree) node;
+
 				Operator op = tree.getOperator();
 				int prec = op.getPrecedence();
 				Assoc assoc = op.getAssoc();
 				int leftPrec = prec - (assoc == Assoc.LEFT ? 1 : 0);
 				int rightPrec = prec - (assoc == Assoc.RIGHT ? 1 : 0);
+				if (op == TermOp.BRACES)
+					leftPrec = rightPrec = 0;
+
 				int len0 = estimateLengths(tree.getLeft(), leftPrec);
 				int len1 = estimateLengths(tree.getRight(), rightPrec);
 				int opLength = op.getName().length();
@@ -180,15 +204,23 @@ public class PrettyPrinter {
 				&& (op == TermOp.AND___ || op == TermOp.OR____ || node == Atom.nil);
 	}
 
-	private void appendOperator(Operator op) {
-		String opName = op.getName();
-		if (op == TermOp.AND___ || op == TermOp.OR____)
-			opName = opName + " ";
-
-		nl();
-		append(FormatUtil.leftTrim(opName));
-		if (op == TermOp.NEXT__)
+	private int appendOperatorLineFeed(Operator op) {
+		boolean lineBreakAfterOp = LINEBREAKAFTEROPERATORS.contains(op);
+		if (!lineBreakAfterOp)
 			nl();
+		int y = appendOperator(op);
+		if (lineBreakAfterOp || op == TermOp.NEXT__)
+			nl();
+		return y;
+	}
+
+	private int appendOperator(Operator op) {
+		String name = op.getName();
+		name += op == TermOp.AND___ || op == TermOp.OR____ ? " " : "";
+		if (isLineBegin())
+			name = FormatUtil.leftTrim(name);
+		append(name);
+		return getY();
 	}
 
 	private void incrementIndent() {
@@ -202,30 +234,32 @@ public class PrettyPrinter {
 	}
 
 	private boolean isLineBegin() {
-		int pos = sb.length();
-
-		while (--pos > 0) {
-			char c = sb.charAt(pos);
-			if (!Character.isWhitespace(c))
-				return false;
-			if (c == '\n')
-				break;
-		}
-
-		return true;
+		boolean result = true;
+		String l = sb.substring(getLineBeginPosition(), sb.length());
+		for (char c : l.toCharArray())
+			result &= Character.isWhitespace(c);
+		return result;
 	}
 
 	private int getX() {
-		int length = sb.length(), pos = length;
-		while (--pos > 0 && sb.charAt(pos) != '\n')
-			;
-		return length - pos;
+		return sb.length() - getLineBeginPosition();
+	}
+
+	private int getY() {
+		return nLines;
 	}
 
 	private void nl() {
 		append("\n");
 		pre(indent);
 		nLines++;
+	}
+
+	private int getLineBeginPosition() {
+		int pos = sb.length();
+		while (--pos > 0 && sb.charAt(pos) != '\n')
+			;
+		return pos + 1;
 	}
 
 	private void pre(int indent) {
