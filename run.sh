724db288ca3870bcfa89f07d0440b5e000b72796
ywsing <stupidsing@gmail.com>
2013-01-14 10:02:31 +0800

    add math as precompiled library

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build and pre-compile by
-# mvn -Dmaven.test.skip=true install assembly:single && ./run.sh -precompile STANDARD < /dev/null
+# mvn -Dmaven.test.skip=true install assembly:single && ./run.sh -precompile STANDARD MATH < /dev/null
 
 HOMEDIR="`dirname ${0}`"
 JAR="${HOMEDIR}/target/suite-1.0-jar-with-dependencies.jar"
