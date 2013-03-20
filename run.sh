e2c1d20421ca91f4e9a138941c29287f1b53de60
ywsing <stupidsing@gmail.com>
2013-03-20 17:21:36 +0800

    correct argument passing in script

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ JAR="${HOMEDIR}/target/suite-1.0-jar-with-dependencies.jar"
 DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 OPTS="-Xss1m ${DEBUGOPTS} -Dhome.dir=${HOMEDIR}"
 
-rlwrap -H ${HOME}/.suite_history java ${OPTS} -jar "${JAR}" $*
+rlwrap -H ${HOME}/.suite_history java ${OPTS} -jar "${JAR}" "$@"
