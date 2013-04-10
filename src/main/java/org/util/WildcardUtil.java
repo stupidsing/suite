82294979a4110acf79f0daf343b19c54a8fed270
ywsing <stupidsing@gmail.com>
2013-04-10 23:00:30 +0800

    fix wildcard methods

--- src/main/java/org/util/WildcardUtil.java
+++ src/main/java/org/util/WildcardUtil.java
@@ -23,9 +23,13 @@ public class WildcardUtil {
 			char h0 = p0.charAt(0), h1 = p1.charAt(0);
 			String t0 = p0.substring(1), t1 = p1.substring(1);
 
-			return h0 == '*' && matchWildcard(t0, p1) //
-					|| h1 == '*' && matchWildcard(p0, t1) //
-					|| h0 == h1 && matchWildcard(t0, t1);
+			boolean isMatch = false;
+			isMatch |= h0 == '*'
+					&& (matchWildcard(t0, p1) || matchWildcard(p0, t1));
+			isMatch |= h1 == '*'
+					&& (matchWildcard(p0, t1) || matchWildcard(t0, p1));
+			isMatch |= h0 == h1 && matchWildcard(t0, t1);
+			return isMatch;
 		} else {
 			boolean isWildcardPatterns = true;
 			for (char c0 : p0.toCharArray())
