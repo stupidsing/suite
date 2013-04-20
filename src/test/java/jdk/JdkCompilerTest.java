90e91f5f0c05ccaf3b9b399ba982aacdd868b428
stupidsing <stupidsing@gmail.com>
2013-04-20 22:18:11 +0800

    add sample test case for calling JDK compiler

---
+++ src/test/java/jdk/JdkCompilerTest.java
@@ -0,0 +1,66 @@
+package jdk;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+
+import javax.tools.JavaCompiler;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+
+import org.junit.Test;
+import org.util.IoUtil;
+
+public class JdkCompilerTest {
+
+	@Test
+	public void test() throws IOException, ReflectiveOperationException {
+		String srcDir = "/tmp/src";
+		String binDir = "/tmp/bin";
+		String className = "HelloWorld";
+
+		new File(srcDir).mkdirs();
+		new File(binDir).mkdirs();
+
+		String srcFile = srcDir + "/" + className + ".java";
+		String src = "" //
+				+ "public class " + className + " implements Runnable {" //
+				+ "  public void run() {" //
+				+ "    System.out.println(\"TEST\");" //
+				+ "  }" //
+				+ "}";
+
+		try (OutputStream os = new FileOutputStream(srcFile)) {
+			os.write(src.getBytes(IoUtil.charset));
+		}
+
+		JavaCompiler jc = ToolProvider.getSystemJavaCompiler();
+
+		try (StandardJavaFileManager sjfm = jc.getStandardFileManager( //
+				null //
+				, null //
+				, null)) {
+			File file = new File(srcFile);
+			jc.getTask(null //
+					, null //
+					, null //
+					, Arrays.asList("-d", binDir) //
+					, null //
+					, sjfm.getJavaFileObjects(file)).call();
+
+			URL urls[] = { new URL("file://" + binDir + "/") };
+
+			try (URLClassLoader ucl = new URLClassLoader(urls)) {
+				Class<?> clazz = ucl.loadClass(className);
+				System.out.println("Class has been successfully loaded");
+				Runnable object = (Runnable) clazz.newInstance();
+				object.run();
+			}
+		}
+	}
+
+}
