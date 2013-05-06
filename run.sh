2a7ca87f060e0682e3c89014fff3a8cef13e579d
stupidsing <stupidsing@gmail.com>
2013-05-06 11:43:41 +0800

    run script to compare files date/time and trigger rebuild

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# Build and pre-compile by
-# mvn -Dmaven.test.skip=true install assembly:single && ./run.sh -precompile STANDARD,MATH < /dev/null
-
 HOMEDIR="`dirname ${0}`"
 JAR="${HOMEDIR}/target/suite-1.0-jar-with-dependencies.jar"
 DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 OPTS="-Xss1m ${DEBUGOPTS} -Dhome.dir=${HOMEDIR}"
+
+[ -f "${JAR}" ] && BUILDTIME=$(stat -c %Y "${JAR}") || BUILDTIME=0
+SRCTIME=$(find "${HOMEDIR}/pom.xml" "${HOMEDIR}/src/" -type f | xargs stat -c %Y | sort -g | tail -1)
+
+[ ${SRCTIME} -ge ${BUILDTIME} ] && (
+    mvn -Dmaven.test.skip=true install assembly:single &&
+    java ${OPTS} -jar "${JAR}" -precompile STANDARD,MATH < /dev/null
+)
 
 rlwrap -H ${HOME}/.suite_history java ${OPTS} -jar "${JAR}" "$@"
