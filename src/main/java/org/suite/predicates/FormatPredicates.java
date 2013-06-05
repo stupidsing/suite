5e6b5f97686aacbd80482fbb35aabc36ecbc263d
ywsing <stupidsing@gmail.com>
2013-06-05 17:31:12 +0800

    extract RPN conversion methods

--- src/main/java/org/suite/predicates/FormatPredicates.java
+++ src/main/java/org/suite/predicates/FormatPredicates.java
@@ -1,17 +1,14 @@
 package org.suite.predicates;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 import org.suite.Suite;
 import org.suite.doer.Formatter;
 import org.suite.doer.PrettyPrinter;
 import org.suite.doer.Prover;
+import org.suite.doer.ReversePolish;
 import org.suite.doer.TermParser.TermOp;
 import org.suite.node.Atom;
 import org.suite.node.Int;
 import org.suite.node.Node;
-import org.suite.node.Reference;
 import org.suite.node.Str;
 import org.suite.node.Tree;
 import org.suite.predicates.SystemPredicates.SystemPredicate;
@@ -74,78 +71,15 @@ public class FormatPredicates {
 	}
 
 	public static class Rpn implements SystemPredicate {
+		private ReversePolish rpn = new ReversePolish();
+
 		public boolean prove(Prover prover, Node ps) {
 			final Node params[] = Predicate.getParameters(ps, 2);
 			Node p0 = params[0].finalNode(), p1 = params[1].finalNode();
 			if (p1 instanceof Str)
-				return prover.bind(p0, fromRpn(((Str) p1).getValue()));
+				return prover.bind(p0, rpn.fromRpn(((Str) p1).getValue()));
 			else
-				return prover.bind(new Str(toRpn(p0)), p1);
-		}
-
-		private static Node fromRpn(String rpn) {
-			String elems[] = rpn.split("\n");
-			Deque<Node> deque = new ArrayDeque<>();
-			int index = elems.length;
-
-			while (index > 0) {
-				String elem = elems[--index];
-				if (elem.isEmpty())
-					continue;
-
-				char type = elem.charAt(0);
-				String s = elem.substring(1);
-				Node n;
-
-				if (type == '\\')
-					n = Atom.create(s);
-				else if (type == '^')
-					n = Suite.parse(s);
-				else if (type == 'i')
-					n = Int.create(Integer.valueOf(s));
-				else if (type == 't') {
-					TermOp op = TermOp.valueOf(s);
-					Node left = deque.pop();
-					Node right = deque.pop();
-					n = Tree.create(op, left, right);
-				} else
-					throw new RuntimeException("RPN conversion error: " + elem);
-
-				deque.push(n);
-			}
-
-			return deque.pop();
-		}
-
-		private static String toRpn(Node node) {
-			StringBuilder sb = new StringBuilder();
-			Deque<Node> deque = new ArrayDeque<>();
-
-			deque.push(node);
-
-			while (!deque.isEmpty()) {
-				Node n = deque.pop().finalNode();
-				String s;
-
-				if (n instanceof Atom)
-					s = "\\" + ((Atom) n).getName();
-				else if (n instanceof Int)
-					s = "i" + ((Int) n).getNumber();
-				else if (n instanceof Reference)
-					s = "\\." + ((Reference) n).getId();
-				else if (n instanceof Tree) {
-					Tree tree = (Tree) n;
-					s = "t" + tree.getOperator();
-					deque.push(tree.getRight());
-					deque.push(tree.getLeft());
-				} else
-					s = "^" + Formatter.dump(n);
-
-				sb.append(s);
-				sb.append('\n');
-			}
-
-			return sb.toString();
+				return prover.bind(new Str(rpn.toRpn(p0)), p1);
 		}
 	}
 
