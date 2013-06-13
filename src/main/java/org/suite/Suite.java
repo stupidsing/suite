53607b4790ea3dbdd7365e7d7a0baea8a36e887b
ywsing <stupidsing@gmail.com>
2013-06-13 12:21:13 +0800

    spaces

--- src/main/java/org/suite/Suite.java
+++ src/main/java/org/suite/Suite.java
@@ -70,7 +70,7 @@ public class Suite {
 
 	/**
 	 * Convert rules in a rule set back into to #-separated format.
-	 *
+	 * 
 	 * May specify a prototype to limit the rules listed.
 	 */
 	public static Node getRuleList(RuleSet rs, Prototype proto) {
