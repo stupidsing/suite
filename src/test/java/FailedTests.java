856b3932cb810a866f20bbfab9eb18a72842e79c
stupidsing <stupidsing@gmail.com>
2013-07-06 21:27:26 +0800

    refine failed case

--- src/test/java/FailedTests.java
+++ src/test/java/FailedTests.java
@@ -54,27 +54,21 @@ public class FailedTests {
 				+ "\n" //
 				+ "cg-optimize-tail-calls .li0 .ri0 \n" //
 				+ "    :- cg-push-pop-pairs .li0/.li1 .li2/.li3 .ri1/.ri2 .ri0/.ri1 \n" //
-				+ "    , member (CALL/JUMP, CALL-REG/JUMP-REG,) .call/.jump \n" //
-				+ "    , .li1 = (_ .call .target, .li2) \n" //
+				+ "    , dump A, nl \n" //
+				+ "    , .li1 = (_ CALL .target, .li2) \n" //
+				+ "    , dump B, nl \n" //
 				+ "    , cg-is-restore-csp .li3/.li4 .ri2/.ri3 \n" //
+				+ "    , dump C, nl \n" //
 				+ "    , cg-is-returning .li4 \n" //
-				+ "    , .ri3 = (_ .jump .target, .ri4) \n" //
-				+ "    , ! \n" //
-				+ "    , cg-optimize-tail-calls .li4 .ri4 \n" //
 				+ "# \n" //
 				+ "cg-optimize-tail-calls (.inst, .insts0) (.inst, .insts1) \n" //
 				+ "    :- !, cg-optimize-tail-calls .insts0 .insts1 \n" //
+				+ "    , dump D, nl \n" //
 				+ "# \n" //
 				+ "cg-optimize-tail-calls () () # \n" //
 				+ "\n" //
-				+ "cg-push-pop-pairs \n" //
-				+ "(_ PUSH .reg, .i0)/.ix (_ POP-ANY, .j0)/.jx \n" //
-				+ "(_ PUSH .reg, .k0)/.kx (_ POP-ANY, .l0)/.lx \n" //
-				+ "    :- !, cg-push-pop-pairs .i0/.ix .j0/.jx .k0/.kx .l0/.lx \n" //
-				+ "# \n" //
 				+ "cg-push-pop-pairs .i/.i .j/.j .k/.k .l/.l # \n" //
 				+ "\n" //
-				+ "cg-is-restore-csp (_ RESTORE-CSP .cspReg, .i)/.i (_ RESTORE-CSP .cspReg, .j)/.j :- ! # \n" //
 				+ "cg-is-restore-csp .i/.i .j/.j # \n" //
 				+ "\n" //
 				+ "cg-is-returning (.inst, .insts) :- cg-is-skip .inst, !, cg-is-returning .insts # \n" //
