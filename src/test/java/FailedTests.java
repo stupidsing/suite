be4a47a8027d75287b825196c87029122e7657a2
stupidsing <stupidsing@gmail.com>
2013-07-06 23:33:11 +0800

    refine failed case

--- src/test/java/FailedTests.java
+++ src/test/java/FailedTests.java
@@ -49,37 +49,22 @@ public class FailedTests {
 	@Test
 	public void test4() {
 		RuleSet rs = Suite.nodeToRuleSet(Suite.parse("" //
-				+ "member (.e, _) .e # \n" //
-				+ "member (_, .tail) .e :- member .tail .e # \n" //
-				+ "\n" //
 				+ "cg-optimize-tail-calls .li0 .ri0 \n" //
-				+ "    :- cg-push-pop-pairs .li0/.li1 .li2/.li3 .ri1/.ri2 .ri0/.ri1 \n" //
-				+ "    , dump A, nl \n" //
-				+ "    , .li1 = (_ CALL .target, .li2) \n" //
-				+ "    , dump B, nl \n" //
-				+ "    , cg-is-restore-csp .li3/.li4 .ri2/.ri3 \n" //
-				+ "    , dump C, nl \n" //
-				+ "    , cg-is-returning .li4 \n" //
+				+ "    :- dump A, nl, cg-is-returning .li0 \n" //
 				+ "# \n" //
 				+ "cg-optimize-tail-calls (.inst, .insts0) (.inst, .insts1) \n" //
-				+ "    :- !, cg-optimize-tail-calls .insts0 .insts1 \n" //
-				+ "    , dump D, nl \n" //
+				+ "    :- dump B, nl, !, cg-optimize-tail-calls .insts0 .insts1 \n" //
 				+ "# \n" //
 				+ "cg-optimize-tail-calls () () # \n" //
-				+ "\n" //
-				+ "cg-push-pop-pairs .i/.i .j/.j .k/.k .l/.l # \n" //
-				+ "\n" //
-				+ "cg-is-restore-csp .i/.i .j/.j # \n" //
 				+ "\n" //
 				+ "cg-is-returning (.inst, .insts) :- cg-is-skip .inst, !, cg-is-returning .insts # \n" //
 				+ "cg-is-returning (_ RETURN, _) # \n" //
 				+ "\n" //
 				+ "cg-is-skip (_ LABEL) # \n" //
-				+ "cg-is-skip (_ REMARK _) # \n" //
 		));
 
 		Node goal = Suite.parse("cg-optimize-tail-calls (_ CALL _, _ LABEL,) _, sink ()");
-		Builder builder = new CompiledProverBuilderLevel1(new ProverConfig(), false);
+		Builder builder = new CompiledProverBuilderLevel1(new ProverConfig(), true);
 		assertTrue(!Suite.evaluateLogic(builder, rs, goal).isEmpty());
 	}
 
