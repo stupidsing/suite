58c2ab862118723f102cb6672f5c3f3a5b37a7ad
ywsing <stupidsing@gmail.com>
2013-07-18 09:41:07 +0800

    remove math from default precompiled library list

--- src/main/java/org/suite/PrecompileMain.java
+++ src/main/java/org/suite/PrecompileMain.java
@@ -13,7 +13,7 @@ import org.suite.doer.ProverConfig;
 public class PrecompileMain {
 
 	public static void main(String args[]) throws IOException {
-		for (String libraryName : Arrays.asList("STANDARD", "MATH"))
+		for (String libraryName : Arrays.asList("STANDARD"))
 			Suite.precompile(libraryName, new ProverConfig());
 
 		System.out.println("please refresh eclipse workspace");
