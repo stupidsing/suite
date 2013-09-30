61dd4a3cbe589919b560a9007bacb6794bafa5b7
ywsing <stupidsing@gmail.com>
2013-09-30 11:54:02 +0800

    add abstract base class for handler

---
+++ src/main/java/suite/http/HttpHandler.java
@@ -0,0 +1,30 @@
+package suite.http;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+import suite.http.HttpServer.Handler;
+import suite.util.FileUtil;
+
+public abstract class HttpHandler implements Handler {
+
+	protected HttpRequest request;
+	protected HttpResponse response;
+
+	protected abstract void handle(Reader reader, Writer writer) throws IOException;
+
+	@Override
+	public void handle(HttpRequest request, HttpResponse response) throws IOException {
+		this.request = request;
+		this.response = response;
+
+		try (Reader reader = new InputStreamReader(request.getInputStream(), FileUtil.charset);
+				Writer writer = new OutputStreamWriter(response.getOutputStream(), FileUtil.charset)) {
+			handle(request, response);
+		}
+	}
+
+}
--- src/main/java/suite/http/HttpServerMain.java
+++ src/main/java/suite/http/HttpServerMain.java
@@ -1,26 +1,20 @@
 package suite.http;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 
 import suite.http.HttpServer.Handler;
-import suite.util.FileUtil;
 
 public class HttpServerMain {
 
-	private Handler handler0 = new Handler() {
-		public void handle(HttpRequest request, HttpResponse response) throws IOException {
-			try (Reader reader = new InputStreamReader(request.getInputStream(), FileUtil.charset);
-					Writer writer = new OutputStreamWriter(response.getOutputStream(), FileUtil.charset)) {
-				writer.write("<html>" //
-						+ "<br/>method = " + request.getMethod() + "<br/>server = " + request.getServer()
-						+ "<br/>path = "
-						+ request.getPath() + "<br/>attrs = " + HttpUtil.getAttrs(request.getQuery()) //
-						+ "<br/>headers = " + request.getHeaders() + "</html>");
-			}
+	private Handler handler0 = new HttpHandler() {
+		protected void handle(Reader reader, Writer writer) throws IOException {
+			writer.write("<html>" //
+					+ "<br/>method = " + request.getMethod() + "<br/>server = " + request.getServer()
+					+ "<br/>path = "
+					+ request.getPath() + "<br/>attrs = " + HttpUtil.getAttrs(request.getQuery()) //
+					+ "<br/>headers = " + request.getHeaders() + "</html>");
 		}
 	};
 
