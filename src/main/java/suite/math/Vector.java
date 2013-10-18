eedad8c6c6cdcaa205f57eef0cb7e41383df6a64
ywsing <stupidsing@gmail.com>
2013-10-18 12:46:56 +0800

    add mathematical vector

---
+++ src/main/java/suite/math/Vector.java
@@ -0,0 +1,25 @@
+package suite.math;
+
+public class Vector {
+
+	private float x, y, z;
+
+	public Vector(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public static Vector add(Vector u, Vector v) {
+		return new Vector(u.x + v.x, u.y + v.y, u.z + v.z);
+	}
+
+	public static float dot(Vector u, Vector v) {
+		return u.x * v.x + u.y * v.y + u.z * v.z;
+	}
+
+	public static Vector cross(Vector u, Vector v) {
+		return new Vector(u.y * v.z - u.z * v.y, u.z * v.x - u.x * v.z, u.x * v.y - u.y * v.x);
+	}
+
+}
