1cabc87dd8011ab1fc14e3b375fbb236faebde19
ywsing <stupidsing@gmail.com>
2013-10-28 11:33:44 +0800

    failed test comment

--- src/test/java/FailedTests.java
+++ src/test/java/FailedTests.java
@@ -13,7 +13,10 @@ public class FailedTests {
 		new FunRbTreeTest().test();
 	}
 
-	// NullPointerException
+	// /tmp/suite.instructionexecutor.InstructionTranslator/suite/instructionexecutor/TranslatedRun88.java:3161:
+	// error: method invoke in class Invocable cannot be applied to given types;
+	// f228_r1 = ((Invocable) data.getData()).invoke(this, Arrays.asList(n0,
+	// n1));
 	@Test
 	public void test1() throws IOException {
 		new InstructionTranslatorTest().testStandardLibrary();
