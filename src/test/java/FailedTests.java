298e47bb524858ee0faab32a70039ece50ab11f9
ywsing <stupidsing@gmail.com>
2013-11-11 23:43:06 +0800

    add failed test case

--- src/test/java/FailedTests.java
+++ src/test/java/FailedTests.java
@@ -1,10 +1,20 @@
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
 
 import suite.Suite;
+import suite.fp.FunCompilerConfig;
 import suite.fp.eval.FunRbTreeTest;
 import suite.instructionexecutor.InstructionTranslatorTest;
+import suite.lp.doer.ProverConfig;
+import suite.lp.search.CompiledProverBuilder;
+import suite.lp.search.InterpretedProverBuilder;
+import suite.lp.search.ProverBuilder.Builder;
+import suite.lp.search.ProverBuilder.Finder;
+import suite.node.Atom;
+import suite.node.Int;
+import suite.node.Node;
 
 public class FailedTests {
 
@@ -32,6 +42,26 @@ public class FailedTests {
 	@Test
 	public void test3() {
 		Suite.evaluateFunType("define f = (v => (v;) = v) >> f");
+	}
+
+	// Functional compilation Goal failed
+	@Test
+	public void test4() {
+		FunCompilerConfig fcc = new FunCompilerConfig();
+		fcc.setNode(Int.create(1));
+
+		Node node = Suite.substitute("" //
+				+ "source .in" //
+				+ ", compile-function .0 .in .out" //
+				+ ", sink .out" //
+		, Atom.create("LAZY"));
+
+		ProverConfig pc = fcc.getProverConfig();
+		Builder builder1 = new InterpretedProverBuilder(pc);
+		Builder builder = CompiledProverBuilder.level1(pc, fcc.isDumpCode());
+		Finder finder = builder.build(rs, compileNode);
+		List<Node> nodes = collect(finder, appendLibraries(fcc));
+		return nodes.size() == 1 ? nodes.get(0).finalNode() : null;
 	}
 
 }
