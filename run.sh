32355b2ab72c0a6ca90cde7eea148a01c56660ec
ywsing <stupidsing@gmail.com>
2013-11-12 04:26:19 +0800

    reduce memory footprint; fix do operation; add failed compilation test case

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 HOMEDIR="`dirname ${0}`"
 JAR="${HOMEDIR}/target/suite-1.0-jar-with-dependencies.jar"
 DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
-OPTS="-Xss2m ${DEBUGOPTS} -Dhome.dir=${HOMEDIR}"
+OPTS="-Xss2m -Xmx256m ${DEBUGOPTS} -Dhome.dir=${HOMEDIR}"
 
 [ -f "${JAR}" ] && BUILDTIME=$(stat -c %Y "${JAR}") || BUILDTIME=0
 SRCTIME=$(find "${HOMEDIR}/pom.xml" "${HOMEDIR}/src/main/" -type f | xargs stat -c %Y | sort -g | tail -1)
