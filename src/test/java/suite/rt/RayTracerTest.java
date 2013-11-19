54045be6f367e7ef97b464d8e7b5443081732086
ywsing <stupidsing@gmail.com>
2013-11-19 22:51:22 +0800

    add test cases

--- src/test/java/suite/rt/RayTracerTest.java
+++ src/test/java/suite/rt/RayTracerTest.java
@@ -34,17 +34,48 @@ public class RayTracerTest {
 	}
 
 	@Test
-	public void test() throws IOException {
+	public void testSphere() throws IOException {
+		RayTraceObject sphere = Sphere.c(v(0f, 0f, 3f), 1f, reflective(cr, 0.4f));
+		Scene scene = new Scene(Arrays.asList(sphere));
+
+		LightSource light = new PointLightSource(v(10000f, 10000f, -10000f), gray(1f));
+		List<LightSource> lights = Arrays.asList(light);
+
+		RayTracer rayTracer = new RayTracer(lights, scene);
+
+		BufferedImage bufferedImage = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+		rayTracer.trace(bufferedImage, 500);
+		ImageIO.write(bufferedImage, "png", new File("/tmp/ray-tracer-sphere.png"));
+	}
+
+	@Test
+	public void testSpheres() throws IOException {
+		RayTraceObject sphere0 = Sphere.c(v(-2f, 0f, 5f), 1f, reflective(cr, 0.4f));
+		RayTraceObject sphere1 = Sphere.c(v(2f, 0f, 5f), 1f, reflective(cr, 0.4f));
+		Scene scene = new Scene(Arrays.asList(sphere0, sphere1));
+
+		LightSource light = new PointLightSource(v(10000f, 10000f, -10000f), gray(1f));
+		List<LightSource> lights = Arrays.asList(light);
+
+		RayTracer rayTracer = new RayTracer(lights, scene);
+
+		BufferedImage bufferedImage = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+		rayTracer.trace(bufferedImage, 500);
+		ImageIO.write(bufferedImage, "png", new File("/tmp/ray-tracer-spheres.png"));
+	}
+
+	@Test
+	public void testMess() throws IOException {
 		RayTraceObject sphere0 = Sphere.c(v(1f, -1f, 3f), 1f, reflective(cr, 0.4f));
-		RayTraceObject sphere1 = Sphere.c(v(-1f, -1f, 3f), 1f, reflective(cr, 0.4f));
-		RayTraceObject sphere2 = Sphere.c(v(0f, 0f, 5f), 1f, reflective(cg, 0.4f));
-		RayTraceObject sphere3 = Sphere.c(v(-1f, 1f, 7f), 1f, reflective(cb, 0.4f));
+		RayTraceObject sphere1 = Sphere.c(v(0f, 0f, 5f), 1f, reflective(cg, 0.4f));
+		RayTraceObject sphere2 = Sphere.c(v(-1f, 1f, 7f), 1f, reflective(cb, 0.4f));
 		RayTraceObject plane = new Plane(v(0f, 1f, 0f), -3f, white());
 		RayTraceObject triangle = new Triangle(v(0.2f, 0.2f, 3f), v(0.2f, 0f, 0f), v(0f, 0.2f, 0f), white());
-		Scene scene = new Scene(Arrays.asList(sphere0, sphere1, sphere2, sphere3, plane, triangle));
+		Scene scene = new Scene(Arrays.asList(sphere0, sphere1, sphere2, plane, triangle));
 
 		LightSource light0 = new PointLightSource(v(10000f, 10000f, -10000f), gray(1f));
 		List<LightSource> lights = Arrays.asList(light0);
+
 		RayTracer rayTracer = new RayTracer(lights, scene);
 
 		BufferedImage bufferedImage = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
@@ -53,9 +84,9 @@ public class RayTracerTest {
 	}
 
 	private Material reflective(Vector color, float index) {
-		float reflectionIndex1 = 1 - index;
-		final Vector litIndex = Vector.mul(color, index);
-		final Vector reflectionIndex = v(reflectionIndex1, reflectionIndex1, reflectionIndex1);
+		float index1 = 1f - index;
+		final Vector litIndex = Vector.mul(color, index1);
+		final Vector reflectionIndex = gray(index);
 
 		return new Material() {
 			public Vector litIndex() {
