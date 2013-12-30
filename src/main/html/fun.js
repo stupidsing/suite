a35bb41af3995aeeb0d236b5803bc1d0af2b716b
ywsing <stupidsing@gmail.com>
2013-12-30 21:27:32 +0800

    refactor

--- src/main/html/fun.js
+++ src/main/html/fun.js
@@ -16,7 +16,7 @@ var filter = function(fun, ins) {
 var concat = function(ins) {
 	var out = [];
 	for (var i = 0; i < ins.length; i++) {
-		child = ins[i];
+		var child = ins[i];
 		for (var j = 0; j < child.length; j++)
 			outs.push(child[j]);
 	}
