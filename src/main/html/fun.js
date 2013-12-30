523743acf5a9bf4b3339a4720b69855f2a7ebf67
ywsing <stupidsing@gmail.com>
2013-12-30 21:08:10 +0800

    extract functional scripts

---
+++ src/main/html/fun.js
@@ -0,0 +1,30 @@
+var map = function(fun, ins) {
+	var outs = [];
+	for (var i = 0; i < ins.length; i++) outs.push(fun(ins[i]));
+	return outs;
+};
+
+var filter = function(fun, ins) {
+	var outs = [];
+	for (var i = 0; i < ins.length; i++) {
+		object = ins[i];
+		if (fun(object)) outs.push(object);
+	}
+	return outs;
+};
+
+var concat = function(ins) {
+	var out = [];
+	for (var i = 0; i < ins.length; i++) {
+		child = ins[i];
+		for (var j = 0; j < child.length; j++)
+			outs.push(child[j]);
+	}
+	return outs;
+};
+
+var fold = function(fun, value, ins) {
+	for (var i = 0; i < ins.length; i++)
+		value = fun(value, ins[i]);
+	return value;
+};
