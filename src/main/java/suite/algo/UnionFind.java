a5c43374f40e8118a853ffcfc621581a892edfaa
ywsing <stupidsing@gmail.com>
2014-01-03 16:07:52 +0800

    refactor union find

--- src/main/java/suite/algo/UnionFind.java
+++ src/main/java/suite/algo/UnionFind.java
@@ -4,42 +4,51 @@ import java.util.Map;
 
 import suite.util.DefaultFunMap;
 import suite.util.FunUtil.Fun;
-import suite.util.Pair;
 
 public class UnionFind<T> {
 
-	private Map<T, Pair<T, Integer>> parents = new DefaultFunMap<>(new Fun<T, Pair<T, Integer>>() {
-		public Pair<T, Integer> apply(T t) {
-			return Pair.create(t, 0);
+	private class Record {
+		private T parent;
+		private int rank;
+
+		public Record(T parent, int rank) {
+			this.parent = parent;
+			this.rank = rank;
+		}
+	}
+
+	private Map<T, Record> nodes = new DefaultFunMap<>(new Fun<T, Record>() {
+		public Record apply(T t) {
+			return new Record(t, 0);
 		}
 	});
 
 	public void union(T t0, T t1) {
-		Pair<T, Integer> pair0 = find0(t0);
-		Pair<T, Integer> pair1 = find0(t1);
+		Record pair0 = find0(t0);
+		Record pair1 = find0(t1);
 
-		if (pair0.t1 < pair1.t1)
-			pair0.t0 = t1;
-		else if (pair0.t1 > pair1.t1)
-			pair1.t0 = t0;
-		else if (pair0.t1 == pair1.t1) {
-			pair1.t0 = t0;
-			pair0.t1++;
+		if (pair0.rank < pair1.rank)
+			pair0.parent = t1;
+		else if (pair0.rank > pair1.rank)
+			pair1.parent = t0;
+		else if (pair0.rank == pair1.rank) {
+			pair1.parent = t0;
+			pair0.rank++;
 		}
 	}
 
 	public T find(T t) {
-		return find0(t).t0;
+		return find0(t).parent;
 	}
 
-	private Pair<T, Integer> find0(T t) {
-		return find0(parents.get(t));
+	private Record find0(T t) {
+		return find0(nodes.get(t));
 	}
 
-	private Pair<T, Integer> find0(Pair<T, Integer> pair) {
-		Pair<T, Integer> parent = parents.get(pair.t0);
-		if (parent.t0 != pair.t0) {
-			parent.t0 = find0(parent.t0).t0;
+	private Record find0(Record pair) {
+		Record parent = nodes.get(pair.parent);
+		if (parent.parent != pair.parent) {
+			parent.parent = find0(parent.parent).parent;
 			return parent;
 		} else
 			return pair;
