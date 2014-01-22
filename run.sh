9d310b9b2dbf03c69966ba606a4faec1a08fa224
ywsing <stupidsing@gmail.com>
2014-01-22 16:21:03 +0800

    fix source/target timestamp comparisons

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-HOMEDIR="`dirname ${0}`"
-JAR="${HOMEDIR}/target/suite-1.0-jar-with-dependencies.jar"
+BASE="`dirname ${0}`"
+JAR="${BASE}/target/suite-1.0-jar-with-dependencies.jar"
 DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
-OPTS="-Xss2m -Xmx256m ${DEBUGOPTS} -Dhome.dir=${HOMEDIR}"
+OPTS="-Xss2m -Xmx256m ${DEBUGOPTS} -Dhome.dir=${BASE}"
 
-[ -f "${JAR}" ] && BUILDTIME=$(stat -c %Y "${JAR}") || BUILDTIME=0
-SRCTIME=$(find "${HOMEDIR}/pom.xml" "${HOMEDIR}/src/main/" -type f | xargs stat -c %Y | sort -g | tail -1)
+SRCTIME=$(find "${BASE}/pom.xml" "${BASE}/src/main/" -type f | xargs stat -c %Y | sort -g | tail -1)
+TARGETTIME=$(echo "${JAR}" "${BASE}/STANDARD.rpn" | xargs -I {} sh -c 'stat -c %Y {} 2> /dev/null || echo 0' | sort -g | head -1)
 
 (
-	[ ${SRCTIME} -le ${BUILDTIME} ] ||
+	[ ${SRCTIME} -le ${TARGETTIME} ] ||
 	(
 		mvn -Dmaven.test.skip=true install assembly:single &&
-		java ${OPTS} -jar "${JAR}" -precompile STANDARD < /dev/null
+		(java ${OPTS} -jar "${JAR}" -precompile STANDARD < /dev/null || rm -f ${BASE}/STANDARD.rpn && false)
 	)
 ) &&
 CMD="java ${OPTS} -jar ${JAR} $@" &&
