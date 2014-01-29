319564f86b692b082ac8f093d14f8ae0561ee7b8
ywsing <stupidsing@gmail.com>
2014-01-29 18:48:04 +0800

    refine test case

--- src/test/java/suite/text/TextUtilTest.java
+++ src/test/java/suite/text/TextUtilTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import suite.text.TextUtil.ConflictException;
 import suite.util.To;
 
 public class TextUtilTest {
@@ -11,8 +12,15 @@ public class TextUtilTest {
 	private TextUtil textUtil = new TextUtil();
 
 	@Test
-	public void test() {
-		PatchData patchData = textUtil.diff(To.bytes("abc12def34ghi"), To.bytes("abc56def78ghi"));
+	public void test() throws ConflictException {
+		String a = "abc12def34ghi";
+		String b = "abc56def78ghi";
+		String c = "abc56qwe78ghi";
+		String d = "abc56qwe78ghi";
+
+		// Test diff
+		PatchData patchData = textUtil.diff(To.bytes(a), To.bytes(b));
+
 		StringBuilder sb = new StringBuilder();
 		patchData.write(sb);
 		System.out.println(sb);
@@ -23,6 +31,14 @@ public class TextUtilTest {
 				+ "10:12|8:10|Y<<34|78>>" //
 				+ "10:13|10:13|N<<ghi>>";
 		assertEquals(expected, sb.toString());
+
+		// Test patch
+		assertEquals(b, To.string(textUtil.patch(To.bytes(a), patchData)));
+
+		// Test merge
+		PatchData patchData1 = textUtil.diff(To.bytes(a), To.bytes(c));
+		PatchData mergedPatchData = textUtil.merge(patchData, patchData1);
+		assertEquals(d, To.string(textUtil.patch(To.bytes(a), mergedPatchData)));
 	}
 
 }
