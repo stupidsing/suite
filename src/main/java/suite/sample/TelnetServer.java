fb08d3bb53e600dc74167a8bb7c645e2b6412eaf
ywsing <stupidsing@gmail.com>
2014-02-14 12:44:03 +0800

    replace telnet server by an interruptible version, instead of original polling

--- src/main/java/suite/sample/TelnetServer.java
+++ src/main/java/suite/sample/TelnetServer.java
@@ -2,10 +2,13 @@ package suite.sample;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InterruptedIOException;
 import java.io.OutputStream;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.HashSet;
+import java.util.Set;
 
-import suite.net.CopyStreamThread;
+import suite.util.Copy;
+import suite.util.LogUtil;
 import suite.util.SocketUtil;
 import suite.util.SocketUtil.Io;
 
@@ -15,39 +18,83 @@ public class TelnetServer {
 		new TelnetServer().run();
 	}
 
+	private class Server {
+		private Set<Thread> threads = new HashSet<>();
+
+		private abstract class InterruptibleThread extends Thread {
+			protected abstract void run0() throws Exception;
+
+			public void run() {
+				try {
+					run0();
+				} catch (InterruptedException ex) {
+				} catch (InterruptedIOException ex) {
+				} catch (Exception ex) {
+					LogUtil.error(ex);
+				} finally {
+
+					// If we are not being interrupted by another thread, issue
+					// interrupt signal to other threads
+					if (!isInterrupted())
+						for (Thread thread : threads)
+							if (thread != this)
+								thread.interrupt();
+				}
+			}
+		}
+
+		private class CopyThread extends InterruptibleThread {
+			private InputStream is;
+			private OutputStream os;
+
+			private CopyThread(InputStream is, OutputStream os) {
+				this.is = is;
+				this.os = os;
+			}
+
+			protected void run0() throws IOException {
+				try (InputStream is_ = is; OutputStream os_ = os) {
+					Copy.stream(is_, os_);
+				}
+			}
+		}
+
+		private void serve(InputStream sis, OutputStream sos) throws IOException {
+
+			// Kills the process if client closes the stream;
+			// closes the stream if process is terminated/ended output.
+			// Therefore we need the interruption mechanism.
+			final Process process = Runtime.getRuntime().exec("bash");
+			InputStream pis = process.getInputStream();
+			InputStream pes = process.getErrorStream();
+			OutputStream pos = process.getOutputStream();
+
+			try {
+				threads.add(new CopyThread(pis, sos));
+				threads.add(new CopyThread(pes, sos));
+				threads.add(new CopyThread(sis, pos));
+				threads.add(new InterruptibleThread() {
+					protected void run0() throws InterruptedException {
+						process.waitFor();
+					}
+				});
+
+				for (Thread thread : threads)
+					thread.start();
+				for (Thread thread : threads)
+					thread.join();
+			} catch (InterruptedException ex) {
+				LogUtil.error(ex);
+			} finally {
+				process.destroy();
+			}
+		}
+	}
+
 	private void run() throws IOException {
 		SocketUtil.listen(2323, new Io() {
 			public void serve(InputStream sis, OutputStream sos) throws IOException {
-
-				// Kills the process if client closes the stream;
-				// closes the stream if process is terminated/ended output.
-				// Therefore we need a quitter object and some pollings.
-				AtomicBoolean quitter = new AtomicBoolean(false);
-				Process process = Runtime.getRuntime().exec("bash");
-
-				try (InputStream pis = process.getInputStream();
-						InputStream pes = process.getErrorStream();
-						OutputStream pos = process.getOutputStream()) {
-					Thread threads[] = { new CopyStreamThread(pis, sos, quitter) //
-							, new CopyStreamThread(pes, sos, quitter) //
-							, new CopyStreamThread(sis, pos, quitter) };
-
-					for (Thread thread : threads)
-						thread.start();
-
-					while (!quitter.get())
-						try {
-							process.exitValue();
-							break;
-						} catch (IllegalThreadStateException ex) {
-							Thread.sleep(100);
-						}
-				} catch (InterruptedException ex) {
-					throw new RuntimeException(ex);
-				} finally {
-					quitter.set(true);
-					process.destroy();
-				}
+				new Server().serve(sis, sos);
 			}
 		});
 	}
