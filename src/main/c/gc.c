45a72c92d306e4be35749fe289326cee84747a8d
ywsing <stupidsing@gmail.com>
2014-02-17 17:15:09 +0800

    add gc functions

--- src/main/c/gc.c
+++ src/main/c/gc.c
@@ -14,11 +14,14 @@ struct Object {
 	struct Object *next;
 };
 
+struct Object *first;
+struct Object *lastAllocated;
+
 int compareaddresses(void *p0, void *p1) {
 	return p0 != p1 ? (p0 < p1 ? -1 : 1) : 0;
 }
 
-struct Object *markAndSweep(struct Object *first, struct Object *root) {
+struct Object *markAndSweep(struct Object *root) {
 	struct Object *object;
 
 	object = first;
@@ -31,7 +34,8 @@ struct Object *markAndSweep(struct Object *first, struct Object *root) {
 	heapcreate(&heap, &compareaddresses);
 
 	object->flag = QUEUED_;
-	heapadd(&heap, object);
+	heapadd(&heap, root);
+	heapadd(&heap, lastAllocated);
 
 	while(object = (struct Object*) heapremove(&heap)) {
 		int *refoffsets = object->class->refoffsets();
@@ -49,11 +53,33 @@ struct Object *markAndSweep(struct Object *first, struct Object *root) {
 
 	struct Object **prev = &first;
 	while(object) {
-		if(object->flag == FRESH__) (*prev) = object->next;
-		else prev = &object->next;
-		object = object->next;
+		struct Object *next = object->next;
+		if(object->flag == FRESH__) {
+			(*prev) = next;
+			memfree(object);
+		} else prev = &object->next;
+		object = next;
 	}
 	return first;
+}
+
+void *gcalloc(struct Class *class, int size) {
+	struct Object *object = lastAllocated = memalloc(size);
+	object->class = class;
+	object->flag = SCANNED;
+	object->next = first;
+
+	first = object;
+	return object;
+}
+
+void gcinit() {
+	first = 0;
+	lastAllocated = 0;
+}
+
+void gcdeinit() {
+	if(lastAllocated) memfree(lastAllocated);
 }
 
 int main() {
