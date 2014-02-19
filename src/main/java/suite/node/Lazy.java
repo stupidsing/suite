3577f54f8c7deda36fb378b783d21fb3bca9a5d6
ywsing <stupidsing@gmail.com>
2014-02-19 16:02:53 +0800

    generalizer to work in lazy fashion to improve performance

---
+++ src/main/java/suite/node/Lazy.java
@@ -0,0 +1,35 @@
+package suite.node;
+
+import suite.util.FunUtil.Source;
+
+public class Lazy extends Node {
+
+	private Source<Node> source;
+	private Node target;
+
+	public Lazy(Source<Node> source) {
+		this.source = source;
+	}
+
+	@Override
+	public Node finalNode() {
+		return target().finalNode();
+	}
+
+	@Override
+	public int hashCode() {
+		return target().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		return target().equals(object);
+	}
+
+	private Node target() {
+		if (target == null)
+			target = source.source();
+		return target;
+	}
+
+}
