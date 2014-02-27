719890205b7aaa36e4acf1bbafc171bdf8f10f83
ywsing <stupidsing@gmail.com>
2014-02-27 14:23:03 +0800

    fix functional 2-3 tree; add test case for it

---
+++ src/test/java/suite/fp/eval/Fun23TreeTest.java
@@ -0,0 +1,30 @@
+package suite.fp.eval;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import suite.Suite;
+import suite.node.Node;
+import suite.node.io.Formatter;
+
+public class Fun23TreeTest {
+
+	@Test
+	public void test() throws IOException {
+		String list10 = "0 until 10 | map {insert} | apply | {Tree (9999, Empty;)}";
+
+		Node fp0 = Suite.substitute("using 23-TREE >> " + list10);
+		Node result0 = Suite.evaluateFun(Suite.fcc(fp0, false));
+		assertNotNull(result0);
+		System.out.println("OUT:\n" + Formatter.dump(result0));
+
+		Node fp1 = Suite.substitute("using 23-TREE >> 0 until 5 | map {remove} | apply | {" + list10 + "}");
+		Node result1 = Suite.evaluateFun(Suite.fcc(fp1, false));
+		assertNotNull(result1);
+		System.out.println("OUT:\n" + Formatter.dump(result1));
+	}
+
+}
