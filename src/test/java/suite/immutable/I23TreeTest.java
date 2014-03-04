7b6d0d0a5da7bcc04aba9ae005c36300bb063a3a
ywsing <stupidsing@gmail.com>
2014-03-04 15:00:54 +0800

    add test case

--- src/test/java/suite/immutable/I23TreeTest.java
+++ src/test/java/suite/immutable/I23TreeTest.java
@@ -1,20 +1,24 @@
 package suite.immutable;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 import org.junit.Test;
 
 import suite.util.FunUtil;
+import suite.util.Util;
+
+import com.google.common.collect.Iterators;
 
 public class I23TreeTest {
 
 	private int max = 32;
 
-	private Comparator<Integer> comparator = new Comparator<Integer>() {
-		public int compare(Integer i0, Integer i1) {
-			return i0 - i1;
-		}
-	};
+	private Comparator<Integer> comparator = Util.<Integer> comparator();
 
 	@Test
 	public void test0() {
@@ -24,38 +28,53 @@ public class I23TreeTest {
 			tree23 = tree23.add(i);
 
 		System.out.println(tree23.toString());
+		assertEquals(max, Iterators.size(FunUtil.iterator(tree23.source())));
+
 		for (int i = 0; i < max; i++)
 			tree23 = tree23.remove(i);
 
 		System.out.println(tree23.toString());
+		assertEquals(0, Iterators.size(FunUtil.iterator(tree23.source())));
 	}
 
 	@Test
 	public void test1() {
 		I23Tree<Integer> tree23 = new I23Tree<>(comparator);
 
-		System.out.println(tree23.toString());
-		dump(tree23);
-
 		for (int i = 0; i < max; i += 2)
 			tree23 = tree23.add(i);
 		for (int i = 1; i < max; i += 2)
 			tree23 = tree23.add(i);
-
-		System.out.println(tree23.toString());
-		dump(tree23);
+		assertEquals(max, Iterators.size(FunUtil.iterator(tree23.source())));
 
 		for (int i = 0; i < max; i += 2)
 			tree23 = tree23.remove(i);
 		for (int i = 1; i < max; i += 2)
 			tree23 = tree23.remove(i);
+		assertEquals(0, Iterators.size(FunUtil.iterator(tree23.source())));
 	}
 
-	private void dump(I23Tree<Integer> tree23) {
-		System.out.print("LIST = ");
-		for (Integer i : FunUtil.iter(tree23.source()))
-			System.out.print(i + " ");
-		System.out.println();
+	@Test
+	public void test2() {
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < max; i++)
+			list.add(i);
+
+		I23Tree<Integer> tree23 = new I23Tree<>(comparator);
+
+		Collections.shuffle(list);
+		for (int i : list)
+			tree23 = tree23.add(i);
+
+		System.out.println(tree23.toString());
+		assertEquals(max, Iterators.size(FunUtil.iterator(tree23.source())));
+
+		Collections.shuffle(list);
+		for (int i : list)
+			tree23 = tree23.remove(i);
+
+		System.out.println(tree23.toString());
+		assertEquals(0, Iterators.size(FunUtil.iterator(tree23.source())));
 	}
 
 }
