86914535b3bf4e3739703752fc953ea57a167347
ywsing <stupidsing@gmail.com>
2014-03-17 09:56:12 +0800

    rename to command utility

---
+++ src/main/java/suite/util/CommandUtil.java
@@ -0,0 +1,45 @@
+package suite.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandUtil<Command> {
+
+	private Map<String, Command> commandsByName;
+	private int maxLength;
+
+	public CommandUtil(Command commands[]) {
+		this(getCommandsByName(commands));
+	}
+
+	public CommandUtil(Map<String, Command> commandsByName) {
+		this.commandsByName = commandsByName;
+		maxLength = 0;
+		for (String name : commandsByName.keySet())
+			maxLength = Math.max(maxLength, name.length());
+	}
+
+	private static <Command> Map<String, Command> getCommandsByName(Command commands[]) {
+		Map<String, Command> commandsByName = new HashMap<>();
+		for (Command command : commands) {
+			String name = command.toString();
+			commandsByName.put(name, command);
+		}
+		return commandsByName;
+	}
+
+	public Pair<Command, String> recognize(String input) {
+		return recognize(input, 0);
+	}
+
+	public Pair<Command, String> recognize(String input, int start) {
+		for (int end = Math.min(start + maxLength, input.length()); end >= start; end--) {
+			String starts = input.substring(start, end);
+			Command command = commandsByName.get(starts);
+			if (command != null)
+				return Pair.create(command, input.substring(end));
+		}
+		return null;
+	}
+
+}
