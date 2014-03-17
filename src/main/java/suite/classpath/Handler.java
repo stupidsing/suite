8cf0dfed5dd135ea97efeedf0586e7060afedd83
ywsing <stupidsing@gmail.com>
2014-03-17 17:57:47 +0800

    add URL handler for classpath

---
+++ src/main/java/suite/classpath/Handler.java
@@ -0,0 +1,33 @@
+package suite.classpath;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+
+import suite.util.Util;
+
+public class Handler extends URLStreamHandler {
+
+	public static void register() {
+		String property = System.getProperty("java.protocol.handler.pkgs");
+		String packageName = "suite";
+
+		if (Util.isBlank(property))
+			property = packageName;
+		else if (!property.contains(packageName))
+			property = packageName + "|" + property;
+
+		System.setProperty("java.protocol.handler.pkgs", property);
+	}
+
+	@Override
+	protected URLConnection openConnection(URL url) throws IOException {
+		URL resource = getClass().getClassLoader().getResource(url.getPath());
+		if (resource != null)
+			return resource.openConnection();
+		else
+			throw new RuntimeException("Resource not found: " + url);
+	}
+
+}
