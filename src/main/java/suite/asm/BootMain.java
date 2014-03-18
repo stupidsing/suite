5c85f866fab1f0e6c20431e16f8edecffcfbfc46
stupidsing <stupidsing@gmail.com>
2014-03-19 00:15:29 +0800

    remove junit dependency

--- src/main/java/suite/asm/BootMain.java
+++ src/main/java/suite/asm/BootMain.java
@@ -1,8 +1,5 @@
 package suite.asm;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -29,20 +26,21 @@ public class BootMain extends ExecutableProgram {
 		Bytes bootLoader = new Assembler(16).assemble(To.string(new File("src/main/asm/bootloader.asm")));
 		Bytes kernel = new ImperativeCompiler().compile(0x40000, To.string(new File("src/main/resources/kernel.il")));
 
-		assertEquals(512, bootLoader.size());
-		assertTrue(kernel.size() < 65536);
+		if (bootLoader.size() == 512 && kernel.size() < 65536) {
 
-		// Combine the images and align to 512 bytes
-		Bytes disk0 = Bytes.concat(bootLoader, kernel);
-		Bytes disk1 = disk0.pad(disk0.size() + 511 & 0xFFFFFE00, (byte) 0);
+			// Combine the images and align to 512 bytes
+			Bytes disk0 = Bytes.concat(bootLoader, kernel);
+			Bytes disk1 = disk0.pad(disk0.size() + 511 & 0xFFFFFE00, (byte) 0);
 
-		Path path = Paths.get("target/boot.bin");
-		Files.write(path, disk1.getBytes());
+			Path path = Paths.get("target/boot.bin");
+			Files.write(path, disk1.getBytes());
 
-		System.out.println("cat target/boot.bin | dd bs=512 count=1 | /opt/udis86-1.7.2/udcli/udcli -16 | less");
-		System.out.println("cat target/boot.bin | dd bs=512 skip=1 | /opt/udis86-1.7.2/udcli/udcli -32 | less");
-		System.out.println("bochs -f src/main/asm/bochsrc");
-		return true;
+			System.out.println("cat target/boot.bin | dd bs=512 count=1 | /opt/udis86-1.7.2/udcli/udcli -16 | less");
+			System.out.println("cat target/boot.bin | dd bs=512 skip=1 | /opt/udis86-1.7.2/udcli/udcli -32 | less");
+			System.out.println("bochs -f src/main/asm/bochsrc");
+			return true;
+		} else
+			throw new RuntimeException("Size not match");
 	}
 
 }
