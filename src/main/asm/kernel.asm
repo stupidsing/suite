921802af034cdaa49d5a175b9c6f4a414939528b
ywsing <stupidsing@gmail.com>
2014-03-19 17:15:33 +0800

    add parentheses

--- src/main/asm/kernel.asm
+++ src/main/asm/kernel.asm
@@ -2,52 +2,52 @@
 	.idtAddress = +x20000
 	.stackTopAddress = +x2FFF0
 
-	MOV  ESP, .stackTopAddress
+	MOV  (ESP, .stackTopAddress)
 
 	-- Sets up IDT
-	MOV  EBX, .idtAddress
-	ADD  EBX, 2048
+	MOV  (EBX, .idtAddress)
+	ADD  (EBX, 2048)
 .nextIdtEntry
-	SUB  EBX, 8
-	MOV  ECX, .generalInterruptHandler
-	MOV  EDX, ECX
-	SHR  EDX, 16
-	MOV  WORD `EBX`, CX
-	MOV  WORD `EBX + 2`, CS
-	MOV  WORD `EBX + 4`, +x8E00
-	MOV  WORD `EBX + 6`, DX
-	CMP  EBX, .idtAddress
-	JNE  .nextIdtEntry
-	LIDT `.idtr`
+	SUB  (EBX, 8)
+	MOV  (ECX, .generalInterruptHandler)
+	MOV  (EDX, ECX)
+	SHR  (EDX, 16)
+	MOV  (WORD `EBX`, CX)
+	MOV  (WORD `EBX + 2`, CS)
+	MOV  (WORD `EBX + 4`, +x8E00)
+	MOV  (WORD `EBX + 6`, DX)
+	CMP  (EBX, .idtAddress)
+	JNE  (.nextIdtEntry)
+	LIDT (`.idtr`)
 
 	-- Sets the 8253 to enable 100 timer ticks per second, and enable keyboard
-	MOV  AL, +x36
-	OUT  +x43, AL
-	MOV  AX, 11932
-	OUT  +x40, AL
-	MOV  AL, AH
-	OUT  +x40, AL
-	MOV  AL, +xFC
-	OUT  +x21, AL
+	MOV  (AL, +x36)
+	OUT  (+x43, AL)
+	MOV  (AX, 11932)
+	OUT  (+x40, AL)
+	MOV  (AL, AH)
+	OUT  (+x40, AL)
+	MOV  (AL, +xFC)
+	OUT  (+x21, AL)
 
 	-- Show some fancy stuff on screen
-	MOV  DWORD `+xB8000`, +x70417041
-	STI
+	MOV  (DWORD `+xB8000`, +x70417041
+	STI  ()
 
 .loop
-	HLT
-	JMP  .loop
+	HLT  ()
+	JMP  (.loop)
 
 .generalInterruptHandler
-	PUSHA
-	INC  BYTE `+xB8000`
+	PUSHA ()
+	INC  (BYTE `+xB8000`)
 
 	-- Sends end of interrupt signal
-	MOV  AL, +x20
-	OUT  +x20, AL
-	POPA
-	IRET
+	MOV  (AL, +x20)
+	OUT  (+x20, AL)
+	POPA ()
+	IRET ()
 
 .idtr
-	D16  +x7FF
-	D32  .idtAddress
+	D16  (+x7FF)
+	D32  (.idtAddress)
