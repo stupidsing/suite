c625a8e9f77d1b524110005e51e69a69b536b40c
ywsing <stupidsing@gmail.com>
2014-03-27 17:58:23 +0800

    refactor

--- src/main/java/suite/asm/BootMain.java
+++ src/main/java/suite/asm/BootMain.java
@@ -3,7 +3,6 @@ package suite.asm;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import suite.ip.ImperativeCompiler;
@@ -29,11 +28,11 @@ public class BootMain extends ExecutableProgram {
 			Bytes disk0 = Bytes.concat(bootLoader, kernel);
 			Bytes disk1 = disk0.pad(disk0.size() + 511 & 0xFFFFFE00, (byte) 0);
 
-			Path path = Paths.get("target/boot.bin");
-			Files.write(path, disk1.getBytes());
+			String image = "target/boot.bin";
+			Files.write(Paths.get(image), disk1.getBytes());
 
-			System.out.println("cat target/boot.bin | dd bs=512 count=1 | /opt/udis86-1.7.2/udcli/udcli -16 | less");
-			System.out.println("cat target/boot.bin | dd bs=512 skip=1 | /opt/udis86-1.7.2/udcli/udcli -32 | less");
+			System.out.println("cat " + image + " | dd bs=512 count=1 | /opt/udis86-1.7.2/udcli/udcli -16 | less");
+			System.out.println("cat " + image + " | dd bs=512 skip=1 | /opt/udis86-1.7.2/udcli/udcli -32 | less");
 			System.out.println("bochs -f src/main/asm/bochsrc");
 			return true;
 		} else
