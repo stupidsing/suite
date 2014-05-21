e83f51a18d4b2382e1288c6a63ca79f8f7b8ab48
ywsing <stupidsing@gmail.com>
2014-05-21 18:45:30 +0800

    simplify memoize hierarchy

--- src/main/java/suite/util/MemoizeUtil.java
+++ src/main/java/suite/util/MemoizeUtil.java
@@ -24,31 +24,24 @@ public class MemoizeUtil {
 		};
 	}
 
-	public static class TimedMemoizer<T> {
-		private Source<T> source;
-		private long duration;
-		private long timestamp = 0;
-		private T result;
+	public static <T> Source<T> memoizeByTime(Source<T> source) {
+		return memoizeByTime(source, 30 * 1000l);
+	}
 
-		public TimedMemoizer(Source<T> source) {
-			this(source, 300 * 1000l);
-		}
+	public static <T> Source<T> memoizeByTime(Source<T> source, long duration) {
+		return new Source<T>() {
+			private long timestamp = 0;
+			private T result;
 
-		public TimedMemoizer(Source<T> source, long duration) {
-			this.duration = duration;
-			this.source = source;
-		}
-
-		public synchronized T get() {
-			long current = System.currentTimeMillis();
-
-			if (result == null || current > timestamp + duration) {
-				timestamp = current;
-				result = source.source();
+			public synchronized T source() {
+				long current = System.currentTimeMillis();
+				if (result == null || current > timestamp + duration) {
+					timestamp = current;
+					result = source.source();
+				}
+				return result;
 			}
-
-			return result;
-		}
+		};
 	}
 
 }
