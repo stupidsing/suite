9cad9924980be92eed25a0bb6157f0c72b3721cf
stupidsing <stupidsing@gmail.com>
2014-05-27 22:36:31 +0800

    refactor testBisect

--- src/test/java/suite/fp/eval/EagerFunTest.java
+++ src/test/java/suite/fp/eval/EagerFunTest.java
@@ -27,7 +27,7 @@ public class EagerFunTest {
 
 	@Test
 	public void testBisect() {
-		assertEquals(eval("(0; 1; 2; 3; 4;), (5; 6; 7; 8; 9;)"), eval("bisect {`< 5`} {0; 1; 2; 3; 4; 5; 6; 7; 8; 9;}"));
+		assertEquals(eval("(0; 2; 4; 6; 8;), (1; 3; 5; 7; 9;)"), eval("0; 1; 2; 3; 4; 5; 6; 7; 8; 9; | bisect {`= 0` . `% 2`}"));
 	}
 
 	@Test
