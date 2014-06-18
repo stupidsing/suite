bdcf8801d325d1f2fc48699b13e4f6d39eca2848
stupidsing <stupidsing@gmail.com>
2014-06-18 22:17:17 +0800

    refactor to create find predicates

--- src/main/java/suite/lp/predicate/SystemPredicates.java
+++ src/main/java/suite/lp/predicate/SystemPredicates.java
@@ -2,19 +2,13 @@ package suite.lp.predicate;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Stack;
 
-import suite.lp.doer.Cloner;
 import suite.lp.doer.Prover;
 import suite.node.Atom;
-import suite.node.Data;
 import suite.node.Node;
-import suite.node.Reference;
 import suite.node.Tree;
 import suite.node.io.Operator;
 import suite.node.io.TermOp;
-import suite.node.util.TermHashKey;
-import suite.util.FunUtil.Source;
 
 public class SystemPredicates {
 
@@ -31,9 +25,6 @@ public class SystemPredicates {
 
 		addPredicate("cut.begin", new CutBegin());
 		addPredicate("cut.end", new CutEnd());
-		addPredicate("find.all", new FindAll());
-		addPredicate("memoize", new MemoizePredicate());
-		addPredicate("memoize.clear", new MemoizeClear());
 		addPredicate("not", new Not());
 		addPredicate("once", new Once());
 		addPredicate("system.predicate", new SystemPredicate_());
@@ -61,6 +52,10 @@ public class SystemPredicates {
 		addPredicate("specialize", new EvalPredicates.Specialize());
 		addPredicate("temp", new EvalPredicates.Temp());
 		addPredicate("tree", new EvalPredicates.TreePredicate());
+
+		addPredicate("find.all", new FindPredicates.FindAll());
+		addPredicate("memoize", new FindPredicates.Memoize());
+		addPredicate("memoize.clear", new FindPredicates.MemoizeClear());
 
 		addPredicate("char.ascii", new FormatPredicates.CharAscii());
 		addPredicate("concat", new FormatPredicates.Concat());
@@ -157,34 +152,6 @@ public class SystemPredicates {
 		}
 	}
 
-	private class FindAll implements SystemPredicate {
-		public boolean prove(Prover prover, Node ps) {
-			Node params[] = Tree.getParameters(ps, 3);
-			return prover.bind(params[2], findAll(prover, params[0], params[1]));
-		}
-	}
-
-	private static Map<TermHashKey, Node> memoizedPredicates = new HashMap<>();
-
-	private class MemoizeClear implements SystemPredicate {
-		public boolean prove(Prover prover, Node ps) {
-			memoizedPredicates.clear();
-			return true;
-		}
-	}
-
-	// memoize is not re-entrant due to using computeIfAbsent()
-	private class MemoizePredicate implements SystemPredicate {
-		public boolean prove(Prover prover, Node ps) {
-			Node params[] = Tree.getParameters(ps, 3);
-			Reference var = (Reference) params[0];
-
-			Node goal = params[1];
-			TermHashKey key = new TermHashKey(new Cloner().clone(Tree.of(TermOp.SEP___, var, goal)));
-			return prover.bind(params[2], memoizedPredicates.computeIfAbsent(key, k -> findAll(prover, var, goal)));
-		}
-	}
-
 	private class Not implements SystemPredicate {
 		public boolean prove(Prover prover, Node ps) {
 			Prover prover1 = new Prover(prover);
@@ -214,22 +181,6 @@ public class SystemPredicates {
 		public boolean prove(Prover prover, Node ps) {
 			return prover.bind(ps, Atom.unique());
 		}
-	}
-
-	private Node findAll(Prover prover, Node var, Node goal) {
-		Stack<Node> stack = new Stack<>();
-
-		Tree subGoal = Tree.of(TermOp.AND___, goal, new Data<Source<Boolean>>(() -> {
-			stack.push(new Cloner().clone(var));
-			return Boolean.FALSE;
-		}));
-
-		new Prover(prover).elaborate(subGoal);
-
-		Node result = Atom.NIL;
-		while (!stack.isEmpty())
-			result = Tree.of(TermOp.AND___, stack.pop(), result);
-		return result;
 	}
 
 }
