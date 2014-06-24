4453b966c476fb3eca89eda774651bea9eff9f96
ywsing <stupidsing@gmail.com>
2014-06-24 18:38:09 +0800

    use bind instead of put

--- src/main/java/suite/lp/predicate/InternMapPredicates.java
+++ src/main/java/suite/lp/predicate/InternMapPredicates.java
@@ -3,10 +3,10 @@ package suite.lp.predicate;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import suite.lp.doer.Cloner;
 import suite.lp.doer.Prover;
 import suite.lp.predicate.SystemPredicates.SystemPredicate;
 import suite.node.Node;
+import suite.node.Reference;
 import suite.node.Tree;
 import suite.node.TreeIntern;
 import suite.node.util.IdHashKey;
@@ -35,8 +35,7 @@ public class InternMapPredicates {
 		public boolean prove(Prover prover, Node ps) {
 			Node params[] = Tree.getParameters(ps, 2);
 			IdHashKey key = new IdHashKey(params[0].finalNode());
-			internMap.put(key, new Cloner().clone(params[1]));
-			return true;
+			return prover.bind(internMap.computeIfAbsent(key, any -> new Reference()), params[1]);
 		}
 	}
 
