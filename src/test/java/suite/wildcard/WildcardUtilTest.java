32e10c180ce1b92f6668342d1d6d5e38a7f7ad09
ywsing <stupidsing@gmail.com>
2014-07-17 11:39:54 +0800

    rename package to suite.wildcard

---
+++ src/test/java/suite/wildcard/WildcardUtilTest.java
@@ -0,0 +1,29 @@
+package suite.wildcard;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import suite.util.Pair;
+import suite.wildcard.WildcardUtil;
+
+public class WildcardUtilTest {
+
+	@Test
+	public void test0() {
+		String match[] = WildcardUtil.match("*def*jkl*", "abcdefghijklmno");
+		System.out.println(Arrays.toString(match));
+		assertTrue(Arrays.equals(match, new String[] { "abc", "ghi", "mno" }));
+	}
+
+	@Test
+	public void test1() {
+		Pair<String[], String> match = WildcardUtil.matchStart("if * then ", "if a = b then if c = d then e = f else g = h");
+		assertTrue(Arrays.equals(match.t0, new String[] { "a = b" }));
+		assertEquals("if c = d then e = f else g = h", match.t1);
+	}
+
+}
