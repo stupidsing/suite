70c35a7c6525780c2dc6a9d778ca91f8f61f96cf
stupidsing <stupidsing@gmail.com>
2014-08-01 21:31:00 +0800

    add replace shell script

---
+++ src/main/sh/replace-all.sh
@@ -0,0 +1,19 @@
+replace() {
+  sed s/Board/Noun/g |
+  sed s/board/noun/g |
+  cat
+}
+
+replace-file() {
+  F0="${1}"
+  F1=$(echo "${F0}" | replace)
+  mkdir -p $(dirname "${F1}")
+  cat "${F0}" | replace > "${F1}"
+  echo ${F1}
+}
+
+while [ "${1}" ]; do
+  F0="${1}"
+  replace-file "${F0}"
+  shift
+done
