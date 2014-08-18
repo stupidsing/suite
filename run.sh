5d50e6d0f38866c4e136df9f9ac400ed5621af4a
stupidsing <stupidsing@gmail.com>
2014-08-18 21:31:02 +0800

    refactor to two stage checks and compiles

--- run.sh
+++ run.sh
@@ -6,15 +6,21 @@ DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n
 OPTS="-Xss2m -Xmx256m ${DEBUGOPTS} -Dhome.dir=${BASE}"
 CMD="java ${OPTS} -jar ${JAR} $@"
 
-SRCTIME=$(find "${BASE}/pom.xml" "${BASE}/src/main/java" "${BASE}/src/main/resources" -type f | xargs stat -c %Y | sort -g | tail -1)
-TARGETTIME=$(echo "${JAR}" "${BASE}/precompiled/STANDARD.node.gz" | xargs -I {} sh -c 'stat -c %Y {} 2> /dev/null || echo 0' | sort -g | head -1)
+getLatestTimestamp() {
+	xargs -I {} sh -c 'stat -c %Y "{}" 2> /dev/null || echo 0' | sort -g | tail -1
+}
 
 (
+	SRCTIME=$(find "${BASE}/pom.xml" "${BASE}/src/main/java/" "${BASE}/src/main/resources/" -type f 2> /dev/null | getLatestTimestamp)
+	TARGETTIME=$(echo "${JAR}" | getLatestTimestamp)
+	[ ${SRCTIME} -le ${TARGETTIME} ] || mvn -Dmaven.test.skip=true install assembly:single
+) &&
+
+(
+	SRCTIME=$(echo "${JAR}" | getLatestTimestamp)
+	TARGETTIME=$(echo "${BASE}/precompiled/STANDARD.node.gz" | getLatestTimestamp)
 	[ ${SRCTIME} -le ${TARGETTIME} ] ||
-	(
-		mvn -Dmaven.test.skip=true install assembly:single &&
-		(echo | java ${OPTS} -jar "${JAR}" precompile-all || (rm -f ${BASE}/precompiled/STANDARD.node.gz && false))
-	)
+	(echo | java ${OPTS} -jar "${JAR}" precompile-all || (rm -f ${BASE}/precompiled/STANDARD.node.gz && false))
 ) &&
 
 if which rlwrap > /dev/null; then
