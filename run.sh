6f660f45674bc87ddcaff31a42e81ddad7e6338c
stupidsing <stupidsing@gmail.com>
2014-08-19 20:46:45 +0800

    refactor

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ BASE="`dirname ${0}`"
 JAR="${BASE}/target/suite-1.0-jar-with-dependencies.jar"
 DEBUGOPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 OPTS="-Xss2m -Xmx256m ${DEBUGOPTS} -Dhome.dir=${BASE}"
-CMD="java ${OPTS} -jar ${JAR} $@"
+CMD="java ${OPTS} -jar ${JAR}"
+FULLCMD="${CMD} $@"
 
 getLatestTimestamp() {
 	TS=$(find "$@" -type f 2> /dev/null | xargs -I {} sh -c 'stat -c %Y "{}" 2> /dev/null' | sort -g | tail -1)
@@ -21,11 +22,11 @@ getLatestTimestamp() {
 	SOURCETIME=$(getLatestTimestamp "${JAR}")
 	TARGETTIME=$(getLatestTimestamp "${BASE}/precompiled/STANDARD.node.gz")
 	[ ${SOURCETIME} -le ${TARGETTIME} ] ||
-	(echo | java ${OPTS} -jar "${JAR}" precompile-all || (rm -f ${BASE}/precompiled/STANDARD.node.gz && false))
+	(echo | ${CMD} precompile-all || (rm -f ${BASE}/precompiled/STANDARD.node.gz && false))
 ) &&
 
 if which rlwrap > /dev/null; then
-	rlwrap -D2 -H "${HOME}/.suite_history" -i ${CMD}
+	rlwrap -D2 -H "${HOME}/.suite_history" -i ${FULLCMD}
 else
-	${CMD}
+	${FULLCMD}
 fi
