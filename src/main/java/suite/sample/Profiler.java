528e6e07bcba674c58a9ed0a23b2e1687e6ac4f5
stupidsing <stupidsing@gmail.com>
2014-09-25 21:23:07 +0800

    remove line number disturbances

--- src/main/java/suite/sample/Profiler.java
+++ src/main/java/suite/sample/Profiler.java
@@ -81,11 +81,8 @@ public class Profiler {
 					&& !Util.stringEquals(threadInfo.getThreadName(), "ReaderThread")) {
 				Set<String> elements = new HashSet<>();
 
-				for (StackTraceElement elem : threadInfo.getStackTrace()) {
-					String m = elem.getClassName() + "." + elem.getMethodName();
-					String l = elem.getFileName() + ":" + elem.getLineNumber();
-					elements.add(m + " (" + l + ")");
-				}
+				for (StackTraceElement elem : threadInfo.getStackTrace())
+					elements.add(elem.getClassName() + "." + elem.getMethodName() + " (" + elem.getFileName() + ")");
 
 				for (String name : elements)
 					record.computeIfAbsent(name, any -> new int[] { 0 })[0]++;
