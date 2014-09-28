d56e752ad3970070c6295ae95fdcc253219b7f31
stupidsing <stupidsing@gmail.com>
2014-09-28 21:10:49 +0800

    change test case

--- src/test/java/gpu/GpuTest.java
+++ src/test/java/gpu/GpuTest.java
@@ -18,7 +18,7 @@ public class GpuTest {
 		String openCl = "" //
 				+ "__kernel void add_floats(__global float *a, __global float *b, __global float *o, int n) { \n" //
 				+ "    int i = get_global_id(0); \n" //
-				+ "    if (i < n) o[i] = a[i] + b[i]; \n" //
+				+ "    if (i < n) o[i] = a[i] * a[i] + b[i] * b[i]; \n" //
 				+ "} \n" //
 		;
 
