9840b6f544623ea2f52594dabf73a4a1e066b72a
ywsing <stupidsing@gmail.com>
2014-10-03 14:46:54 +0800

    refactor

--- src/test/java/suite/immutable/btree/FileSystemTest.java
+++ src/test/java/suite/immutable/btree/FileSystemTest.java
@@ -3,6 +3,9 @@ package suite.immutable.btree;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
@@ -14,31 +17,45 @@ import suite.fs.impl.IbTreeFileSystemImpl;
 import suite.immutable.btree.impl.IbTreeConfiguration;
 import suite.primitive.Bytes;
 import suite.util.FileUtil;
+import suite.util.FunUtil;
+import suite.util.FunUtil.Source;
 import suite.util.To;
 
 public class FileSystemTest {
 
+	private interface TestCase {
+		public void test(FileSystem fs) throws IOException;
+	}
+
 	@Test
 	public void testIbTreeFileSystem() throws IOException {
+		testIbTree(FileUtil.tmp + "/ibTree-fs", this::test0);
+	}
+
+	@Test
+	public void testB_TreeFileSystem() throws IOException {
+		testB_Tree(FileUtil.tmp + "/b_tree-fs", this::test0);
+	}
+
+	private void testIbTree(String name, TestCase testCase) throws IOException {
 		IbTreeConfiguration<Bytes> config = new IbTreeConfiguration<>();
-		config.setFilenamePrefix(FileUtil.tmp + "/ibTree-fs");
+		config.setFilenamePrefix(name);
 		config.setPageSize(PageFile.defaultPageSize);
 		config.setMaxBranchFactor(PageFile.defaultPageSize / 64);
 		config.setCapacity(64 * 1024);
 
 		try (FileSystem fs = new IbTreeFileSystemImpl(config)) {
-			test(fs);
+			testCase.test(fs);
 		}
 	}
 
-	@Test
-	public void testB_TreeFileSystem() throws IOException {
-		try (FileSystem fs = new B_TreeFileSystemImpl(FileUtil.tmp + "/b_tree-fs", 4096)) {
-			test(fs);
+	private void testB_Tree(String name, TestCase testCase) throws IOException {
+		try (FileSystem fs = new B_TreeFileSystemImpl(name, 4096)) {
+			testCase.test(fs);
 		}
 	}
 
-	private void test(FileSystem fs) throws IOException {
+	private void test0(FileSystem fs) throws IOException {
 		Bytes filename = To.bytes("file");
 		Bytes data = To.bytes("data");
 
@@ -51,6 +68,16 @@ public class FileSystemTest {
 
 		fsm.replace(filename, null);
 		assertEquals(0, fsm.list(filename, null).size());
+	}
+
+	private void test1(FileSystem fs) throws IOException {
+		Source<Path> paths = FileUtil.findPaths(Paths.get("src"));
+
+		fs.create();
+		FileSystemMutator fsm = fs.mutate();
+
+		for (Path path : FunUtil.iter(paths))
+			fsm.replace(Bytes.of(path.toString().getBytes(FileUtil.charset)), Bytes.of(Files.readAllBytes(path)));
 	}
 
 }
