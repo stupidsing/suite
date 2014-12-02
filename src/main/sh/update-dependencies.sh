cc7f6915b43a3613ee44d03e0f56fb7f2c0bac70
stupidsing <stupidsing@gmail.com>
2014-12-02 21:38:28 +0800

    fix artifact id

--- src/main/sh/update-dependencies.sh
+++ src/main/sh/update-dependencies.sh
@@ -1,7 +1,8 @@
 DEPS=$(find src/ -name \*.java | xargs grep -h ^import | cut -d' ' -f2 | tr -d \; | sort | uniq |
 sed 's#com.fasterxml.jackson.databind.*#<groupId>com.fasterxml.jackson.core</groupId><artifactId>jackson-databind</artifactId>#g' |
 sed 's#com.jcraft.jsch.*#<groupId>com.jcraft</groupId><artifactId>jsch</artifactId>#g' |
-sed 's#com.nativelibs4java.opencl.*#<groupId>com.nativelibs4java</groupId><artifactId>bridj</artifactId>#g' |
+sed 's#com.nativelibs4java.bridj.*#<groupId>com.nativelibs4java</groupId><artifactId>bridj</artifactId>#g' |
+sed 's#com.nativelibs4java.opencl.*#<groupId>com.nativelibs4java</groupId><artifactId>javacl</artifactId>#g' |
 sed 's#org.apache.commons.logging.*#<groupId>commons-logging</groupId><artifactId>commons-logging</artifactId>#g' |
 sed 's#org.apache.log4j.*#<groupId>log4j</groupId><artifactId>log4j</artifactId>#g' |
 sed 's#org.junit.*#<groupId>junit</groupId><artifactId>junit</artifactId>#g' |
