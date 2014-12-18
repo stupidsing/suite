7947032eb869d8fb23c8a3bf5de7d9d38ec47f65
ywsing <stupidsing@gmail.com>
2014-12-18 09:54:12 +0800

    fix run script for Mac OS X

--- run.sh
+++ run.sh
@@ -7,22 +7,26 @@ OPTS="-Xss2m -Xmx256m ${DEBUGOPTS} -Dhome.dir=${BASE}"
 CMD="java ${OPTS} -jar ${JAR}"
 FULLCMD="${CMD} $@"
 
+{ uname -a | grep Mac > /dev/null; } && STAT="stat -f %m" || STAT="stat -c %Y"
+
 getLatestTimestamp() {
-	TS=$(find "$@" -type f 2> /dev/null | xargs stat -c %Y 2> /dev/null | sort -g | tail -1)
+	TS=$(find "$@" -type f 2> /dev/null | xargs ${STAT} 2> /dev/null | sort -g | tail -1)
 	[ ${TS} ] && echo ${TS} || echo 0
 }
 
 (
 	SOURCETIME=$(getLatestTimestamp "${BASE}/pom.xml" "${BASE}/src/main/" "${BASE}/src/main/resources/")
 	TARGETTIME=$(getLatestTimestamp "${JAR}")
-	[ ${SOURCETIME} -le ${TARGETTIME} ] || (cd ${BASE} && mvn -Dmaven.test.skip=true -T4 install assembly:single)
+	[ ${SOURCETIME} -le ${TARGETTIME} ] || { cd ${BASE} && mvn -Dmaven.test.skip=true -T4 install assembly:single; }
 ) &&
 
 (
 	SOURCETIME=$(getLatestTimestamp "${JAR}")
 	TARGETTIME=$(getLatestTimestamp "${BASE}/precompiled/STANDARD.node.gz")
 	[ ${SOURCETIME} -le ${TARGETTIME} ] ||
-	(echo | ${CMD} precompile-all || (rm -f ${BASE}/precompiled/STANDARD.node.gz && false))
+	{ echo | ${CMD} precompile-all ||
+		{ rm -f ${BASE}/precompiled/STANDARD.node.gz && false; }
+	}
 ) &&
 
 if which rlwrap > /dev/null; then
