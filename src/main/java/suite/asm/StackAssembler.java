7051ae33294123280fed5af4cc01380d2100745c
ywsing <stupidsing@gmail.com>
2014-12-23 07:53:41 +0800

    add stack save/restore

--- src/main/java/suite/asm/StackAssembler.java
+++ src/main/java/suite/asm/StackAssembler.java
@@ -21,6 +21,9 @@ public class StackAssembler extends Assembler {
 	private Fun<Node, Node[]> matchRr = Suite.matcher("RR: .0");
 	private Fun<Node, Node[]> matchTop = Suite.matcher("TOP: .0");
 
+	private Fun<Node, Node[]> matchRest = Suite.matcher("RRESTORE:");
+	private Fun<Node, Node[]> matchSave = Suite.matcher("RSAVE:");
+
 	public StackAssembler(int bits) {
 		super(bits);
 	}
@@ -32,20 +35,29 @@ public class StackAssembler extends Assembler {
 
 		for (Pair<Reference, Node> lni0 : lnis0) {
 			Node node0 = lni0.t1;
-			Node node1;
 			Node m[];
-			if ((m = matchPop.apply(node0)) != null)
-				node1 = new Rewriter(stackOperand, getRegister(--sp)).replace(m[0]);
-			else if ((m = matchPush.apply(node0)) != null)
-				node1 = new Rewriter(stackOperand, getRegister(sp++)).replace(m[0]);
-			else if ((m = matchRr.apply(node0)) != null) {
-				sp--;
-				node1 = Suite.substitute(".0 (.1, .2)", m[0], getRegister(sp - 1), getRegister(sp));
-			} else if ((m = matchTop.apply(node0)) != null)
-				node1 = new Rewriter(stackOperand, getRegister(sp - 1)).replace(m[0]);
-			else
-				node1 = node0;
-			lnis1.add(Pair.of(lni0.t0, node1));
+
+			if ((m = matchRest.apply(node0)) != null)
+				for (int r = sp - 1; r >= 0; r--)
+					lnis1.add(Pair.of(new Reference(), Suite.substitute("POP .0", getRegister(r))));
+			else if ((m = matchSave.apply(node0)) != null)
+				for (int r = 0; r < sp; r++)
+					lnis1.add(Pair.of(new Reference(), Suite.substitute("PUSH .0", getRegister(r))));
+			else {
+				Node node1;
+				if ((m = matchPop.apply(node0)) != null)
+					node1 = new Rewriter(stackOperand, getRegister(--sp)).replace(m[0]);
+				else if ((m = matchPush.apply(node0)) != null)
+					node1 = new Rewriter(stackOperand, getRegister(sp++)).replace(m[0]);
+				else if ((m = matchRr.apply(node0)) != null) {
+					sp--;
+					node1 = Suite.substitute(".0 (.1, .2)", m[0], getRegister(sp - 1), getRegister(sp));
+				} else if ((m = matchTop.apply(node0)) != null)
+					node1 = new Rewriter(stackOperand, getRegister(sp - 1)).replace(m[0]);
+				else
+					node1 = node0;
+				lnis1.add(Pair.of(lni0.t0, node1));
+			}
 		}
 
 		return lnis1;
