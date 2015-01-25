cd9808775745595d88074a9a12f50f62ca2f2c61
ywsing <stupidsing@gmail.com>
2015-01-26 07:48:06 +0800

    test animation

--- src/main/java/suite/ansi/TermiosMain.java
+++ src/main/java/suite/ansi/TermiosMain.java
@@ -3,8 +3,9 @@ package suite.ansi;
 import java.io.IOException;
 
 import suite.ansi.Termios.AnsiColor;
+import suite.util.Util;
 
-// java -cp target/suite-1.0-jar-with-dependencies.jar suite.ansi.TermiosMain
+// mvn assembly:single && java -cp target/suite-1.0-jar-with-dependencies.jar suite.ansi.TermiosMain
 public class TermiosMain {
 
 	public static void main(String args[]) throws IOException {
@@ -15,15 +16,25 @@ public class TermiosMain {
 			termios.background(AnsiColor.GREEN_);
 			termios.foreground(AnsiColor.RED___);
 			termios.puts("test red on green");
-			termios.resetColors();
 
-			for (int r = 0; r < 6; r++)
-				for (int g = 0; g < 6; g++)
-					for (int b = 0; b < 6; b++) {
-						termios.gotoxy(b + 8 * r, g + 1);
-						termios.background(r, g, b);
-						termios.puts(" ");
-					}
+			for (int i = 0; i < 40; i++) {
+				termios.resetColors();
+				termios.clear();
+
+				for (int r = 0; r < 6; r++)
+					for (int g = 0; g < 6; g++)
+						for (int b = 0; b < 6; b++) {
+							termios.gotoxy(b + 8 * r, g + 1);
+							termios.background(r, g, b);
+							termios.puts(" ");
+						}
+
+				termios.resetColors();
+
+				termios.gotoxy(i, i + 8);
+				termios.puts("string is moving on its way\n");
+				Util.sleepQuietly(300l);
+			}
 
 			termios.resetColors();
 
