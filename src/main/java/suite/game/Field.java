0e2c6e03462823a9d0c658bac561b9b354bb8e84
ywsing <stupidsing@gmail.com>
2015-01-25 22:32:58 +0800

    add game field

---
+++ src/main/java/suite/game/Field.java
@@ -0,0 +1,29 @@
+package suite.game;
+
+import java.util.List;
+
+import suite.game.Hex.XY;
+
+public class Field<O> {
+
+	private List<Occupation> occupations;
+
+	public class Occupation {
+		private XY xy;
+		private int radius;
+		private O object;
+	}
+
+	public Field(List<Occupation> occupations) {
+		this.occupations = occupations;
+	}
+
+	public O getObject(XY xy) {
+		Hex hex = new Hex();
+		for (Occupation occupation : occupations)
+			if (hex.distance(hex.diff(xy, occupation.xy)) < occupation.radius)
+				return occupation.object;
+		return null;
+	}
+
+}
