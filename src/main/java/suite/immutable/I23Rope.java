2617c7adbb03f0bd1f902846ed30b85b5e69dfc6
ywsing <stupidsing@gmail.com>
2015-02-05 22:47:03 +0800

    refactor

--- src/main/java/suite/immutable/I23Rope.java
+++ src/main/java/suite/immutable/I23Rope.java
@@ -23,14 +23,6 @@ public class I23Rope {
 		this.nodes = nodes;
 	}
 
-	public static I23Rope merge(I23Rope rope0, I23Rope rope1) {
-		List<I23Rope> nodes = merge0(rope0, rope1);
-		if (nodes.size() == 1)
-			return nodes.get(0);
-		else
-			return new I23Rope(nodes);
-	}
-
 	public I23Rope left(int w) {
 		return left0(weight - w);
 	}
@@ -39,28 +31,12 @@ public class I23Rope {
 		return right0(w);
 	}
 
-	private static List<I23Rope> merge0(I23Rope rope0, I23Rope rope1) {
-		List<I23Rope> nodes;
-
-		if (rope0.depth > rope1.depth)
-			nodes = Util.add(Util.left(rope0.nodes, -1), merge0(Util.last(rope0.nodes), rope1));
-		else if (rope0.depth < rope1.depth)
-			nodes = Util.add(merge0(rope0, Util.first(rope1.nodes)), Util.right(rope1.nodes, 1));
+	public static I23Rope merge(I23Rope rope0, I23Rope rope1) {
+		List<I23Rope> nodes = merge0(rope0, rope1);
+		if (nodes.size() == 1)
+			return nodes.get(0);
 		else
-			nodes = Util.add(rope0.nodes, rope1.nodes);
-
-		List<I23Rope> list;
-		int size1 = nodes.size();
-
-		if (size1 > 3) {
-			int halfSize = size1 / 2;
-			List<I23Rope> left = Util.left(nodes, halfSize);
-			List<I23Rope> right = Util.right(nodes, halfSize);
-			list = Arrays.asList(new I23Rope(left), new I23Rope(right));
-		} else
-			list = Arrays.asList(new I23Rope(nodes));
-
-		return list;
+			return new I23Rope(nodes);
 	}
 
 	private I23Rope left0(int weight) {
@@ -87,6 +63,30 @@ public class I23Rope {
 
 	private I23Rope normalize(List<I23Rope> nodes) {
 		return nodes.size() == 1 ? new I23Rope(nodes) : nodes.get(0);
+	}
+
+	private static List<I23Rope> merge0(I23Rope rope0, I23Rope rope1) {
+		List<I23Rope> nodes;
+
+		if (rope0.depth > rope1.depth)
+			nodes = Util.add(Util.left(rope0.nodes, -1), merge0(Util.last(rope0.nodes), rope1));
+		else if (rope0.depth < rope1.depth)
+			nodes = Util.add(merge0(rope0, Util.first(rope1.nodes)), Util.right(rope1.nodes, 1));
+		else
+			nodes = Util.add(rope0.nodes, rope1.nodes);
+
+		List<I23Rope> list;
+		int size1 = nodes.size();
+
+		if (size1 > 3) {
+			int halfSize = size1 / 2;
+			List<I23Rope> left = Util.left(nodes, halfSize);
+			List<I23Rope> right = Util.right(nodes, halfSize);
+			list = Arrays.asList(new I23Rope(left), new I23Rope(right));
+		} else
+			list = Arrays.asList(new I23Rope(nodes));
+
+		return list;
 	}
 
 }
