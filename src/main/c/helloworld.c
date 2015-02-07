f58d1efbdee3b23cd7463335de3dd00f08209b8a
ywsing <stupidsing@gmail.com>
2015-02-07 19:01:58 +0800

    add win32 hello world sample

---
+++ src/main/c/helloworld.c
@@ -0,0 +1,60 @@
+// gcc -mwindows src/main/c/helloworld.c
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow) {
+	static char szAppName[] = "winhello";
+	HWND hwnd;
+	MSG msg;
+	WNDCLASSEX wndclass;
+
+	wndclass.cbSize = sizeof(wndclass);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+
+	RegisterClassEx(&wndclass);
+
+	hwnd = CreateWindow(szAppName, "Hello, world!",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while ( GetMessage(&msg, NULL, 0, 0) ) {
+		TranslateMessage(&msg); /* for certain keyboard messages */
+		DispatchMessage(&msg); /* send message to WndProc */
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (iMsg) {
+		case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		TextOut(hdc, 100, 100, "Hello, world!", 13);
+		EndPaint(hwnd, &ps);
+		return 0;
+		case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+		default:
+		return DefWindowProc(hwnd, iMsg, wParam, lParam);
+	}
+}
