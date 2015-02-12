e6ec45578e82eeea8004be6f48acda01e435026b
ywsing <stupidsing@gmail.com>
2015-02-12 15:41:30 +0800

    fix predicate call checker

--- src/main/java/suite/lp/checker/PredicateCallChecker.java
+++ src/main/java/suite/lp/checker/PredicateCallChecker.java
@@ -57,11 +57,10 @@ public class PredicateCallChecker {
 		Prototype prototype = Prototype.of(node);
 		Integer np0;
 		int np1 = getParameters(node);
-		if ((np0 = nParametersByPrototype.get(prototype)) != null)
-			if (np0 == np1)
-				nParametersByPrototype.put(prototype, np1);
-			else
-				LogUtil.warn("Wrong number of parameters: " + prototype);
+		if ((np0 = nParametersByPrototype.get(prototype)) == null)
+			nParametersByPrototype.put(prototype, np1);
+		else if (np0 != np1)
+			LogUtil.warn("Wrong number of parameters: " + prototype);
 	}
 
 	private int getParameters(Node node) {
