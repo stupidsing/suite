84937133ff472c6195c8dc05f828c5c868f22ea9
ywsing <stupidsing@gmail.com>
2015-03-02 21:57:46 +1100

    add node writer

--- src/main/java/suite/node/io/Lister.java
+++ src/main/java/suite/node/io/Lister.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import suite.adt.Pair;
 import suite.immutable.IList;
+import suite.node.Atom;
 import suite.node.Dict;
 import suite.node.Node;
 import suite.node.Reference;
@@ -28,6 +29,7 @@ public class Lister {
 	public static class NodeReader {
 		public final String type;
 		public final Node terminal;
+		public final Operator op;
 		public final List<Pair<String, Node>> children;
 
 		public NodeReader(Node node) {
@@ -36,30 +38,65 @@ public class Lister {
 				Map<Node, Reference> map = ((Dict) node).map;
 				type = "dict";
 				terminal = null;
+				op = null;
 				children = Read.from(map).map(p -> Pair.of(p.t0.toString(), (Node) p.t1)).toList();
 			} else if ((tree = Tree.decompose(node)) != null) {
 				Operator operator = tree.getOperator();
 				if (Arrays.asList(TermOp.AND___, TermOp.OR____).contains(operator)) {
 					Streamlet<Node> st = Read.from(Tree.iter(node, operator));
-					type = operator.toString();
+					type = "list";
 					terminal = null;
+					op = operator;
 					children = st.index((i, n) -> Pair.of(i.toString(), n)).toList();
 				} else {
 					Pair<String, Node> p0 = Pair.of("l", tree.getLeft());
 					Pair<String, Node> p1 = Pair.of("r", tree.getRight());
-					type = operator.toString();
+					type = "tree";
 					terminal = null;
+					op = operator;
 					children = Arrays.asList(p0, p1);
 				}
 			} else if (node instanceof Tuple) {
 				List<Node> nodes = ((Tuple) node).nodes;
 				type = "tuple";
 				terminal = null;
+				op = null;
 				children = Read.from(nodes).index((i, n) -> Pair.of(i.toString(), n)).toList();
 			} else {
 				type = "term";
 				terminal = node;
+				op = null;
 				children = Collections.emptyList();
+			}
+		}
+	}
+
+	public static class NodeWriter {
+		public final Node node;
+
+		public NodeWriter(String type, Node terminal, Operator op, List<Pair<String, Node>> children) {
+			switch (type) {
+			case "dict":
+				node = new Dict(Read.from(children).toMap(p -> new Str(p.t0), p -> Reference.of(p.t1)));
+				break;
+			case "list":
+				List<Node> list = Read.from(children).map(p -> p.t1).toList();
+				Node n = Atom.NIL;
+				for (int i = list.size() - 1; i >= 0; i--)
+					n = Tree.of(op, list.get(i), n);
+				node = n;
+				break;
+			case "term":
+				node = terminal;
+				break;
+			case "tree":
+				node = Tree.of(op, children.get(0).t1, children.get(1).t1);
+				break;
+			case "tuple":
+				node = new Tuple(Read.from(children).map(p -> p.t1).toList());
+				break;
+			default:
+				throw new RuntimeException();
 			}
 		}
 	}
--- src/test/java/suite/node/parser/RecursiveFactorizerTest.java
+++ src/test/java/suite/node/parser/RecursiveFactorizerTest.java
@@ -8,11 +8,13 @@ import org.junit.Test;
 
 import suite.inspect.Inspect;
 import suite.node.Node;
+import suite.node.Str;
 import suite.node.io.Lister;
 import suite.node.io.TermOp;
 import suite.node.parser.RecursiveFactorizer.FNode;
 import suite.node.parser.RecursiveFactorizer.FTerminal;
 import suite.node.parser.RecursiveFactorizer.FTree;
+import suite.node.util.Rewriter;
 import suite.os.FileUtil;
 import suite.streamlet.Read;
 import suite.util.Nodify;
@@ -35,6 +37,21 @@ public class RecursiveFactorizerTest {
 		String s0 = FileUtil.read("src/main/ll/ic/ic.sl").trim();
 		FNode fn0 = recursiveFactorizer.parse(s0);
 		FNode fnx = transform(fn0);
+		String sx = recursiveFactorizer.unparse(fnx);
+		System.out.println(sx);
+	}
+
+	@Test
+	public void testRefactorFile1() throws IOException {
+		Nodify nodify = new Nodify(new Inspect());
+		Rewriter rewriter = new Rewriter(new Str("ic-compile0"), new Str("ic-compile1"));
+
+		String s0 = FileUtil.read("src/main/ll/ic/ic.sl").trim();
+		FNode fn0 = recursiveFactorizer.parse(s0);
+		Node node0 = nodify.nodify(FNode.class, fn0);
+		System.out.println(node0);
+		Node nodex = rewriter.replace(node0);
+		FNode fnx = nodify.unnodify(FNode.class, nodex);
 		String sx = recursiveFactorizer.unparse(fnx);
 		System.out.println(sx);
 	}
