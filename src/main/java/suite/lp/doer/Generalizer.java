5daa3c440a1d4b34be1248fedfa600ee2529a356
ywsing <stupidsing@gmail.com>
2015-03-07 19:40:24 +1100

    add read node case

--- src/main/java/suite/lp/doer/Generalizer.java
+++ src/main/java/suite/lp/doer/Generalizer.java
@@ -1,13 +1,18 @@
 package suite.lp.doer;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import suite.adt.Pair;
 import suite.lp.sewing.SewingGeneralizer;
 import suite.node.Atom;
 import suite.node.Node;
 import suite.node.Reference;
 import suite.node.Tree;
+import suite.node.io.Rewriter.NodeRead;
+import suite.node.io.Rewriter.NodeWrite;
+import suite.streamlet.Read;
 
 public class Generalizer {
 
@@ -33,6 +38,14 @@ public class Generalizer {
 					right = getVariable(right);
 			} else if ((rt = Tree.decompose(right)) != null)
 				right = nextTree = Tree.of(rt.getOperator(), generalize(rt.getLeft()), rt.getRight());
+			else {
+				NodeRead nr = new NodeRead(right);
+				List<Pair<Node, Node>> children1 = Read.from(nr.children) //
+						.map(p -> Pair.of(p.t0, generalize(p.t1))) //
+						.toList();
+				NodeWrite nw = new NodeWrite(nr.type, nr.terminal, nr.op, children1);
+				right = nw.node;
+			}
 
 			Tree.forceSetRight(tree, right);
 			tree = nextTree;
