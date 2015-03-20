32f968bc98537edd46aa52987a9936549b136c28
ywsing <stupidsing@gmail.com>
2015-03-20 19:05:55 +0800

    refactor

---
+++ src/main/sh/replace.sh
@@ -0,0 +1,30 @@
+replace() {
+  REPLACER="${1}"
+  F0="${2}"
+  F1=$(echo "${F0}" | sh -c "${REPLACER}")
+  mkdir -p $(dirname "${F1}")
+  cat "${F0}" | sh -c "${REPLACER}" > /tmp/replaced
+  [ "${REMOVEOLD}" ] && rm -f "${F0}" || true
+  mv /tmp/replaced "${F1}"
+  echo ${F1}
+}
+
+replace-file() {
+  REPLACER="${1}"
+  shift
+  while [ "${1}" ]; do
+    F0="${1}"
+    shift
+    replace "${REPLACER}" "${F0}"
+  done
+}
+
+replace-files() {
+  while read F; do
+    replace "${1}" "${F}"
+  done
+}
+
+replace-example() {
+  replace "sed 's/board/Board/g'" file.txt
+}
