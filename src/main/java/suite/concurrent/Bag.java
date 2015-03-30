12a8e26c3814301b74f230121c737610ac81de80
ywsing <stupidsing@gmail.com>
2015-03-31 07:14:38 +0800

    refactor

--- src/main/java/suite/concurrent/Bag.java
+++ src/main/java/suite/concurrent/Bag.java
@@ -7,15 +7,15 @@ import suite.immutable.IList;
 
 public class Bag<S> implements Iterable<S> {
 
-	private AtomicStampedReference<IList<S>> ar = new AtomicStampedReference<>(IList.end(), 0);
+	private AtomicStampedReference<IList<S>> asr = new AtomicStampedReference<>(IList.end(), 0);
 
 	public void add(S s) {
 		while (true) {
 			int a[] = new int[1];
-			IList<S> list0 = ar.get(a);
+			IList<S> list0 = asr.get(a);
 			IList<S> list1 = IList.cons(s, list0);
 			int stamp = a[0];
-			if (!ar.compareAndSet(list0, list1, stamp, stamp + 1))
+			if (!asr.compareAndSet(list0, list1, stamp, stamp + 1))
 				Thread.yield(); // Back-off
 			else
 				break;
@@ -23,7 +23,7 @@ public class Bag<S> implements Iterable<S> {
 	}
 
 	public Iterator<S> iterator() {
-		return ar.getReference().iterator();
+		return asr.getReference().iterator();
 	}
 
 }
