e93a6cac4cb61c31e42966b358f612c17829a1d5
ywsing <stupidsing@gmail.com>
2015-04-24 09:27:23 +0800

    refactor head recursion reduction logic

--- src/main/java/suite/ebnf/EbnfHeadRecursion.java
+++ src/main/java/suite/ebnf/EbnfHeadRecursion.java
@@ -2,31 +2,43 @@ package suite.ebnf;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import suite.ebnf.EbnfGrammar.EbnfGrammarType;
+import suite.streamlet.Read;
 import suite.util.Util;
 
+/**
+ * Transform head-recursion rule as follows:
+ *
+ * A = B0 | B1 | ... | Bm | A C0 | A C1 | ... | A Cn
+ *
+ * becomes
+ *
+ * A = (B0 | B1 | ... | Bm) (C0 | C1 | ... | Cn)*
+ */
 public class EbnfHeadRecursion {
 
 	private Map<String, EbnfGrammar> nodesByEntity;
+
+	private class HeadRecursionForm {
+		private List<EbnfGrammar> listb;
+		private List<EbnfGrammar> listc;
+
+		private HeadRecursionForm(List<EbnfGrammar> listb, List<EbnfGrammar> listc) {
+			this.listb = listb;
+			this.listc = listc;
+		}
+	}
 
 	public EbnfHeadRecursion(Map<String, EbnfGrammar> nodesByEntity) {
 		this.nodesByEntity = nodesByEntity;
 	}
 
-	/**
-	 * Transform head-recursion rule as follows:
-	 *
-	 * A = B0 | B1 | ... | Bm | A C0 | A C1 | ... | A Cn
-	 *
-	 * becomes
-	 *
-	 * A = (B0 | B1 | ... | Bm) (C0 | C1 | ... | Cn)*
-	 */
 	public EbnfGrammar reduceHeadRecursion(EbnfGrammar en0) {
-		EbnfGrammar en = lookup(en0);
+		EbnfGrammar en = expand(en0);
 		if (en.type == EbnfGrammarType.NAMED_)
 			return reduceHeadRecursion(en.children.get(0), en.content);
 		else
@@ -34,50 +46,51 @@ public class EbnfHeadRecursion {
 	}
 
 	public EbnfGrammar reduceHeadRecursion(EbnfGrammar en0, String entity) {
-		EbnfGrammar en = lookup(en0);
+		HeadRecursionForm hrf = getHeadRecursionForm(en0, entity);
 		EbnfGrammar en1;
+
+		if (!hrf.listc.isEmpty()) {
+			EbnfGrammar enb = new EbnfGrammar(EbnfGrammarType.OR____, hrf.listb);
+			EbnfGrammar enc = new EbnfGrammar(EbnfGrammarType.OR____, hrf.listc);
+			en1 = new EbnfGrammar(EbnfGrammarType.REPT0H, entity, Arrays.asList(enb, enc));
+		} else
+			en1 = new EbnfGrammar(EbnfGrammarType.NAMED_, entity, Arrays.asList(en0));
+
+		return en1;
+	}
+
+	public HeadRecursionForm getHeadRecursionForm(EbnfGrammar en0, String entity) {
+		EbnfGrammar en = expand(en0);
+		List<EbnfGrammar> ens;
+		HeadRecursionForm hrf;
 
 		if (en.type == EbnfGrammarType.OR____) {
 			List<EbnfGrammar> listb = new ArrayList<>();
 			List<EbnfGrammar> listc = new ArrayList<>();
 
-			for (EbnfGrammar childEn : en.children) {
-				if (childEn.type == EbnfGrammarType.AND___) {
-					List<EbnfGrammar> ens = childEn.children;
-
-					if (resolve(ens.get(0)) == en) {
-						listc.add(new EbnfGrammar(EbnfGrammarType.AND___, Util.right(ens, 1)));
-						continue;
-					}
-				}
-
-				listb.add(childEn);
+			for (HeadRecursionForm hrf1 : Read.from(en.children).map(en_ -> getHeadRecursionForm(en_, entity))) {
+				listb.addAll(hrf1.listb);
+				listc.addAll(hrf1.listc);
 			}
 
-			if (!listc.isEmpty()) {
-				EbnfGrammar enb = new EbnfGrammar(EbnfGrammarType.OR____, listb);
-				EbnfGrammar enc = new EbnfGrammar(EbnfGrammarType.OR____, listc);
-				en1 = new EbnfGrammar(EbnfGrammarType.REPT0H, entity, Arrays.asList(enb, enc));
-			} else
-				en1 = new EbnfGrammar(EbnfGrammarType.NAMED_, entity, Arrays.asList(en));
-		} else
-			en1 = new EbnfGrammar(EbnfGrammarType.NAMED_, entity, Arrays.asList(en));
-
-		return en1;
-	}
-
-	private EbnfGrammar resolve(EbnfGrammar en0) {
-		EbnfGrammar en = lookup(en0);
-		if (en.type == EbnfGrammarType.NAMED_)
-			return en.children.get(0);
+			hrf = new HeadRecursionForm(listb, listc);
+		} else if (en.type == EbnfGrammarType.AND___ && Util.stringEquals(name((ens = en.children).get(0)), entity))
+			hrf = new HeadRecursionForm(Collections.emptyList(), Util.right(ens, 1));
 		else
-			return en;
+			hrf = new HeadRecursionForm(Arrays.asList(en), Collections.emptyList());
+
+		return hrf;
 	}
 
-	private EbnfGrammar lookup(EbnfGrammar en) {
+	private String name(EbnfGrammar en0) {
+		EbnfGrammar en = expand(en0);
+		return en.type == EbnfGrammarType.NAMED_ ? en.content : null;
+	}
+
+	private EbnfGrammar expand(EbnfGrammar en) {
 		if (en.type == EbnfGrammarType.ENTITY) {
 			EbnfGrammar en1 = nodesByEntity.get(en.content);
-			return en1 != null ? lookup(en1) : en;
+			return en1 != null ? expand(en1) : en;
 		} else
 			return en;
 	}
