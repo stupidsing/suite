7d92f3261c079b4c9e9cf0d4c23271cf04790e1e
ywsing <stupidsing@gmail.com>
2015-05-27 16:16:51 +0800

    add class creation test case

---
+++ src/test/java/suite/jdk/ClassCreator.java
@@ -0,0 +1,57 @@
+package suite.jdk;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.Callable;
+
+import org.bridj.relocated.org.objectweb.asm.ClassWriter;
+import org.bridj.relocated.org.objectweb.asm.MethodVisitor;
+import org.bridj.relocated.org.objectweb.asm.Opcodes;
+import org.junit.Test;
+
+public class ClassCreator {
+
+	private class Creator implements Opcodes {
+		private Object create() throws Exception {
+			ClassWriter cw = new ClassWriter(0);
+
+			cw.visit(49, ACC_PUBLIC + ACC_SUPER, "Hello", null, "java/lang/Object",
+					new String[] { "java/util/concurrent/Callable" });
+			cw.visitSource("Hello.java", null);
+
+			{
+				MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+				mv.visitVarInsn(ALOAD, 0);
+				mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
+				mv.visitInsn(RETURN);
+				mv.visitMaxs(1, 1);
+				mv.visitEnd();
+			}
+
+			{
+				MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "call", "()Ljava/lang/Object;", null, null);
+				mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+				mv.visitLdcInsn("hello world");
+				mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V");
+				mv.visitLdcInsn("");
+				mv.visitInsn(ARETURN);
+				mv.visitMaxs(2, 1);
+				mv.visitEnd();
+			}
+
+			cw.visitEnd();
+
+			byte bytes[] = cw.toByteArray();
+
+			UnsafeUtil unsafeUtil = new UnsafeUtil();
+			Callable<?> callable = unsafeUtil.defineClass(Callable.class, "Hello", bytes).newInstance();
+			return callable.call();
+		}
+	}
+
+	@Test
+	public void testCreateClass() throws Exception {
+		assertEquals("", new Creator().create());
+	}
+
+}
