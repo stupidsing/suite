aba62371a008d174cb4f9e821ebe2637a6b71f70
stupidsing <stupidsing@gmail.com>
2015-05-31 12:00:47 +0800

    add failed test case

--- src/test/java/FailedTests.java
+++ src/test/java/FailedTests.java
@@ -78,4 +78,10 @@ public class FailedTests {
 		assertFalse(true);
 	}
 
+	// Where is the error message?
+	@Test
+	public void testFix() {
+		Suite.evaluateFunType("fix {cons {0}} | head {3}");
+	}
+
 }
