23082a0e42704470294a37501cd16c20ce36b4b5
stupidsing <stupidsing@gmail.com>
2015-06-16 22:08:35 +0800

    use executor to schedule

--- src/main/java/suite/animation/AnimationMain.java
+++ src/main/java/suite/animation/AnimationMain.java
@@ -4,24 +4,28 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.lang.management.ManagementFactory;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
-import org.junit.Test;
+import suite.util.Util;
+import suite.util.Util.ExecutableProgram;
 
-import suite.os.LogUtil;
-
-public class AnimationTest {
+public class AnimationMain extends ExecutableProgram {
 
 	private class PaintInput {
 		private int i;
 	}
 
-	@Test
-	public void testAnimation() throws InterruptedException {
+	public static void main(String args[]) {
+		Util.run(AnimationTest.class, args);
+	}
+
+	protected boolean run(String args[]) throws InterruptedException {
 		JFrame frame = new JFrame("Animation");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setSize(new Dimension(1024, 768));
@@ -33,23 +37,24 @@ public class AnimationTest {
 			private static final long serialVersionUID = 1l;
 
 			protected void paintComponent(Graphics graphics) {
+				super.paintComponent(graphics);
+
 				Graphics2D g2d = (Graphics2D) graphics;
 				g2d.setColor(Color.BLACK);
-				g2d.fillRect(pi.i, pi.i, pi.i, pi.i);
+				g2d.fillRect(pi.i, pi.i, 32, 32);
 			}
 		});
 
-		long n0 = 0;
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(4);
 
-		while (true) {
+		executor.scheduleAtFixedRate(() -> {
 			pi.i++;
 			frame.repaint();
-			Thread.sleep(1000 / 24);
-			long n1 = System.nanoTime();
-			int nGcs = ManagementFactory.getGarbageCollectorMXBeans().size();
-			LogUtil.info(nGcs + "/" + (n1 - n0));
-			n0 = n1;
-		}
+		}, 10, 10, TimeUnit.MILLISECONDS);
+
+		executor.awaitTermination(100, TimeUnit.SECONDS);
+
+		return true;
 	}
 
 }
