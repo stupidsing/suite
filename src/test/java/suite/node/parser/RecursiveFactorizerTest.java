f16f2c19a6ee772d2d3400af1fe04a77773b1ba1
ywsing <stupidsing@gmail.com>
2015-07-14 21:53:36 +0800

    extract method to add new argument

--- src/test/java/suite/node/parser/RecursiveFactorizerTest.java
+++ src/test/java/suite/node/parser/RecursiveFactorizerTest.java
@@ -80,6 +80,15 @@ public class RecursiveFactorizerTest {
 
 	@Test
 	public void testRefactorRewrite() throws IOException {
+		String pred0 = "ic-compile-better-option";
+		String predx = "ic-new-compile-better-option";
+		String sx = rewriteNewArgument(pred0, predx, ".type", FileUtil.read("src/main/ll/ic/ic.sl").trim());
+
+		System.out.println(sx);
+		assertFalse(sx.contains(pred0));
+	}
+
+	private String rewriteNewArgument(String pred0, String predx, String newArgument, String s0) {
 		Source<Node[]> source = () -> {
 			Reference r[] = new Reference[64];
 
@@ -89,21 +98,19 @@ public class RecursiveFactorizerTest {
 			Fun<String, Fun<Boolean, Node>> fun = hs -> b -> {
 				Source<Node> g = To.source(r);
 				Node head = terminalNode(hs);
-				Node n0 = !b ? g.source() : operatorNode(TermOp.TUPLE_,
-						Arrays.asList(g.source(), terminalNode(" "), terminalNode(".type")));
+				Node n0 = !b ? g.source()
+						: operatorNode(TermOp.TUPLE_, Arrays.asList(g.source(), terminalNode(" "), terminalNode(newArgument)));
 				Node n1 = operatorNode(g, TermOp.TUPLE_, g.source(), n0);
 				Node n2 = operatorNode(g, TermOp.TUPLE_, g.source(), n1);
 				return operatorNode(g, TermOp.TUPLE_, head, n2);
 			};
 
-			return new Node[] { fun.apply("ic-compile-better-option").apply(false),
-					fun.apply("ic-new-compile-better-option").apply(true) };
+			return new Node[] { fun.apply(pred0).apply(false), fun.apply(predx).apply(true) };
 		};
 
 		TreeRewriter tr = new TreeRewriter();
 		Nodify nodify = new Nodify(new Inspect());
 
-		String s0 = FileUtil.read("src/main/ll/ic/ic.sl").trim();
 		FR fr0 = recursiveFactorizer.parse(s0);
 		FNode fn0 = fr0.node;
 		Node node0 = nodify.nodify(FNode.class, fn0);
@@ -111,8 +118,7 @@ public class RecursiveFactorizerTest {
 		FNode fnx = nodify.unnodify(FNode.class, nodex);
 		FR frx = new FR(fr0.pre, fnx, fr0.post);
 		String sx = recursiveFactorizer.unparse(frx);
-		System.out.println(sx);
-		assertFalse(sx.contains("ic-compile-better-option"));
+		return sx;
 	}
 
 	private Node operatorNode(Source<Node> g, TermOp op, Node n0, Node n1) {
