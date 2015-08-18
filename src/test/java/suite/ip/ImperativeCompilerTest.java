1b59ae98f80ea44bd3fe0b71ee9ae525386a2781
ywsing <stupidsing@gmail.com>
2015-08-18 22:13:42 +0800

    add test cases; add SSE registers

--- src/test/java/suite/ip/ImperativeCompilerTest.java
+++ src/test/java/suite/ip/ImperativeCompilerTest.java
@@ -8,9 +8,32 @@ import suite.primitive.Bytes;
 
 public class ImperativeCompilerTest {
 
+	private ImperativeCompiler imperativeCompiler = new ImperativeCompiler();
+
 	@Test
-	public void test() {
-		Bytes bytes = new ImperativeCompiler().compile(0, "`0` = 1;");
+	public void testDeclare() {
+		Bytes bytes = imperativeCompiler.compile(0, "declare v = 1; let v = 2;");
+		assertNotNull(bytes);
+		System.out.println(bytes);
+	}
+
+	@Test
+	public void testExpr() {
+		Bytes bytes = imperativeCompiler.compile(0, "`0` + 1 = 2;");
+		assertNotNull(bytes);
+		System.out.println(bytes);
+	}
+
+	@Test
+	public void testLet() {
+		Bytes bytes = imperativeCompiler.compile(0, "let `0` = 1;");
+		assertNotNull(bytes);
+		System.out.println(bytes);
+	}
+
+	@Test
+	public void testSubroutine() {
+		Bytes bytes = imperativeCompiler.compile(0, "declare t = this; declare s = [i,] ( i; ); t:s [1,];");
 		assertNotNull(bytes);
 		System.out.println(bytes);
 	}
