2b0c85c80e20e07612105cf3f4b2d17c9a159d4c
ywsing <stupidsing@gmail.com>
2015-09-15 12:28:28 +0800

    fix collection count

--- src/main/java/suite/os/TimeUtil.java
+++ src/main/java/suite/os/TimeUtil.java
@@ -11,10 +11,10 @@ public class TimeUtil {
 	public class TimedResult<T> {
 		public T result;
 		public long duration;
-		public int nGcs;
+		public long nGcs;
 		public long gcDuration;
 
-		public TimedResult(T result, long duration, int nGcs, long gcDuration) {
+		public TimedResult(T result, long duration, long nGcs, long gcDuration) {
 			this.result = result;
 			this.duration = duration;
 			this.nGcs = nGcs;
@@ -25,14 +25,14 @@ public class TimeUtil {
 	public <T> TimedResult<T> time(Source<T> source) {
 		List<GarbageCollectorMXBean> gcBeans0 = ManagementFactory.getGarbageCollectorMXBeans();
 		long t0 = System.currentTimeMillis();
-		int nGcs0 = gcBeans0.size();
+		long nGcs0 = gcBeans0.stream().mapToLong(GarbageCollectorMXBean::getCollectionCount).sum();
 		long gcDuration0 = gcBeans0.stream().mapToLong(GarbageCollectorMXBean::getCollectionTime).sum();
 
 		T t = source.source();
 
 		List<GarbageCollectorMXBean> gcBeans1 = ManagementFactory.getGarbageCollectorMXBeans();
 		long t1 = System.currentTimeMillis();
-		int nGcs1 = gcBeans1.size();
+		long nGcs1 = gcBeans1.stream().mapToLong(GarbageCollectorMXBean::getCollectionCount).sum();
 		long gcDuration1 = gcBeans1.stream().mapToLong(GarbageCollectorMXBean::getCollectionTime).sum();
 
 		return new TimedResult<>(t, t1 - t0, nGcs1 - nGcs0, gcDuration1 - gcDuration0);
