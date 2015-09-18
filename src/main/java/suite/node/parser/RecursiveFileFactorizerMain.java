13a4065442759934d806172e00d4a930fd187d69
ywsing <stupidsing@gmail.com>
2015-09-18 09:22:29 +0800

    add file factorizer

---
+++ src/main/java/suite/node/parser/RecursiveFileFactorizerMain.java
@@ -0,0 +1,46 @@
+package suite.node.parser;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import suite.node.io.TermOp;
+import suite.os.FileUtil;
+import suite.util.To;
+import suite.util.Util;
+import suite.util.Util.ExecutableProgram;
+
+// mvn assembly:single && java -cp target/suite-1.0-jar-with-dependencies.jar suite.node.parser.RecursiveFactorizerMain
+public class RecursiveFileFactorizerMain extends ExecutableProgram {
+
+	public static void main(String args[]) {
+		Util.run(RecursiveFileFactorizerMain.class, args);
+	}
+
+	protected boolean run(String args[]) throws IOException {
+		// String from = "ic-parse .0 .1";
+		// String to = "ic-parse .f .0 .1";
+		String from = "ic-compile .0 .1 .2";
+		String to = "ic-compile .f/.ve .0 .1 .2";
+
+		FileUtil.findPaths(Paths.get("src/")) //
+				.filter(path -> path.getFileName().startsWith("ic") && path.getFileName().endsWith(".sl")) //
+				.forEach(path -> {
+					try {
+						RecursiveFactorizer recursiveFactorizer = new RecursiveFactorizer(TermOp.values());
+						String s0 = FileUtil.read(path);
+						String sx = recursiveFactorizer.rewrite(from, to, s0);
+						Files.write(path, sx.getBytes(FileUtil.charset));
+					} catch (IOException ex) {
+						throw new RuntimeException(ex);
+					}
+				});
+
+		RecursiveFactorizer recursiveFactorizer = new RecursiveFactorizer(TermOp.values());
+		String s0 = To.string(System.in);
+		String sx = recursiveFactorizer.rewrite(args[0], args[1], s0);
+		System.out.println(sx);
+		return true;
+	}
+
+}
