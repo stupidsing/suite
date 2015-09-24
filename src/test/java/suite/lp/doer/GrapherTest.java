f316649c0965ade40f836f60dc30dfded2bf5523
stupidsing <stupidsing@gmail.com>
2015-09-24 20:58:37 +0800

    add test case

--- src/test/java/suite/lp/doer/GrapherTest.java
+++ src/test/java/suite/lp/doer/GrapherTest.java
@@ -14,9 +14,11 @@ public class GrapherTest {
 	@Test
 	public void testBind() {
 		assertTrue(prove(".u = hello, .v = hello, graph.bind .u .v"));
-		assertTrue(prove(".u = 1 .u, .v = 1 .v, graph.bind .u .v"));
+		assertTrue(prove(".u = 1 .u:.u, .v = 1 .v:.v, graph.bind .u .v"));
+		assertTrue(prove(".u = 1 .u:.v, .v = 1 .v:.u, graph.bind .u .v"));
 		assertFalse(prove(".u = 1 .u, .v = 2 .v, graph.bind .u .v"));
 		assertFalse(prove(".u = 1 .u, .v = .v 1, graph.bind .u .v"));
+		assertFalse(prove(".u = .u 1, .v = .v 2, graph.bind .u .v"));
 	}
 
 	private boolean prove(String lp) {
