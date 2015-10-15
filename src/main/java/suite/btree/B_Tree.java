42a726b535eea52202ac2a339c70f1d48135c2d4
ywsing <stupidsing@gmail.com>
2015-10-15 11:27:27 +0800

    extract key data store interface

--- src/main/java/suite/btree/B_Tree.java
+++ src/main/java/suite/btree/B_Tree.java
@@ -4,24 +4,16 @@ import java.io.Closeable;
 import java.io.PrintStream;
 
 import suite.adt.Pair;
+import suite.fs.KeyDataStore;
 import suite.fs.KeyValueStore;
-import suite.primitive.Bytes;
 import suite.streamlet.Streamlet;
 
-public interface B_Tree<Key, Value> extends Closeable, KeyValueStore<Key, Value> {
+public interface B_Tree<Key, Value> extends Closeable, KeyDataStore<Key>, KeyValueStore<Key, Value> {
 
 	public void create();
 
 	public void dump(PrintStream w);
 
-	public Bytes getPayload(Key key);
-
-	public boolean getTerminal(Key key);
-
 	public Streamlet<Pair<Key, Value>> range(Key start, Key end);
-
-	public void putPayload(Key key, Bytes payload);
-
-	public void putTerminal(Key key);
 
 }
--- src/main/java/suite/fs/KeyDataStoreMutator.java
+++ src/main/java/suite/fs/KeyDataStoreMutator.java
@@ -1,24 +1,5 @@
 package suite.fs;
 
-import suite.primitive.Bytes;
-
-public interface KeyDataStoreMutator<Key> extends KeyValueStoreMutator<Key, Integer> {
-
-	public Bytes getPayload(Key key);
-
-	public boolean getTerminal(Key key);
-
-	/**
-	 * Replaces a value with another, attached with a payload of page data. For
-	 * dictionary cases to replace stored value of the same key.
-	 *
-	 * Asserts comparator.compare(<original-key>, key) == 0.
-	 */
-	public void putPayload(Key key, Bytes payload);
-
-	/**
-	 * Puts a key only, without any value or payload.
-	 */
-	public void putTerminal(Key key);
+public interface KeyDataStoreMutator<Key> extends KeyDataStore<Key>, KeyValueStoreMutator<Key, Integer> {
 
 }
