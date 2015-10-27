1da68565b849e452c84a4a8cb4aad7b117437795
ywsing <stupidsing@gmail.com>
2015-10-27 21:05:17 +0800

    put pre-compile all into test case

--- src/main/java/suite/fp/PrecompileMain.java
+++ src/main/java/suite/fp/PrecompileMain.java
@@ -1,12 +1,6 @@
 package suite.fp;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
 
 import suite.Suite;
 import suite.lp.Configuration.ProverConfig;
@@ -21,23 +15,6 @@ import suite.util.Util.ExecutableProgram;
  */
 public class PrecompileMain extends ExecutableProgram {
 
-	private List<String> allLibraries = Arrays.asList( //
-			"23-TREE" //
-			, "ARRAY" //
-			, "CHARS" //
-			, "DEBUG" //
-			, "FREQ" //
-			, "HEAP" //
-			, "MAPACCUM" //
-			, "MATCH" //
-			, "MATH" //
-			, "MONAD" //
-			, "PERMUTE" //
-			, "RB-TREE" //
-			, "SUITE" //
-			, "TEXT" //
-	);
-
 	public static void main(String args[]) throws IOException {
 		Util.run(PrecompileMain.class, args);
 	}
@@ -50,26 +27,6 @@ public class PrecompileMain extends ExecutableProgram {
 		return LogUtil.duration(getClass().getSimpleName(), () -> {
 			ProverConfig pc = new ProverConfig();
 			boolean ok = Suite.precompile("STANDARD", pc);
-			List<Future<Boolean>> futures = new ArrayList<>();
-
-			if (ok) {
-				ThreadPoolExecutor executor = Util.createExecutorByProcessors();
-
-				try {
-					for (String libraryName : allLibraries)
-						futures.add(executor.submit(() -> Boolean.valueOf(Suite.precompile(libraryName, pc))));
-				} finally {
-					executor.shutdown();
-				}
-
-				for (Future<Boolean> future : futures)
-					try {
-						ok &= future.get();
-					} catch (InterruptedException | ExecutionException ex) {
-						ex.printStackTrace();
-						ok = false;
-					}
-			}
 
 			if (ok)
 				System.out.println("Please refresh eclipse workspace");
--- src/test/java/suite/fp/PrecompileTest.java
+++ src/test/java/suite/fp/PrecompileTest.java
@@ -1,16 +1,42 @@
 package suite.fp;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import suite.Suite;
 import suite.lp.Configuration.ProverConfig;
 import suite.os.LogUtil;
 import suite.sample.Profiler;
+import suite.streamlet.Read;
 
 public class PrecompileTest {
 
+	private List<String> allLibraries = Arrays.asList( //
+			"23-TREE" //
+			, "ARRAY" //
+			, "CHARS" //
+			, "DEBUG" //
+			, "FREQ" //
+			, "HEAP" //
+			, "MAPACCUM" //
+			, "MATCH" //
+			, "MATH" //
+			, "MONAD" //
+			, "PERMUTE" //
+			, "RB-TREE" //
+			, "SUITE" //
+			, "TEXT" //
+	);
+
 	@Test
-	public void test() {
+	public void testPrecompileAll() {
+		Read.from(allLibraries).sink(lib -> Suite.precompile(lib, new ProverConfig()));
+	}
+
+	@Test
+	public void testThreeTimes() {
 		System.out.println(new Profiler().profile(() -> {
 			for (int i = 0; i < 3; i++)
 				LogUtil.duration("", () -> Suite.precompile("STANDARD", new ProverConfig()));
