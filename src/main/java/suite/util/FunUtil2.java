0e8ef742d462267eecb08c931314ccac6e185cce
ywsing <stupidsing@gmail.com>
2015-12-17 14:45:36 +0800

    add key-value source and functional processes

---
+++ src/main/java/suite/util/FunUtil2.java
@@ -0,0 +1,165 @@
+package suite.util;
+
+import java.util.Iterator;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+
+import suite.adt.Pair;
+import suite.os.LogUtil;
+import suite.util.FunUtil.Fun;
+import suite.util.FunUtil.Sink;
+import suite.util.FunUtil.Source;
+
+public class FunUtil2 {
+
+	@FunctionalInterface
+	public interface Source2<K, V> {
+		public boolean source(Pair<K, V> pair);
+	}
+
+	public static <K, V> Source2<K, V> concat(Source<Source2<K, V>> source) {
+		return new Source2<K, V>() {
+			private Source2<K, V> source0 = nullSource();
+
+			public boolean source(Pair<K, V> pair) {
+				boolean b = false;
+				while (source0 != null && !(b = source0.source(pair)))
+					source0 = source.source();
+				return b;
+			}
+		};
+	}
+
+	public static <K, V> Source2<K, V> cons(K key, V value, Source2<K, V> source) {
+		return new Source2<K, V>() {
+			private boolean isFirst = true;
+
+			public boolean source(Pair<K, V> pair) {
+				if (!isFirst)
+					return source.source(pair);
+				else {
+					pair.t0 = key;
+					pair.t1 = value;
+					return true;
+				}
+			}
+		};
+	}
+
+	public static <K, V> Source2<K, V> filter(BiPredicate<K, V> fun, Source2<K, V> source) {
+		return pair -> {
+			boolean b;
+			while ((b = source.source(pair)) && !fun.test(pair.t0, pair.t1))
+				;
+			return b;
+		};
+	}
+
+	public static <K, V, R> R fold(Fun<Pair<R, Pair<K, V>>, R> fun, R init, Source2<K, V> source) {
+		Pair<K, V> pair = Pair.<K, V> of(null, null);
+		while (source.source(pair))
+			init = fun.apply(Pair.of(init, pair));
+		return init;
+	}
+
+	public static <K, V> Iterator<Pair<K, V>> iterator(Source2<K, V> source) {
+		return new Iterator<Pair<K, V>>() {
+			private Pair<K, V> next = null;
+
+			public boolean hasNext() {
+				if (next == null) {
+					Pair<K, V> next1 = Pair.<K, V> of(null, null);
+					if (source.source(next1))
+						next = next1;
+				}
+				return next != null;
+			}
+
+			public Pair<K, V> next() {
+				Pair<K, V> next0 = next;
+				next = null;
+				return next0;
+			}
+		};
+	}
+
+	public static <K, V> Iterable<Pair<K, V>> iter(Source2<K, V> source) {
+		return () -> iterator(source);
+	}
+
+	public static <T0, T1> Source<T1> map(Fun<T0, T1> fun, Source<T0> source) {
+		return () -> {
+			T0 e = source.source();
+			return e != null ? fun.apply(e) : null;
+		};
+	}
+
+	public static <K, V> Source2<K, V> nullSource() {
+		return pair -> false;
+	}
+
+	public static <I> Sink<I> nullSink() {
+		return i -> {
+		};
+	}
+
+	/**
+	 * Problematic split: all data must be read, i.e. the children lists must
+	 * not be skipped.
+	 */
+	public static <K, V> Source<Source2<K, V>> split(Source2<K, V> source, BiFunction<K, V, Boolean> fun) {
+		return new Source<Source2<K, V>>() {
+			private Pair<K, V> pair = Pair.<K, V> of(null, null);
+			private boolean isAvailable;
+			private Source2<K, V> source_ = pair_ -> (isAvailable &= source.source(pair_)) && !fun.apply(pair.t0, pair.t1);
+
+			{
+				isAvailable = source.source(pair);
+			}
+
+			public Source2<K, V> source() {
+				return isAvailable ? FunUtil2.cons(pair.t0, pair.t1, source_) : null;
+			}
+		};
+	}
+
+	/**
+	 * Sucks data from a sink and produce into a source.
+	 */
+	public static <K, V> Source2<K, V> suck(Sink<Sink<Pair<K, V>>> fun) {
+		NullableSynchronousQueue<Pair<K, V>> queue = new NullableSynchronousQueue<>();
+		Sink<Pair<K, V>> enqueue = pair -> enqueue(queue, pair);
+
+		Thread thread = Util.startThread(() -> {
+			try {
+				fun.sink(enqueue);
+			} finally {
+				enqueue(queue, null);
+			}
+		});
+
+		return pair -> {
+			try {
+				Pair<K, V> p = queue.take();
+				if (p != null) {
+					pair.t0 = p.t0;
+					pair.t1 = p.t1;
+					return true;
+				} else
+					return false;
+			} catch (InterruptedException ex) {
+				thread.interrupt();
+				throw new RuntimeException(ex);
+			}
+		};
+	}
+
+	private static <T> void enqueue(NullableSynchronousQueue<T> queue, T t) {
+		try {
+			queue.offer(t);
+		} catch (InterruptedException ex) {
+			LogUtil.error(ex);
+		}
+	}
+
+}
