b6c89483f60aa9f3f1facc87e33ef4dfb5d15e20
ywsing <stupidsing@gmail.com>
2015-12-21 12:36:44 +0800

    add static factory methods

--- src/main/java/suite/search/DirectedGraph.java
+++ src/main/java/suite/search/DirectedGraph.java
@@ -5,6 +5,7 @@ import java.util.List;
 import suite.adt.ListMultimap;
 import suite.adt.Pair;
 import suite.streamlet.Read;
+import suite.streamlet.Streamlet;
 
 public class DirectedGraph<V> {
 
@@ -12,10 +13,23 @@ public class DirectedGraph<V> {
 	public final ListMultimap<V, V> forwards;
 	public final ListMultimap<V, V> backwards;
 
-	public DirectedGraph(List<V> vertices, List<Pair<V, V>> edges) {
+	public static <V> DirectedGraph<V> of(List<Pair<V, V>> edges) {
+		Streamlet<V> vertices0 = Read.from(edges).map(edge -> edge.t0);
+		Streamlet<V> vertices1 = Read.from(edges).map(edge -> edge.t1);
+		List<V> vertices = Streamlet.concat(vertices0, vertices1).distinct().toList();
+		return DirectedGraph.of(vertices, edges);
+	}
+
+	public static <V> DirectedGraph<V> of(List<V> vertices, List<Pair<V, V>> edges) {
+		ListMultimap<V, V> forwards = Read.from(edges).toMultimap(Pair::first_, Pair::second);
+		ListMultimap<V, V> backwards = Read.from(edges).toMultimap(Pair::second, Pair::first_);
+		return new DirectedGraph<>(vertices, forwards, backwards);
+	}
+
+	private DirectedGraph(List<V> vertices, ListMultimap<V, V> forwards, ListMultimap<V, V> backwards) {
 		this.vertices = vertices;
-		forwards = Read.from(edges).toMultimap(Pair::first_, Pair::second);
-		backwards = Read.from(edges).toMultimap(Pair::second, Pair::first_);
+		this.forwards = forwards;
+		this.backwards = backwards;
 	}
 
 }
