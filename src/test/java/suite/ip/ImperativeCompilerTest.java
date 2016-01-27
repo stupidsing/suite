39c9350e411ee7ba2a58114fdf5bb5889860cfda
ywsing <stupidsing@gmail.com>
2016-01-27 15:17:20 +0800

    refactor array indexing operator

--- src/test/java/suite/ip/ImperativeCompilerTest.java
+++ src/test/java/suite/ip/ImperativeCompilerTest.java
@@ -12,7 +12,7 @@ public class ImperativeCompilerTest {
 
 	@Test
 	public void testArray() {
-		Bytes bytes = imperativeCompiler.compile(0, "declare array as (int * 2) = array (1, 2,); let array:3 = array:4;");
+		Bytes bytes = imperativeCompiler.compile(0, "declare array as (int * 2) = array (1, 2,); let array/:3 = array/:4;");
 		assertNotNull(bytes);
 		System.out.println(bytes);
 	}
