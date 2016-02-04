1d9a351533f875bf701580af7189002ee84c7256
ywsing <stupidsing@gmail.com>
2016-02-05 02:37:30 +0800

    add a queue pretending lazy queue

---
+++ src/main/java/suite/adt/LockFreeLazyQueue.java
@@ -0,0 +1,83 @@
+package suite.adt;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import suite.concurrent.CasReference;
+
+public class LockFreeLazyQueue<T> {
+
+	private BackFront empty = new BackFront(0, null, null, null);
+	private CasReference<BackFront> cas = new CasReference<>(empty);
+
+	private class BackFront {
+		private int size;
+		private BackFront back;
+		private BackFront front;
+		private T t;
+
+		private BackFront(int size, BackFront back, BackFront front, T t) {
+			this.size = size;
+			this.back = back;
+			this.front = front;
+			this.t = t;
+		}
+	}
+
+	public LockFreeLazyQueue() {
+		empty.back = empty;
+		empty.front = empty;
+	}
+
+	public void enqueue(T t) {
+		cas.apply(backFront -> enqueue_(t, backFront));
+	}
+
+	/**
+	 * @return null if the queue is empty.
+	 */
+	public T dequeue() {
+		List<T> list = new ArrayList<>(Arrays.asList((T) null));
+		cas.apply(backFront -> {
+			Pair<T, BackFront> pair = dequeue_(backFront);
+			list.set(0, pair.t0);
+			return pair.t1;
+		});
+		return list.get(0);
+	}
+
+	private BackFront enqueue_(T t, BackFront bf0) {
+		BackFront bf1;
+		if (bf0.size != 0)
+			bf1 = make(enqueue_(t, bf0.back), bf0.front, bf0.t);
+		else
+			bf1 = new BackFront(1, empty, empty, t);
+		return bf1;
+	}
+
+	private Pair<T, BackFront> dequeue_(BackFront bf0) {
+		T t;
+		BackFront bf1;
+		if (bf0.size > 1) {
+			Pair<T, BackFront> pair = dequeue_(bf0.front);
+			t = bf0.t;
+			bf1 = make(bf0.back, pair.t1, pair.t0);
+		} else {
+			t = bf0.t;
+			bf1 = empty;
+		}
+		return Pair.of(t, bf1);
+	}
+
+	private BackFront make(BackFront back, BackFront front, T t) {
+		int size = back.size + front.size + 1;
+		if (back.size <= front.size)
+			return new BackFront(size, back, front, t);
+		else {
+			Pair<T, BackFront> pair = dequeue_(back);
+			return new BackFront(size, pair.t1, enqueue_(pair.t0, front), t);
+		}
+	}
+
+}
---
+++ src/test/java/suite/adt/LockFreeLazyQueueTest.java
@@ -0,0 +1,20 @@
+package suite.adt;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LockFreeLazyQueueTest {
+
+	@Test
+	public void test() {
+		LockFreeLazyQueue<Integer> lfq = new LockFreeLazyQueue<>();
+		for (int i = 0; i < 256; i++) {
+			for (int j = 0; j < 256; j++)
+				lfq.enqueue(j);
+			for (int j = 0; j < 256; j++)
+				assertEquals(j, (int) lfq.dequeue());
+		}
+	}
+
+}
