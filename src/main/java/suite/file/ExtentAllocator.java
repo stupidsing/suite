e4e04978b147a4784192e03cf9dbfa24413dd44f
ywsing <stupidsing@gmail.com>
2016-02-24 07:40:19 +0800

    add equals/hashCode

--- src/main/java/suite/file/ExtentAllocator.java
+++ src/main/java/suite/file/ExtentAllocator.java
@@ -1,6 +1,9 @@
 package suite.file;
 
 import java.io.Closeable;
+import java.util.Objects;
+
+import suite.util.Util;
 
 public interface ExtentAllocator extends Closeable {
 
@@ -11,6 +14,18 @@ public interface ExtentAllocator extends Closeable {
 		public Extent(int start, int end) {
 			this.start = start;
 			this.end = end;
+		}
+
+		public boolean equals(Object object) {
+			if (Util.clazz(object) == Extent.class) {
+				Extent other = (Extent) object;
+				return Objects.equals(start, other.start) && Objects.equals(end, other.end);
+			} else
+				return false;
+		}
+
+		public int hashCode() {
+			return Objects.hashCode(start) ^ Objects.hashCode(end);
 		}
 	}
 
