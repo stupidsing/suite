c6c411422325227a3c446d16a05f9ee335374634
ywsing <stupidsing@gmail.com>
2016-03-03 06:04:04 +0800

    more data to test case

--- src/test/java/suite/file/DatabaseTest.java
+++ src/test/java/suite/file/DatabaseTest.java
@@ -15,7 +15,8 @@ public class DatabaseTest {
 	public void testRollback() throws IOException {
 		try (Database database = new Database(FileUtil.tmp + "/database")) {
 			database.transact(tx -> {
-				tx.put(0, "sample");
+				for (int i = 0; i < 65536; i++)
+					tx.put(i, "sample");
 				throw new RuntimeException();
 			});
 		} catch (RuntimeException ex) {
@@ -26,18 +27,21 @@ public class DatabaseTest {
 	public void testUpdate() throws IOException {
 		try (Database database = new Database(FileUtil.tmp + "/database")) {
 			database.transact(tx -> {
-				tx.put(0, "sample");
+				for (int i = 0; i < 65536; i++)
+					tx.put(i, "sample");
 				return true;
 			});
 
 			database.transact(tx -> {
-				tx.put(0, "updated-" + tx.get(0));
+				for (int i = 0; i < 65536; i += 2)
+					tx.put(i, "updated-" + tx.get(i));
 				return true;
 			});
 
-			String value = database.transact(tx -> tx.get(0));
-
-			assertEquals("updated-sample", value);
+			assertEquals("updated-sample", database.transact(tx -> tx.get(0)));
+			assertEquals("sample", database.transact(tx -> tx.get(1)));
+			assertEquals("updated-sample", database.transact(tx -> tx.get(2)));
 		}
 	}
+
 }
