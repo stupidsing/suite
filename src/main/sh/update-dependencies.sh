6aeabbc0cb63a013ffbc2ab6e43c28d5b481ab6c
ywsing <stupidsing@gmail.com>
2016-03-18 22:41:01 +0800

    add telegram API

--- src/main/sh/update-dependencies.sh
+++ src/main/sh/update-dependencies.sh
@@ -1,3 +1,17 @@
+REPOS=$(find src/ -name \*.java |
+xargs grep -h ^import |
+cut -d' ' -f2 |
+tr -d \; |
+sort |
+uniq |
+sed 's#org.telegram.telegrambots.bots.*#<id>jitpack.io</id><url>https://jitpack.io</url>#g' |
+sort |
+uniq |
+grep '<url>' |
+while read REPO; do
+  echo -n "<repository>${REPO}</repository>"
+done) &&
+
 DEPS=$(find src/ -name \*.java |
 xargs grep -h ^import |
 cut -d' ' -f2 |
@@ -14,9 +28,10 @@ sed 's#org.apache.commons.logging.*#<groupId>commons-logging</groupId><artifactI
 sed 's#org.apache.log4j.*#<groupId>log4j</groupId><artifactId>log4j</artifactId>#g' |
 sed 's#org.junit.*#<groupId>junit</groupId><artifactId>junit</artifactId>#g' |
 sed 's#org.slf4j.*#<groupId>org.slf4j</groupId><artifactId>slf4j-api</artifactId>#g' |
+sed 's#org.telegram.telegrambots.bots.*#<groupId>com.github.rubenlagus</groupId><artifactId>TelegramBots</artifactId>#g' |
 sort |
 uniq |
-grep artifactId |
+grep '<artifactId>' |
 while read DEP; do
   echo -n "<dependency>${DEP}<version>LATEST</version></dependency>"
 done) &&
@@ -24,6 +39,9 @@ done) &&
 mv pom.xml pom0.xml &&
 cat pom0.xml |
 tr '\n' '@' |
+sed "s#<repositories>.*</repositories>#<repositories>${REPOS}</repositories>#g" |
 sed "s#<dependencies>.*</dependencies>#<dependencies>${DEPS}</dependencies>#g" |
 tr '@' '\n' |
 xmllint --format - > pom.xml
+
+mvn eclipse:clean eclipse:eclipse dependency:resolve
