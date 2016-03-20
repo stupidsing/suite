2d944ccc9946e13f87cd720b286aa651674b906e
ywsing <stupidsing@gmail.com>
2016-03-20 17:02:29 +0800

    fix

--- src/test/java/suite/telegram/TelegramTest.java
+++ src/test/java/suite/telegram/TelegramTest.java
@@ -7,9 +7,13 @@ import java.nio.file.Paths;
 import org.junit.Test;
 import org.telegram.telegrambots.TelegramApiException;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.SendMessage;
+import org.telegram.telegrambots.api.objects.Chat;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
+import suite.os.FileUtil;
 import suite.util.Rethrow;
 
 public class TelegramTest {
@@ -19,17 +23,31 @@ public class TelegramTest {
 		try {
 			new TelegramBotsApi().registerBot(new TelegramLongPollingBot() {
 				public String getBotUsername() {
-					return "kowloonbot";
+					return "Kowloonbot";
 				}
 
 				public String getBotToken() {
-					Path path = Paths.get("/Users/ywsing/kowloonbot.token");
+					Path path = Paths.get(FileUtil.tmp + "/kowloonbot.token");
 					return Rethrow.ioException(() -> Files.readAllLines(path)).iterator().next();
 				}
 
 				public void onUpdateReceived(Update update) {
-					if (update.hasMessage())
-						System.out.println(update.getMessage().getText());
+					if (update.hasMessage()) {
+						Message message = update.getMessage();
+						Chat chat = message.getChat();
+
+						SendMessage sendMessage = new SendMessage();
+						sendMessage.setChatId(chat.getId().toString());
+						sendMessage.setText(message.getText());
+
+						try {
+							sendMessage(sendMessage);
+						} catch (TelegramApiException ex) {
+							throw new RuntimeException(ex);
+						}
+
+						System.out.println(message.getText());
+					}
 				}
 			});
 		} catch (TelegramApiException ex) {
