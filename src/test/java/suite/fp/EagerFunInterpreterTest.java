4cbfdc285105bea35279ffed85a0c8d6c367c7fd
ywsing <stupidsing@gmail.com>
2016-03-20 21:14:42 +0800

    test both eager and lazy

--- src/test/java/suite/fp/EagerFunInterpreterTest.java
+++ src/test/java/suite/fp/EagerFunInterpreterTest.java
@@ -7,34 +7,39 @@ import org.junit.Test;
 import suite.Suite;
 import suite.node.Atom;
 import suite.node.Int;
+import suite.node.Node;
 
 public class EagerFunInterpreterTest {
 
 	@Test
 	public void testDecons() {
-		String expr = "let l := (1; 3;) >> if (l = `$a; $b;`) then b else error";
-		assertEquals(Int.of(3), new EagerFunInterpreter().eager(Suite.parse(expr)));
+		expect("let l := (1; 3;) >> if (l = `$a; $b;`) then b else error", Int.of(3));
 	}
 
 	@Test
 	public void testFibonacci() {
-		String expr = "define fib := (n => if (1 < n) then (fib {n - 1} + fib {n - 2}) else n) >> fib {12}";
-		assertEquals(Int.of(144), new EagerFunInterpreter().eager(Suite.parse(expr)));
+		expect("define fib := (n => if (1 < n) then (fib {n - 1} + fib {n - 2}) else n) >> fib {12}", Int.of(144));
 	}
 
 	@Test
 	public void testLazy() {
-		EagerFunInterpreter interpreter = new EagerFunInterpreter();
-		interpreter.setLazyify(true);
-
-		String expr = "define fib := (n => if (1 < n) then (fib {n - 1} + fib {n - 2}) else n) >> fib {12}";
-		assertEquals(Int.of(144), interpreter.eager(Suite.parse(expr)));
+		expect("define fib := (n => if (1 < n) then (fib {n - 1} + fib {n - 2}) else n) >> fib {12}", Int.of(144));
 	}
 
 	@Test
 	public void testUsing() {
-		String expr = "using source STANDARD >> and {true} {true}";
-		assertEquals(Atom.TRUE, new EagerFunInterpreter().eager(Suite.parse(expr)));
+		expect("using source STANDARD >> and {true} {true}", Atom.TRUE);
+	}
+
+	private void expect(String expr, Node expect) {
+		expect(true, expr, expect);
+		expect(false, expr, expect);
+	}
+
+	private void expect(boolean isLazyify, String expr, Node expect) {
+		EagerFunInterpreter interpreter = new EagerFunInterpreter();
+		interpreter.setLazyify(isLazyify);
+		assertEquals(expect, new EagerFunInterpreter().eager(Suite.parse(expr)));
 	}
 
 }
