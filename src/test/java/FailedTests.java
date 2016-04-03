92bc7d9a3f3c782363cec556307984f802a053d6
stupidsing <stupidsing@gmail.com>
2016-04-03 10:30:38 +0800

    comment

--- src/test/java/FailedTests.java
+++ src/test/java/FailedTests.java
@@ -16,8 +16,8 @@ import suite.primitive.Bytes;
 public class FailedTests {
 
 	// Cannot understand the error message
-	// "Cannot assign from (METHOD THIS METHOD0 NUMBER 0) to MEMORY 8 TREE +
-	// (NUMBER .143093) THIS"
+s	// "Cannot resolve type of (IF (TYPE-CAST I32 VAR f) (NUMBER 1) BOOLEAN 0)
+	// to .144591"
 	@Test
 	public void testAssignWrongSize() {
 		new ImperativeCompiler().compile(0, "declare (function [] int) f = function [] 0; (f as int && 1);");
