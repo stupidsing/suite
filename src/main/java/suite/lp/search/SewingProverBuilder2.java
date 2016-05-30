1583ad4a7670818f526a2cb93d7a6bedbaf4ffc7
ywsing <stupidsing@gmail.com>
2016-05-30 09:32:45 +0800

    do not rewrite queries when trace is on

--- src/main/java/suite/lp/search/SewingProverBuilder2.java
+++ src/main/java/suite/lp/search/SewingProverBuilder2.java
@@ -24,13 +24,22 @@ public class SewingProverBuilder2 implements Builder {
 
 	@Override
 	public Fun<Node, Finder> build(RuleSet ruleSet) {
-		QueryRewriter qr = new QueryRewriter(Prototype.multimap(ruleSet));
+		boolean isRewrite = !proverConfig.isTrace();
+		QueryRewriter qr = isRewrite ? new QueryRewriter(Prototype.multimap(ruleSet)) : null;
+		RuleSet ruleSet1;
 
-		RuleSet ruleSet1 = Suite.createRuleSet();
-		qr.rules().entries().forEach(p -> ruleSet1.addRule(p.t1));
+		if (qr != null) {
+			ruleSet1 = Suite.createRuleSet();
+			qr.rules().entries().forEach(p -> ruleSet1.addRule(p.t1));
+		} else
+			ruleSet1 = ruleSet;
 
 		Fun<Node, Finder> fun = new SewingProverBuilder(proverConfig).build(ruleSet1);
-		return goal -> fun.apply(qr.rewriteClause(goal));
+
+		if (qr != null)
+			return goal -> fun.apply(qr.rewriteClause(goal));
+		else
+			return fun;
 	}
 
 }
