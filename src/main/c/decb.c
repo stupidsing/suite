6eed72dae9de407de07ab615486985212a5d61ac
ywsing <stupidsing@gmail.com>
2016-08-30 11:26:12 +0800

    add binary encoder/decoder

---
+++ src/main/c/decb.c
@@ -0,0 +1,28 @@
+// gcc -std=c99 -g src/main/c/decb.c -o target/decb
+#include <stdio.h>
+#include <stdlib.h>
+
+void fail(char *msg) {
+	fprintf(stderr, msg);
+	exit(1);
+}
+
+int v(char c) {
+	return c > 'A' ? c - 'A' + 10 : c - '0';
+}
+
+int main(int argc, char **argv) {
+	char input[4096];
+	char output[1024];
+	while(!feof(stdin)) {
+		int count = fread(input, 3, 1024, stdin);
+		if(count >= 0) {
+			int size = 3 * count;
+			int o = 0;
+			for(int i = 0; i < size; i += 3)
+				output[o++] = (v(input[i]) << 4) + v(input[i + 1]);
+			if(fwrite(output, 1, o, stdout) != o) fail("output error");
+		}
+		else fail("input error");
+	}
+}
---
+++ src/main/c/encb.c
@@ -0,0 +1,28 @@
+// gcc -std=c99 -g src/main/c/encb.c -o target/encb
+#include <stdio.h>
+#include <stdlib.h>
+
+void fail(char *msg) {
+	fprintf(stderr, msg);
+	exit(1);
+}
+
+int main(int argc, char **argv) {
+	char input[1024];
+	char output[4096];
+	while(!feof(stdin)) {
+		int size = fread(input, 1, 1024, stdin);
+		if(size >= 0) {
+			int o = 0;
+			for(int i = 0; i < size; i++) {
+				char *hex = "0123456789ABCDEF";
+				char b = input[i];
+				output[o++] = hex[b >> 4];
+				output[o++] = hex[b & 15];
+				output[o++] = '_';
+			}
+			if(fwrite(output, 1, o, stdout) != o) fail("output error");
+		}
+		else fail("input error");
+	}
+}
