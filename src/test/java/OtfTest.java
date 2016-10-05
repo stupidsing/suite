0a6e83220cbc2fcbae46ef29e52fc30c90c249e3
stupidsing <stupidsing@gmail.com>
2016-10-05 21:59:48 +0800

    add program to refactor font directories

---
+++ src/test/java/OtfTest.java
@@ -0,0 +1,51 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import suite.os.Execute;
+import suite.os.FileUtil;
+import suite.streamlet.Read;
+import suite.util.Util;
+
+public class OtfTest {
+
+	@Test
+	public void otfTest() {
+		String familyKey = "Family";
+		String subfamilyKey = "Subfamily";
+		List<String> keys = Arrays.asList(familyKey, subfamilyKey);
+
+		List<String> commands = FileUtil //
+				.findPaths(Paths.get("/home/ywsing/.fonts")) //
+				.map2(Path::toString, path -> {
+					Execute exec = new Execute(new String[] { "otfinfo", "-i", path.toString(), }, "");
+					return Read.from(exec.out.split("\n")) //
+							.map(line -> line.split(":")) //
+							.filter(arr -> 2 <= arr.length) //
+							.map2(arr -> arr[0].trim(), arr -> arr[1].trim()) //
+							.filter((k, v) -> keys.contains(k)) //
+							.toMap();
+				}) //
+				.filter((k, m) -> false //
+						|| k.toLowerCase().endsWith(".otf") //
+						|| k.toLowerCase().endsWith(".ttf") //
+				) //
+				.map((k, m) -> {
+					String f = m.get(familyKey);
+					String sf = m.get(subfamilyKey);
+					String dir = "/home/ywsing/.fonts/" + f + "/";
+					String ext = k.substring(k.lastIndexOf(".") + 1).toLowerCase();
+
+					return "mkdir -p '" + dir + "'; mv '" + k + "' '" + dir + f + " " + sf + "." + ext + "'";
+				}) //
+				.sort(Util::compare) //
+				.toList();
+
+		for (String command : commands)
+			System.out.println(command);
+	}
+
+}
