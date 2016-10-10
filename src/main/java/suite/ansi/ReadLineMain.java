d04aab7be82bd092dfe6738cf1bffe532b225cb2
stupidsing <stupidsing@gmail.com>
2016-10-10 10:55:39 +0800

    write as loop

--- src/main/java/suite/ansi/ReadLineMain.java
+++ src/main/java/suite/ansi/ReadLineMain.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import com.sun.jna.Native;
 
 import suite.adt.Pair;
 import suite.adt.Trie;
+import suite.streamlet.Outlet;
 import suite.streamlet.Reactive;
 import suite.streamlet.Reactive.Redirector;
 import suite.streamlet.Read;
@@ -51,37 +53,44 @@ public class ReadLineMain extends ExecutableProgram {
 				return 0 <= ch ? (char) ch : null;
 			};
 
-			Reactive.from(source0).redirect(new Redirector<Character, Pair<VK, Character>>() {
-				private List<Character> chs = new ArrayList<>();
-				private Trie<Integer, VK> t = trie;
+			Outlet<Pair<VK, Character>> keys = Reactive.from(source0) //
+					.redirect(new Redirector<Character, Pair<VK, Character>>() {
+						private List<Character> chs = new ArrayList<>();
+						private Trie<Integer, VK> t = trie;
 
-				public void accept(Character ch_, Reactive<Pair<VK, Character>> reactive) {
-					if (ch_ != null) {
-						chs.add(ch_);
-						VK vk;
+						public void accept(Character ch_, Reactive<Pair<VK, Character>> reactive) {
+							if (ch_ != null) {
+								chs.add(ch_);
+								VK vk;
 
-						if ((t = t.getMap().get(ch_)) != null)
-							if ((vk = t.getValue()) != null) {
-								reactive.fire(Pair.of(vk, null));
-								reset();
+								if ((t = t.getMap().get(ch_)) != null)
+									if ((vk = t.getValue()) != null) {
+										reactive.fire(Pair.of(vk, null));
+										reset();
+									} else
+										;
+								else
+									flush(reactive);
 							} else
-								;
-						else
-							flush(reactive);
-					} else
-						flush(reactive);
-				}
+								flush(reactive);
+						}
 
-				private void flush(Reactive<Pair<VK, Character>> reactive) {
-					Read.from(chs).sink(ch -> reactive.fire(Pair.of(null, ch)));
-					reset();
-				}
+						private void flush(Reactive<Pair<VK, Character>> reactive) {
+							Read.from(chs).sink(ch -> reactive.fire(Pair.of(null, ch)));
+							reset();
+						}
 
-				private void reset() {
-					chs.clear();
-					t = trie;
-				}
-			}).outlet().source().source();
+						private void reset() {
+							chs.clear();
+							t = trie;
+						}
+					}) //
+					.outlet();
+
+			Pair<VK, Character> pair;
+
+			while (!Objects.equals(pair = keys.next(), Pair.of(null, 'q')))
+				System.out.println(pair);
 		}
 
 		return true;
