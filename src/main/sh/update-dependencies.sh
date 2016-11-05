c71272eda8555b96efe1d3301bb538cba19ce68c
Sing Yiu Wah <stupidsing@gmail.com>
2016-11-05 20:42:06 +0800

    add objectweb asm

--- src/main/sh/update-dependencies.sh
+++ src/main/sh/update-dependencies.sh
@@ -1,10 +1,9 @@
 REPOS=$(find src/ -name \*.java |
 xargs grep -h ^import |
-cut -d' ' -f2 |
 tr -d \; |
 sort |
 uniq |
-#sed 's#org.telegram.telegrambots.bots.*#<id>jitpack.io</id><url>https://jitpack.io</url>#g' |
+#sed 's#import org.telegram.telegrambots.bots.*#<id>jitpack.io</id><url>https://jitpack.io</url>#g' |
 sort |
 uniq |
 grep '<url>' |
@@ -14,22 +13,22 @@ done) &&
 
 DEPS=$(find src/ -name \*.java |
 xargs grep -h ^import |
-cut -d' ' -f2 |
 tr -d \; |
 sort |
 uniq |
-sed 's#ch.qos.logback.*#<groupId>ch.qos.logback</groupId><artifactId>logback-classic</artifactId>#g' |
-sed 's#com.fasterxml.jackson.databind.*#<groupId>com.fasterxml.jackson.core</groupId><artifactId>jackson-databind</artifactId>#g' |
-sed 's#com.jcraft.jsch.*#<groupId>com.jcraft</groupId><artifactId>jsch</artifactId>#g' |
-sed 's#com.nativelibs4java.bridj.*#<groupId>com.nativelibs4java</groupId><artifactId>bridj</artifactId>#g' |
-sed 's#com.nativelibs4java.opencl.*#<groupId>com.nativelibs4java</groupId><artifactId>javacl</artifactId>#g' |
-sed 's#com.sun.jna.*#<groupId>net.java.dev.jna</groupId><artifactId>jna</artifactId>#g' |
-sed 's#jline.console.*#<groupId>jline</groupId><artifactId>jline</artifactId>#g' |
-sed 's#org.apache.commons.logging.*#<groupId>commons-logging</groupId><artifactId>commons-logging</artifactId>#g' |
-sed 's#org.apache.log4j.*#<groupId>log4j</groupId><artifactId>log4j</artifactId>#g' |
-sed 's#org.junit.*#<groupId>junit</groupId><artifactId>junit</artifactId>#g' |
-sed 's#org.slf4j.*#<groupId>org.slf4j</groupId><artifactId>slf4j-api</artifactId>#g' |
-sed 's#org.telegram.telegrambots.bots.*#<groupId>org.telegram</groupId><artifactId>telegrambots</artifactId>#g' |
+sed 's#import ch.qos.logback.*#<groupId>ch.qos.logback</groupId><artifactId>logback-classic</artifactId>#g' |
+sed 's#import com.fasterxml.jackson.databind.*#<groupId>com.fasterxml.jackson.core</groupId><artifactId>jackson-databind</artifactId>#g' |
+sed 's#import com.jcraft.jsch.*#<groupId>com.jcraft</groupId><artifactId>jsch</artifactId>#g' |
+sed 's#import com.nativelibs4java.bridj.*#<groupId>com.nativelibs4java</groupId><artifactId>bridj</artifactId>#g' |
+sed 's#import com.nativelibs4java.opencl.*#<groupId>com.nativelibs4java</groupId><artifactId>javacl</artifactId>#g' |
+sed 's#import com.sun.jna.*#<groupId>net.java.dev.jna</groupId><artifactId>jna</artifactId>#g' |
+sed 's#import jline.console.*#<groupId>jline</groupId><artifactId>jline</artifactId>#g' |
+sed 's#import org.apache.commons.logging.*#<groupId>commons-logging</groupId><artifactId>commons-logging</artifactId>#g' |
+sed 's#import org.apache.log4j.*#<groupId>log4j</groupId><artifactId>log4j</artifactId>#g' |
+sed 's#import org.junit.*#<groupId>junit</groupId><artifactId>junit</artifactId>#g' |
+sed 's#import org.objectweb.asm.*#<groupId>org.ow2.asm</groupId><artifactId>asm</artifactId>#g' |
+sed 's#import org.slf4j.*#<groupId>org.slf4j</groupId><artifactId>slf4j-api</artifactId>#g' |
+sed 's#import org.telegram.telegrambots.bots.*#<groupId>org.telegram</groupId><artifactId>telegrambots</artifactId>#g' |
 sort |
 uniq |
 grep '<artifactId>' |
