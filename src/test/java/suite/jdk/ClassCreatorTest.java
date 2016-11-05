2d0bc02c32c188efe3e7b616d6bbb8bd1e7efd99
Sing Yiu Wah <stupidsing@gmail.com>
2016-11-05 20:16:44 +0800

    refactor

--- src/test/java/suite/jdk/ClassCreatorTest.java
+++ src/test/java/suite/jdk/ClassCreatorTest.java
@@ -4,53 +4,51 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.Callable;
 
-import org.bridj.relocated.org.objectweb.asm.ClassWriter;
-import org.bridj.relocated.org.objectweb.asm.MethodVisitor;
-import org.bridj.relocated.org.objectweb.asm.Opcodes;
 import org.junit.Test;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
-public class ClassCreatorTest {
-
-	private class Creator implements Opcodes {
-		private Object create() throws Exception {
-			ClassWriter cw = new ClassWriter(0);
-
-			cw.visit(49, ACC_PUBLIC + ACC_SUPER, "Hello", null, "java/lang/Object",
-					new String[] { "java/util/concurrent/Callable" });
-
-			{
-				MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
-				mv.visitVarInsn(ALOAD, 0);
-				mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
-				mv.visitInsn(RETURN);
-				mv.visitMaxs(1, 1);
-				mv.visitEnd();
-			}
-
-			{
-				MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "call", "()Ljava/lang/Object;", null, null);
-				mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-				mv.visitLdcInsn("hello world");
-				mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V");
-				mv.visitLdcInsn("");
-				mv.visitInsn(ARETURN);
-				mv.visitMaxs(2, 1);
-				mv.visitEnd();
-			}
-
-			cw.visitEnd();
-
-			byte bytes[] = cw.toByteArray();
-
-			UnsafeUtil unsafeUtil = new UnsafeUtil();
-			Callable<?> callable = unsafeUtil.defineClass(Callable.class, "Hello", bytes).newInstance();
-			return callable.call();
-		}
-	}
+public class ClassCreatorTest implements Opcodes {
 
 	@Test
 	public void testCreateClass() throws Exception {
-		assertEquals("", new Creator().create());
+		assertEquals("", create());
+	}
+
+	private Object create() throws Exception {
+		ClassWriter cw = new ClassWriter(0);
+
+		cw.visit(49, ACC_PUBLIC + ACC_SUPER, "Hello", null, "java/lang/Object",
+				new String[] { "java/util/concurrent/Callable", });
+
+		{
+			MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+			mv.visitVarInsn(ALOAD, 0);
+			mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+			mv.visitInsn(RETURN);
+			mv.visitMaxs(1, 1);
+			mv.visitEnd();
+		}
+
+		{
+			MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "call", "()Ljava/lang/Object;", null, null);
+			mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+			mv.visitLdcInsn("hello world");
+			mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+			mv.visitLdcInsn("");
+			mv.visitInsn(ARETURN);
+			mv.visitMaxs(2, 1);
+			mv.visitEnd();
+		}
+
+		cw.visitEnd();
+
+		byte bytes[] = cw.toByteArray();
+
+		UnsafeUtil unsafeUtil = new UnsafeUtil();
+		Callable<?> callable = unsafeUtil.defineClass(Callable.class, "Hello", bytes).newInstance();
+		return callable.call();
 	}
 
 }
