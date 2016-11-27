9ced967f998ae965f7a20a73be4c3cd847563973
stupidsing <stupidsing@gmail.com>
2016-11-27 10:16:44 +0800

    fix

--- src/main/python/proxy.py
+++ src/main/python/proxy.py
@@ -3,38 +3,38 @@
 # Sample TCP proxy server in python
 
 from select import *
-	from signal import *
-	from socket import *
-		from threading import *
-	from time import *
-	
-	class Worker(Thread):
-	def __init__(self, sock):
-		Thread.__init__(self)
-		self.fro = sock
+from signal import *
+from socket import *
+from threading import *
+from time import *
 
-	def run(self):
-		to = socket(AF_INET, SOCK_STREAM)
-		to.connect(("www.google.com", 80))
-		try:
-			while True:
-				selected = select([self.fro, to], [], [self.fro, to])[0][0]
-				data = selected.recv(1024)
-				if data:
-					if selected == self.fro: to.sendall(data)
-					elif selected == to: self.fro.sendall(data)
-				else: break
-		finally:
-			self.fro.close()
-			to.close()
+class Worker(Thread):
+def __init__(self, sock):
+	Thread.__init__(self)
+	self.fro = sock
+
+def run(self):
+	to = socket(AF_INET, SOCK_STREAM)
+	to.connect(("www.google.com", 80))
+	try:
+		while True:
+			selected = select([self.fro, to], [], [self.fro, to])[0][0]
+			data = selected.recv(1024)
+			if data:
+				if selected == self.fro: to.sendall(data)
+				elif selected == to: self.fro.sendall(data)
+			else: break
+	finally:
+		self.fro.close()
+		to.close()
 
 
 signal(SIGPIPE, SIG_IGN)
 
-	listener = socket(AF_INET, SOCK_STREAM)
-	listener.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-	listener.bind(("", 1sdg161))
-	listener.listen(32)
+listener = socket(AF_INET, SOCK_STREAM)
+listener.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+listener.bind(("", 1161))
+listener.listen(32)
 
 while select([listener], [], [], None) [0]:
 	sock, address = listener.accept()
