1156103d72090ccc1ab104a6368a836259fae423
stupidsing <stupidsing@gmail.com>
2016-12-18 22:24:51 +0800

    refactor

--- src/main/java/suite/assembler/AssemblerAmd64.java
+++ src/main/java/suite/assembler/AssemblerAmd64.java
@@ -70,19 +70,19 @@ public class AssemblerAmd64 {
 		Bytes bytes;
 		if (instruction.op2 instanceof OpNone)
 			if (isRm(instruction.op0) && instruction.op1 instanceof OpReg)
-				bytes = assembleModRmReg(b_modrm, num, new ModRm(instruction.op0), (OpReg) instruction.op1);
+				bytes = assembleModRmReg(b_modrm, new ModRm(instruction.op0, num), (OpReg) instruction.op1);
 			else if (instruction.op0 instanceof OpReg && isRm(instruction.op1))
-				bytes = assembleModRmReg(b_modrm + 2, num, new ModRm(instruction.op1), (OpReg) instruction.op0);
+				bytes = assembleModRmReg(b_modrm + 2, new ModRm(instruction.op1, num), (OpReg) instruction.op0);
 			else if (instruction.op1 instanceof OpImm) {
 				OpImm op1 = (OpImm) instruction.op1;
 				BytesBuilder bb = new BytesBuilder();
 				if (isAcc(instruction.op0))
 					bb.append((byte) (b_imm + 4 + (1 < instruction.op0.size ? 1 : 0)));
 				else if (isRm(instruction.op0)) {
-					ModRm modRm = new ModRm(instruction.op0);
+					ModRm modRm = new ModRm(instruction.op0, num);
 					int b0 = op1.size == 1 ? 3 : (1 < instruction.op0.size ? 1 : 0);
 					bb.append((byte) (b_imm + b0));
-					bb.append((byte) ((modRm.mod << 6) + (num << 3) + modRm.rm));
+					bb.append(modRm.modNumRm());
 				} else
 					throw new RuntimeException("Bad instruction");
 				appendImm(bb, op1.imm, op1.size);
@@ -101,12 +101,17 @@ public class AssemblerAmd64 {
 		}
 	}
 
-	private Bytes assembleModRmReg(int b0, int num, ModRm modRm, OpReg op1) {
+	private Bytes assembleModRmReg(int b0, ModRm modRm, OpReg op1) {
 		BytesBuilder bb = new BytesBuilder();
+		int rex = modRm.rex();
+		int sib = modRm.sib();
+
+		if (0 <= modRm.rex())
+			bb.append((byte) rex);
 		bb.append((byte) (b0 + (1 < op1.size ? 1 : 0)));
-		bb.append((byte) ((modRm.mod << 6) + (num << 3) + modRm.rm));
-		if (0 <= modRm.sib)
-			bb.append((byte) modRm.sib);
+		bb.append(modRm.modNumRm());
+		if (0 <= sib)
+			bb.append((byte) sib);
 		appendImm(bb, modRm.disp, modRm.dispSize);
 		return bb.toBytes();
 	}
@@ -120,15 +125,16 @@ public class AssemblerAmd64 {
 	}
 
 	private class ModRm {
-		private int mod, rm, sib, dispSize;
+		private int mod, num, rm, s, i, b, dispSize;
 		private long disp;
 
-		public ModRm(Operand operand) {
+		private ModRm(Operand operand, int n) {
+			num = n;
 			if (operand instanceof OpReg) { // EAX
 				OpReg op = (OpReg) operand;
 				mod = 3;
 				rm = op.reg;
-				sib = -1;
+				s = i = b = -1;
 				dispSize = 0;
 			} else if (operand instanceof OpMem) {
 				OpMem op = (OpMem) operand;
@@ -142,7 +148,7 @@ public class AssemblerAmd64 {
 						int ds = op.dispSize == 0 && (op.baseReg & 7) == 5 ? 1 : op.dispSize;
 						mod = dispMod(ds);
 						rm = op.baseReg;
-						sib = -1;
+						s = i = b = -1;
 						dispSize = ds;
 					} else
 						throw new RuntimeException("Bad operand");
@@ -150,7 +156,9 @@ public class AssemblerAmd64 {
 					if ((op.indexReg & 7) != 4) { // [4 * EBX + 0x1234]
 						mod = 0;
 						rm = 4;
-						sib = sib(op, 5);
+						s = scale(op);
+						i = op.indexReg;
+						b = 5;
 						dispSize = 4;
 					} else
 						throw new RuntimeException("Bad operand");
@@ -159,7 +167,10 @@ public class AssemblerAmd64 {
 						// [4 * EBX + EAX + 0x1234]
 						mod = dispMod(op.dispSize);
 						rm = 4;
-						sib = sib(op, op.baseReg);
+						s = scale(op);
+						;
+						i = op.indexReg;
+						b = op.baseReg;
 						dispSize = op.dispSize;
 					} else
 						throw new RuntimeException("Bad operand");
@@ -170,14 +181,33 @@ public class AssemblerAmd64 {
 			}
 		}
 
+		private byte rex() {
+			int isOpSize64 = 0;
+			int b04 = (isOpSize64 << 3) //
+					+ (((num >> 3) & 1) << 2) //
+					+ (((i >> 3) & 1) << 1) //
+					+ (((b >> 3) & 1) << 0);
+			return b04 != 0 ? (byte) (0x40 + b04) : -1;
+		}
+
+		private byte modNumRm() {
+			return b(rm, num, mod);
+		}
+
 		private int dispMod(int dispSize) {
 			return dispSize == 0 ? 0 : dispSize == 1 ? 1 : 2;
 		}
 
-		private int sib(OpMem op, int b) {
-			return ((op.scale == 1 ? 0 : op.scale == 2 ? 1 : op.scale == 4 ? 2 : 3) << 6) //
-					+ (op.indexReg << 3) //
-					+ b;
+		private int sib() {
+			return 0 <= s ? b(b, i, s) : -1;
+		}
+
+		private int scale(OpMem op) {
+			return op.scale == 1 ? 0 : op.scale == 2 ? 1 : op.scale == 4 ? 2 : 3;
+		}
+
+		private byte b(int b03, int b36, int b68) {
+			return (byte) ((b03 & 7) + ((b36 & 7) << 3) + ((b68 & 3) << 6));
 		}
 	}
 
