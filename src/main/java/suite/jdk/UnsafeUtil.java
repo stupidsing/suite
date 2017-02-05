66acdcef9dd34f420cfc22fb257236cd6d0b30a9
stupidsing <stupidsing@gmail.com>
2017-02-05 10:01:52 +0800

    refactor

--- src/main/java/suite/jdk/UnsafeUtil.java
+++ src/main/java/suite/jdk/UnsafeUtil.java
@@ -8,12 +8,12 @@ import sun.misc.Unsafe;
 public class UnsafeUtil {
 
 	public Class<?> defineClass(byte bytes[]) {
-		Unsafe unsafe = new UnsafeUtil().getUnsafe();
+		Unsafe unsafe = getUnsafe();
 		return unsafe.defineAnonymousClass(getClass(), bytes, null);
 	}
 
 	public <T> Class<? extends T> defineClass(Class<T> interfaceClazz, String className, byte bytes[]) {
-		Unsafe unsafe = new UnsafeUtil().getUnsafe();
+		Unsafe unsafe = getUnsafe();
 		@SuppressWarnings("unchecked")
 		Class<? extends T> clazz = (Class<? extends T>) unsafe.defineClass( //
 				className, bytes, 0, bytes.length, getClass().getClassLoader(), null);
