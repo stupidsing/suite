21173c9fae910682ccb4027657ebc9255e4012fa
stupidsing <stupidsing@gmail.com>
2017-02-12 11:30:36 +0800

    remove obsolete utilities

--- src/main/java/suite/java7util/FormatUtil.java
+++ src/main/java/suite/java7util/FormatUtil.java
@@ -33,21 +33,4 @@ public class FormatUtil {
 		}
 	}
 
-	public static String leftTrim(String s) {
-		int pos = 0;
-		do
-			if (!Character.isWhitespace(s.charAt(pos)))
-				break;
-		while (++pos < s.length());
-		return s.substring(pos);
-	}
-
-	public static String rightTrim(String s) {
-		int pos = s.length();
-		while (0 <= --pos)
-			if (!Character.isWhitespace(s.charAt(pos)))
-				break;
-		return s.substring(0, pos + 1);
-	}
-
 }
