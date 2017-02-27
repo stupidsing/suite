de05b4a6fdb59b929518748d2fd2d29739937004
Sing Yiu Wah <stupidsing@gmail.com>
2017-02-27 20:08:48 +0800

    compile class only once

--- src/main/java/suite/jdk/gen/FunConfig.java
+++ src/main/java/suite/jdk/gen/FunConfig.java
@@ -4,10 +4,14 @@ import java.util.Map;
 
 import org.apache.bcel.generic.Type;
 
+import suite.util.FunUtil.Fun;
+
 public class FunConfig<I> {
 
 	public final LambdaImplementation<I> lambda;
 	public final Map<String, Type> fieldTypes;
+
+	private Fun<Map<String, Object>, I> fun;
 
 	public static <I> FunConfig<I> of(LambdaImplementation<I> lambda, Map<String, Type> fieldTypes) {
 		return new FunConfig<>(lambda, fieldTypes);
@@ -16,12 +20,12 @@ public class FunConfig<I> {
 	private FunConfig(LambdaImplementation<I> lambda, Map<String, Type> fieldTypes) {
 		this.lambda = lambda;
 		this.fieldTypes = fieldTypes;
+
+		fun = FunCreator.of(lambda.lambdaInterface, fieldTypes).create(lambda.expr);
 	}
 
 	public I newFun(Map<String, Object> fieldValues) {
-		return FunCreator.of(lambda.lambdaInterface, fieldTypes) //
-				.create(lambda.expr) //
-				.apply(fieldValues);
+		return fun.apply(fieldValues);
 	}
 
 }
