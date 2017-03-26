e7ea975f4e5d8d896201d7aadbc6bd7993db2111
stupidsing <stupidsing@gmail.com>
2017-03-27 07:16:44 +0800

    add test case for DHWT

--- src/main/java/suite/math/DiscreteHaarWaveletTransform.java
+++ src/main/java/suite/math/DiscreteHaarWaveletTransform.java
@@ -4,19 +4,42 @@ package suite.math;
 public class DiscreteHaarWaveletTransform {
 
 	// assumes input.length >= 2 and input.length = 2^n
-	public static int[] dhwt(int input[]) {
-		for (int length = input.length >> 1; length > 1; length >>= 1) {
-			int output[] = new int[input.length];
+	public int[] dhwt(int input[]) {
+		int length2 = input.length;
+		int output[] = new int[length2];
 
-			for (int i = 0; i < length; ++i) {
+		while (1 < length2) {
+			int length = length2 / 2;
+			for (int i = 0; i < length; i++) {
 				int i2 = i * 2;
 				int a = input[i2];
 				int b = input[i2 + 1];
 				output[i] = a + b;
-				output[length + i] = a - b;
+				output[i + length] = a - b;
 			}
 
-			input = output;
+			System.arraycopy(output, 0, input, 0, length2);
+			length2 = length;
+		}
+
+		return input;
+	}
+
+	public int[] idhwt(int input[]) {
+		int length = 1, length2;
+		int output[] = new int[input.length];
+
+		while ((length2 = length * 2) <= input.length) {
+			for (int i = 0; i < length; i++) {
+				int sum = input[i];
+				int diff = input[i + length];
+				int i2 = i * 2;
+				output[i2] = (sum + diff) / 2;
+				output[i2 + 1] = (sum - diff) / 2;
+			}
+
+			System.arraycopy(output, 0, input, 0, length2);
+			length = length2;
 		}
 
 		return input;
---
+++ src/test/java/suite/math/DiscreteHaarWaveletTransformTest.java
@@ -0,0 +1,39 @@
+package suite.math;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.junit.Test;
+
+import suite.util.Util;
+
+public class DiscreteHaarWaveletTransformTest {
+
+	@Test
+	public void testFft() {
+		int data[] = new int[16];
+
+		for (int i = 0; i < data.length; i++)
+			data[i] = i;
+
+		for (int i = 0; i < data.length; i++) {
+			int j = new Random().nextInt(data.length);
+			int temp = data[i];
+			data[i] = data[j];
+			data[j] = temp;
+		}
+
+		int expect[] = data.clone();
+
+		DiscreteHaarWaveletTransform dhwt = new DiscreteHaarWaveletTransform();
+		data = dhwt.dhwt(data);
+		data = dhwt.idhwt(data);
+
+		Util.dump("actual", data);
+		Util.dump("expect", expect);
+		assertTrue(Arrays.equals(data, expect));
+	}
+
+}
