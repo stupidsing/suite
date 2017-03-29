4d002482b2a873e8d2f742dc2d1f8e9b32c02edc
ywsing <stupidsing@gmail.com>
2017-03-29 14:50:23 +0800

    refactor

--- src/test/java/suite/trade/TradeTest.java
+++ src/test/java/suite/trade/TradeTest.java
@@ -11,7 +11,6 @@ import suite.os.StoreCache;
 import suite.primitive.Bytes;
 import suite.primitive.BytesUtil;
 import suite.streamlet.As;
-import suite.streamlet.Outlet;
 import suite.util.Rethrow;
 
 public class TradeTest {
@@ -26,17 +25,17 @@ public class TradeTest {
 
 	@Test
 	public void backTest() {
-		String url = "http://chart.finance.yahoo.com/table.csv" //
+		String urlString = "http://chart.finance.yahoo.com/table.csv" //
 				+ "?s=" + stockCode + "." + market //
 				+ "&a=" + frDate.getMonthValue() + "&b=" + frDate.getDayOfMonth() + "&c=" + frDate.getYear() //
 				+ "&d=" + toDate.getMonthValue() + "&e=" + toDate.getDayOfMonth() + "&f=" + toDate.getYear() //
 				+ "&g=d&ignore=.csv";
 
-		Bytes keyBytes = Bytes.of(url.getBytes(Constants.charset));
+		Bytes keyBytes = Bytes.of(urlString.getBytes(Constants.charset));
 		StoreCache sc = new StoreCache();
-		Outlet<Bytes> outlet = sc.getOutlet(keyBytes, () -> HttpUtil.http("GET", Rethrow.ex(() -> new URL(url))).out);
+		URL url = Rethrow.ex(() -> new URL(urlString));
 
-		double prices[] = outlet //
+		double prices[] = sc.getOutlet(keyBytes, () -> HttpUtil.http("GET", url).out) //
 				.collect(BytesUtil.split(Bytes.of((byte) 10))) //
 				.skip(1) //
 				.map(bytes -> new String(bytes.toBytes(), Constants.charset).split(",")) //
