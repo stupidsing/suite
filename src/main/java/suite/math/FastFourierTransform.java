88c2aacd5ad06468faa941275a3426b646599b5d
Sing Yiu Wah <stupidsing@gmail.com>
2017-03-29 22:59:18 +0800

    add float array version

--- src/main/java/suite/math/FastFourierTransform.java
+++ src/main/java/suite/math/FastFourierTransform.java
@@ -44,6 +44,57 @@ public class FastFourierTransform {
 		return fds;
 	}
 
+	public float[] fft(float tds[]) {
+		int size = tds.length / 2;
+		float fds[] = new float[size * 2];
+		int s = size;
+		int bits = 0;
+
+		while (1 < s) {
+			s >>= 1;
+			bits++;
+		}
+
+		for (int i = 0; i < size; i++) {
+			fds[reverseBits(bits, i) * 2 + 0] = tds[i * 2 + 0];
+			fds[reverseBits(bits, i) * 2 + 1] = tds[i * 2 + 1];
+		}
+
+		for (int g = 2; g <= size; g <<= 1) {
+			float cis[] = new float[g * 2];
+
+			for (int i = 0; i < g; i++) {
+				double angle = 2 * Math.PI * i / g;
+				cis[i * 2 + 0] = (float) Math.cos(angle);
+				cis[i * 2 + 1] = (float) -Math.sin(angle);
+			}
+
+			int step = g / 2;
+
+			for (int i = 0; i < size; i += g)
+				for (int k = 0; k < step; k++) {
+					int ie = (i + k) * 2;
+					int io = (i + k + step) * 2;
+
+					float ceReal = fds[ie + 0];
+					float ceImag = fds[ie + 1];
+					float coReal = fds[io + 0];
+					float coImag = fds[io + 1];
+					float cisReal = cis[k * 2 + 0];
+					float cisImag = cis[k * 2 + 1];
+					float expReal = cisReal * coReal - cisImag * coImag;
+					float expImag = cisReal * coImag + cisImag * coReal;
+
+					fds[ie + 0] = ceReal + expReal;
+					fds[ie + 1] = ceImag + expImag;
+					fds[io + 0] = ceReal - expReal;
+					fds[io + 1] = ceImag - expImag;
+				}
+		}
+
+		return fds;
+	}
+
 	private static int reverseBits(int bits, int n0) {
 		int n1 = 0;
 		while (0 < bits--) {
--- src/test/java/suite/math/FastFourierTransformTest.java
+++ src/test/java/suite/math/FastFourierTransformTest.java
@@ -15,22 +15,51 @@ public class FastFourierTransformTest {
 		for (Complex f : fs)
 			System.out.println(f);
 
-		assertTrue(Math.abs(fs[0].r - 4) < .1);
-		assertTrue(Math.abs(fs[0].i - 0) < .1);
-		assertTrue(Math.abs(fs[1].r - 1) < .1);
-		assertTrue(Math.abs(fs[1].i - -2.4) < .1);
-		assertTrue(Math.abs(fs[2].r - 0) < .1);
-		assertTrue(Math.abs(fs[2].i - 0) < .1);
-		assertTrue(Math.abs(fs[3].r - 1) < .1);
-		assertTrue(Math.abs(fs[3].i - -0.4) < .1);
-		assertTrue(Math.abs(fs[4].r - 0) < .1);
-		assertTrue(Math.abs(fs[4].i - 0) < .1);
-		assertTrue(Math.abs(fs[5].r - 1) < .1);
-		assertTrue(Math.abs(fs[5].i - 0.4) < .1);
-		assertTrue(Math.abs(fs[6].r - 0) < .1);
-		assertTrue(Math.abs(fs[6].i - 0) < .1);
-		assertTrue(Math.abs(fs[7].r - 1) < .1);
-		assertTrue(Math.abs(fs[7].i - 2.4) < .1);
+		assertTrue(equals(fs[0].r, 4f));
+		assertTrue(equals(fs[0].i, 0f));
+		assertTrue(equals(fs[1].r, 1f));
+		assertTrue(equals(fs[1].i, -2.4f));
+		assertTrue(equals(fs[2].r, 0f));
+		assertTrue(equals(fs[2].i, 0f));
+		assertTrue(equals(fs[3].r, 1f));
+		assertTrue(equals(fs[3].i, -0.4f));
+		assertTrue(equals(fs[4].r, 0f));
+		assertTrue(equals(fs[4].i, 0f));
+		assertTrue(equals(fs[5].r, 1f));
+		assertTrue(equals(fs[5].i, 0.4f));
+		assertTrue(equals(fs[6].r, 0f));
+		assertTrue(equals(fs[6].i, 0f));
+		assertTrue(equals(fs[7].r, 1f));
+		assertTrue(equals(fs[7].i, 2.4f));
+	}
+
+	@Test
+	public void testFftFloat() {
+		float fs[] = new FastFourierTransform().fft(new float[] { 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, });
+
+		for (float f : fs)
+			System.out.println(f);
+
+		assertTrue(equals(fs[0], 4f));
+		assertTrue(equals(fs[1], 0f));
+		assertTrue(equals(fs[2], 1f));
+		assertTrue(equals(fs[3], -2.4f));
+		assertTrue(equals(fs[4], 0f));
+		assertTrue(equals(fs[5], 0f));
+		assertTrue(equals(fs[6], 1f));
+		assertTrue(equals(fs[7], -0.4f));
+		assertTrue(equals(fs[8], 0f));
+		assertTrue(equals(fs[9], 0f));
+		assertTrue(equals(fs[10], 1f));
+		assertTrue(equals(fs[11], 0.4f));
+		assertTrue(equals(fs[12], 0f));
+		assertTrue(equals(fs[13], 0f));
+		assertTrue(equals(fs[14], 1f));
+		assertTrue(equals(fs[15], 2.4f));
+	}
+
+	private boolean equals(float a, float b) {
+		return Math.abs(a - b) < .1;
 	}
 
 }
