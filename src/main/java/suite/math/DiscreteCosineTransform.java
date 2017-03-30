6d291a2f56425a779e02b598882687a16ede6362
ywsing <stupidsing@gmail.com>
2017-03-30 17:49:29 +0800

    refactor

--- src/main/java/suite/math/DiscreteCosineTransform.java
+++ src/main/java/suite/math/DiscreteCosineTransform.java
@@ -1,61 +1,62 @@
 package suite.math;
 
+import java.util.Arrays;
+
 public class DiscreteCosineTransform {
 
+	private FastFourierTransform fft = new FastFourierTransform();
+
+	// http://dsp.stackexchange.com/questions/2807/fast-cosine-transform-via-fft
 	public float[] dct(float fs0[]) {
 		int size = fs0.length;
-		float fs1[] = new float[size * 4];
-		int i = 0;
+		int size21 = 2 * size - 1;
+		float fs1[] = new float[size * 8];
 
-		for (; i < size; i++) {
-			fs1[i * 2 + 0] = fs0[i];
-			fs1[i * 2 + 1] = 0;
+		// signal [a, b, c, d] becomes
+		// [0, a, 0, b, 0, c, 0, d, 0, d, 0, c, 0, b, 0, a]
+		for (int i = 0; i < size; i++) {
+			int j = size21 - i;
+			float v = fs0[i];
+			fs1[i * 4 + 2] = v;
+			fs1[j * 4 + 2] = v;
 		}
 
-		for (; i < size * 2; i++) {
-			fs1[i * 2 + 0] = fs0[2 * size - i - 1];
-			fs1[i * 2 + 1] = 0;
-		}
+		// take the FFT to get the spectrum
+		// [A, B, C, D, 0, -D, -C, -B, -A, -B, -C, -D, 0, D, C, B]
+		float fs2[] = fft.fft(fs1);
 
-		float fs2[] = new FastFourierTransform().fft(fs1);
-		float fs3[] = new float[size];
-		double angleStep = Math.PI / (2 * size);
+		// throw away everything but the first [A, B, C, D]
+		float fs3[] = Arrays.copyOf(fs2, size);
 
-		for (int j = 0; j < size; j++) {
-			double angle = angleStep * j;
-			float cisReal = (float) Math.cos(angle);
-			float cisImag = -(float) Math.sin(angle);
-			float fs2Real = fs2[j * 2 + 0];
-			float fs2Imag = fs2[j * 2 + 1];
-			fs3[j] = cisReal * fs2Real - cisImag * fs2Imag;
-		}
-
+		// and you are done
 		return fs3;
 	}
 
-	public float[] idct(float fs0[]) {
-		int size = fs0.length;
-		float fs1[] = new float[size * 4];
-		double angleStep = Math.PI / (2 * size);
+	public float[] idct(float fs3[]) {
+		int size = fs3.length;
+		int size0 = 0;
+		int size4 = size * 4;
+		float fs2[] = new float[size * 8];
 
 		for (int i = 0; i < size; i++) {
-			double angle = angleStep * i;
-			float cisReal = (float) Math.cos(angle);
-			float cisImag = -(float) Math.sin(angle);
-			float iabs2 = 1 / (cisReal * cisReal + cisImag * cisImag);
-			float icisReal = cisReal * iabs2;
-			float icisImag = -cisImag * iabs2;
-			fs1[i * 2 + 0] = icisReal * fs0[i];
-			fs1[i * 2 + 1] = icisImag * fs0[i];
+			int i2 = i * 2;
+			float v = fs3[i];
+			fs2[size0 + i2] = v;
+			fs2[size4 + i2] = v;
+			if (0 < i) {
+				int j2 = size4 - i2;
+				fs2[size0 + j2] = -v;
+				fs2[size4 + j2] = v;
+			}
 		}
 
-		float fs2[] = new FastFourierTransform().ifft(fs1);
-		float fs3[] = new float[size];
+		float fs1[] = fft.fft(fs2);
+		float fs0[] = new float[size];
 
 		for (int i = 0; i < size; i++)
-			fs3[i] = fs2[i * 2];
+			fs0[i] = fs1[i * 4 + 2];
 
-		return fs3;
+		return fs0;
 	}
 
 }
--- src/main/java/suite/math/FastFourierTransform.java
+++ src/main/java/suite/math/FastFourierTransform.java
@@ -34,10 +34,11 @@ public class FastFourierTransform {
 			fds[reverseBits(bits, i)] = tds[i];
 
 		for (int g = 2; g <= size; g <<= 1) {
+			double angleDiff = 2 * Math.PI / g;
 			Complex cis[] = new Complex[g];
 
 			for (int i = 0; i < g; i++) {
-				double angle = 2 * Math.PI * i / g;
+				double angle = angleDiff * i;
 				cis[i] = Complex.of((float) Math.cos(angle), (float) -Math.sin(angle));
 			}
 
@@ -61,28 +62,30 @@ public class FastFourierTransform {
 	}
 
 	public float[] ifft(float fs0[]) {
-		int size = fs0.length / 2;
-		float fs1[] = new float[size * 2];
+		int size2 = fs0.length;
+		int size = size2 / 2;
+		float fs1[] = new float[size2];
 
-		for (int i = 0; i < size; i++) {
-			fs1[i * 2 + 0] = fs0[i * 2 + 0];
-			fs1[i * 2 + 1] = -fs0[i * 2 + 1];
+		for (int i2 = 0; i2 < size2; i2 += 2) {
+			fs1[i2 + 0] = fs0[i2 + 0];
+			fs1[i2 + 1] = -fs0[i2 + 1];
 		}
 
 		float fs2[] = fft(fs1);
 		float inv = 1.0f / size;
 
-		for (int i = 0; i < size; i++) {
-			fs2[i * 2 + 0] = fs2[i * 2 + 0] * inv;
-			fs2[i * 2 + 1] = -fs2[i * 2 + 1] * inv;
+		for (int i2 = 0; i2 < size2; i2 += 2) {
+			fs2[i2 + 0] = fs2[i2 + 0] * inv;
+			fs2[i2 + 1] = fs2[i2 + 1] * -inv;
 		}
 
 		return fs2;
 	}
 
 	public float[] fft(float tds[]) {
-		int size = tds.length / 2;
-		float fds[] = new float[size * 2];
+		int size2 = tds.length;
+		int size = size2 / 2;
+		float fds[] = new float[size2];
 		int s = size;
 		int bits = 0;
 
@@ -92,32 +95,33 @@ public class FastFourierTransform {
 		}
 
 		for (int i = 0; i < size; i++) {
-			fds[reverseBits(bits, i) * 2 + 0] = tds[i * 2 + 0];
-			fds[reverseBits(bits, i) * 2 + 1] = tds[i * 2 + 1];
+			int i2 = i * 2;
+			fds[reverseBits(bits, i) * 2 + 0] = tds[i2 + 0];
+			fds[reverseBits(bits, i) * 2 + 1] = tds[i2 + 1];
 		}
 
 		for (int g = 2; g <= size; g <<= 1) {
-			float cis[] = new float[g * 2];
+			int g2 = g * 2;
+			float cis[] = new float[g2];
+			double angleDiff = Math.PI / g;
 
-			for (int i = 0; i < g; i++) {
-				double angle = 2 * Math.PI * i / g;
-				cis[i * 2 + 0] = (float) Math.cos(angle);
-				cis[i * 2 + 1] = (float) -Math.sin(angle);
+			for (int i2 = 0; i2 < g2; i2 += 2) {
+				double angle = angleDiff * i2;
+				cis[i2 + 0] = (float) Math.cos(angle);
+				cis[i2 + 1] = (float) -Math.sin(angle);
 			}
 
-			int step = g / 2;
-
-			for (int i = 0; i < size; i += g)
-				for (int k = 0; k < step; k++) {
-					int ie = (i + k) * 2;
-					int io = (i + k + step) * 2;
+			for (int i2 = 0; i2 < size2; i2 += g2)
+				for (int k2 = 0; k2 < g; k2 += 2) {
+					int ie = i2 + k2;
+					int io = ie + g;
 
 					float ceReal = fds[ie + 0];
 					float ceImag = fds[ie + 1];
 					float coReal = fds[io + 0];
 					float coImag = fds[io + 1];
-					float cisReal = cis[k * 2 + 0];
-					float cisImag = cis[k * 2 + 1];
+					float cisReal = cis[k2 + 0];
+					float cisImag = cis[k2 + 1];
 					float expReal = cisReal * coReal - cisImag * coImag;
 					float expImag = cisReal * coImag + cisImag * coReal;
 
