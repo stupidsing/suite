931ba82325165f1deb87269bf4f5ae34f6be79e2
ywsing <stupidsing@gmail.com>
2017-03-30 17:50:00 +0800

    refactor test case

--- src/test/java/suite/math/DiscreteCosineTransformTest.java
+++ src/test/java/suite/math/DiscreteCosineTransformTest.java
@@ -6,14 +6,19 @@ import org.junit.Test;
 
 public class DiscreteCosineTransformTest {
 
+	private DiscreteCosineTransform dct = new DiscreteCosineTransform();
+
 	@Test
 	public void testDct() {
-		DiscreteCosineTransform dct = new DiscreteCosineTransform();
 		float fs0[] = { 0, 1, 2, 3, 4, 5, 6, 7, };
 		float fs1[] = dct.dct(fs0);
 		float fs2[] = dct.idct(fs1);
 		for (int i = 0; i < fs0.length; i++)
-			assertTrue(0.1 < Math.abs(fs0[i] - fs2[i]));
+			assertEquals(fs0[i], fs2[i]);
+	}
+
+	private void assertEquals(float a, float b) {
+		assertTrue(Math.abs(a - b) < .1);
 	}
 
 }
