e190b18d9758e28924d1db6dafb51a741fd58242
stupidsing <stupidsing@gmail.com>
2017-03-31 20:09:51 +0800

    introduce parameters

--- src/test/java/suite/trade/TradeTest.java
+++ src/test/java/suite/trade/TradeTest.java
@@ -9,9 +9,6 @@ import suite.os.LogUtil;
 
 public class TradeTest {
 
-	private float threshold = 0.15f;
-	private int nPastDays = 64;
-	private int nFutureDays = 8;
 	private String stockCode = "0005.HK"; // "JPY%3DX"
 	private LocalDate frDate = LocalDate.of(2013, 1, 1);
 	private LocalDate toDate = LocalDate.of(2018, 1, 1);
@@ -21,9 +18,9 @@ public class TradeTest {
 		DataSource source = DataSource.yahoo(stockCode, frDate, toDate);
 		source.validate();
 
-		backTest(source, "LowPassMeanReverting", lowPassFilterPrediction);
+		backTest(source, "LowPassMeanReverting", lowPassFilterPrediction(64, 8, 0.02f));
 		backTest(source, "LongHold", longHold);
-		backTest(source, "MovingAverageMeanReverting", movingAverageMeanReverting);
+		backTest(source, "MovingAverageMeanReverting", movingAverageMeanReverting(64, 8, 0.15f));
 	}
 
 	private void backTest(DataSource source, String name, Strategy strategy) {
@@ -34,74 +31,78 @@ public class TradeTest {
 		LogUtil.info("total net gain = " + account.cash());
 	}
 
-	private Strategy lowPassFilterPrediction = prices -> {
-		DiscreteCosineTransform dct = new DiscreteCosineTransform();
-		int nDaysMovingWindow = nPastDays;
-		int nHistoryDays = nDaysMovingWindow - 8;
+	private Strategy lowPassFilterPrediction(int windowSize, int nFutureDays, float threshold) {
+		return prices -> {
+			DiscreteCosineTransform dct = new DiscreteCosineTransform();
+			int nPastDays = windowSize - nFutureDays;
 
-		return day -> {
-			if (nHistoryDays <= day) {
-				float fs0[] = new float[nDaysMovingWindow]; // moving window
-				float price0 = prices[day];
-				int i = 0;
+			return day -> {
+				if (nPastDays <= day) {
+					float fs0[] = new float[windowSize]; // moving window
+					float price0 = prices[day];
+					int i = 0;
 
-				for (; i < nHistoryDays; i++)
-					fs0[i] = prices[day - nHistoryDays + i];
-				for (; i < nDaysMovingWindow; i++)
-					fs0[i] = price0;
+					for (; i < nPastDays; i++)
+						fs0[i] = prices[day - nPastDays + i];
+					for (; i < windowSize; i++)
+						fs0[i] = price0;
 
-				float fs1[] = dct.dct(fs0);
-				float fs2[] = new float[nDaysMovingWindow];
+					float fs1[] = dct.dct(fs0);
+					float fs2[] = new float[windowSize];
 
-				for (int j = 0; j < 4; j++)
-					fs2[j] = fs1[j];
+					for (int j = 0; j < 4; j++)
+						fs2[j] = fs1[j];
 
-				float fs3[] = dct.idct(fs2);
+					float fs3[] = dct.idct(fs2);
 
-				float predict = fs3[fs3.length - 1];
-				return getSignal(price0, predict, 0.02f);
-			} else
-				return 0;
+					float predict = fs3[fs3.length - 1];
+					return getSignal(price0, predict, threshold);
+				} else
+					return 0;
+			};
 		};
-	};
+	}
 
 	private Strategy longHold = prices -> day -> day != 0 ? 0 : 1;
 
-	private Strategy movingAverageMeanReverting = prices -> {
-		int nDaysMovingWindow = nPastDays;
-		float movingAverages[] = new float[prices.length];
-		float movingSum = 0;
+	private Strategy movingAverageMeanReverting(int nPastDays, int nFutureDays, float threshold) {
+		return prices -> {
+			int nDaysMovingWindow = nPastDays;
+			float movingAverages[] = new float[prices.length];
+			float movingSum = 0;
 
-		for (int day = 0; day < prices.length; day++) {
-			if (nDaysMovingWindow <= day) {
-				movingAverages[day] = movingSum / nDaysMovingWindow;
-				movingSum -= prices[day - nDaysMovingWindow];
+			for (int day = 0; day < prices.length; day++) {
+				if (nDaysMovingWindow <= day) {
+					movingAverages[day] = movingSum / nDaysMovingWindow;
+					movingSum -= prices[day - nDaysMovingWindow];
+				}
+				movingSum += prices[day];
 			}
-			movingSum += prices[day];
-		}
 
-		int signals[] = new int[prices.length];
+			int signals[] = new int[prices.length];
 
-		for (int day = 0; day < prices.length; day++) {
-			int signal;
+			for (int day = 0; day < prices.length; day++) {
+				int signal;
 
-			if (nPastDays <= day) {
-				float price0 = prices[day];
-				float predict = movingAverages[day];
-				signal = getSignal(price0, predict, threshold);
-			} else
-				signal = 0;
+				if (nPastDays <= day) {
+					float price0 = prices[day];
+					float predict = movingAverages[day];
+					signal = getSignal(price0, predict, threshold);
+				} else
+					signal = 0;
 
-			signals[day] = signal;
-		}
+				signals[day] = signal;
+			}
 
-		// buy/sell if ratio is positive/negative; sell/buy nFutureDays after
-		return day -> {
-			int signal0 = nFutureDays < day ? -signals[day - nFutureDays] : 0;
-			int signal1 = signals[day];
-			return signal0 + signal1;
+			// buy/sell if ratio is positive/negative; sell/buy nFutureDays
+			// after
+			return day -> {
+				int signal0 = nFutureDays < day ? -signals[day - nFutureDays] : 0;
+				int signal1 = signals[day];
+				return signal0 + signal1;
+			};
 		};
-	};
+	}
 
 	// get buy/sell signal according to predicted price move direction
 	private int getSignal(float price0, float price1, float thr) {
