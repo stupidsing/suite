5a1879f375aef64c1436850dc2c6926b418748dd
stupidsing <stupidsing@gmail.com>
2017-04-04 09:41:00 +0800

    refactor

--- src/test/java/suite/trade/TradeTest.java
+++ src/test/java/suite/trade/TradeTest.java
@@ -44,25 +44,25 @@ public class TradeTest {
 
 	private void backTest(String stockCode, String stockName) {
 		backTest_(stockCode, stockCode + " " + stockName);
-
 	}
 
 	private void backTest_(String stockCode, String disp) {
 		Strategos sr = new Strategos();
-		DataSource source = DataSource.yahoo(stockCode, frDate, toDate);
-		backTest_(source, disp + ", strategy = lowPassPrediction", sr.lowPassPrediction(128, 8, 8, .02f));
-		backTest_(source, disp + ", strategy = longHold", sr.longHold);
-		backTest_(source, disp + ", strategy = movingAvgMeanReverting", sr.movingAvgMeanReverting(64, 8, .15f));
-		backTest_(source, disp + ", strategy = macdSignalLineX", sr.movingAvgConvDivSignalLineCrossover(.8f, .9f, .85f));
-		backTest_(source, disp + ", strategy = macdZeroLineX", sr.movingAvgConvDivZeroCrossover(.8f, .9f));
+		DataSource ds = DataSource.yahoo(stockCode, frDate, toDate);
+		backTest_(ds, disp + ", strategy = longHold", sr.longHold);
+		backTest_(ds, disp + ", strategy = lowPassPrediction", sr.lowPassPrediction(128, 8, 8, .02f));
+		backTest_(ds, disp + ", strategy = movingAvgMeanReverting", sr.movingAvgMeanReverting(64, 8, .15f));
+		backTest_(ds, disp + ", strategy = macdSignalLineX", sr.macdSignalLineX(.8f, .9f, .85f));
+		backTest_(ds, disp + ", strategy = macdZeroLineX", sr.macdZeroLineX(.8f, .9f));
 	}
 
-	private void backTest_(DataSource source, String disp, Strategy strategy) {
-		BackTest backTest = BackTest.test(source, strategy);
+	private BackTest backTest_(DataSource ds, String disp, Strategy strategy) {
+		BackTest backTest = BackTest.test(ds, strategy);
 		Account account = backTest.account;
 		LogUtil.info(disp //
 				+ ", number of transactions = " + account.nTransactions() //
 				+ ", net gain = " + String.format("%.2f", account.cash()));
+		return backTest;
 	}
 
 }
