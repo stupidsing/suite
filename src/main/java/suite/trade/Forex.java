c1595f7cb6e268a04ed42e036573231f6098d7ef
stupidsing <stupidsing@gmail.com>
2017-04-11 22:02:33 +0800

    refactor

--- src/main/java/suite/trade/Forex.java
+++ src/main/java/suite/trade/Forex.java
@@ -7,36 +7,36 @@ import suite.streamlet.Streamlet2;
 public class Forex {
 
 	public Streamlet2<String, String> currencies = Read.<String, String> empty2() //
+			.cons("AUDUSD=X", "AUD/USD") //
 			.cons("EURUSD=X", "EUR/USD") //
 			.cons("GBPUSD=X", "GBP/USD") //
-			.cons("AUDUSD=X", "AUD/USD") //
 			.cons("NZDUSD=X", "NZD/USD") //
 			.collect(As::streamlet2);
 
 	public Streamlet2<String, String> crossCurrencies = Read.<String, String> empty2() //
+			.cons("EURCAD=X", "EUR/CAD") //
+			.cons("EURCHF=X", "EUR/CHF") //
+			.cons("EURGBP=X", "EUR/GBP") //
+			.cons("EURHUF=X", "EUR/HUF") //
 			.cons("EURJPY=X", "EUR/JPY") //
 			.cons("GBPJPY=X", "GBP/JPY") //
-			.cons("EURGBP=X", "EUR/GBP") //
-			.cons("EURCAD=X", "EUR/CAD") //
 			.cons("EURSEK=X", "EUR/SEK") //
-			.cons("EURCHF=X", "EUR/CHF") //
-			.cons("EURHUF=X", "EUR/HUF") //
 			.collect(As::streamlet2);
 
 	// require ^ -1
 	public Streamlet2<String, String> invertedCurrencies = Read.<String, String> empty2() //
-			.cons("JPY=X", "USD/JPY") //
 			.cons("CNY=X", "USD/CNY") //
 			.cons("HKD=X", "USD/HKD") //
-			.cons("SGD=X", "USD/SGD") //
-			.cons("INR=X", "USD/INR") //
-			.cons("MXN=X", "USD/MXN") //
-			.cons("PHP=X", "USD/PHP") //
 			.cons("IDR=X", "USD/IDR") //
-			.cons("THB=X", "USD/THB") //
+			.cons("INR=X", "USD/INR") //
+			.cons("JPY=X", "USD/JPY") //
+			.cons("MXN=X", "USD/MXN") //
 			.cons("MYR=X", "USD/MYR") //
-			.cons("ZAR=X", "USD/ZAR") //
+			.cons("PHP=X", "USD/PHP") //
 			.cons("RUB=X", "USD/RUB") //
+			.cons("SGD=X", "USD/SGD") //
+			.cons("THB=X", "USD/THB") //
+			.cons("ZAR=X", "USD/ZAR") //
 			.collect(As::streamlet2);
 
 }
