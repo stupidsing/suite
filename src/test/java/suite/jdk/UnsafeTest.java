e039d94d0a36891b5daf8d82429c07e224b22974
ywsing <stupidsing@gmail.com>
2017-04-12 17:13:14 +0800

    fix test case

--- src/test/java/suite/jdk/UnsafeTest.java
+++ src/test/java/suite/jdk/UnsafeTest.java
@@ -5,13 +5,15 @@ import java.nio.file.Paths;
 
 import org.junit.Test;
 
+import suite.util.Util.ExecutableProgram;
+
 public class UnsafeTest {
 
 	@Test
 	public void test() throws Exception {
 		String className = "suite.cli.Main";
 		byte bytes[] = Files.readAllBytes(Paths.get("target/classes/" + className.replace(".", "/") + ".class"));
-		Class<? extends AutoCloseable> clazz = new UnsafeUtil().defineClass(AutoCloseable.class, className, bytes);
+		Class<? extends AutoCloseable> clazz = new UnsafeUtil().defineClass(ExecutableProgram.class, className, bytes);
 		clazz.newInstance().close();
 	}
 
