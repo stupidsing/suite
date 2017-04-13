6b666fc0f873f155b18c52a7a54bc7c7b1b8df62
ywsing <stupidsing@gmail.com>
2017-04-13 10:43:42 +0800

    add backward propagation

--- src/main/java/suite/algo/LongShortTermMemoryNetwork.java
+++ src/main/java/suite/algo/LongShortTermMemoryNetwork.java
@@ -6,20 +6,34 @@ import suite.util.Copy;
 
 public class LongShortTermMemoryNetwork {
 
+	private float learningRate = 1f;
 	private int memoryLength = 8;
 	private int inputLength = 8;
 	private int outputLength = 8;
 	private int ll = inputLength + outputLength + 1;
 
+	public class Result {
+		private float[] memory0;
+		private float[] io0;
+		private float[] is;
+		private float[] cs;
+		private float[] os;
+		private float[] tanh_memory1;
+		private float[] output1;
+	}
+
 	public class Unit {
 		private float[] memory = new float[memoryLength];
-		private float[] output0 = new float[outputLength];
+		private float[] output = new float[outputLength];
 		private float[][] wf = new float[memoryLength][ll];
 		private float[][] wi = new float[memoryLength][ll];
 		private float[][] wc = new float[memoryLength][ll];
 		private float[][] wo = new float[memoryLength][ll];
 
-		public float[] activateForward(float[] input) {
+		public Result activateForward(float[] input) {
+			float[] memory0 = memory;
+			float[] output0 = output;
+
 			float[] io0 = new float[ll];
 			Copy.primitiveArray(input, 0, io0, 0, inputLength);
 			Copy.primitiveArray(output0, 0, io0, inputLength, outputLength);
@@ -29,11 +43,47 @@ public class LongShortTermMemoryNetwork {
 			float[] is = sigmoidOn(Matrix.mul(wi, io0));
 			float[] cs = tanhOn(Matrix.mul(wc, io0));
 			float[] os = sigmoidOn(Matrix.mul(wo, io0));
-			float[] memory1 = Matrix.addOn(forgetOn(memory, fs), forgetOn(cs, is));
-			float[] output1 = forgetOn(os, tanh(memory1));
+			float[] memory1 = memory = Matrix.addOn(forget(memory0, fs), forget(cs, is));
+			float[] tanh_memory1 = tanh(memory1);
+			float[] output1 = output = forget(os, tanh_memory1);
 
-			memory = memory1;
-			return output0 = output1;
+			Result r = new Result();
+			r.memory0 = memory0;
+			r.io0 = io0;
+			r.is = is;
+			r.cs = cs;
+			r.os = os;
+			r.tanh_memory1 = tanh_memory1;
+			r.output1 = output1;
+
+			return r;
+		}
+
+		public void propagateBackward(float[] expected, Result r) {
+			float[] e_output1 = Matrix.sub(expected, r.output1);
+			float[] e_tanh_memory1 = forgetOn(r.os, e_output1);
+			float[] e_memory1 = tanhGradientOn(e_tanh_memory1);
+			float[] e_os = forgetOn(r.tanh_memory1, e_output1);
+			float[] e_cs = forget(e_memory1, r.is);
+			float[] e_is = forget(e_memory1, r.cs);
+			float[] e_fs = forget(e_memory1, r.memory0);
+			float[] e_wo = sigmoidGradientOn(e_os);
+			float[] e_wc = tanhGradientOn(e_cs);
+			float[] e_wi = sigmoidGradientOn(e_is);
+			float[] e_wf = sigmoidGradientOn(e_fs);
+
+			for (int j = 0; j < outputLength; j++)
+				for (int i = 0; i < ll; i++) {
+					float d = learningRate * r.io0[i];
+					wo[i][j] += d * e_wo[j];
+					wc[i][j] += d * e_wc[j];
+					wi[i][j] += d * e_wi[j];
+					wf[i][j] += d * e_wf[j];
+				}
+		}
+
+		private float[] forget(float[] fs, float[] n) {
+			return forgetOn(Matrix.of(fs), n);
 		}
 
 		private float[] forgetOn(float[] m, float[] n) {
@@ -53,6 +103,13 @@ public class LongShortTermMemoryNetwork {
 			return fs;
 		}
 
+		private float[] sigmoidGradientOn(float[] fs) {
+			int length = fs.length;
+			for (int i = 0; i < length; i++)
+				fs[i] = Sigmoid.sigmoidGradient(fs[i]);
+			return fs;
+		}
+
 		private float[] tanh(float[] fs) {
 			return tanhOn(Matrix.of(fs));
 		}
@@ -63,6 +120,15 @@ public class LongShortTermMemoryNetwork {
 				fs[i] = (float) Math.tanh(fs[i]);
 			return fs;
 		}
+	}
+
+	private float[] tanhGradientOn(float[] fs) {
+		int length = fs.length;
+		for (int i = 0; i < length; i++) {
+			float f = fs[i];
+			fs[i] = 1 - f * f;
+		}
+		return fs;
 	}
 
 }
