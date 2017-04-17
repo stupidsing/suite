be98736bbfb6981dfa2f38955c9c663264e8cf23
stupidsing <stupidsing@gmail.com>
2017-04-17 11:17:38 +0800

    add other primitive functions

--- src/main/java/suite/primitive/PrimitiveFun.java
+++ src/main/java/suite/primitive/PrimitiveFun.java
@@ -5,8 +5,18 @@ import java.util.function.ToIntFunction;
 public class PrimitiveFun {
 
 	@FunctionalInterface
+	public interface IntFloatFun {
+		public float apply(int i);
+	}
+
+	@FunctionalInterface
 	public interface IntIntFun {
 		public int apply(int i);
+	}
+
+	@FunctionalInterface
+	public interface IntIntFloatFun {
+		public float apply(int i, int j);
 	}
 
 	@FunctionalInterface
