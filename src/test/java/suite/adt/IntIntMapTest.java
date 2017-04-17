f644137eed94b18d43e36a93865726d2fa3878d9
stupidsing <stupidsing@gmail.com>
2017-04-18 04:39:04 +0800

    add test case

---
+++ src/test/java/suite/adt/IntIntMapTest.java
@@ -0,0 +1,38 @@
+package suite.adt;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import suite.primitive.PrimitiveSource.IntIntSource2;
+
+public class IntIntMapTest {
+
+	@Test
+	public void test() {
+		IntIntMap map = new IntIntMap();
+		map.put(1, 2);
+		map.put(3, 4);
+		map.put(5, 6);
+		assertEquals(2, map.get(1));
+		assertEquals(4, map.get(3));
+		assertEquals(6, map.get(5));
+
+		Set<String> expected = new HashSet<>();
+		expected.add("1:2");
+		expected.add("3:4");
+		expected.add("5:6");
+
+		Set<String> actual = new HashSet<>();
+
+		IntIntSource2 source = map.source();
+		IntIntPair pair = IntIntPair.of(0, 0);
+		while (source.source2(pair))
+			actual.add(pair.t0 + ":" + pair.t1);
+		assertEquals(expected, actual);
+	}
+
+}
