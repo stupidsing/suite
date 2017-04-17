9f65bbdcc09a57f4fba11471f9ab6cee6c0e16a1
stupidsing <stupidsing@gmail.com>
2017-04-17 11:16:57 +0800

    add backoff class

---
+++ src/main/java/suite/concurrent/Backoff.java
@@ -0,0 +1,22 @@
+package suite.concurrent;
+
+import java.util.Random;
+
+import suite.util.Util;
+
+public class Backoff {
+
+	private static Random random = new Random();
+	private int duration = 50;
+
+	public boolean exponentially() {
+		Util.sleepQuietly(duration);
+		duration = duration * 5 / 4 + random.nextInt(50);
+		return false;
+	}
+
+	public void yield() {
+		Thread.yield();
+	}
+
+}
