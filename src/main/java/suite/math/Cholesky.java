c5cdadb591cdce46ac8970e5b1f7cd35a6f26623
ywsing <stupidsing@gmail.com>
2017-04-24 12:08:06 +0800

    add LDL* decomposition

--- src/main/java/suite/math/Cholesky.java
+++ src/main/java/suite/math/Cholesky.java
@@ -1,17 +1,62 @@
 package suite.math;
 
+import suite.adt.Pair;
+import suite.util.FunUtil.Fun;
+import suite.util.To;
+
 public class Cholesky {
 
 	private Matrix mtx = new Matrix();
 
 	/**
+	 * @param m
+	 *            a Hermitian (i.e. symmetric), positive-definite matrix.
+	 * @return a function that calculates x -> m^-1 * x.
+	 */
+	public Fun<float[], float[]> inverseMul(float[][] m) {
+		Pair<float[][], float[]> ldlt = ldlt(m);
+		float[][] l = ldlt.t0;
+		float[] d = ldlt.t1;
+		float[] reciprocalsD = To.floatArray(d.length, i -> 1f / d[i]);
+		return fs0 -> {
+			int height = mtx.height(m);
+			int width = mtx.width(m);
+			float[] fs1 = new float[height]; // will be inverse(L) * fs0
+
+			for (int i = 0; i < height; i++) {
+				float sum = fs0[i];
+				for (int j = 0; j < i; j++)
+					sum -= l[i][j] * fs1[j];
+				fs1[i] = sum;
+			}
+
+			// will be inverse(D) * fs1
+			float[] fs2 = To.floatArray(fs1.length, i -> fs1[i] * reciprocalsD[i]);
+			float[] fs3 = new float[width]; // will be inverse(L*) * fs2
+
+			for (int i = width - 1; 0 <= i; i--) {
+				float sum = fs2[i];
+				for (int j = height - 1; i < j; j--)
+					sum -= l[j][i] * fs3[j];
+				fs3[i] = sum;
+			}
+
+			return fs3;
+		};
+	}
+
+	/**
 	 * Performs Cholesky decomposition. Input m must be symmetric and positive
 	 * definite. Also its contents will be destroyed as the decomposition is
 	 * performed in-place.
+	 * 
+	 * @param m
+	 *            a Hermitian (i.e. symmetric), positive-definite matrix.
+	 * @return lower-triangular matrix L that satisfy m = L * L*
 	 */
 	public float[][] decompose(float[][] m) {
 		int length = mtx.height(m);
-		float[][] inverse = mtx.identity(length);
+		float[][] l = mtx.identity(length);
 		if (length == mtx.width(m))
 			for (int c = 0; c < length; c++) {
 				float mii = m[c][c];
@@ -19,11 +64,11 @@ public class Cholesky {
 				float imii = 1f / mii;
 				double imii_sqrt = 1f / mii_sqrt;
 
-				for (int i = 0; i < length; i++) {
-					double sum = inverse[i][c] * mii_sqrt;
+				for (int i = c; i < length; i++) {
+					double sum = l[i][c] * mii_sqrt;
 					for (int j = c + 1; j < length; j++)
-						sum += inverse[i][j] * imii_sqrt * m[j][c];
-					inverse[i][c] = (float) sum;
+						sum += imii_sqrt * l[i][j] * m[j][c];
+					l[i][c] = (float) sum;
 				}
 
 				for (int i = c + 1; i < length; i++)
@@ -32,7 +77,37 @@ public class Cholesky {
 			}
 		else
 			throw new RuntimeException("Wrong input sizes");
-		return inverse;
+		return l;
+	}
+
+	/**
+	 * Performs Cholesky decomposition. Input m must be symmetric and positive
+	 * definite. Also its contents will be destroyed as the decomposition is
+	 * performed in-place.
+	 * 
+	 * @param m
+	 *            a Hermitian (i.e. symmetric), positive-definite matrix.
+	 * @return A pair of lower-triangular matrix L and diagonal vector D that
+	 *         satisfies m = L * D * L*.
+	 */
+	public Pair<float[][], float[]> ldlt(float[][] m) {
+		int length = mtx.height(m);
+		float[][] l = mtx.identity(length);
+		float[] d = new float[length];
+		if (length == mtx.width(m))
+			for (int c = 0; c < length; c++) {
+				float imii = 1f / (d[c] = m[c][c]);
+
+				for (int i = c + 1; i < length; i++)
+					for (int j = c + 1; j < length; j++) {
+						float imii_mjc = imii * m[j][c];
+						l[i][c] += imii_mjc * l[i][j];
+						m[i][j] -= imii_mjc * m[i][c];
+					}
+			}
+		else
+			throw new RuntimeException("Wrong input sizes");
+		return Pair.of(l, d);
 	}
 
 }
--- src/test/java/suite/math/CholeskyTest.java
+++ src/test/java/suite/math/CholeskyTest.java
@@ -1,26 +1,65 @@
 package suite.math;
 
+import java.util.Random;
+
 import org.junit.Test;
+
+import suite.adt.Pair;
+import suite.util.FunUtil.Fun;
+import suite.util.To;
 
 public class CholeskyTest {
 
-	private static Matrix mtx = new Matrix();
+	private Matrix mtx = new Matrix();
+	private Cholesky cholesky = new Cholesky();
+
+	private float[][] m0 = { //
+			{ 4f, 12f, -16f, }, //
+			{ 12f, 37f, -43f, }, //
+			{ -16f, -43f, 98f, }, //
+	};
 
 	@Test
-	public void test() {
-		float[][] m0 = { //
-				{ 4f, 12f, -16f, }, //
-				{ 12f, 37f, -43f, }, //
-				{ -16f, -43f, 98f, }, //
+	public void testInverseMul() {
+		Random random = new Random();
+		float[] fs = To.floatArray(3, i -> random.nextFloat());
+		Fun<float[], float[]> invm0 = cholesky.inverseMul(mtx.of(m0));
+		float[] actual0 = mtx.mul(m0, invm0.apply(fs));
+		float[] actual1 = invm0.apply(mtx.mul(m0, fs));
+		mtx.verifyEquals(fs, actual0, .01f);
+		mtx.verifyEquals(fs, actual1, .01f);
+	}
+
+	@Test
+	public void testLdlt() {
+		float[][] expectL = { //
+				{ 1f, 0f, 0f, }, //
+				{ 3f, 1f, 0f, }, //
+				{ -4f, 5f, 1f, }, //
 		};
 
+		float[] expectD = { 4f, 1f, 9f, };
+
+		Pair<float[][], float[]> ldlt = cholesky.ldlt(mtx.of(m0));
+		float[][] actualL = ldlt.t0;
+		float[] actualD = ldlt.t1;
+		mtx.verifyEquals(actualL, expectL);
+		mtx.verifyEquals(actualD, expectD);
+
+		float[][] matrixD = To.floatArray(actualD.length, actualD.length, (i, j) -> i == j ? actualD[i] : 0f);
+		float[][] m1 = mtx.mul(mtx.mul(actualL, matrixD), mtx.transpose(actualL));
+		mtx.verifyEquals(m0, m1);
+	}
+
+	@Test
+	public void testLlt() {
 		float[][] expect = { //
 				{ 2f, 0f, 0f, }, //
 				{ 6f, 1f, 0f, }, //
 				{ -8f, 5f, 3f, }, //
 		};
 
-		float[][] actual = new Cholesky().decompose(mtx.of(m0));
+		float[][] actual = cholesky.decompose(mtx.of(m0));
 		mtx.verifyEquals(actual, expect);
 
 		float[][] conjugate = mtx.transpose(actual);
