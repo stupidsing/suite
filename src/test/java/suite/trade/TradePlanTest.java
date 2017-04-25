037da2c14f8a7db782d7dd6af0595b0b377a6c6f
ywsing <stupidsing@gmail.com>
2017-04-25 12:29:07 +0800

    add ADF

--- src/test/java/suite/trade/TradePlanTest.java
+++ src/test/java/suite/trade/TradePlanTest.java
@@ -1,5 +1,6 @@
 package suite.trade;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,24 +8,29 @@ import org.junit.Test;
 
 import suite.algo.Statistic;
 import suite.algo.Statistic.LinearRegression;
+import suite.math.Matrix;
 import suite.os.LogUtil;
 import suite.streamlet.Read;
 import suite.trade.Hkex.Company;
+import suite.util.Copy;
 import suite.util.To;
 import suite.util.Util;
 
 public class TradePlanTest {
 
+	private Matrix mtx = new Matrix();
 	private Hkex hkex = new Hkex();
 	private Yahoo yahoo = new Yahoo();
 	private Statistic stat = new Statistic();
 
 	public class MeanReversionStats {
+		public final float adf;
 		public final float hurst;
 		public final float varianceRatio;
 		public final float halfLife;
 
 		private MeanReversionStats(DataSource dataSource) {
+			adf = adf(dataSource);
 			hurst = hurst(dataSource);
 			varianceRatio = varianceRatio(dataSource);
 			halfLife = halfLife(dataSource);
@@ -53,7 +59,7 @@ public class TradePlanTest {
 				.toMap();
 
 		// make sure all time-series are mean-reversions:
-		// calculate ADF < 0f
+		// calculate ADF < 0f to make sure data is not random walk
 		// calculate Hurst exponent < .5f
 		// calculate 0f < variable ratio
 		// calculate 0 < half-life
@@ -61,11 +67,32 @@ public class TradePlanTest {
 		Util.dump(meanReversionStatsByStockCode);
 	}
 
+	// Augmented Dickey-Fuller
+	private float adf(DataSource dataSource) {
+		int tor = 16;
+		float[] prices = dataSource.prices;
+		float[] diffs = differences(prices, 1);
+		float[][] deps = new float[prices.length][];
+		for (int i = tor; i < deps.length; i++) {
+			float[] fs = Arrays.copyOf(diffs, tor + 3);
+			fs[0] = prices[i - 1];
+			fs[1] = 1f;
+			fs[2] = i; // drift term, necessary?
+			Copy.primitiveArray(diffs, i - tor, fs, 3, tor);
+			deps[i] = fs;
+		}
+		float[][] deps1 = drop(deps, tor);
+		float[] diffs1 = drop(diffs, tor);
+		LinearRegression lr = stat.linearRegression(deps1, diffs1);
+		float lambda = lr.betas[0];
+		return (float) (lambda / lr.standardError);
+	}
+
 	private float hurst(DataSource dataSource) {
 		int tor = 16;
 		float[] prices = dataSource.prices;
 		float[] logs = To.floatArray(prices.length, i -> (float) Math.log(prices[i]));
-		float[] diffsTor = differences(logs, tor);
+		float[] diffsTor = dropDiff(logs, tor);
 		float[] vr = To.floatArray(diffsTor.length, i -> {
 			float diff = diffsTor[i];
 			return diff * diff;
@@ -81,8 +108,8 @@ public class TradePlanTest {
 		int tor = 16;
 		float[] prices = dataSource.prices;
 		float[] logs = To.floatArray(prices.length, i -> (float) Math.log(prices[i]));
-		float[] diffsTor = differences(logs, tor);
-		float[] diffs1 = differences(logs, 1);
+		float[] diffsTor = dropDiff(logs, tor);
+		float[] diffs1 = dropDiff(logs, 1);
 		return stat.variance(diffsTor) / (tor * stat.variance(diffs1));
 	}
 
@@ -90,15 +117,36 @@ public class TradePlanTest {
 		int tor = 1;
 		float[] prices = dataSource.prices;
 		float[][] deps = To.array(float[].class, prices.length - tor, i -> new float[] { prices[i], 1f, });
-		float[] diffs1 = differences(prices, tor);
+		float[] diffs1 = dropDiff(prices, tor);
 		LinearRegression lr = stat.linearRegression(deps, diffs1);
 		float[] ps = lr.betas;
 		float beta = ps[0];
 		return (float) (-Math.log(2) / Math.log(beta));
 	}
 
-	private float[] differences(float[] logs, int tor) {
-		return To.floatArray(logs.length - tor, i -> logs[i + tor] - logs[i]);
+	private float[] dropDiff(float[] logs, int tor) {
+		return drop(differences(logs, tor), tor);
+	}
+
+	private float[] differences(float[] fs, int tor) {
+		return differencesOn(mtx.of(fs), tor);
+	}
+
+	private float[] differencesOn(float[] fs, int tor) {
+		int i = fs.length;
+		while (tor <= --i)
+			fs[i] -= fs[i - tor];
+		while (0 <= --i)
+			fs[i] = 0f;
+		return fs;
+	}
+
+	private float[] drop(float[] fs, int tor) {
+		return Arrays.copyOfRange(fs, tor, fs.length);
+	}
+
+	private float[][] drop(float[][] fs, int tor) {
+		return Arrays.copyOfRange(fs, tor, fs.length);
 	}
 
 }
