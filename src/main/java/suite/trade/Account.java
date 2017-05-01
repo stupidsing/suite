f1bb508ba61109b1e87c12b3dde79f31f7013052
stupidsing <stupidsing@gmail.com>
2017-05-02 05:20:41 +0800

    allow trading several stocks

--- src/main/java/suite/trade/Account.java
+++ src/main/java/suite/trade/Account.java
@@ -1,20 +1,33 @@
 package suite.trade;
 
+import suite.adt.ObjIntMap;
+
 public class Account {
 
+	private static String defaultStockCode = "-";
+
 	private float cash = 0;
-	private int nLots = 0;
+	private ObjIntMap<String> nLots = new ObjIntMap<>();
 	private int nTransactions = 0;
 
 	public void buySell(int buySell, float price) {
+		buySell(defaultStockCode, buySell, price);
+	}
+
+	public void buySell(String stockCode, int buySell, float price) {
 		cash -= buySell * price;
-		nLots += buySell;
+		nLots.computeIfAbsent(stockCode, s -> 0);
+		nLots.update(stockCode, lot -> lot + buySell);
 		nTransactions += Math.abs(buySell);
 	}
 
 	public void validate() {
-		if (cash < 0 || nLots < 0)
+		if (cash < 0)
 			throw new RuntimeException("invalid condition");
+		nLots.forEach((buySell, stockCode) -> {
+			if (buySell < 0)
+				throw new RuntimeException("invalid condition");
+		});
 	}
 
 	public float cash() {
@@ -22,7 +35,11 @@ public class Account {
 	}
 
 	public int nLots() {
-		return nLots;
+		return nLots(defaultStockCode);
+	}
+
+	public int nLots(String stockCode) {
+		return nLots.get(stockCode);
 	}
 
 	public int nTransactions() {
