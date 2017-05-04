48f32d1f589d4e1affa09e9830a64651495abeac
ywsing <stupidsing@gmail.com>
2017-05-04 09:42:59 +0800

    find shell executable

--- src/main/java/suite/os/Execute.java
+++ src/main/java/suite/os/Execute.java
@@ -4,6 +4,9 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import suite.Constants;
 import suite.util.Copy;
@@ -18,11 +21,20 @@ public class Execute {
 	private Thread threads[];
 
 	public static String shell(String sh) {
-		Execute execute = new Execute(new String[] { "/bin/sh", }, sh);
-		if (execute.code == 0)
-			return execute.out;
-		else
-			throw new RuntimeException(execute.toString());
+		String[] command = null;
+
+		for (String s : Arrays.asList("/bin/sh", "C:\\cygwin\\bin\\sh.exe", "C:\\cygwin64\\bin\\sh.exe"))
+			if (Files.exists(Paths.get(s)))
+				command = new String[] { s, };
+
+		if (command != null) {
+			Execute execute = new Execute(command, sh);
+			if (execute.code == 0)
+				return execute.out;
+			else
+				throw new RuntimeException(execute.toString());
+		} else
+			throw new RuntimeException("Cannot find shell executable");
 	}
 
 	public Execute(String[] command) {
