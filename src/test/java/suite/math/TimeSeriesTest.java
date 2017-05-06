ad5af386b93777a61624c642494638fe5c332163
stupidsing <stupidsing@gmail.com>
2017-05-07 07:56:05 +0800

    add test case

---
+++ src/test/java/suite/math/TimeSeriesTest.java
@@ -0,0 +1,24 @@
+package suite.math;
+
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+import suite.trade.DataSource;
+import suite.trade.DatePeriod;
+import suite.trade.Yahoo;
+
+public class TimeSeriesTest {
+
+	@Test
+	public void testSharpeRatio() {
+		DatePeriod period = DatePeriod.of(LocalDate.of(2016, 1, 1), LocalDate.of(2017, 5, 1));
+		DataSource ds = new Yahoo().dataSource("0002.HK").limit(period);
+		double sharpe = new TimeSeries().sharpeRatio(ds.prices, period.nYears());
+		System.out.println("sharpe = " + sharpe);
+		assertTrue(0.4d < sharpe);
+	}
+
+}
