919f594b43a9df6b164981fa3fbad1f2fa8cbf2e
stupidsing <stupidsing@gmail.com>
2017-05-07 10:20:47 +0800

    reorganize imports

--- src/main/java/suite/trade/Portfolio.java
+++ src/main/java/suite/trade/Portfolio.java
@@ -16,13 +16,11 @@ import suite.os.LogUtil;
 import suite.streamlet.Read;
 import suite.streamlet.Streamlet;
 import suite.trade.DataSource.Datum;
-import suite.trade.Portfolio.MeanReversionStats;
-import suite.trade.Portfolio.Simulate;
 import suite.util.FormatUtil;
-import suite.util.To;
-import suite.util.Util;
 import suite.util.FunUtil.Fun;
 import suite.util.FunUtil.Sink;
+import suite.util.To;
+import suite.util.Util;
 
 public class Portfolio {
 
