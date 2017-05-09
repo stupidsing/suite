2ee3f03a80e3c8152cf87715473c14ced693ead4
ywsing <stupidsing@gmail.com>
2017-05-09 15:26:22 +0800

    calculate transaction fee

---
+++ src/main/java/suite/trade/data/Hsbc.java
@@ -0,0 +1,22 @@
+package suite.trade.data;
+
+// https://www.personal.hsbc.com.hk/1/2/hk/investments/stocks/detail
+public class Hsbc {
+
+	public float transactionFee(double transactionAmount) {
+		double hsbcInternetBanking = Math.min(transactionAmount * .01d * .25d, 100d);
+
+		double stampDuty = transactionAmount * .01d * .1d;
+		double sfcTxLevy = transactionAmount * .01d * .0027d;
+		double sfcInvestorLevy = transactionAmount * .01d * .002d; // suspended
+		double hkex = transactionAmount * .01d * .005d;
+
+		// TODO deposit transaction charge (for purchase transaction only)
+		// HKD5/RMB5 per board lot (minimum charge: HKD30/RMB30, maximum charge:
+		// HKD200/RMB200), waived if the same stocks are purchased and then sold
+		// on the same trading day or the subsequent trading day (T or T+1)
+
+		return (float) (hsbcInternetBanking + stampDuty + sfcTxLevy + 0d * sfcInvestorLevy + hkex);
+	}
+
+}
