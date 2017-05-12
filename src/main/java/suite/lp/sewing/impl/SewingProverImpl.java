83032cc1783ccf1e828a65bbdcd747e9fe59cae2
ywsing <stupidsing@gmail.com>
2017-05-12 11:06:58 +0800

    fix

--- src/main/java/suite/lp/sewing/impl/SewingProverImpl.java
+++ src/main/java/suite/lp/sewing/impl/SewingProverImpl.java
@@ -265,7 +265,7 @@ public class SewingProverImpl implements SewingProver {
 	private Trampoline compileRule(Node head, Node tail, boolean isHasCut) {
 		SewingBinder sb = new SewingBinderImpl0();
 		BindPredicate p = sb.compileBind(head);
-		Trampoline tr1 = isHasCut ? compile_(sb, tail) : compileNoCut_(sb, tail);
+		Trampoline tr1 = isHasCut || Boolean.TRUE ? compile_(sb, tail) : compileNoCut_(sb, tail);
 		return newEnv(sb, rt -> p.test(rt, rt.query) ? tr1 : fail);
 	}
 
