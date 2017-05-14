d917893b2ce08618fa36949731846c3e22481f0a
stupidsing <stupidsing@gmail.com>
2017-05-14 08:51:21 +0800

    add even()

--- src/main/java/suite/trade/assetalloc/AssetAllocator_.java
+++ src/main/java/suite/trade/assetalloc/AssetAllocator_.java
@@ -19,13 +19,29 @@ public class AssetAllocator_ {
 		};
 	}
 
+	public static AssetAllocator even(AssetAllocator assetAllocator) {
+		return (dataSourceBySymbol, tradeDates, backTestDate) -> {
+			List<Pair<String, Double>> potentialBySymbol = assetAllocator.allocate(dataSourceBySymbol, tradeDates, backTestDate);
+
+			if (potentialBySymbol != null) {
+				double each = 1d / Read.from2(potentialBySymbol).size();
+
+				return Read.from2(potentialBySymbol) //
+						.filterKey(symbol -> !String_.equals(symbol, Asset.cashCode)) //
+						.mapValue(potential -> 1d / each) //
+						.toList();
+			} else
+				return null;
+		};
+	}
+
 	public static AssetAllocator reallocate(AssetAllocator assetAllocator) {
 		return (dataSourceBySymbol, tradeDates, backTestDate) -> {
 			List<Pair<String, Double>> potentialBySymbol = assetAllocator.allocate(dataSourceBySymbol, tradeDates, backTestDate);
 
 			if (potentialBySymbol != null) {
 				double totalPotential = Read.from2(potentialBySymbol) //
-						.collectAsDouble(As.<String, Double>sumOfDoubles((symbol, potential) -> potential));
+						.collectAsDouble(As.<String, Double> sumOfDoubles((symbol, potential) -> potential));
 
 				return Read.from2(potentialBySymbol) //
 						.filterKey(symbol -> !String_.equals(symbol, Asset.cashCode)) //
