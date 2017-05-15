397d267ee5b4c3d3b4ee7e5915be9a3d8dbc97d5
stupidsing <stupidsing@gmail.com>
2017-05-16 07:13:57 +0800

    refactor

--- src/main/java/suite/trade/Account.java
+++ src/main/java/suite/trade/Account.java
@@ -14,12 +14,14 @@ public class Account {
 
 	public static final String cashCode = Asset.cashCode;
 
-	private Map<String, Integer> assets = new HashMap<>();
-	private int nTransactions = 0;
-	private float transactionAmount = 0f;
+	public static Account fromCash(float cash) {
+		Map<String, Integer> assets = new HashMap<>();
+		assets.put(cashCode, (int) cash);
+		return new Account(assets);
+	}
 
 	public static Account fromHistory(Iterable<Trade> trades) {
-		Account account = new Account();
+		Account account = new Account(new HashMap<>());
 		account.play(trades);
 		return account;
 	}
@@ -28,37 +30,24 @@ public class Account {
 		return new Account(TradeUtil.portfolio(trades));
 	}
 
-	public static Account fromCash(float cash) {
-		Map<String, Integer> assets = new HashMap<>();
-		assets.put(cashCode, (int) cash);
-		return new Account(assets);
-	}
-
-	private Account() {
-		this(new HashMap<>());
-	}
+	private Map<String, Integer> assets = new HashMap<>();
+	private int nTransactions = 0;
+	private float transactionAmount = 0f;
 
 	private Account(Map<String, Integer> assets) {
 		this.assets = assets;
 	}
 
-	public float valuation(Map<String, Float> prices0) {
-		Map<String, Float> prices1 = new HashMap<>(prices0);
-		prices1.put(cashCode, 1f);
-		return (float) Read.from2(assets()) //
-				.collectAsDouble(As.<String, Integer> sumOfDoubles((symbol, n) -> prices1.get(symbol) * n));
+	public Map<String, Integer> assets() {
+		return assets;
 	}
 
-	public String switchPortfolio(Map<String, Integer> assets1, Map<String, Float> prices) {
-		Map<String, Integer> assets0 = assets;
-		List<Trade> trades = TradeUtil.diff(assets0, assets1, prices);
+	public int cash() {
+		return get(cashCode);
+	}
 
-		play(trades);
-
-		return Read.from(trades) //
-				.filter(trade -> trade.buySell != 0) //
-				.map(Trade::toString) //
-				.collect(As.joined());
+	public int nShares(String symbol) {
+		return get(symbol);
 	}
 
 	private void play(Iterable<Trade> trades) {
@@ -83,6 +72,32 @@ public class Account {
 		transactionAmount += Math.abs(cost);
 	}
 
+	public String switchPortfolio(Map<String, Integer> assets1, Map<String, Float> prices) {
+		Map<String, Integer> assets0 = assets;
+		List<Trade> trades = TradeUtil.diff(assets0, assets1, prices);
+
+		play(trades);
+
+		return Read.from(trades) //
+				.filter(trade -> trade.buySell != 0) //
+				.map(Trade::toString) //
+				.collect(As.joined());
+	}
+
+	@Override
+	public String toString() {
+		return TradeUtil.format(assets());
+	}
+
+	public float transactionAmount() {
+		return transactionAmount;
+	}
+
+	public String transactionSummary(Double_Double transactionFeeFun) {
+		double fee = transactionFeeFun.apply(transactionAmount);
+		return "transactions = " + To.string(transactionAmount) + "/" + nTransactions + ", fee = " + To.string(fee);
+	}
+
 	private void update(String code, int amount) {
 		if (amount != 0)
 			assets.put(code, amount);
@@ -97,34 +112,15 @@ public class Account {
 		});
 	}
 
-	public int cash() {
-		return get(cashCode);
-	}
-
-	public int nShares(String symbol) {
-		return get(symbol);
+	public float valuation(Map<String, Float> prices0) {
+		Map<String, Float> prices1 = new HashMap<>(prices0);
+		prices1.put(cashCode, 1f);
+		return (float) Read.from2(assets()) //
+				.collectAsDouble(As.<String, Integer> sumOfDoubles((symbol, n) -> prices1.get(symbol) * n));
 	}
 
 	private int get(String code) {
-		return assets().computeIfAbsent(code, s -> 0);
-	}
-
-	public String transactionSummary(Double_Double transactionFeeFun) {
-		double fee = transactionFeeFun.apply(transactionAmount);
-		return "transactions = " + To.string(transactionAmount) + "/" + nTransactions + ", fee = " + To.string(fee);
-	}
-
-	@Override
-	public String toString() {
-		return TradeUtil.format(assets());
-	}
-
-	public Map<String, Integer> assets() {
-		return assets;
-	}
-
-	public float transactionAmount() {
-		return transactionAmount;
+		return assets.computeIfAbsent(code, s -> 0);
 	}
 
 }
