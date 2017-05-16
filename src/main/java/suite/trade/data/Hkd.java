596ee8f52357e74a5ec0e196bf14f6012b8e2bf1
ywsing <stupidsing@gmail.com>
2017-05-16 20:32:25 +0800

    refactor

--- src/main/java/suite/trade/data/Hkd.java
+++ src/main/java/suite/trade/data/Hkd.java
@@ -5,9 +5,18 @@ import java.util.Set;
 
 import suite.streamlet.Read;
 import suite.trade.Asset;
+import suite.trade.DatePeriod;
+import suite.util.FormatUtil;
 import suite.util.String_;
 
 public class Hkd {
+
+	public DataSource dataSource(String symbol, DatePeriod period) {
+		if (String_.equals(symbol, Asset.cashCode))
+			return new DataSource(new String[] { FormatUtil.formatDate(period.to), }, new float[] { 1f, });
+		else
+			throw new RuntimeException();
+	}
 
 	public Map<String, Float> quote(Set<String> symbols) {
 		return Read.from(symbols) //
