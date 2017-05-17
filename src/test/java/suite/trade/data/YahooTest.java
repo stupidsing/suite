46cb367b27aa421956651880416e81af2914f313
stupidsing <stupidsing@gmail.com>
2017-05-18 07:44:23 +0800

    refactor

--- src/test/java/suite/trade/data/YahooTest.java
+++ src/test/java/suite/trade/data/YahooTest.java
@@ -3,6 +3,7 @@ package suite.trade.data;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
+import java.util.function.BiFunction;
 
 import org.junit.Test;
 
@@ -13,10 +14,21 @@ public class YahooTest {
 	private Yahoo yahoo = new Yahoo();
 
 	@Test
-	public void test() {
-		DataSource dataSource = yahoo.dataSourceYql("0005.HK", DatePeriod.of(LocalDate.of(2016, 1, 1), LocalDate.of(2017, 1, 1)));
-		assertTrue(0 < dataSource.dates.length);
-		assertTrue(0 < dataSource.prices.length);
+	public void testL1() {
+		test(yahoo::dataSourceL1);
+	}
+
+	@Test
+	public void testYql() {
+		test(yahoo::dataSourceYql);
+	}
+
+	private void test(BiFunction<String, DatePeriod, DataSource> fun) {
+		DataSource dataSource = fun.apply("0005.HK", DatePeriod.of(LocalDate.of(2016, 1, 1), LocalDate.of(2017, 1, 1)));
+		int datesLength = dataSource.dates.length;
+		int pricesLength = dataSource.prices.length;
+		assertTrue(datesLength == pricesLength);
+		assertTrue(0 < datesLength);
 	}
 
 }
