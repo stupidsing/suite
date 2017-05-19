c471af929ff19d1b494727a542936618aa29a8fe
ywsing <stupidsing@gmail.com>
2017-05-19 12:54:28 +0800

    add back

---
+++ src/main/sh/replace0.sh
@@ -0,0 +1,40 @@
+replace-word() {
+  FROM="${1}"
+  shift
+  TO="${1}"
+  shift
+  FILES=$(find $* -print0 | xargs -0 grep -l "${FROM}")
+  echo ${FILES}
+  echo ${FILES} | xargs sed "s/${FROM}/${TO}/g" -i
+}
+
+replace_() {
+  REPLACER="${1}"
+  F0="${2}"
+  F1=$(echo "${F0}" | sh -c "${REPLACER}")
+  mkdir -p $(dirname "${F1}")
+  cat "${F0}" | sh -c "${REPLACER}" > /tmp/replaced
+  [ "${REMOVEOLD}" ] && rm -f "${F0}" || true
+  mv /tmp/replaced "${F1}"
+  echo ${F1}
+}
+
+replace() {
+  REPLACER="${1}"
+  shift
+  while [ "${1}" ]; do
+    F0="${1}"
+    shift
+    replace_ "${REPLACER}" "${F0}"
+  done
+}
+
+replace-files() {
+  while read F; do
+    replace_ "${1}" "${F}"
+  done
+}
+
+replace-example() {
+  replace "sed 's/board/Board/g'" file.txt
+}
