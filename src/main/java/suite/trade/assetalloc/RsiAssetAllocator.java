6736b1fa55aab9b0a985a8486f02eb72d437c95f
stupidsing <stupidsing@gmail.com>
2017-05-20 22:16:07 +0800

    add RSI asset allocator

---
+++ src/main/java/suite/trade/assetalloc/RsiAssetAllocator.java
@@ -0,0 +1,45 @@
+package suite.trade.assetalloc;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import suite.adt.pair.Pair;
+import suite.streamlet.Read;
+import suite.trade.data.DataSource;
+
+public class RsiAssetAllocator implements AssetAllocator {
+
+	private int window = 32;
+	private double threshold0 = .3f;
+	private double threshold1 = .7f;
+
+	public static AssetAllocator Of() {
+		return AssetAllocator_.unleverage(new RsiAssetAllocator());
+	}
+
+	private RsiAssetAllocator() {
+	}
+
+	@Override
+	public List<Pair<String, Double>> allocate(Map<String, DataSource> dataSourceBySymbol, LocalDate backTestDate) {
+		return Read.from2(dataSourceBySymbol) //
+				.mapValue(dataSource -> {
+					float[] prices = dataSource.prices;
+					int length = prices.length;
+					int u = 0;
+					for (int i = length - window; i < length; i++)
+						if (prices[i - 1] < prices[i])
+							u++;
+					float rsi = (float) u / length;
+					if (rsi < threshold0)
+						return 1d;
+					else if (threshold1 < rsi)
+						return -1d;
+					else
+						return 0d;
+				}) //
+				.toList();
+	}
+
+}
