381af6347aa2fff5c7e049b05297f2af6148cb59
ywsing <stupidsing@gmail.com>
2017-05-26 11:42:19 +0800

    refactor

--- src/main/sh/replace.sh
+++ src/main/sh/replace.sh
@@ -1,4 +1,4 @@
-replace() {
+replace-ci() {
   sed 's/Character/Integer/g' |
   sed 's/Char/Int/g' |
   sed 's/character/integer/g' |
@@ -7,24 +7,25 @@ replace() {
 }
 
 replace-file() {
-  F0=${1}
-  F1=$(echo ${F0} | replace ${F0})
-  cat ${F0} | replace > /tmp/replaced
+  C=${1}
+  F0=${2}
+  F1=$(echo ${F0} | replace-${C} ${F0})
+  cat ${F0} | replace-${C} > /tmp/replaced
   mv /tmp/replaced ${F1}
 }
 
-replace-file src/main/java/suite/adt/map/CharObjMap.java
-replace-file src/main/java/suite/adt/map/ObjCharMap.java
-replace-file src/main/java/suite/adt/pair/CharCharPair.java
-replace-file src/main/java/suite/adt/pair/CharObjPair.java
-replace-file src/main/java/suite/primitive/CharMutable.java
-replace-file src/main/java/suite/primitive/CharObjFunUtil.java
-replace-file src/main/java/suite/primitive/CharPrimitiveFun.java
-replace-file src/main/java/suite/primitive/CharPrimitivePredicate.java
-replace-file src/main/java/suite/primitive/CharPrimitiveSink.java
-replace-file src/main/java/suite/primitive/CharPrimitiveSource.java
-replace-file src/main/java/suite/primitive/CharRethrow.java
-replace-file src/main/java/suite/primitive/Chars.java
-replace-file src/main/java/suite/primitive/Chars_.java
-replace-file src/main/java/suite/streamlet/CharObjOutlet.java
-replace-file src/main/java/suite/streamlet/CharObjStreamlet.java
+replace-file ci src/main/java/suite/adt/map/CharObjMap.java
+replace-file ci src/main/java/suite/adt/map/ObjCharMap.java
+replace-file ci src/main/java/suite/adt/pair/CharCharPair.java
+replace-file ci src/main/java/suite/adt/pair/CharObjPair.java
+replace-file ci src/main/java/suite/primitive/CharMutable.java
+replace-file ci src/main/java/suite/primitive/CharObjFunUtil.java
+replace-file ci src/main/java/suite/primitive/CharPrimitiveFun.java
+replace-file ci src/main/java/suite/primitive/CharPrimitivePredicate.java
+replace-file ci src/main/java/suite/primitive/CharPrimitiveSink.java
+replace-file ci src/main/java/suite/primitive/CharPrimitiveSource.java
+replace-file ci src/main/java/suite/primitive/CharRethrow.java
+replace-file ci src/main/java/suite/primitive/Chars.java
+replace-file ci src/main/java/suite/primitive/Chars_.java
+replace-file ci src/main/java/suite/streamlet/CharObjOutlet.java
+replace-file ci src/main/java/suite/streamlet/CharObjStreamlet.java
