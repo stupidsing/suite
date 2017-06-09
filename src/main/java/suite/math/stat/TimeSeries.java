b31ae9e8274831b9a94110d63fffa100f50f9cc0
ywsing <stupidsing@gmail.com>
2017-06-09 16:47:16 +0800

    fix

--- src/main/java/suite/math/stat/TimeSeries.java
+++ src/main/java/suite/math/stat/TimeSeries.java
@@ -170,7 +170,7 @@ public class TimeSeries {
 			float[] returns_ = returns_(prices);
 			MeanVariance mv = stat.meanVariance(returns_);
 
-			return_ = Math.expm1(Math.log(vx / v0) * scale / returns.length);
+			return_ = Math.expm1(Math.log(vx / v0) * scale / returns_.length);
 			returns = returns_;
 			mean = mv.mean - interestRate;
 			variance = scale * mv.variance;
