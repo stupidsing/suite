878f5dc211f731cade7d5b7e24bb968e0d5ba099
stupidsing <stupidsing@gmail.com>
2017-06-27 03:11:18 +0800

    add ofSingle()

--- src/main/java/suite/trade/backalloc/BackAllocConfiguration.java
+++ src/main/java/suite/trade/backalloc/BackAllocConfiguration.java
@@ -1,5 +1,6 @@
 package suite.trade.backalloc;
 
+import suite.streamlet.Read;
 import suite.streamlet.Streamlet;
 import suite.trade.Asset;
 
@@ -7,6 +8,10 @@ public class BackAllocConfiguration {
 
 	public final Streamlet<Asset> assets;
 	public final BackAllocator backAllocator;
+
+	public static BackAllocConfiguration ofSingle(Asset asset) {
+		return new BackAllocConfiguration(Read.each(asset), BackAllocator_.ofSingle(asset.symbol));
+	}
 
 	public BackAllocConfiguration(Streamlet<Asset> assets, BackAllocator backAllocator) {
 		this.assets = assets;
