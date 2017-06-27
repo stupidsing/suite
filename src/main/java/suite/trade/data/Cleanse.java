00e661b72c0ff532e3728bbbba2bfa7d8f4dc139
ywsing <stupidsing@gmail.com>
2017-06-27 12:18:08 +0800

    refactor

--- src/main/java/suite/trade/data/Cleanse.java
+++ src/main/java/suite/trade/data/Cleanse.java
@@ -1,5 +1,7 @@
 package suite.trade.data;
 
+import suite.primitive.IntFlt_Flt;
+import suite.primitive.Int_Flt;
 import suite.primitive.adt.pair.LngFltPair;
 
 /**
@@ -16,28 +18,48 @@ public class Cleanse {
 	}
 
 	public float[] cleanse(float[] prices) {
-		for (int i = 2; i < prices.length; i++) {
-			float price0 = prices[i - 2];
-			float price1 = prices[i - 1];
-			float price2 = prices[i - 0];
-			if (isValid(price0, price2) && !isValid(price0, price1) && !isValid(price1, price2))
-				prices[i - 1] = price0;
-		}
+		cleanse(prices.length, //
+				i -> prices[i], //
+				(i, price) -> prices[i] = price);
 		return prices;
 	}
 
 	public LngFltPair[] cleanse(LngFltPair[] pairs) {
-		for (int i = 2; i < pairs.length; i++) {
-			float price0 = pairs[i - 2].t1;
-			float price1 = pairs[i - 1].t1;
-			float price2 = pairs[i - 0].t1;
-			if (isValid(price0, price2) && !isValid(price0, price1) && !isValid(price1, price2))
-				pairs[i - 1].t1 = price0;
-		}
+		cleanse(pairs.length, //
+				i -> pairs[i].t1, //
+				(i, price) -> pairs[i].t1 = price);
 		return pairs;
 	}
 
 	public boolean isValid(float price0, float price1) {
+		return isValid_(price0, price1);
+	}
+
+	private void cleanse(int length, Int_Flt get, IntFlt_Flt set) {
+
+		// fill zeroes in the middle
+		float ppos = 0f;
+		int i0 = 0;
+		for (int i = 0; i < length; i++) {
+			float price = get.apply(i);
+			if (price != 0f) {
+				while (i0 < i)
+					set.apply(i0++, ppos);
+				ppos = price;
+			}
+		}
+
+		// eliminate spikes
+		for (int i = 2; i < length; i++) {
+			float price0 = get.apply(i - 2);
+			float price1 = get.apply(i - 1);
+			float price2 = get.apply(i - 0);
+			if (isValid(price0, price2) && !isValid(price0, price1) && !isValid(price1, price2))
+				set.apply(i - 1, price0);
+		}
+	}
+
+	private boolean isValid_(float price0, float price1) {
 		float ratio = price1 / price0;
 		return Float.isFinite(ratio) && 1f / 2f < ratio && ratio < 2f / 1f;
 	}
