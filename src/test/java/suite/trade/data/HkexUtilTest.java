ee16f616356ec7c812b94da0b50cc8cef1b44869
stupidsing <stupidsing@gmail.com>
2017-06-29 22:19:34 +0800

    refactor

--- src/test/java/suite/trade/data/HkexUtilTest.java
+++ src/test/java/suite/trade/data/HkexUtilTest.java
@@ -10,13 +10,13 @@ public class HkexUtilTest {
 
 	@Test
 	public void test() {
-		Time on = Time.of("2017-06-29 12:48:00");
+		Time on_ = Time.of("2017-06-29 12:48:00");
 		Time off = Time.of("2017-06-29 22:48:00");
 
-		assertEquals(Time.of("2017-06-29 12:48:00"), HkexUtil.getTradeTimeBefore(on));
-		assertEquals(Time.of("2017-06-29 12:48:00"), HkexUtil.getTradeTimeAfter(on));
-		assertEquals(Time.of("2017-06-29 09:00:00"), HkexUtil.getOpenTimeBefore(on));
-		assertEquals(Time.of("2017-06-28 16:30:00"), HkexUtil.getCloseTimeBefore(on));
+		assertEquals(Time.of("2017-06-29 12:48:00"), HkexUtil.getTradeTimeBefore(on_));
+		assertEquals(Time.of("2017-06-29 12:48:00"), HkexUtil.getTradeTimeAfter(on_));
+		assertEquals(Time.of("2017-06-29 09:00:00"), HkexUtil.getOpenTimeBefore(on_));
+		assertEquals(Time.of("2017-06-28 16:30:00"), HkexUtil.getCloseTimeBefore(on_));
 
 		assertEquals(Time.of("2017-06-29 16:29:59"), HkexUtil.getTradeTimeBefore(off));
 		assertEquals(Time.of("2017-06-30 09:00:00"), HkexUtil.getTradeTimeAfter(off));
