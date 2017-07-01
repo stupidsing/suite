68698584b4c18fedfb9591ed5437834827106612
Sing Yiu Wah <stupidsing@gmail.com>
2017-07-01 17:38:19 +0800

    refactor

--- src/test/java/suite/trade/FactorTest.java
+++ src/test/java/suite/trade/FactorTest.java
@@ -29,33 +29,13 @@ public class FactorTest {
 
 	@Test
 	public void test() {
-		TimeRange period = TimeRange.daysBefore(HkexUtil.getOpenTimeBefore(Time.now()), 250 * 3);
-
-		Streamlet<DataSource> dataSources = Read //
-				.each("^DJI", "^GSPC", "NDAQ") //
-				.map(symbol -> cfg.dataSource(symbol).range(period)) //
-				.collect(As::streamlet);
-
-		AlignDataSource alignDataSource = DataSource.alignAll(dataSources);
-
-		float[] indexReturns = dataSources //
-				.map(ds -> ts.returns(alignDataSource.align(ds).prices)) //
-				.fold(new float[alignDataSource.ts.length], mtx::add);
-
-		Streamlet<Asset> assets = cfg.queryCompaniesByMarketCap(Time.now());
-
-		List<Pair<Asset, Double>> pairs = assets //
-				.cons(Asset.hsi) //
-				.cons(cfg.queryCompany("1169.HK")) //
-				.cons(cfg.queryCompany("2638.HK")) //
-				.cons(cfg.queryCompany("0880.HK")) //
-				.map2(asset -> {
-					DataSource ds = cfg.dataSource(asset.symbol).range(period).align(alignDataSource.ts);
-					float[] returns = ts.returns(ds.prices);
-					return stat.correlation(indexReturns, returns);
-				}) //
-				.sortByValue(Object_::compare) //
-				.toList();
+		List<Pair<Asset, Double>> pairs = test( //
+				Read.each("^DJI", "^GSPC", "NDAQ"), //
+				cfg.queryCompaniesByMarketCap(Time.now()) //
+						.cons(Asset.hsi) //
+						.cons(cfg.queryCompany("1169.HK")) //
+						.cons(cfg.queryCompany("2638.HK")) //
+						.cons(cfg.queryCompany("0880.HK")));
 
 		for (Pair<Asset, Double> pair : pairs)
 			System.out.println(pair);
@@ -65,6 +45,29 @@ public class FactorTest {
 		};
 
 		ba.getClass();
+	}
+
+	private List<Pair<Asset, Double>> test(Streamlet<String> indices, Streamlet<Asset> assets) {
+		TimeRange period = TimeRange.daysBefore(HkexUtil.getOpenTimeBefore(Time.now()), 250 * 3);
+
+		Streamlet<DataSource> dataSources = indices //
+				.map(symbol -> cfg.dataSource(symbol).range(period)) //
+				.collect(As::streamlet);
+
+		AlignDataSource alignDataSource = DataSource.alignAll(dataSources);
+
+		float[] indexReturns = dataSources //
+				.map(ds -> ts.returns(alignDataSource.align(ds).prices)) //
+				.fold(new float[alignDataSource.ts.length], mtx::add);
+
+		return assets //
+				.map2(asset -> {
+					DataSource ds = cfg.dataSource(asset.symbol).range(period).align(alignDataSource.ts);
+					float[] returns = ts.returns(ds.prices);
+					return stat.correlation(indexReturns, returns);
+				}) //
+				.sortByValue(Object_::compare) //
+				.toList();
 	}
 
 }
