55b4c0e169f95efabf578501d0967e016e2c9ac6
stupidsing <stupidsing@gmail.com>
2017-07-08 09:41:17 +0800

    test case

--- src/test/java/suite/trade/analysis/FactorTest.java
+++ src/test/java/suite/trade/analysis/FactorTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.Test;
 
 import suite.adt.pair.Pair;
+import suite.streamlet.Read;
 import suite.streamlet.Streamlet;
 import suite.trade.Asset;
 import suite.trade.Time;
@@ -17,15 +18,18 @@ public class FactorTest {
 
 	@Test
 	public void test() {
+		Streamlet<String> indices = Read.each("CLQ17.NYM");
+
 		Streamlet<Asset> assets0 = cfg.queryCompaniesByMarketCap(Time.now());
 		Streamlet<Asset> assets1 = cfg.queryHistory().map(trade -> trade.symbol).distinct().map(cfg::queryCompany);
 
-		Streamlet<Asset> assets = Streamlet.concat(assets0, assets1) //
+		Streamlet<Asset> assets = Streamlet //
+				.concat(assets0, assets1) //
 				.cons(Asset.hsi) //
 				.cons(cfg.queryCompany("0753.HK")) //
 				.distinct();
 
-		List<Pair<Asset, Double>> pairs = Factor.ofCrudeOil(cfg).query(assets);
+		List<Pair<Asset, Double>> pairs = Factor.of(cfg, indices).query(assets);
 
 		for (Pair<Asset, Double> pair : pairs)
 			System.out.println(pair);
