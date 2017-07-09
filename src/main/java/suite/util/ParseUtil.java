dee7436ac20517fc107526475fa3ae042f9dca4b
stupidsing <stupidsing@gmail.com>
2017-07-10 03:35:30 +0800

    refactor

--- src/main/java/suite/util/ParseUtil.java
+++ src/main/java/suite/util/ParseUtil.java
@@ -13,68 +13,17 @@ import suite.util.FunUtil.Source;
 
 public class ParseUtil {
 
-	private static int checkDepth(int depth, char c) {
-		if (c == '(' || c == '[' || c == '{')
-			depth++;
-		if (c == ')' || c == ']' || c == '}')
-			depth--;
-		return depth;
-	}
+	public static List<String> searchn(String s, String name, Assoc assoc) {
+		List<String> list = new ArrayList<>();
+		Pair<String, String> pair;
 
-	public static int getQuoteChange(int quote, char c) {
-		if (c == quote)
-			quote = 0;
-		else if (quote == 0 && (c == '\'' || c == '"' || c == '`'))
-			quote = c;
-		return quote;
-	}
-
-	public static boolean isParseable(String s) {
-		return isParseable(s, false);
-	}
-
-	/**
-	 * Judges if the input string has balanced quote characters and bracket
-	 * characters.
-	 *
-	 * @param isThrow
-	 *            if this is set to true, and the string is deemed unparseable
-	 *            even if more characters are added, throw exception.
-	 */
-	public static boolean isParseable(String s, boolean isThrow) {
-		int quote = 0, depth = 0;
-
-		// shows warning if the atom has mismatched quotes or brackets
-		for (char c : String_.chars(s)) {
-			quote = getQuoteChange(quote, c);
-			if (quote == 0)
-				depth = checkDepth(depth, c);
+		while ((pair = search(s, name, assoc)) != null) {
+			list.add(pair.t0);
+			s = pair.t1;
 		}
 
-		if (!isThrow || 0 <= depth)
-			return quote == 0 && depth == 0;
-		else
-			throw new RuntimeException("parse error");
-	}
-
-	public static boolean isWhitespace(byte b) {
-		return b == 0;
-	}
-
-	public static boolean isWhitespace(char c) {
-		return Character.isWhitespace(c);
-	}
-
-	public static boolean isWhitespace(double d) {
-		return d == 0d;
-	}
-
-	public static boolean isWhitespace(float f) {
-		return f == 0f;
-	}
-
-	public static boolean isWhitespace(int i) {
-		return i == 0;
+		list.add(s);
+		return list;
 	}
 
 	public static int search(String s, int start, String toMatch) {
@@ -93,11 +42,6 @@ public class ParseUtil {
 		return -1;
 	}
 
-	public static Pair<String, String> search(String s, String name) {
-		Pair<String, String> pair = search(s, name, Assoc.RIGHT);
-		return pair != null ? pair : Pair.of(s, "");
-	}
-
 	public static Pair<String, String> search(String s, String name, Assoc assoc) {
 		return search(s, Segment.of(0, s.length()), name, assoc, true);
 	}
@@ -113,17 +57,28 @@ public class ParseUtil {
 			return null;
 	}
 
-	public static List<String> searchn(String s, String name, Assoc assoc) {
-		List<String> list = new ArrayList<>();
-		Pair<String, String> pair;
+	public static Streamlet<String> split(String in, String name) {
+		char[] chars = To.arrayOfChars(in);
+		int length = chars.length;
 
-		while ((pair = search(s, name, assoc)) != null) {
-			list.add(pair.t0);
-			s = pair.t1;
-		}
+		return new Streamlet<>(() -> Outlet.of(new Source<String>() {
+			private int pos = 0;
 
-		list.add(s);
-		return list;
+			public String source() {
+				if (pos < length) {
+					Segment segment = searchPosition(chars, Segment.of(pos, length), name, Assoc.LEFT, true);
+					int pos0 = pos;
+					int end;
+					if (segment != null) {
+						end = segment.start;
+						pos = segment.end;
+					} else
+						end = pos = length;
+					return new String(chars, pos0, end);
+				} else
+					return null;
+			}
+		}));
 	}
 
 	public static Segment searchPosition(char[] cs, Segment segment, Operator operator) {
@@ -169,28 +124,68 @@ public class ParseUtil {
 		return null;
 	}
 
-	public static Streamlet<String> split(String in, String name) {
-		char[] chars = To.arrayOfChars(in);
-		int length = chars.length;
+	public static boolean isParseable(String s) {
+		return isParseable(s, false);
+	}
 
-		return new Streamlet<>(() -> Outlet.of(new Source<String>() {
-			private int pos = 0;
+	/**
+	 * Judges if the input string has balanced quote characters and bracket
+	 * characters.
+	 *
+	 * @param isThrow
+	 *            if this is set to true, and the string is deemed unparseable
+	 *            even if more characters are added, throw exception.
+	 */
+	public static boolean isParseable(String s, boolean isThrow) {
+		int quote = 0, depth = 0;
 
-			public String source() {
-				if (pos < length) {
-					Segment segment = searchPosition(chars, Segment.of(pos, length), name, Assoc.LEFT, true);
-					int pos0 = pos;
-					int end;
-					if (segment != null) {
-						end = segment.start;
-						pos = segment.end;
-					} else
-						end = pos = length;
-					return new String(chars, pos0, end);
-				} else
-					return null;
-			}
-		}));
+		// shows warning if the atom has mismatched quotes or brackets
+		for (char c : String_.chars(s)) {
+			quote = getQuoteChange(quote, c);
+			if (quote == 0)
+				depth = checkDepth(depth, c);
+		}
+
+		if (!isThrow || 0 <= depth)
+			return quote == 0 && depth == 0;
+		else
+			throw new RuntimeException("parse error");
+	}
+
+	private static int checkDepth(int depth, char c) {
+		if (c == '(' || c == '[' || c == '{')
+			depth++;
+		if (c == ')' || c == ']' || c == '}')
+			depth--;
+		return depth;
+	}
+
+	public static int getQuoteChange(int quote, char c) {
+		if (c == quote)
+			quote = 0;
+		else if (quote == 0 && (c == '\'' || c == '"' || c == '`'))
+			quote = c;
+		return quote;
+	}
+
+	public static boolean isWhitespace(byte b) {
+		return b == 0;
+	}
+
+	public static boolean isWhitespace(char c) {
+		return Character.isWhitespace(c);
+	}
+
+	public static boolean isWhitespace(double d) {
+		return d == 0d;
+	}
+
+	public static boolean isWhitespace(float f) {
+		return f == 0f;
+	}
+
+	public static boolean isWhitespace(int i) {
+		return i == 0;
 	}
 
 }
