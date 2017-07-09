8d2b23f3adef63b3a1ffdae458a2b7037ad36514
stupidsing <stupidsing@gmail.com>
2017-07-09 11:05:08 +0800

    tune Donchian

--- src/main/java/suite/trade/backalloc/BackAllocConfigurations.java
+++ src/main/java/suite/trade/backalloc/BackAllocConfigurations.java
@@ -26,7 +26,7 @@ public class BackAllocConfigurations {
 		private BackAllocator ba_facoil = Factor.ofCrudeOil(cfg).backAllocator().longOnly().pick(3).even();
 
 		public final BackAllocConfiguration bac_bb = ba_bb.cfgUnl(fun);
-		public final BackAllocConfiguration bac_donchian = ba_donchian.cfgUnl(fun);
+		public final BackAllocConfiguration bac_donchian = ba_donchian.holdMinimum(2).cfgUnl(fun);
 		public final BackAllocConfiguration bac_ema = BackAllocator_.ema().pick(3).cfgUnl(fun);
 		public final BackAllocConfiguration bac_facoil = ba_facoil.cfgUnl(fun);
 		public final BackAllocConfiguration bac_hsi = BackAllocConfiguration.ofSingle(Asset.hsi);
