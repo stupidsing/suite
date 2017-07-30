326de694810a8094a9797d447b20be6c0f5420b4
stupidsing <stupidsing@gmail.com>
2017-07-30 19:53:10 +0800

    add Gauss-Seidel iterative method to solve system of linear equations

---
+++ src/main/java/suite/math/linalg/GaussSeidel.java
@@ -0,0 +1,25 @@
+package suite.math.linalg;
+
+public class GaussSeidel {
+
+	private Matrix mtx = new Matrix();
+
+	// solve x in A*x = b
+	public float[] solve(float[][] a, float[] b) {
+		int size = mtx.sqSize(a);
+		float[] phi = new float[size];
+
+		for (int iteration = 0; iteration < 16; iteration++)
+			for (int i = 0; i < size; i++) {
+				float[] ai = a[i];
+				double o = 0d;
+				for (int j = 0; j < size; j++)
+					if (i != j)
+						o += ai[j] * phi[j];
+				phi[i] = (float) ((b[i] - o) / a[i][i]);
+			}
+
+		return phi;
+	}
+
+}
