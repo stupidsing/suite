a099d417a8d048c7d524eea30fabcc833537acc7
stupidsing <stupidsing@gmail.com>
2017-08-13 11:47:31 +0800

    fix

--- src/main/java/suite/trade/analysis/Factor.java
+++ src/main/java/suite/trade/analysis/Factor.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import suite.adt.pair.Pair;
 import suite.math.linalg.Matrix;
 import suite.math.stat.Quant;
-import suite.math.stat.Statistic;
 import suite.streamlet.Read;
 import suite.streamlet.Streamlet;
 import suite.streamlet.Streamlet2;
@@ -27,7 +26,6 @@ public class Factor {
 
 	private Configuration cfg;
 	private Matrix mtx = new Matrix();
-	private Statistic stat = new Statistic();
 	private Time now = Time.now();
 
 	public static Factor ofCrudeOil(Configuration cfg) {
@@ -58,7 +56,7 @@ public class Factor {
 		TimeRange period = TimeRange.daysBefore(HkexUtil.getOpenTimeBefore(now), 250 * 3);
 
 		return assets //
-				.map2(asset -> correlate(ids, cfg.dataSource(asset.symbol), period)) //
+				.map2(asset -> project(ids, cfg.dataSource(asset.symbol), period)) //
 				.sortByValue(Object_::compare) //
 				.toList();
 	}
@@ -69,7 +67,7 @@ public class Factor {
 			Map<String, DataSource> dsBySymbol_ = dsBySymbol.toMap();
 
 			DataSourceView<String, Double> dsv = DataSourceView.of(0, 64, akds,
-					(symbol, ds, period) -> correlate(ids, dsBySymbol_.get(symbol), period));
+					(symbol, ds, period) -> project(ids, dsBySymbol_.get(symbol), period));
 
 			return index -> {
 				float[] indexPrices = ids.prices;
@@ -82,10 +80,12 @@ public class Factor {
 		};
 	}
 
-	private double correlate(DataSource irds0, DataSource rds0, TimeRange period) {
+	private double project(DataSource irds0, DataSource rds0, TimeRange period) {
 		DataSource rds1 = rds0.range(period);
 		DataSource irds1 = irds0.range(period).alignBeforePrices(rds1.ts);
-		return stat.correlation(irds1.returns(), rds1.returns());
+		float[] returns = rds1.returns();
+		float[] irReturns = irds1.returns();
+		return mtx.dot(returns, irReturns) / mtx.dot(irReturns);
 	}
 
 }
