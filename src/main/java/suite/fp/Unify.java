8e9d59d7257015e9aaa5454e31f0ad60edea321d
stupidsing <stupidsing@gmail.com>
2017-08-20 11:37:51 +0800

    refactor

---
+++ src/main/java/suite/fp/Unify.java
@@ -0,0 +1,89 @@
+package suite.fp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import suite.fp.Unify.UnNode;
+import suite.util.AutoObject;
+import suite.util.Util;
+
+public class Unify<T extends UnNode<T>> {
+
+	public interface UnNode<T extends UnNode<T>> {
+		public boolean unify(UnNode<T> t);
+
+		public default UnNode<T> final_() {
+			UnNode<T> object = this;
+			while (true)
+				if (object instanceof UnRef) {
+					UnNode<T> ref1 = ((UnRef<T>) object).target;
+					if (object != ref1)
+						object = ref1;
+					else
+						return object;
+				} else
+					return object;
+		}
+	}
+
+	private static class UnRef<T extends UnNode<T>> implements UnNode<T> {
+		private UnNode<T> target;
+		private int id = Util.temp();
+
+		public boolean unify(UnNode<T> un) {
+			return false;
+		}
+	}
+
+	public UnNode<T> clone(UnNode<T> node) {
+		return clone(new HashMap<>(), node);
+	}
+
+	public UnNode<T> newRef() {
+		UnRef<T> ref = new UnRef<>();
+		ref.target = ref;
+		return ref;
+	}
+
+	public boolean unify(UnNode<T> u0, UnNode<T> u1) {
+		UnNode<T> target0 = u0.final_();
+		UnNode<T> target1 = u1.final_();
+		if (target0 instanceof UnRef)
+			return addBind((UnRef<T>) u0, target1);
+		else if (target1 instanceof UnRef)
+			return addBind((UnRef<T>) u1, target0);
+		else {
+			@SuppressWarnings("unchecked")
+			T t1 = (T) target1;
+			return target0.unify(t1);
+		}
+	}
+
+	public boolean addBind(UnRef<T> reference, UnNode<T> target) {
+		if (target instanceof Unify.UnRef) {
+			UnRef<T> reference1 = (UnRef<T>) target;
+			if (reference.id < reference1.id)
+				return bindDirect(reference1, reference);
+			else
+				return bindDirect(reference, reference1);
+		} else
+			return bindDirect(reference, target);
+	}
+
+	private UnNode<T> clone(Map<UnRef<?>, UnNode<T>> map, UnNode<T> node0) {
+		UnNode<T> node1 = node0.final_();
+		if (node1 instanceof UnRef)
+			return (UnRef<T>) map.computeIfAbsent((UnRef<?>) node1, r -> newRef());
+		else {
+			@SuppressWarnings("unchecked")
+			UnNode<T> object1 = (UnNode<T>) ((AutoObject<?>) node1).clone();
+			return object1;
+		}
+	}
+
+	private boolean bindDirect(UnRef<T> reference, UnNode<T> target) {
+		reference.target = target;
+		return true;
+	}
+
+}
