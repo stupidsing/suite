0ee037317c9b8a3175add12052129ebe1ee86dd9
Sing Yiu Wah <yw.sing_contractor@hactl.com>
2017-09-13 14:56:29 +0800

    refactor

--- src/main/c/kbhook.c
+++ src/main/c/kbhook.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <windows.h>
 
+int isMod = 0;
+int rightCtrl = 0;
+int rightAlt = 0;
+
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	KBDLLHOOKSTRUCT *pKeyboard = (KBDLLHOOKSTRUCT*) lParam;
 	char pressedKey;
@@ -9,10 +13,54 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	case WM_KEYDOWN:
 		pressedKey = (char) pKeyboard->vkCode;
 		printf("DN %d\n", pressedKey);
+		if(isMod)
+			isMod = 0;
+		else if(rightCtrl) {
+			INPUT input;
+			input.type = INPUT_KEYBOARD;
+			input.ki.dwExtraInfo = 0;
+			input.ki.dwFlags = KEYEVENTF_KEYUP;
+			input.ki.wScan = 0;
+			input.ki.wVk = VK_RCONTROL;
+			input.ki.time = 0;
+			SendInput(1, &input, sizeof(INPUT));
+			rightCtrl = 0;
+		} else if(rightAlt) {
+			INPUT input;
+			input.type = INPUT_KEYBOARD;
+			input.ki.dwExtraInfo = 0;
+			input.ki.dwFlags = KEYEVENTF_KEYUP;
+			input.ki.wScan = 0;
+			input.ki.wVk = VK_MENU;
+			input.ki.time = 0;
+			SendInput(1, &input, sizeof(INPUT));
+			rightAlt = 0;
+		}
 		break;
 	case WM_KEYUP:
 		pressedKey = (char) pKeyboard->vkCode;
 		printf("UP %d\n", pressedKey);
+		if(pressedKey == -23) {
+			INPUT input;
+			input.type = INPUT_KEYBOARD;
+			input.ki.dwExtraInfo = 0;
+			input.ki.dwFlags = 0;
+			input.ki.wScan = 0;
+			input.ki.wVk = VK_RCONTROL;
+			input.ki.time = 0;
+			SendInput(1, &input, sizeof(INPUT));
+			isMod = rightCtrl = 1;
+		} else if(pressedKey == -1) {
+			INPUT input;
+			input.type = INPUT_KEYBOARD;
+			input.ki.dwExtraInfo = 0;
+			input.ki.dwFlags = 0;
+			input.ki.wScan = 0;
+			input.ki.wVk = VK_MENU;
+			input.ki.time = 0;
+			SendInput(1, &input, sizeof(INPUT));
+			isMod = rightAlt = 1;
+		}
 		break;
 	default:
 		printf("UNK %d %d %d\n", nCode, wParam, lParam);
