675b42aee1a1fb0c1f9758554ff93e91f8c609b2
stupidsing <stupidsing@gmail.com>
2017-09-17 09:09:13 +0800

    refactor

--- src/test/java/suite/math/transform/DiscreteCosineTransformTest.java
+++ src/test/java/suite/math/transform/DiscreteCosineTransformTest.java
@@ -9,8 +9,16 @@ public class DiscreteCosineTransformTest {
 	private DiscreteCosineTransform dct = new DiscreteCosineTransform();
 
 	@Test
-	public void testDct() {
-		float[] fs0 = { 0f, 1f, 2f, 3f, 4f, 5f, 6f, 7f, };
+	public void testDct0() {
+		test(new float[] { 0f, 1f, 2f, 3f, 4f, 5f, 6f, 7f, });
+	}
+
+	@Test
+	public void testDct1() {
+		test(new float[] { 0f, 1f, 0f, 1f, 0f, 1f, 0f, 1f, });
+	}
+
+	private void test(float[] fs0) {
 		float[] fs1 = dct.dct(fs0);
 		float[] fs2 = dct.idct(fs1);
 		for (int i = 0; i < fs0.length; i++)
