3e9c60cf3a682b3911ad7b168ee5eb303c53c644
Sing Yiu Wah <yw.sing_contractor@hactl.com>
2017-09-18 10:53:36 +0900

    refactor

--- src/main/c/kbhook.c
+++ src/main/c/kbhook.c
@@ -15,7 +15,6 @@ void send(int dwf, int vk) {
 	input.ki.wVk = vk;
 	input.ki.time = 0;
 	SendInput(1, &input, sizeof(INPUT));
-	rightCtrl = 0;
 }
 
 void restore() {
@@ -24,22 +23,24 @@ void restore() {
 	isIssued = rightCtrl = rightAlt = 0;
 }
 
+void issued() { isIssued = rightCtrl || rightAlt; }
+
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	KBDLLHOOKSTRUCT *pKeyboard = (KBDLLHOOKSTRUCT*) lParam;
 	char vk;
 	switch(wParam) {
 	case WM_KEYDOWN:
 		printf("DN %d\n", vk = (char) pKeyboard->vkCode);
-		if(isIssued)
-			if(vk != VK_NEXT && vk != VK_PRIOR) restore();
-		isIssued = rightCtrl || rightAlt;
+		if(vk != VK_NEXT && vk != VK_PRIOR)
+			if(isIssued) restore();
+		issued();
 		break;
 	case WM_KEYUP:
 		printf("UP %d\n", vk = (char) pKeyboard->vkCode);
 		if(vk == -23) { send(0, VK_RCONTROL); rightCtrl = 1; }
 		else if(vk == -1) { send(0, VK_MENU); rightAlt = 1; }
 		else if(vk != VK_NEXT && vk != VK_PRIOR) restore();
-		else isIssued = rightCtrl || rightAlt;
+		else issued();
 		break;
 	// case WM_SYSKEYDOWN: break;
 	// case WM_SYSKEYUP: break;
@@ -51,13 +52,13 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	MSLLHOOKSTRUCT *pMouse = (MSLLHOOKSTRUCT*) lParam;
 	switch(wParam) {
-    case WM_LBUTTONDOWN: restore(); break;
-    // case WM_LBUTTONUP: break;
+    // case WM_LBUTTONDOWN: break;
+    case WM_LBUTTONUP: issued(); break;
     // case WM_MOUSEHWHEEL: break;
     // case WM_MOUSEMOVE: break;
     // case WM_MOUSEWHEEL: break;
-    case WM_RBUTTONDOWN: restore(); break;
-    // case WM_RBUTTONUP: break;
+    // case WM_RBUTTONDOWN: break;
+    case WM_RBUTTONUP: issued(); break;
     default: printf("UNK %d %d %d\n", nCode, wParam, lParam);
 	}
 	return CallNextHookEx(NULL, nCode, wParam, lParam);
