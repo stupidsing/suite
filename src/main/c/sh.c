29e98ed07e45e43f6e70f5f50d749d1fd8d59363
stupidsing <stupidsing@gmail.com>
2017-10-01 09:22:58 +0800

    refactor

--- src/main/c/sh.c
+++ src/main/c/sh.c
@@ -47,9 +47,9 @@ char **splitLine(char *line) {
 }
 
 int launch(char **args) {
-	pid_t pid;
+	pid_t pid = fork();
 
-	if((pid = fork()) == 0) // child process
+	if(pid == 0) // child process
 		if(execvp(args[0], args) == -1) perror("execvp()");
 		else exit(1);
 	else if(0 <= pid) { // parent process
