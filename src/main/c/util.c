73bf24623e55afbef43697cd0ea34441d92e8df6
stupidsing <stupidsing@gmail.com>
2017-10-07 11:39:54 +0800

    refactor

--- src/main/c/util.c
+++ src/main/c/util.c
@@ -9,19 +9,19 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) < (b) ? (b) : (a))
 
-int msg(char *t, int line, char *m) {
-	fprintf(stderr, "[%s] %s in line %d\n", t, m, line);
+int msg(char *t, char *fn, int line, char *m) {
+	fprintf(stderr, "[%s] %s in %s:%d\n", t, m, fn, line);
 	return 1;
 }
 
-int fmsg(char *t, int line, char *m) {
-	msg(t, line, m);
+int fmsg(char *t, char *fn, int line, char *m) {
+	msg(t, fn, line, m);
 	exit(1);
 }
 
-#define info(m) msg("INFO", __LINE__, (m))
-#define err(m) msg("ERROR", __LINE__, (m))
-#define fatal(m) fmsg("FATAL", __LINE__, (m))
+#define info(m) msg("INFO", __FILE__, __LINE__, (m))
+#define err(m) msg("ERROR", __FILE__, __LINE__, (m))
+#define fatal(m) fmsg("FATAL", __FILE__, __LINE__, (m))
 
 int nAllocs;
 
