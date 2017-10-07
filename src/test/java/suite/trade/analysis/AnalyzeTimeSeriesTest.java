6a5189b77a52600a8ecf0ed2b9c93b5e68a62268
stupidsing <stupidsing@gmail.com>
2017-10-07 09:40:36 +0800

    refactor

--- src/test/java/suite/trade/analysis/AnalyzeTimeSeriesTest.java
+++ src/test/java/suite/trade/analysis/AnalyzeTimeSeriesTest.java
@@ -18,6 +18,7 @@ import suite.primitive.Int_Flt;
 import suite.primitive.Ints_;
 import suite.primitive.adt.pair.IntFltPair;
 import suite.streamlet.As;
+import suite.trade.Asset;
 import suite.trade.Time;
 import suite.trade.TimeRange;
 import suite.trade.Trade_;
@@ -31,7 +32,7 @@ public class AnalyzeTimeSeriesTest {
 
 	private static AnalyzeTimeSeriesTest me = new AnalyzeTimeSeriesTest();
 
-	private String symbol = "^HSI";
+	private String symbol = Asset.hsiSymbol;
 	private TimeRange period = TimeRange.of(Time.of(2005, 1, 1), TimeRange.max);
 	// TimeRange.of(Time.of(2013, 1, 1), Time.of(2014, 1, 1));
 	// TimeRange.threeYears();
@@ -100,7 +101,7 @@ public class AnalyzeTimeSeriesTest {
 				+ "\ndct period = " + max.t0 //
 				+ Ints_ //
 						.range(10) //
-						.map(d -> "\ndct component, " + d + " days = " + fds[d]) //
+						.map(d -> "\ndct component [" + d + "d] = " + fds[d]) //
 						.collect(As::joined) //
 				+ "\nreturn yearly sharpe = " + rmv.mean / Math.sqrt(variance / nYears) //
 				+ "\nreturn kelly = " + kelly //
@@ -108,22 +109,22 @@ public class AnalyzeTimeSeriesTest {
 				+ "\nreturn kurt = " + stat.kurtosis(returns) //
 				+ Ints_ //
 						.of(1, 2, 4, 8, 16, 32) //
-						.map(d -> "\nmean reversion ols, " + d + " days = " + ts.meanReversion(prices, d).coefficients[0]) //
+						.map(d -> "\nmean reversion ols [" + d + "d] = " + ts.meanReversion(prices, d).coefficients[0]) //
 						.collect(As::joined) //
 				+ Ints_ //
 						.of(4, 16) //
-						.map(d -> "\nvariance ratio, " + d + " days over 1 day = " + ts.varianceRatio(prices, d)) //
+						.map(d -> "\nvariance ratio [" + d + "d over 1d] = " + ts.varianceRatio(prices, d)) //
 						.collect(As::joined) //
 				+ "\nhold " + buySell(d -> 1d).invest(prices) //
 				+ "\nkelly " + buySell(d -> kelly).invest(prices) //
 				+ "\nma200 trend " + mat.invest(prices) //
 				+ Ints_ //
 						.range(1, 8) //
-						.map(d -> "\nrevert " + d + " " + reverts[d].invest(prices)) //
+						.map(d -> "\nrevert [" + d + "d] " + reverts[d].invest(prices)) //
 						.collect(As::joined) //
 				+ Ints_ //
 						.range(1, 8) //
-						.map(d -> "\nrevert " + d + " long-only " + reverts[d].longOnly().invest(prices)) //
+						.map(d -> "\nrevert [" + d + "d] long-only " + reverts[d].longOnly().invest(prices)) //
 						.collect(As::joined) //
 				+ "\ntanh " + tanh.invest(prices) //
 				+ "\ntimed " + mt_.invest(prices) //
