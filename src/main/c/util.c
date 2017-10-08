0edae40c939c8c2997a68d9330541f3dc55b5455
stupidsing <stupidsing@gmail.com>
2017-10-08 11:09:19 +0800

    refactor

--- src/main/c/util.c
+++ src/main/c/util.c
@@ -23,43 +23,7 @@ int fmsg(char *t, char *fn, int line, char *m) {
 #define err(m) msg("ERROR", __FILE__, __LINE__, (m))
 #define fatal(m) fmsg("FATAL", __FILE__, __LINE__, (m))
 
-int nAllocs;
-
-void meminit() {
-	nAllocs = 0;
-}
-
-void memdeinit() {
-	!nAllocs || err("some memory not freed");
-}
-
-void *memalloc_(int size) {
-	void *p = malloc(size);
-	if(p) return p;
-	else err("out of memory");
-}
-
-void memrealloc(void *p, int size) {
-	void **p1 = (void**) p;
-	if(*p1 = realloc(*p1, size)) return;
-	else err("out of memory");
-}
-
-void *memalloc(int size) {
-	nAllocs++;
-	return memalloc_(size);
-}
-
-void *memalloczeroed(int size) {
-	void *p = memalloc(size);
-	memset(p, 0, size);
-	return p;
-}
-
-void memfree(void *p) {
-	free(p);
-	nAllocs--;
-}
+#include "mem.c"
 
 char *substr(char *start, char *end) {
 	char *result = memalloc(end - start + 1), *s = start, *d = result;
