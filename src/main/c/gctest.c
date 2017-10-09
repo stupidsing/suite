e725561a19f7702618c7b447e6df622217787beb
ywsing <stupidsing@gmail.com>
2017-10-09 10:31:05 +0800

    refactor

--- src/main/c/gctest.c
+++ src/main/c/gctest.c
@@ -1,39 +1,12 @@
 // gcc -std=c99 -g src/main/c/gctest.c -o target/gctest && target/gctest
 
-#include <ctype.h>
-
-#include "gc.c"
-
-typedef struct TestObject TestObject;
-
-struct TestObject {
-	int type;
-	TestObject *left;
-	TestObject *right;
-};
-
-int testRefOffsets[] = { offsetof(TestObject, left), offsetof(TestObject, right), 0 };
-
-int *testRefOffsetsFunction(GcObject *object) { return testRefOffsets; }
-
-GcClass testClass = {
-	.name = "testClass",
-	.size = sizeof(TestObject),
-	.refoffsets = &testRefOffsetsFunction,
-};
-
-TestObject *newTest(int type, TestObject *left, TestObject *right) {
-	TestObject *test = gcalloc(&testClass);
-	test->type = type;
-	test->left = left;
-	test->right = right;
-}
+#include "cons.c"
 
 int main(int argc, char **argv) {
 	gcinit();
-	TestObject *t0 = newTest(0, 0, 0);
-	TestObject *t1 = newTest(0, 0, 0);
-	TestObject *t2 = newTest(0, t0, t1);
-	TestObject *t3 = newTest(0, 0, t2);
+	Cons *t0 = newCons(0, 0, 0);
+	Cons *t1 = newCons(0, 0, 0);
+	Cons *t2 = newCons(0, t0, t1);
+	Cons *t3 = newCons(0, 0, t2);
 	gcdeinit();
 }
