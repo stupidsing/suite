2abee4ca3b38fe17d4a2e0d7bd666aee5dda6945
ywsing <stupidsing@gmail.com>
2017-10-19 10:50:32 +0800

    refactor

--- src/main/c/list.c
+++ src/main/c/list.c
@@ -31,17 +31,4 @@ void listadd(List *list, void *item) {
 	list->items[loc] = item;
 }
 
-#define listmap(list, item0, item1) { \
-	for(int i##__LINE__ = 0; i##__LINE__ < list.size; i##__LINE__++) { \
-		void **p = list.items + [i##__LINE__]; void *item0 = *p; *p = item1
-	} \
-}
-
-#define listfilter(list, item, pred) { \
-	for(int si##__LINE__ = 0, di##__LINE__ = 0; si##__LINE__ < list.size; si##__LINE__++) { \
-		void *item = list.items[si##__LINE__]; int b = (pred); if(b) list.items[di##__LINE__++] = item; \
-	} \
-	list.size = di##__LINE__; \
-}
-
 #endif
