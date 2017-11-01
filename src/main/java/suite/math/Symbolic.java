fa07ed3403b876f57567b2cf27a4685f8a47740a
stupidsing <stupidsing@gmail.com>
2017-11-01 22:21:48 +0800

    refactor

--- src/main/java/suite/math/Symbolic.java
+++ src/main/java/suite/math/Symbolic.java
@@ -39,10 +39,10 @@ public class Symbolic {
 	}
 
 	private class Rewrite {
-		private Node var;
+		private Node x;
 
-		private Rewrite(Node var) {
-			this.var = var;
+		private Rewrite(Node x) {
+			this.x = x;
 		}
 
 		// allowed:
@@ -60,11 +60,11 @@ public class Symbolic {
 			Tree tree;
 			Node[] m;
 			if ((m = Suite.match(".0 - .1").apply(node)) != null && m[0] != N0)
-				return Suite.match(".0 + neg .1").substitute(rewrite(m[0]), rewrite(m[1]));
+				return add.apply(rewrite(m[0]), matchNeg.substitute(rewrite(m[1])));
 			else if ((m = Suite.match(".0 / .1").apply(node)) != null && m[0] != N1)
-				return Suite.match(".0 * inv .1").substitute(rewrite(m[0]), rewrite(m[1]));
+				return mul.apply(rewrite(m[0]), matchInv.substitute(rewrite(m[1])));
 			else if ((m = matchPow.apply(node)) != null)
-				return Suite.match("exp (ln .0 * .1)").substitute(m[0], m[1]);
+				return matchExp.substitute(matchLn.substitute(rewrite(m[0])), rewrite(m[1]));
 			else if (node instanceof Int)
 				return intOf((Int) node);
 			else if ((m = Suite.match(".0 .1").apply(node)) != null)
@@ -105,13 +105,13 @@ public class Symbolic {
 						return pos(m[0]).map(matchLn::substitute);
 					else if ((m = Suite.match("sin (.0 + .1)").apply(node_)) != null)
 						return Read.each( //
-								Suite.substitute("sin .0 * cos .1", m), //
-								Suite.substitute("cos .0 * sin .1", m)) //
+								mul.apply(matchSin.substitute(m[0]), matchCos.substitute(m[1])), //
+								mul.apply(matchCos.substitute(m[0]), matchSin.substitute(m[1]))) //
 								.map(this::productOfSums);
 					else if ((m = Suite.match("cos (.0 + .1)").apply(node_)) != null)
 						return Read.each( //
-								Suite.substitute("cos .0 * cos .1", m), //
-								Suite.substitute("neg sin .0 * sin .1", m)) //
+								mul.apply(matchCos.substitute(m[0]), matchCos.substitute(m[1])), //
+								mul.apply(matchNeg.substitute(matchSin.substitute(m[0])), matchSin.substitute(m[1]))) //
 								.map(this::productOfSums);
 					else if (node_ instanceof Tree)
 						return Read.each(productOfSums(node_));
@@ -120,11 +120,11 @@ public class Symbolic {
 				}
 
 				private Node productOfSums(Node node) {
-					return mul.recompose(var, pos(node));
+					return mul.recompose(x, pos(node));
 				}
 
 				private Node sumOfProducts(Node node) {
-					return add.recompose(var, sop(node));
+					return add.recompose(x, sop(node));
 				}
 			}
 
@@ -151,11 +151,11 @@ public class Symbolic {
 								sum = add.apply(mul.apply(ps0[j], ps1[i - j]), sum);
 							return sum;
 						});
-					} else if (node == var)
+					} else if (node.compareTo(x) == 0)
 						return new Node[] { N0, N1, };
 					else if (node == N0)
 						return new Node[] {};
-					else if (!isContainsVariable(node))
+					else if (!isContains_x(node))
 						return new Node[] { node, };
 					else
 						throw new PolynomializeException();
@@ -168,7 +168,7 @@ public class Symbolic {
 
 			for (Node child : nodes) {
 				sum = add.apply(mul.apply(child, power), sum);
-				power = mul.apply(var, power);
+				power = mul.apply(x, power);
 			}
 
 			return sum;
@@ -177,22 +177,22 @@ public class Symbolic {
 		private Node d(Node node) { // differentiation
 			Node[] m;
 			if ((m = matchAdd.apply(node)) != null)
-				return matchAdd.substitute(d(m[0]), d(m[1]));
+				return add.apply(d(m[0]), d(m[1]));
 			else if ((m = matchNeg.apply(node)) != null)
 				return matchNeg.substitute(d(m[0]));
 			else if ((m = matchMul.apply(node)) != null)
-				return Suite.match(".0 * .1 + .2 * .3").substitute(m[0], d(m[1]), m[1], d(m[0]));
+				return add.apply(mul.apply(m[0], d(m[1])), mul.apply(m[1], d(m[0])));
 			else if ((m = matchInv.apply(node)) != null)
-				return Suite.match("inv (.0 * .0) * neg .1").substitute(m[0], d(m[0]));
+				return mul.apply(matchInv.substitute(mul.apply(m[0], m[0])), matchNeg.substitute(d(m[0])));
 			else if ((m = matchExp.apply(node)) != null)
-				return Suite.match("exp .0 * .1").substitute(m[0], d(m[0]));
+				return mul.apply(matchExp.substitute(m[0]), d(m[0]));
 			else if ((m = matchLn.apply(node)) != null)
-				return Suite.match("inv .0 * .1").substitute(m[0], d(m[0]));
+				return mul.apply(matchInv.substitute(m[0]), d(m[0]));
 			else if ((m = Suite.match("sin .0").apply(node)) != null)
-				return Suite.match("cos .0 * .1").substitute(m[0], d(m[0]));
+				return mul.apply(matchCos.substitute(m[0]), d(m[0]));
 			else if ((m = Suite.match("cos .0").apply(node)) != null)
-				return Suite.match("neg sin .0 * .1").substitute(m[0], d(m[0]));
-			else if (node == var)
+				return mul.apply(matchNeg.substitute(matchSin.substitute(m[0])), d(m[0]));
+			else if (node == x)
 				return N1;
 			else if (node instanceof Int)
 				return N0;
@@ -200,11 +200,40 @@ public class Symbolic {
 				throw new RuntimeException();
 		}
 
-		private boolean isContainsVariable(Node node) {
+		private Streamlet<Node> i(Node node) { // integration
+			Node[] m;
+			if ((m = matchAdd.apply(node)) != null) {
+				Streamlet<Node> iudxs = i(m[0]);
+				Streamlet<Node> ivdxs = i(m[1]);
+				return iudxs.join2(ivdxs).map(add::apply);
+			} else if ((m = matchNeg.apply(node)) != null)
+				return i(m[0]).map(matchNeg::substitute);
+			else if ((m = matchMul.apply(node)) != null) {
+				Node u = m[0];
+				Streamlet<Node> vs = i(m[1]);
+				Node dudx = d(u);
+				return vs.concatMap(v -> i(mul.apply(v, dudx)).map(ivdu -> add.apply(mul.apply(u, v), matchNeg.substitute(ivdu))));
+			} else if ((m = matchInv.apply(node)) != null && m[0].compareTo(x) == 0)
+				return Read.each(Suite.match("ln .0").substitute(x));
+			else if ((m = matchExp.apply(node)) != null && m[0].compareTo(x) == 0)
+				return Read.each(node);
+			else if ((m = Suite.match("sin .0").apply(node)) != null && m[0].compareTo(x) == 0)
+				return Read.each(matchNeg.substitute(matchCos.apply(x)));
+			else if ((m = Suite.match("cos .0").apply(node)) != null && m[0].compareTo(x) == 0)
+				return Read.each(matchSin.apply(x));
+			else if (node.compareTo(x) == 0)
+				return Read.each(mul.apply(matchInv.substitute(Int.of(2)), mul.apply(x, x)));
+			else if (node instanceof Int)
+				return Read.each(mul.apply(node, x));
+			else
+				return Read.empty();
+		}
+
+		private boolean isContains_x(Node node) {
 			Tree tree = Tree.decompose(node);
 			return tree != null //
-					? isContainsVariable(tree.getLeft()) || isContainsVariable(tree.getRight()) //
-					: node == var;
+					? isContains_x(tree.getLeft()) || isContains_x(tree.getRight()) //
+					: node == x;
 		}
 	}
 
@@ -222,7 +251,7 @@ public class Symbolic {
 			this.e = e;
 		}
 
-		private Node recompose(Node var, Streamlet<Node> nodes0) {
+		private Node recompose(Node x, Streamlet<Node> nodes0) {
 			List<Node> list = new ArrayList<>();
 			int xn = 0;
 			Node constant = e;
@@ -234,13 +263,13 @@ public class Symbolic {
 					constant = apply(child, constant);
 				else if ((m = matchNeg.apply(child)) != null && (n = m[0]) instanceof Int)
 					constant = apply(Int.of(-((Int) n).number), constant);
-				else if (child == var)
+				else if (child.compareTo(x) == 0)
 					xn++;
 				else
 					list.add(child);
 
 			for (int i = 0; i < xn; i++)
-				list.add(var);
+				list.add(x);
 
 			if (e != constant)
 				list.add(intOf(constant));
@@ -284,5 +313,7 @@ public class Symbolic {
 	private Match matchPow = Suite.match(".0^.1");
 	private Match matchExp = Suite.match("exp .0");
 	private Match matchLn = Suite.match("ln .0");
+	private Match matchSin = Suite.match("sin .0");
+	private Match matchCos = Suite.match("cos .0");
 
 }
