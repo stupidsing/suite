2628b5fe496c85221201175e2c1581a5c87f3c4b
stupidsing <stupidsing@gmail.com>
2017-11-05 10:34:03 +0800

    refactor

--- src/test/java/suite/os/ElfTest.java
+++ src/test/java/suite/os/ElfTest.java
@@ -14,8 +14,28 @@ public class ElfTest {
 	@Test
 	public void test() {
 		Execute exec = test("" //
-				+ "define id := i => i + 1 >>\n" //
+				+ "define linux-read := `buffer, length` => (\n" //
+				+ "     type buffer = address (256 * array byte _) >>\n" //
+				+ "     type length = 0 >>\n" //
+				+ "     asm (ECX = buffer; EDX = length;) {\n" //
+				+ "             MOV (EAX, 3);\n" //
+				+ "             XOR (EBX, EBX);\n" //
+				+ "             INT (-128);\n" //
+				+ "             -- length in EAX\n" //
+				+ "     }\n" //
+				+ ") >>\n" //
+				+ "define linux-write := `buffer, length` => (\n" //
+				+ "     type buffer = address (256 * array byte _) >>\n" //
+				+ "     type length = 0 >>\n" //
+				+ "     asm (ECX = buffer; EDX = length;) {\n" //
+				+ "             MOV (EAX, 4);\n" //
+				+ "             MOV (EBX, 1);\n" //
+				+ "             INT (-128);\n" //
+				+ "             -- length in EAX\n" //
+				+ "     }\n" //
+				+ ") >>\n" //
 				+ "iterate v 0 (v < 100) (v + 1)\n");
+
 		assertEquals(100, exec.code);
 		assertEquals("", exec.out);
 	}
