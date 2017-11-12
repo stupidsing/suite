aae5c5e2d93ac21cf11a4e674dbe4b9845b1cbbb
stupidsing <stupidsing@gmail.com>
2017-11-12 10:49:49 +0800

    refactor

--- src/main/java/suite/util/AutoInterface.java
+++ src/main/java/suite/util/AutoInterface.java
@@ -4,12 +4,13 @@ import suite.util.FunUtil.Fun;
 
 public interface AutoInterface<T> {
 
-	public default <U extends T> U cast(Class<U> clazz) {
-		return clazz.isInstance(this) ? clazz.cast(this) : null;
+	public default <U extends T, V> V cast(Class<U> clazz, Fun<U, V> fun) {
+		U u = cast(clazz);
+		return u != null ? fun.apply(u) : null;
 	}
 
-	public default <U extends T, V> V cast(Class<U> clazz, Fun<U, V> fun) {
-		return clazz.isInstance(this) ? fun.apply(clazz.cast(this)) : null;
+	public default <U extends T> U cast(Class<U> clazz) {
+		return clazz.isInstance(this) ? clazz.cast(this) : null;
 	}
 
 }
