d4cc4b8c09df5e582eae329021e140832ab78514
ywsing <stupidsing@gmail.com>
2017-11-29 17:08:16 +0800

    refactor

--- src/test/java/suite/trade/data/AastocksTest.java
+++ src/test/java/suite/trade/data/AastocksTest.java
@@ -11,14 +11,12 @@ public class AastocksTest {
 	@Test
 	public void testHsi() {
 		float hsi = aa.hsi();
-		System.out.println(hsi);
 		assertTrue(20000f < hsi);
 	}
 
 	@Test
 	public void testQuote() {
 		float quote = aa.quote("0005.HK");
-		System.out.println(quote);
 		assertTrue(70f < quote);
 	}
 
