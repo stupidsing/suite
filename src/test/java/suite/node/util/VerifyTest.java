ae897cc94321d4a1c168bf80a8c7940ca17da59a
Sing Yiu Wah <yw.sing_contractor@hactl.com>
2017-12-01 10:13:15 +0900

    refactor

--- src/test/java/suite/node/util/VerifyTest.java
+++ src/test/java/suite/node/util/VerifyTest.java
@@ -1,17 +1,11 @@
 package suite.node.util;
 
-import static java.util.Map.entry;
-
-import java.util.Map;
-import java.util.Map.Entry;
-
 import org.junit.Test;
 
 import suite.Suite;
-import suite.adt.pair.Fixie_.FixieFun3;
+import suite.immutable.IMap;
 import suite.lp.Trail;
 import suite.lp.doer.Binder;
-import suite.lp.doer.Generalizer;
 import suite.node.Atom;
 import suite.node.Node;
 import suite.node.Tree;
@@ -19,59 +13,56 @@ import suite.node.io.TermOp;
 
 public class VerifyTest {
 
-	private class Rule {
-		private Node t0, t1;
-
-		private Rule(Node t0, Node t1) {
-			this.t0 = t0;
-			this.t1 = t1;
-		}
-	}
-
 	@Test
 	public void test() {
-		FixieFun3<String, String, String, Entry<String, Rule>> e = (name, head, tail) -> entry(name,
-				new Rule(Suite.parse(head), Suite.parse(tail)));
+		IMap<String, Node> rules = IMap //
+				.<String, Node>empty() //
+				.put("nat.0", Suite.parse("true => is-nat 0")) //
+				.put("nat.1", Suite.parse("is-nat N => is-nat (succ N)"));
 
-		Map<String, Rule> rules = Map.ofEntries( //
-				e.apply("nat.0", "true", "is-nat 0"), //
-				e.apply("nat.1", "is-nat .n", "is-nat (succ .n)"));
-
-		new Verify(rules).verify(Suite.parse("axiom nat.0 | imply nat.1"));
+		System.out.println(new Verify(rules).verify(Suite.parse("axiom nat.0 | satisfy (nat.1 | subst N 0)")));
 	}
 
 	private class Verify {
-		private Map<String, Rule> rules;
+		private IMap<String, Node> rules;
 
-		private Verify(Map<String, Rule> rules) {
+		private Verify(IMap<String, Node> rules) {
 			this.rules = rules;
 		}
 
 		private Node verify(Node proof) {
-			Node[] m;
+			Node[] m, m1;
 			if ((m = Suite.match(".0 && .1").apply(proof)) != null)
 				return Tree.of(TermOp.BIGAND, verify(m[0]), verify(m[1]));
 			else if ((m = Suite.match("axiom .0").apply(proof)) != null)
-				return verify(Suite.substitute("true | imply .0", m));
-			else if ((m = Suite.match(".0 | imply .1").apply(proof)) != null) {
-				Rule rule = rules.get(((Atom) m[1]).name);
-				Generalizer generalizer = new Generalizer();
-				bind(verify(m[0]), generalizer.generalize(rule.t0));
-				return generalizer.generalize(rule.t1);
-			} else if ((m = Suite.match(".0 | subst .1 .2").apply(proof)) != null) {
-				Generalizer generalizer = new Generalizer();
-				Node node = generalizer.generalize(verify(m[0]));
-				bind(generalizer.generalize(m[1]), m[2]);
-				return node;
-			} else if ((m = Suite.match("true").apply(proof)) != null)
+				return verify(Suite.substitute("true | satisfy .0", m));
+			else if ((m = Suite.match("lemma .0 := .1 >> .2").apply(proof)) != null)
+				return new Verify(rules.put(((Atom) m[0]).name, verify(m[1]))).verify(m[2]);
+			else if ((m = Suite.match(".0 | subst .1 .2").apply(proof)) != null)
+				return replace(verify(m[0]), m[1], m[2]);
+			else if ((m = Suite.match(".0 | satisfy .1").apply(proof)) != null)
+				if ((m1 = Suite.match(".0 => .1").apply(verify(m[1]))) != null && Binder.bind(verify(m[0]), m1[0], new Trail()))
+					return m1[1];
+				else
+					throw new RuntimeException();
+			else if ((m = Suite.match("true").apply(proof)) != null)
 				return Atom.TRUE;
+			else if (proof instanceof Atom)
+				return rules.get(((Atom) proof).name);
 			else
 				throw new RuntimeException();
 		}
 
-		private void bind(Node n0, Node n1) {
-			if (!Binder.bind(n0, n1, new Trail()))
-				throw new RuntimeException();
+		private Node replace(Node node, Node from, Node to) {
+			return new Object() {
+				private Node replace(Node node_) {
+					Tree tree;
+					if ((tree = Tree.decompose(node_)) != null)
+						return Tree.of(tree.getOperator(), replace(tree.getLeft()), replace(tree.getRight()));
+					else
+						return node_ == from ? to : node_;
+				}
+			}.replace(node);
 		}
 	}
 
