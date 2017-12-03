864ecc95eed380049b2a2efe5a6e2ccc5d38052f
stupidsing <stupidsing@gmail.com>
2017-12-03 09:45:39 +0800

    refactor

--- src/test/java/suite/node/util/VerifyTest.java
+++ src/test/java/suite/node/util/VerifyTest.java
@@ -8,10 +8,13 @@ import suite.Suite;
 import suite.immutable.IMap;
 import suite.lp.Trail;
 import suite.lp.doer.Binder;
+import suite.lp.doer.Generalizer;
 import suite.node.Atom;
 import suite.node.Node;
+import suite.node.Reference;
 import suite.node.Tree;
 import suite.node.io.TermOp;
+import suite.util.FunUtil2.Fun2;
 import suite.util.FunUtil2.Sink2;
 
 /**
@@ -23,41 +26,61 @@ public class VerifyTest {
 
 	@Test
 	public void test() {
+		Fun2<String, String, Definition> def = (t0, t1) -> new Definition(Suite.parse(t0), Suite.parse(t1));
+
+		IMap<String, Definition> defs = IMap //
+				.<String, Definition> empty() //
+				.put("group", def.apply("group .isElem .zero .op", "" //
+						+ ".isElem .zero, \n" //
+						+ "is.op2 .isElem .op, \n" //
+						+ "(.isElem P0 => .op/.zero/P0 = .zero), \n" //
+						+ "(.isElem P0, .isElem P1, .isElem P2 => .op/P0/(.op/P1/P2) = .op/(.op/P0/P1)/P2), \n" //
+						+ "true"));
+
 		IMap<String, Node> rules = IMap //
 				.<String, Node> empty() //
 				.put("and.0", Suite.parse("P, Q => P")) //
-				.put("and.1", Suite.parse("P, Q => Q")) //
-				.put("op1.0", Suite.parse("is.op1 P Op, IsElem P0 => IsElem (Op P0)")) //
-				.put("op2.0", Suite.parse("is.op2 P Op, IsElem P1 => is.op1 IsElem (Op P1)")) //
+				.put("and.1", Suite.parse("P, Q, R => Q")) //
+				.put("and.2", Suite.parse("P, Q, R, S => R")) //
+				.put("and.3", Suite.parse("P, Q, R, S, T => S")) //
+				.put("op1.0", Suite.parse("is.op1 P Op, IsElem P0 => IsElem Op/P0")) //
+				.put("op2.0", Suite.parse("is.op2 P Op, IsElem P1 => is.op1 IsElem Op/P1")) //
 				.put("eq.0", Suite.parse("A = B => B = A")) //
 				.put("eq.1", Suite.parse("A = B, B = C => A = C")) //
-				.put("grp.0", Suite.parse("is.group IsElem Zero Op => IsElem Zero")) //
-				.put("grp.1", Suite.parse("is.group IsElem Zero Op => is.op2 IsElem Op")) //
-				.put("grp.2", Suite.parse("is.group IsElem Zero Op, IsElem P0 => Op Zero P0 = Zero")) //
-				.put("grp.3", Suite.parse("is.group IsElem Zero Op, IsElem P0, IsElem P1, IsElem P2 => " //
-						+ "Op P0 (Op P1 P2) = Op (Op P0 P1) P2")) //
-				.put("nat.0", Suite.parse("true => is.group is.nat 0 ADD")) //
+				.put("nat.0", Suite.parse("true => group is.nat 0 ADD")) //
 				.put("nat.1", Suite.parse("is.nat N => is.nat (succ N)")) //
 				.put("nat.add.0", Suite.parse("is.nat N => 0 + N = N")) //
 				.put("nat.add.1", Suite.parse("is.nat N => M + succ N = succ (M + N)"));
 
 		Sink2<String, String> test = (expect, proof) -> {
 			Node expect_ = Suite.parse(expect);
-			Node proven = new Verify(rules).verify(Suite.parse(proof));
+			Node proven = new Verify(defs, rules).verify(Suite.parse(proof));
 			System.out.println("proven :: " + proven);
 			assertTrue(Binder.bind(expect_, proven, new Trail()));
 		};
 
-		String p = "lemma is.nat.zero := axiom nat.0 | satisfy (grp.0 {IsElem:is.nat} {Zero:0} {Op:ADD}) >> ";
+		String p = "lemma is.nat.zero := axiom nat.0 | expand group | satisfy (and.0 {P, Q,}) >> ";
 
 		test.sink2("is.nat 0", p + "is.nat.zero");
 		test.sink2("is.nat succ 0", p + "is.nat.zero | satisfy (nat.1 {N:0})");
 	}
 
+	private class Definition {
+		private Node t0;
+		private Node t1;
+
+		private Definition(Node t0, Node t1) {
+			this.t0 = t0;
+			this.t1 = t1;
+		}
+	}
+
 	private class Verify {
+		private IMap<String, Definition> defs;
 		private IMap<String, Node> rules;
 
-		private Verify(IMap<String, Node> rules) {
+		private Verify(IMap<String, Definition> defs, IMap<String, Node> rules) {
+			this.defs = defs;
 			this.rules = rules;
 		}
 
@@ -65,18 +88,28 @@ public class VerifyTest {
 			Node[] m, m1;
 			if ((m = Suite.match(".0, .1").apply(proof)) != null)
 				return Tree.of(TermOp.AND___, verify(m[0]), verify(m[1]));
+			else if ((m = Suite.match(".0 {}").apply(proof)) != null)
+				return verify(m[0]);
+			else if ((m = Suite.match(".0 {.1, .2}").apply(proof)) != null)
+				return replace(verify(Suite.substitute(".0 {.1}", m[0], m[2])), m[1], new Reference());
 			else if ((m = Suite.match(".0 {.1:.2}").apply(proof)) != null)
 				return replace(verify(m[0]), m[1], m[2]);
 			else if ((m = Suite.match("axiom .0").apply(proof)) != null)
 				return verify(Suite.substitute("true | satisfy .0", m));
 			else if ((m = Suite.match("contradict .0:.1 >> .2").apply(proof)) != null)
-				if (Binder.bind(new Verify(rules.put(((Atom) m[0]).name, m[1])).verify(m[2]), Atom.FALSE, new Trail()))
+				if (Binder.bind(new Verify(defs, rules.put(name(m[0]), m[1])).verify(m[2]), Atom.FALSE, new Trail()))
 					return Suite.substitute("not .0", m[1]);
 				else
 					throw new RuntimeException();
-			else if ((m = Suite.match("lemma .0 := .1 >> .2").apply(proof)) != null)
-				return new Verify(rules.put(((Atom) m[0]).name, verify(m[1]))).verify(m[2]);
-			else if ((m = Suite.match(".0 | satisfy .1").apply(proof)) != null)
+			else if ((m = Suite.match(".0 | expand .1").apply(proof)) != null) {
+				Definition def = defs.get(name(m[1]));
+				return replaceBind(verify(m[0]), def.t0, def.t1);
+			} else if ((m = Suite.match("lemma .0 := .1 >> .2").apply(proof)) != null)
+				return new Verify(defs, rules.put(name(m[0]), verify(m[1]))).verify(m[2]);
+			else if ((m = Suite.match(".0 | rexpand .1").apply(proof)) != null) {
+				Definition def = defs.get(name(m[1]));
+				return replaceBind(verify(m[0]), def.t1, def.t0);
+			} else if ((m = Suite.match(".0 | satisfy .1").apply(proof)) != null)
 				if ((m1 = Suite.match(".0 => .1").apply(verify(m[1]))) != null && Binder.bind(verify(m[0]), m1[0], new Trail()))
 					return m1[1];
 				else
@@ -84,9 +117,31 @@ public class VerifyTest {
 			else if ((m = Suite.match("true").apply(proof)) != null)
 				return Atom.TRUE;
 			else if (proof instanceof Atom)
-				return rules.get(((Atom) proof).name);
+				return rules.get(name(proof));
 			else
 				throw new RuntimeException("cannot verify " + proof);
+		}
+
+		private Node replaceBind(Node node, Node from, Node to) {
+			return new Object() {
+				private Node replace(Node node_) {
+					Generalizer generalizer = new Generalizer();
+					Node t0 = generalizer.generalize(from);
+					Node t1 = generalizer.generalize(to);
+					Trail trail = new Trail();
+
+					if (Binder.bind(node_, t0, trail))
+						return t1;
+					else
+						trail.unwindAll();
+
+					Tree tree = Tree.decompose(node_);
+
+					return tree != null //
+							? Tree.of(tree.getOperator(), replace(tree.getLeft()), replace(tree.getRight())) //
+							: node_;
+				}
+			}.replace(node);
 		}
 
 		private Node replace(Node node, Node from, Node to) {
@@ -99,6 +154,10 @@ public class VerifyTest {
 						return node_ == from ? to : node_;
 				}
 			}.replace(node);
+		}
+
+		private String name(Node node) {
+			return ((Atom) node).name;
 		}
 	}
 
