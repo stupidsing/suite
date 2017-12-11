f34492e149c48fb952dc9d6462e12e5050b676fb
Sing Yiu Wah <yw.sing_contractor@hactl.com>
2017-12-11 13:24:50 +0900

    refactor

--- src/main/java/suite/math/stat/Kalman.java
+++ src/main/java/suite/math/stat/Kalman.java
@@ -9,13 +9,13 @@ public class Kalman {
 	private Vector_ vec = new Vector_();
 
 	private int stateSize = 16;
-	private float[][] F; // state transition
-	private float[][] B; // input control
-	private float[][] H; // observation
-	private float[][] Q; // state noise
-	private float[][] R; // observation noise
-	private float[][] estimatedCovariance0;
-	private float[] estimatedState0;
+	private float[][] F; // state transition (stateSize x stateSize)
+	private float[][] B; // input control (stateSize x inputSize)
+	private float[][] H; // observation (observeSize x stateSize)
+	private float[][] Q; // state noise (stateSize x stateSize)
+	private float[][] R; // observation noise (observeSize x observeSize)
+	private float[][] estimatedStateCov0; // (stateSize x stateSize)
+	private float[] estimatedState0; // estimated state (stateSize)
 
 	// hidden equations:
 	// state1 = F * state0 + B * input0 + noise(0, Q)
@@ -27,16 +27,21 @@ public class Kalman {
 		float[][] Ht = mtx.transpose(H);
 
 		// predict
+		// predicted next state (stateSize)
+		// predicted next state covariance (stateSize x stateSize)
 		float[] predictedState1 = vec.add(mtx.mul(F, estimatedState0), mtx.mul(B, input0));
-		float[][] predictedCovariance1 = mtx.add(mul(F, estimatedCovariance0, Ft), Q);
+		float[][] predictedStateCov1 = mtx.add(mul(F, estimatedStateCov0, Ft), Q);
 
 		// update
-		float[][] kalmanGain = mul(predictedCovariance1, Ht, mtx.inverse(mtx.add(R, mul(H, predictedCovariance1, Ht))));
+		// Kalman gain (stateSize x observeSize)
+		// estimated next state (stateSize)
+		// estimated next state covariance (stateSize x stateSize)
+		float[][] kalmanGain = mul(predictedStateCov1, Ht, mtx.inverse(mtx.add(R, mul(H, predictedStateCov1, Ht))));
 		float[] estimatedState1 = vec.add(predictedState1, mtx.mul(kalmanGain, vec.sub(observed0, mtx.mul(H, predictedState1))));
-		float[][] estimatedCovariance1 = mtx.mul(mtx.add(identity, mtx.neg(mtx.mul(kalmanGain, H))), predictedCovariance1);
+		float[][] estimatedStateCov1 = mtx.mul(mtx.add(identity, mtx.neg(mtx.mul(kalmanGain, H))), predictedStateCov1);
 		// residual1 = observed0 - H * estimatedState1
 
-		estimatedCovariance0 = estimatedCovariance1;
+		estimatedStateCov0 = estimatedStateCov1;
 		estimatedState0 = estimatedState1;
 	}
 
