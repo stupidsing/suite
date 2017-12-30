b277beebcb5050690aaafd09d1c46c7763c2580c
stupidsing <stupidsing@gmail.com>
2017-12-30 12:52:17 +0800

    refactor

---
+++ src/test/java/suite/adt/map/IntObjMapTest.java
@@ -0,0 +1,40 @@
+package suite.adt.map;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import suite.primitive.IntPrimitives.IntObjSource;
+import suite.primitive.adt.map.IntObjMap;
+import suite.primitive.adt.pair.IntObjPair;
+
+public class IntObjMapTest {
+
+	@Test
+	public void test() {
+		IntObjMap<Integer> map = new IntObjMap<>();
+		map.put(1, 2);
+		map.put(3, 4);
+		map.put(5, 6);
+		assertEquals(Integer.valueOf(2), map.get(1));
+		assertEquals(Integer.valueOf(4), map.get(3));
+		assertEquals(Integer.valueOf(6), map.get(5));
+
+		Set<String> expected = new HashSet<>();
+		expected.add("1:2");
+		expected.add("3:4");
+		expected.add("5:6");
+
+		Set<String> actual = new HashSet<>();
+
+		IntObjSource<Integer> source = map.source();
+		IntObjPair<Integer> pair = IntObjPair.of(0, 0);
+		while (source.source2(pair))
+			actual.add(pair.t0 + ":" + pair.t1);
+		assertEquals(expected, actual);
+	}
+
+}
