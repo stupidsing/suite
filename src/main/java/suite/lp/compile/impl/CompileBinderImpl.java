9e0ec00b4a1fddb0641208760503fa60be40bbac
stupidsing <stupidsing@gmail.com>
2018-01-06 21:39:36 +0800

    refactor

--- src/main/java/suite/lp/compile/impl/CompileBinderImpl.java
+++ src/main/java/suite/lp/compile/impl/CompileBinderImpl.java
@@ -1,16 +1,10 @@
 package suite.lp.compile.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.HashMap;
 
-import org.apache.bcel.generic.Type;
-
+import suite.jdk.gen.FunCreator;
 import suite.jdk.gen.FunExpression.FunExpr;
 import suite.jdk.gen.FunFactory;
-import suite.jdk.lambda.LambdaImplementation;
-import suite.jdk.lambda.LambdaInstance;
-import suite.jdk.lambda.LambdaInterface;
 import suite.lp.doer.Binder;
 import suite.lp.doer.BinderFactory;
 import suite.node.Atom;
@@ -20,15 +14,14 @@ import suite.node.Reference;
 import suite.node.Str;
 import suite.node.Tree;
 import suite.node.Tuple;
-import suite.node.io.Operator;
-import suite.streamlet.Read;
-import suite.util.FunUtil.Fun;
-import suite.util.FunUtil.Iterate;
+import suite.node.io.SwitchNode;
+import suite.util.FunUtil2.BinOp;
 
 public class CompileBinderImpl extends CompileClonerImpl implements BinderFactory {
 
 	private static FunFactory f = new FunFactory();
-	private static LambdaInterface<Bind_> lambdaClass = LambdaInterface.of(Bind_.class, "test");
+	private static FunExpr fail = f._false();
+	private static FunExpr ok = f._true();
 
 	private boolean isBindTrees;
 
@@ -41,131 +34,69 @@ public class CompileBinderImpl extends CompileClonerImpl implements BinderFactor
 	}
 
 	public Bind_ binder(Node node) {
-		return compileBind_(node).newFun();
-	}
+		FunCreator<Bind_> fc = FunCreator.of(Bind_.class, false);
 
-	private LambdaInstance<Bind_> compileBind_(Node node) {
-		Tree tree;
+		return fc.create(new BinOp<>() {
+			private FunExpr env, trail, b;
 
-		if (node instanceof Atom)
-			return compileBindAtom((Atom) node);
-		else if (node instanceof Int)
-			return compileBindInt((Int) node);
-		else if (node instanceof Reference) {
-			int index = computeIndex(node);
-			return compileBindPredicate((be, n) -> Binder.bind(n, be.env.get(index), be.trail));
-		} else if (node instanceof Str)
-			return compileBindStr((Str) node);
-		else if ((tree = Tree.decompose(node)) != null) {
-			Operator operator = tree.getOperator();
-			LambdaInstance<Bind_> lambda0 = compileBind_(tree.getLeft());
-			LambdaInstance<Bind_> lambda1 = compileBind_(tree.getRight());
-			Fun<FunExpr, Iterate<FunExpr>> bindRef = bindRef(cloner(node));
+			public FunExpr apply(FunExpr bindEnv, FunExpr target) {
+				this.env = bindEnv.field("env");
+				this.trail = bindEnv.field("trail");
+				return f.declare(ok, b -> {
+					this.b = b;
+					return compile_(node, target);
+				});
+			}
 
-			Fun<FunExpr, Iterate<FunExpr>> bindTree = be -> n_ -> f //
-					.declare(f.invokeStatic(Tree.class, "decompose", n_, f.object(operator)), t -> f //
-							.ifNonNullAnd(t, f.and( //
-									lambda0.invoke(be, t.invoke("getLeft")), //
-									lambda1.invoke(be, t.invoke("getRight")))));
+			private FunExpr compile_(Node node, FunExpr target) {
+				FunExpr br = bind(f.object_(node, Node.class), target);
+				FunExpr brc = bindClone(node, target);
 
-			return LambdaInstance.of(lambdaClass, ifRef(bindRef, bindTree));
-		} else if (node instanceof Tuple) {
-			List<LambdaInstance<Bind_>> lambdas = Read //
-					.from(((Tuple) node).nodes) //
-					.map(this::compileBind_) //
-					.toList();
+				return new SwitchNode<FunExpr>(node //
+				).applyIf(Atom.class, n -> {
+					return f.ifEquals(target, f.object(node), ok, br);
+				}).applyIf(Int.class, n -> {
+					return f.ifInstance(Int.class, target, i -> f.ifEquals(i.field("number"), f.int_(n.number), ok, fail), br);
+				}).applyIf(Reference.class, n -> {
+					FunExpr ref = env.field("refs").index(f.int_(computeIndex(node)));
+					return f.invokeStatic(Binder.class, "bind", target, ref.cast(Node.class), trail);
+				}).applyIf(Str.class, n -> {
+					return f.ifInstance(Str.class, target, s -> f.object(n.value).invoke("equals", s.field("value")), br);
+				}).applyIf(Tree.class, tree -> {
+					return f.declare(f.invokeStatic(Tree.class, "decompose", target, f.object(tree.getOperator())), t -> {
+						Node lt = tree.getLeft();
+						Node rt = tree.getRight();
+						return f.ifNonNull(t, compile_(lt, t.invoke("getLeft"), compile_(rt, t.invoke("getRight"), ok)), brc);
+					});
+				}).applyIf(Tuple.class, n -> {
+					return f.ifInstance(Tuple.class, target, tuple -> f.declare(tuple.field("nodes"), targets -> {
+						Node[] nodes = n.nodes;
+						FunExpr fe = ok;
+						for (int i = 0; i < nodes.length; i++)
+							fe = compile_(nodes[i], targets.index(f.int_(i)), fe);
+						return f.if_(targets.length(), fe, brc);
+					}), brc);
+				}).applyIf(Node.class, n -> {
+					Clone_ cloner = cloner(n);
+					return f.invokeStatic(Binder.class, "bind", target, f.object(cloner).invoke("apply", env), trail);
+				}).nonNullResult();
+			}
 
-			Fun<FunExpr, Iterate<FunExpr>> bindRef = bindRef(cloner(node));
+			private FunExpr compile_(Node node, FunExpr target, FunExpr cps) {
+				return f.assign(b, compile_(node, target), f.if_(b, cps, fail));
+			}
 
-			Fun<FunExpr, Iterate<FunExpr>> bindTuple = be -> n_ -> f //
-					.ifInstanceAnd(Tuple.class, n_, tuple -> f //
-							.declare(tuple.field("nodes"), nodes -> {
-								List<FunExpr> cond = new ArrayList<>();
-								for (int i = 0; i < lambdas.size(); i++) {
-									LambdaInstance<Bind_> lambda = lambdas.get(i);
-									FunExpr ni = nodes.index(f.int_(i));
-									cond.add(lambda.invoke(be, ni));
-								}
-								return f.and(cond.toArray(new FunExpr[0]));
-							}));
+			private FunExpr bindClone(Node node, FunExpr target) {
+				if (isBindTrees)
+					return bind(f.object(cloner(node)).apply(env), target);
+				else
+					return fail;
+			}
 
-			return LambdaInstance.of(lambdaClass, ifRef(bindRef, bindTuple));
-		} else {
-			Clone_ n_ = cloner(node);
-			return compileBindPredicate((be, n) -> Binder.bind(n, n_.apply(be.env), be.trail));
-		}
-	}
-
-	private LambdaInstance<Bind_> compileBindPredicate(Bind_ pred) {
-		return LambdaInstance.of(LambdaImplementation.of( //
-				lambdaClass, //
-				Map.of("pred", Type.getType(Bind_.class)), //
-				f.parameter2((be, n) -> f.inject("pred").invoke("test", be, n))), //
-				Map.of("pred", pred));
-	}
-
-	private LambdaInstance<Bind_> compileBindAtom(Atom a) {
-		return LambdaInstance.of(compiledBindAtom, Map.of(key0, a));
-	}
-
-	private LambdaInstance<Bind_> compileBindInt(Int i) {
-		return LambdaInstance.of(compiledBindInt, Map.of(key0, i, key1, i.number));
-	}
-
-	private LambdaInstance<Bind_> compileBindStr(Str s) {
-		return LambdaInstance.of(compiledBindStr, Map.of(key0, s, key1, s.value));
-	}
-
-	private static String key0 = "k0";
-	private static String key1 = "k1";
-	private static LambdaImplementation<Bind_> compiledBindAtom = compileBindAtom_();
-	private static LambdaImplementation<Bind_> compiledBindInt = compileBindInt_();
-	private static LambdaImplementation<Bind_> compiledBindStr = compileBindStr_();
-
-	private static LambdaImplementation<Bind_> compileBindAtom_() {
-		Map<String, Type> fieldTypes = Map.of(key0, Type.getType(Node.class));
-		Iterate<FunExpr> expr = n_ -> f.ifEquals(n_, f.inject(key0), f._true(), f._false());
-		return bind(fieldTypes, expr);
-	}
-
-	private static LambdaImplementation<Bind_> compileBindInt_() {
-		Map<String, Type> fieldTypes = Map.of(key0, Type.getType(Node.class), key1, Type.INT);
-		Iterate<FunExpr> expr = n_ -> f.ifInstanceAnd( //
-				Int.class, n_, //
-				i -> f.ifEquals(i.field("number"), f.inject(key1), f._true(), f._false()));
-		return bind(fieldTypes, expr);
-	}
-
-	private static LambdaImplementation<Bind_> compileBindStr_() {
-		Map<String, Type> fieldTypes = Map.of(key0, Type.getType(Node.class), key1, Type.STRING);
-		Iterate<FunExpr> expr = n_ -> f.ifInstanceAnd( //
-				Str.class, n_, //
-				i -> f.inject(key1).invoke("equals", i.field("value").cast(Object.class)));
-		return bind(fieldTypes, expr);
-	}
-
-	private static LambdaImplementation<Bind_> bind(Map<String, Type> fieldTypes, Iterate<FunExpr> compare) {
-		return LambdaImplementation.of(lambdaClass, fieldTypes,
-				ifRef(be -> ref -> bindRef(be, ref, f.inject(key0)), be -> compare));
-	}
-
-	private Fun<FunExpr, Iterate<FunExpr>> bindRef(Clone_ n_) {
-		Fun<FunExpr, Iterate<FunExpr>> bindRef;
-		if (isBindTrees)
-			bindRef = be -> ref -> bindRef(be, ref, f.object(n_).apply(be.field("env")));
-		else
-			bindRef = be -> ref -> f._false();
-		return bindRef;
-	}
-
-	private static FunExpr bindRef(FunExpr bindEnv, FunExpr ref, FunExpr n1) {
-		return f.seq(bindEnv.field("trail").invoke("addBind", ref, n1), f._true());
-	}
-
-	private static FunExpr ifRef(Fun<FunExpr, Iterate<FunExpr>> bindRef, Fun<FunExpr, Iterate<FunExpr>> bindTree) {
-		return f.parameter2((be, n) -> f.declare( //
-				n.invoke("finalNode"), //
-				n_ -> f.ifInstance(Reference.class, n_, bindRef.apply(be), bindTree.apply(be).apply(n_))));
+			private FunExpr bind(FunExpr node, FunExpr target) {
+				return f.ifInstanceAnd(Reference.class, target, ref -> f.seq(trail.invoke("addBind", ref, node), ok));
+			}
+		}).apply(new HashMap<>());
 	}
 
 }
---
+++ src/main/java/suite/lp/compile/impl/CompileBinderImpl0.java
@@ -0,0 +1,171 @@
+package suite.lp.compile.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.bcel.generic.Type;
+
+import suite.jdk.gen.FunExpression.FunExpr;
+import suite.jdk.gen.FunFactory;
+import suite.jdk.lambda.LambdaImplementation;
+import suite.jdk.lambda.LambdaInstance;
+import suite.jdk.lambda.LambdaInterface;
+import suite.lp.doer.Binder;
+import suite.lp.doer.BinderFactory;
+import suite.node.Atom;
+import suite.node.Int;
+import suite.node.Node;
+import suite.node.Reference;
+import suite.node.Str;
+import suite.node.Tree;
+import suite.node.Tuple;
+import suite.node.io.Operator;
+import suite.streamlet.Read;
+import suite.util.FunUtil.Fun;
+import suite.util.FunUtil.Iterate;
+
+public class CompileBinderImpl0 extends CompileClonerImpl implements BinderFactory {
+
+	private static FunFactory f = new FunFactory();
+	private static LambdaInterface<Bind_> lambdaClass = LambdaInterface.of(Bind_.class, "test");
+
+	private boolean isBindTrees;
+
+	public CompileBinderImpl0() {
+		this(true);
+	}
+
+	public CompileBinderImpl0(boolean isBindTrees) {
+		this.isBindTrees = isBindTrees;
+	}
+
+	public Bind_ binder(Node node) {
+		return compileBind_(node).newFun();
+	}
+
+	private LambdaInstance<Bind_> compileBind_(Node node) {
+		Tree tree;
+
+		if (node instanceof Atom)
+			return compileBindAtom((Atom) node);
+		else if (node instanceof Int)
+			return compileBindInt((Int) node);
+		else if (node instanceof Reference) {
+			int index = computeIndex(node);
+			return compileBindPredicate((be, n) -> Binder.bind(n, be.env.get(index), be.trail));
+		} else if (node instanceof Str)
+			return compileBindStr((Str) node);
+		else if ((tree = Tree.decompose(node)) != null) {
+			Operator operator = tree.getOperator();
+			LambdaInstance<Bind_> lambda0 = compileBind_(tree.getLeft());
+			LambdaInstance<Bind_> lambda1 = compileBind_(tree.getRight());
+			Fun<FunExpr, Iterate<FunExpr>> bindRef = bindRef(cloner(node));
+
+			Fun<FunExpr, Iterate<FunExpr>> bindTree = be -> n_ -> f //
+					.declare(f.invokeStatic(Tree.class, "decompose", n_, f.object(operator)), t -> f //
+							.ifNonNullAnd(t, f.and( //
+									lambda0.invoke(be, t.invoke("getLeft")), //
+									lambda1.invoke(be, t.invoke("getRight")))));
+
+			return LambdaInstance.of(lambdaClass, ifRef(bindRef, bindTree));
+		} else if (node instanceof Tuple) {
+			List<LambdaInstance<Bind_>> lambdas = Read //
+					.from(((Tuple) node).nodes) //
+					.map(this::compileBind_) //
+					.toList();
+
+			Fun<FunExpr, Iterate<FunExpr>> bindRef = bindRef(cloner(node));
+
+			Fun<FunExpr, Iterate<FunExpr>> bindTuple = be -> n_ -> f //
+					.ifInstanceAnd(Tuple.class, n_, tuple -> f //
+							.declare(tuple.field("nodes"), nodes -> {
+								List<FunExpr> cond = new ArrayList<>();
+								for (int i = 0; i < lambdas.size(); i++) {
+									LambdaInstance<Bind_> lambda = lambdas.get(i);
+									FunExpr ni = nodes.index(f.int_(i));
+									cond.add(lambda.invoke(be, ni));
+								}
+								return f.and(cond.toArray(new FunExpr[0]));
+							}));
+
+			return LambdaInstance.of(lambdaClass, ifRef(bindRef, bindTuple));
+		} else {
+			Clone_ n_ = cloner(node);
+			return compileBindPredicate((be, n) -> Binder.bind(n, n_.apply(be.env), be.trail));
+		}
+	}
+
+	private LambdaInstance<Bind_> compileBindPredicate(Bind_ pred) {
+		return LambdaInstance.of(LambdaImplementation.of( //
+				lambdaClass, //
+				Map.of("pred", Type.getType(Bind_.class)), //
+				f.parameter2((be, n) -> f.inject("pred").invoke("test", be, n))), //
+				Map.of("pred", pred));
+	}
+
+	private LambdaInstance<Bind_> compileBindAtom(Atom a) {
+		return LambdaInstance.of(compiledBindAtom, Map.of(key0, a));
+	}
+
+	private LambdaInstance<Bind_> compileBindInt(Int i) {
+		return LambdaInstance.of(compiledBindInt, Map.of(key0, i, key1, i.number));
+	}
+
+	private LambdaInstance<Bind_> compileBindStr(Str s) {
+		return LambdaInstance.of(compiledBindStr, Map.of(key0, s, key1, s.value));
+	}
+
+	private static String key0 = "k0";
+	private static String key1 = "k1";
+	private static LambdaImplementation<Bind_> compiledBindAtom = compileBindAtom_();
+	private static LambdaImplementation<Bind_> compiledBindInt = compileBindInt_();
+	private static LambdaImplementation<Bind_> compiledBindStr = compileBindStr_();
+
+	private static LambdaImplementation<Bind_> compileBindAtom_() {
+		Map<String, Type> fieldTypes = Map.of(key0, Type.getType(Node.class));
+		Iterate<FunExpr> expr = n_ -> f.ifEquals(n_, f.inject(key0), f._true(), f._false());
+		return bind(fieldTypes, expr);
+	}
+
+	private static LambdaImplementation<Bind_> compileBindInt_() {
+		Map<String, Type> fieldTypes = Map.of(key0, Type.getType(Node.class), key1, Type.INT);
+		Iterate<FunExpr> expr = n_ -> f.ifInstanceAnd( //
+				Int.class, n_, //
+				i -> f.ifEquals(i.field("number"), f.inject(key1), f._true(), f._false()));
+		return bind(fieldTypes, expr);
+	}
+
+	private static LambdaImplementation<Bind_> compileBindStr_() {
+		Map<String, Type> fieldTypes = Map.of(key0, Type.getType(Node.class), key1, Type.STRING);
+		Iterate<FunExpr> expr = n_ -> f.ifInstanceAnd( //
+				Str.class, n_, //
+				i -> f.inject(key1).invoke("equals", i.field("value").cast(Object.class)));
+		return bind(fieldTypes, expr);
+	}
+
+	private static LambdaImplementation<Bind_> bind(Map<String, Type> fieldTypes, Iterate<FunExpr> compare) {
+		return LambdaImplementation.of(lambdaClass, fieldTypes,
+				ifRef(be -> ref -> bindRef(be, ref, f.inject(key0)), be -> compare));
+	}
+
+	private Fun<FunExpr, Iterate<FunExpr>> bindRef(Clone_ n_) {
+		Fun<FunExpr, Iterate<FunExpr>> bindRef;
+		if (isBindTrees)
+			bindRef = be -> ref -> bindRef(be, ref, f.object(n_).apply(be.field("env")));
+		else
+			bindRef = be -> ref -> f._false();
+		return bindRef;
+	}
+
+	private static FunExpr bindRef(FunExpr bindEnv, FunExpr ref, FunExpr n1) {
+		return f.seq(bindEnv.field("trail").invoke("addBind", ref, n1), f._true());
+	}
+
+	private static FunExpr ifRef(Fun<FunExpr, Iterate<FunExpr>> bindRef, Fun<FunExpr, Iterate<FunExpr>> bindTree) {
+		return f.parameter2((be, n) -> f.declare( //
+				n.invoke("finalNode"), //
+				n_ -> f.ifInstance(Reference.class, n_, bindRef.apply(be), bindTree.apply(be).apply(n_))));
+	}
+
+}
