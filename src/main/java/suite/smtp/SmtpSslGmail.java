fc25a5e8fbdd630b885a2662e6205249e9ab1284
stupidsing <stupidsing@gmail.com>
2018-01-09 22:21:43 +0800

    refactor

--- src/main/java/suite/smtp/SmtpSslGmail.java
+++ src/main/java/suite/smtp/SmtpSslGmail.java
@@ -13,11 +13,14 @@ import javax.mail.internet.MimeMessage;
 import javax.net.ssl.SSLSocketFactory;
 
 import suite.Constants;
+import suite.primitive.Chars.CharsBuilder;
 
 public class SmtpSslGmail {
 
 	public void send(String to, String subject, String body) {
-		Constants.bindSecrets("gmail .0 .1").map((username, password) -> {
+		Constants.bindSecrets("gmail .0 .1").map((username, enc) -> {
+			String password = decode(System.getenv("USER").toCharArray(), enc);
+
 			Properties props = new Properties();
 			props.put("mail.smtp.auth", "true");
 			props.put("mail.smtp.host", "smtp.gmail.com");
@@ -47,6 +50,47 @@ public class SmtpSslGmail {
 
 			return true;
 		});
+	}
+
+	public static void main(String[] args) {
+		char[] salt = "abc123".toCharArray();
+		String in = "def456";
+		String encoded = encode(salt, in);
+		String decoded = decode(salt, encoded);
+		System.out.println("encoded = " + encoded);
+		System.out.println("decoded = " + decoded);
+	}
+
+	private static String encode(char[] salt, String in) {
+		CharsBuilder cb0 = new CharsBuilder();
+		char[] text0 = in.toCharArray();
+
+		for (int i = 0; i < text0.length; i++) {
+			int a = text0[i] + salt[i % salt.length];
+			while (a < 32)
+				a += 128 - 32;
+			while (128 < a)
+				a -= 128 - 32;
+			cb0.append((char) a);
+		}
+
+		return cb0.toChars().toString();
+	}
+
+	private static String decode(char[] salt, String in) {
+		CharsBuilder cb1 = new CharsBuilder();
+		char[] text1 = in.toCharArray();
+
+		for (int i = 0; i < text1.length; i++) {
+			int a = text1[i] - salt[i % salt.length];
+			while (a < 32)
+				a += 128 - 32;
+			while (128 < a)
+				a -= 128 - 32;
+			cb1.append((char) a);
+		}
+
+		return cb1.toChars().toString();
 	}
 
 }
