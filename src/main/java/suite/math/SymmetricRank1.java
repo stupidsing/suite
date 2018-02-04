f533540fc035108218684da3ae9e1bc2165d50d4
stupidsing <stupidsing@gmail.com>
2018-02-04 09:26:27 +0800

    refactor

--- src/main/java/suite/math/SymmetricRank1.java
+++ src/main/java/suite/math/SymmetricRank1.java
@@ -18,20 +18,22 @@ public class SymmetricRank1 {
 	public float[] sr1(Obj_Dbl<float[]> fun, Fun<float[], float[]> gradientFun, float[] initials) {
 		float[] xs = initials;
 		float[] gradient = gradientFun.apply(initials);
-		float[][] h = mtx.identity(xs.length); // approximated inverse of Hessian
+		float[][] invh = mtx.identity(xs.length); // approximated inverse of Hessian
 
 		for (int iter = 0; iter < 9; iter++) {
-			float[] dxs = mtx.mul(h, gradient);
+			float[] dxs = mtx.mul(invh, gradient);
 			float[] xs1 = vec.add(xs, dxs);
 
-			float[] ys = vec.sub(gradientFun.apply(xs1), gradient);
-			float[] v = vec.sub(dxs, mtx.mul(h, ys));
+			float[] gradient1 = gradientFun.apply(xs1);
+			float[] ys = vec.sub(gradient1, gradient);
+			float[] v = vec.sub(dxs, mtx.mul(invh, ys));
 			float[][] mt = new float[][] { v, };
 			float[][] m = mtx.transpose(mt);
-			float[][] h1 = mtx.add(h, mtx.scale(mtx.mul(m, mt), vec.dot(v, ys)));
+			float[][] invh1 = mtx.add(invh, mtx.scale(mtx.mul(m, mt), vec.dot(v, ys)));
 
 			xs = xs1;
-			h = h1;
+			gradient = gradient1;
+			invh = invh1;
 		}
 
 		return xs;
