0aaf369d31da778aaa335afe30c4aab3246e6f0d
stupidsing <stupidsing@gmail.com>
2018-02-04 09:35:03 +0800

    refactor

--- src/main/java/suite/math/SymmetricRank1.java
+++ src/main/java/suite/math/SymmetricRank1.java
@@ -4,6 +4,7 @@ import suite.math.linalg.Matrix_;
 import suite.math.linalg.Vector_;
 import suite.primitive.DblPrimitives.Obj_Dbl;
 import suite.util.FunUtil.Fun;
+import suite.util.To;
 
 /**
  * https://en.wikipedia.org/wiki/Symmetric_rank-one
@@ -15,7 +16,23 @@ public class SymmetricRank1 {
 	private Matrix_ mtx = new Matrix_();
 	private Vector_ vec = new Vector_();
 
-	public float[] sr1(Obj_Dbl<float[]> fun, Fun<float[], float[]> gradientFun, float[] initials) {
+	// using finite differences to find gradient
+	public float[] sr1(Obj_Dbl<float[]> fun, float[] initials) {
+		double step = .001d;
+		Fun<float[], float[]> gradientFun = xs -> {
+			double ys = fun.apply(xs);
+			return To.vector(xs.length, i -> {
+				float x0 = xs[i];
+				xs[i] += step;
+				double gradient = (fun.apply(xs) - ys) / step;
+				xs[i] = x0;
+				return gradient;
+			});
+		};
+		return sr1(fun, gradientFun, initials);
+	}
+
+	private float[] sr1(Obj_Dbl<float[]> fun, Fun<float[], float[]> gradientFun, float[] initials) {
 		float[] xs = initials;
 		float[] gradient = gradientFun.apply(initials);
 		float[][] invh = mtx.identity(xs.length); // approximated inverse of Hessian
