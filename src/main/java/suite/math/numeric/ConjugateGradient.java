b7cc85718d4f3125c2e1845f321779a5d9bf5765
stupidsing <stupidsing@gmail.com>
2018-02-04 21:42:09 +0800

    refactor

---
+++ src/main/java/suite/math/numeric/ConjugateGradient.java
@@ -0,0 +1,36 @@
+package suite.math.numeric;
+
+import suite.math.linalg.Matrix_;
+import suite.math.linalg.Vector_;
+
+/**
+ * https://en.wikipedia.org/wiki/Conjugate_gradient_method
+ *
+ * @author ywsing
+ */
+public class ConjugateGradient {
+
+	private Matrix_ mtx = new Matrix_();
+	private Vector_ vec = new Vector_();
+
+	public float[] cg(float[][] a, float[] b, float[] initials) {
+		float[] xs = initials;
+		float[] rs = vec.sub(b, mtx.mul(a, xs));
+		float[] ps = rs;
+
+		for (int iter = 0; iter < initials.length; iter++) {
+			double alpha = vec.dot(rs) / vec.dot(ps, mtx.mul(a, ps));
+			float[] xs1 = vec.add(xs, vec.scale(ps, alpha));
+			float[] rs1 = vec.sub(rs, vec.scale(mtx.mul(a, ps), alpha));
+			double beta = vec.dot(rs1) / vec.dot(rs);
+			float[] ps1 = vec.add(rs1, vec.scale(ps, beta));
+
+			xs = xs1;
+			rs = rs1;
+			ps = ps1;
+		}
+
+		return xs;
+	}
+
+}
