62494afb2a2f8533035c7af40e9996c78f4c0559
ywsing <stupidsing@gmail.com>
2018-02-09 09:31:17 +0800

    refactor

--- src/main/java/suite/DailyMain.java
+++ src/main/java/suite/DailyMain.java
@@ -48,7 +48,7 @@ import suite.util.To;
 // mvn compile exec:java -Dexec.mainClass=suite.DailyMain
 public class DailyMain extends ExecutableProgram {
 
-	private Set<String> blackList = To.set("2914.HK");
+	private Set<String> blackList = To.set("0805.HK");
 
 	private Configuration cfg = new ConfigurationImpl();
 	private Serialize serialize = Serialize.me;
