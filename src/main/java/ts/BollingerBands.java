7aaf2b2d9336589507070a400d59ecc26e0da8ba
stupidsing <stupidsing@gmail.com>
2018-02-10 09:23:55 +0800

    refactor

--- src/main/java/ts/BollingerBands.java
+++ src/main/java/ts/BollingerBands.java
@@ -2,20 +2,50 @@ package ts;
 
 import java.util.Arrays;
 
+import suite.adt.pair.Pair;
+import suite.math.linalg.VirtualVector;
 import suite.math.numeric.Statistic;
 import suite.math.numeric.Statistic.MeanVariance;
+import suite.primitive.Int_Flt;
 
-/**
- * Auto-regressive distributed lag.
- *
- * @author ywsing
- */
 public class BollingerBands {
 
 	private Statistic stat = new Statistic();
 
 	public Bb bb(float[] fs, int backPos0, int backPos1, float k) {
 		return new Bb(fs, backPos0, backPos1, k);
+	}
+
+	public Pair<float[], float[]> meanVariances(VirtualVector v, int backPos0, int backPos1) {
+		int length = v.length;
+		Int_Flt fun = v.get;
+
+		float[] means = new float[length];
+		float[] variances = new float[length];
+		int d = backPos0 - backPos1;
+		double il = 1d / d;
+		int i = 0, j;
+		double sum = 0d;
+		double sumSq = 0d;
+
+		for (; i < d; i++) {
+			float f = fun.apply(i);
+			sum += f;
+			sumSq += f * f;
+		}
+
+		for (; (j = i + backPos1) < length; i++) {
+			double mean = sum * il;
+			means[j] = (float) mean;
+			variances[j] = (float) (sumSq * il - mean * mean);
+
+			float f0 = fun.apply(i - d);
+			float fx = fun.apply(i);
+			sum += fx - f0;
+			sumSq += fx * fx - f0 * f0;
+		}
+
+		return Pair.of(means, variances);
 	}
 
 	public class Bb {
