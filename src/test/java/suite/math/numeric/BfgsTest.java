334c69b96e3e88ac721848123e8ede4e36d5adb8
ywsing <stupidsing@gmail.com>
2018-02-12 10:03:09 +0800

    refactor

---
+++ src/test/java/suite/math/numeric/BfgsTest.java
@@ -0,0 +1,33 @@
+package suite.math.numeric;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import suite.primitive.DblPrimitives.Obj_Dbl;
+
+public class BfgsTest {
+
+	private Bfgs bfgs = new Bfgs();
+
+	// https://en.wikipedia.org/wiki/Rosenbrock_function
+	@Test
+	public void test() {
+		double a = 1d;
+		double b = 1d;
+
+		// global minimum (a, a * a)
+		Obj_Dbl<float[]> rosenbrock = xy -> {
+			double x = xy[0];
+			double y = xy[1];
+			double a_x = a - x;
+			double y_x = y - x;
+			return a_x * a_x + b * y_x * y_x;
+		};
+
+		float[] xs = bfgs.bfgs(rosenbrock, new float[] { 0f, 0f });
+
+		System.out.println(Arrays.toString(xs));
+	}
+
+}
