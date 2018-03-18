5baa1735b19054cf6d13368a14537e751fc96586
stupidsing <stupidsing@gmail.com>
2018-03-18 09:35:54 +0800

    refactor

--- src/main/java/suite/ansi/Keyboard.java
+++ src/main/java/suite/ansi/Keyboard.java
@@ -9,74 +9,89 @@ import suite.streamlet.Read;
 import suite.streamlet.Signal;
 import suite.streamlet.Signal.Redirector;
 import suite.util.FunUtil.Sink;
-import suite.util.FunUtil.Source;
 
 public class Keyboard {
 
 	private LibcJna libc;
-	private Trie<Integer, VK> trie;
+	private Trie<Integer, VK> trie = new Trie<>();
 
 	public enum VK {
+		DEL__, //
+		DOWN_, //
+		END__, //
+		HOME_, //
+		INS__, //
 		LEFT_, //
+		PGUP_, //
+		PGDN_, //
 		RIGHT, //
 		UP___, //
-		DOWN_, //
 	}
 
 	public Keyboard(LibcJna libc) {
 		this.libc = libc;
 
 		List<Pair<List<Integer>, VK>> pairs = new ArrayList<>();
-		pairs.add(Pair.of(List.of(27, 91, 68), VK.LEFT_));
-		pairs.add(Pair.of(List.of(27, 91, 67), VK.RIGHT));
+		pairs.add(Pair.of(List.of(27, 91, 50, 126), VK.INS__));
+		pairs.add(Pair.of(List.of(27, 91, 51, 126), VK.DEL__));
+		pairs.add(Pair.of(List.of(27, 91, 53, 126), VK.PGUP_));
+		pairs.add(Pair.of(List.of(27, 91, 54, 126), VK.PGDN_));
 		pairs.add(Pair.of(List.of(27, 91, 65), VK.UP___));
 		pairs.add(Pair.of(List.of(27, 91, 66), VK.DOWN_));
-
-		trie = new Trie<>();
+		pairs.add(Pair.of(List.of(27, 91, 67), VK.RIGHT));
+		pairs.add(Pair.of(List.of(27, 91, 68), VK.LEFT_));
+		pairs.add(Pair.of(List.of(27, 91, 70), VK.END__));
+		pairs.add(Pair.of(List.of(27, 91, 72), VK.HOME_));
 
 		for (Pair<List<Integer>, VK> pair : pairs)
 			trie.add(pair.t0, pair.t1);
 	}
 
-	public Signal<Pair<VK, Character>> signal() {
-		Source<Character> source0 = () -> {
-			int ch = libc.getchar();
-			return 0 <= ch ? (char) ch : null;
-		};
-
-		return Signal //
-				.from(source0) //
-				.redirect(new Redirector<Character, Pair<VK, Character>>() {
-					private List<Character> chs = new ArrayList<>();
-					private Trie<Integer, VK> t = trie;
-
-					public void accept(Character ch_, Sink<Pair<VK, Character>> fire) {
-						if (ch_ != null) {
-							chs.add(ch_);
-							VK vk;
-
-							if ((t = t.getMap().get((int) ch_)) != null)
-								if ((vk = t.getValue()) != null) {
-									fire.sink(Pair.of(vk, null));
-									reset();
-								} else
-									;
-							else
-								flush(fire);
-						} else
-							flush(fire);
-					}
-
-					private void flush(Sink<Pair<VK, Character>> fire) {
-						Read.from(chs).sink(ch -> fire.sink(Pair.of(null, ch)));
-						reset();
-					}
-
-					private void reset() {
-						chs.clear();
-						t = trie;
-					}
-				});
+	public void loop(Sink<Signal<Pair<VK, Character>>> sink) {
+		Signal.loop(this::get, signal -> sink.sink(signal.redirect(redirector)));
 	}
+
+	public Signal<Pair<VK, Character>> signal() {
+		return Signal.from(this::get).redirect(redirector);
+	}
+
+	private Character get() {
+		int ch = libc.getchar();
+		return 0 <= ch ? (char) ch : null;
+	}
+
+	private Redirector<Character, Pair<VK, Character>> redirector = new Redirector<>() {
+		private List<Character> chs = new ArrayList<>();
+		private Trie<Integer, VK> t = trie;
+
+		public void accept(Character ch_, Sink<Pair<VK, Character>> fire) {
+			if (ch_ != null) {
+				Trie<Integer, VK> t1 = t.getMap().get((int) ch_);
+				VK vk;
+
+				chs.add(ch_);
+
+				if (t1 != null)
+					if ((vk = (t = t1).getValue()) != null) {
+						fire.sink(Pair.of(vk, null));
+						reset();
+					} else
+						;
+				else
+					flush(fire);
+			} else
+				flush(fire);
+		}
+
+		private void flush(Sink<Pair<VK, Character>> fire) {
+			Read.from(chs).sink(ch -> fire.sink(Pair.of(null, ch)));
+			reset();
+		}
+
+		private void reset() {
+			chs.clear();
+			t = trie;
+		}
+	};
 
 }
--- src/main/java/suite/dev/DevMain.java
+++ src/main/java/suite/dev/DevMain.java
@@ -2,7 +2,6 @@ package suite.dev;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 import com.sun.jna.Native;
@@ -15,7 +14,7 @@ import suite.ansi.Termios;
 import suite.primitive.Chars_;
 import suite.primitive.Ints_;
 import suite.primitive.adt.pair.IntIntPair;
-import suite.util.FunUtil.Iterate;
+import suite.util.Fail;
 import suite.util.FunUtil.Sink;
 import suite.util.Rethrow;
 
@@ -31,23 +30,25 @@ public class DevMain {
 	}
 
 	private void run(int screenSizeX, int screenSizeY) {
+		int viewSizeX = screenSizeX;
+		int viewSizeY = screenSizeY - 1;
 		List<String> input = Rethrow.ex(() -> Files.readAllLines(Paths.get("src/main/java/suite/dev/DevMain.java")));
 
-		char[] cs = new char[screenSizeX];
-		Arrays.fill(cs, ' ');
-		String blankY = new String(cs);
-
 		try (Termios termios = new Termios(libc);) {
+			termios.clear();
 			Keyboard keyboard = new Keyboard(libc);
 
 			Sink<State> redraw = state -> state.apply((lines, oc, cc) -> cc.apply((cx, cy) -> oc.apply((ox, oy) -> {
-				termios.clear();
 				termios.cursor(false);
 
-				for (int screenY = 0; screenY < screenSizeY - 1; screenY++) {
-					int y = screenY - oy;
+				for (int screenY = 0; screenY < viewSizeY; screenY++) {
+					int y = screenY + oy;
 					String line = 0 <= y && y < lines.size() ? lines.get(y) : "";
-					String s = (line.substring(ox) + blankY).substring(0, screenSizeX);
+
+					String s = new String(Chars_.toArray(viewSizeX, screenX -> {
+						int x = screenX + ox;
+						return 0 <= x && x < line.length() ? line.charAt(x) : ' ';
+					}));
 
 					termios.gotoxy(0, screenY);
 					termios.puts(s);
@@ -61,41 +62,49 @@ public class DevMain {
 			State state0 = State.of(input, IntIntPair.of(0, 0), IntIntPair.of(0, 0));
 			redraw.sink(state0);
 
-			keyboard //
-					.signal() //
-					.map(pair -> pair.map((vk, ch) -> {
-						Iterate<State> mutate = state -> state.apply((lines, oc, cc) -> cc.apply((cx, cy) -> {
-							if (vk == VK.LEFT_)
-								return State.of(lines, oc, IntIntPair.of(cx - 1, cy));
-							else if (vk == VK.RIGHT)
-								return State.of(lines, oc, IntIntPair.of(cx + 1, cy));
-							else if (vk == VK.UP___)
-								return State.of(lines, oc, IntIntPair.of(cx, cy - 1));
-							else if (vk == VK.DOWN_)
-								return State.of(lines, oc, IntIntPair.of(cx, cy + 1));
-							else if (ch != null) {
-								List<String> lines1 = Ints_ //
-										.range(lines.size()) //
-										.map(y -> {
-											String line = lines.get(y);
-											if (cy != y)
-												return line;
-											else {
-												char[] cs_ = Chars_.toArray(cx, i -> i < line.length() ? line.charAt(i) : ' ');
-												String s0 = new String(cs_);
-												String sx = line.substring(cx);
-												return s0 + ch + sx;
-											}
-										}) //
-										.toList();
-								return State.of(lines1, oc, IntIntPair.of(cx + 1, cy));
-							} else
-								return state;
-						}));
-						return mutate;
-					})) //
-					.fold(state0, (state, mutate) -> mutate.apply(state)) //
-					.wire(redraw);
+			FixieFun3<VK, Character, State, State> mutate = (vk, ch, state) -> state //
+					.apply((lines, oc, cc) -> cc.apply((cx, cy) -> {
+						if (vk == VK.LEFT_)
+							return State.of(lines, oc, IntIntPair.of(cx - 1, cy));
+						else if (vk == VK.RIGHT)
+							return State.of(lines, oc, IntIntPair.of(cx + 1, cy));
+						else if (vk == VK.UP___)
+							return State.of(lines, oc, IntIntPair.of(cx, cy - 1));
+						else if (vk == VK.DOWN_)
+							return State.of(lines, oc, IntIntPair.of(cx, cy + 1));
+						else if (ch == 'q')
+							return Fail.t();
+						else if (ch != null) {
+							List<String> lines1 = Ints_ //
+									.range(lines.size()) //
+									.map(y -> {
+										String line = lines.get(y);
+										if (cy != y)
+											return line;
+										else {
+											char[] cs_ = Chars_.toArray(cx, i -> i < line.length() ? line.charAt(i) : ' ');
+											String s0 = new String(cs_);
+											String sx = line.substring(cx);
+											return s0 + ch + sx;
+										}
+									}) //
+									.toList();
+							return State.of(lines1, oc, IntIntPair.of(cx + 1, cy));
+						} else
+							return state;
+					})).apply((lines, oc, cc) -> oc.apply((ox, oy) -> cc.apply((cx, cy) -> {
+						int cx_ = Math.max(0, cx);
+						int cy_ = Math.max(0, Math.min(lines.size(), cy));
+						return State.of(lines, oc, IntIntPair.of(cx_, cy_));
+					}))).apply((lines, oc, cc) -> oc.apply((ox, oy) -> cc.apply((cx, cy) -> {
+						int ox_ = Math.max(cx - viewSizeX + 1, Math.min(cx, ox));
+						int oy_ = Math.max(cy - viewSizeY + 1, Math.min(cy, oy));
+						return State.of(lines, IntIntPair.of(ox_, oy_), cc);
+					})));
+
+			keyboard.loop(signal -> signal //
+					.fold(state0, (state, pair_) -> pair_.map((vk, ch) -> mutate.apply(vk, ch, state))) //
+					.wire(redraw));
 		}
 	}
 
