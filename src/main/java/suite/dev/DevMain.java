ae9f79fdad77e239a54d649fc5ae8e0c352f50cc
stupidsing <stupidsing@gmail.com>
2018-03-18 08:26:14 +0800

    refactor

--- src/main/java/suite/dev/DevMain.java
+++ src/main/java/suite/dev/DevMain.java
@@ -2,7 +2,6 @@ package suite.dev;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,6 +12,8 @@ import suite.ansi.Keyboard;
 import suite.ansi.Keyboard.VK;
 import suite.ansi.LibcJna;
 import suite.ansi.Termios;
+import suite.primitive.Chars_;
+import suite.primitive.Ints_;
 import suite.primitive.adt.pair.IntIntPair;
 import suite.util.FunUtil.Iterate;
 import suite.util.FunUtil.Sink;
@@ -43,7 +44,7 @@ public class DevMain {
 				termios.clear();
 				termios.cursor(false);
 
-				for (int screenY = 0; screenY < screenSizeY; screenY++) {
+				for (int screenY = 0; screenY < screenSizeY - 1; screenY++) {
 					int y = screenY - oy;
 					String line = 0 <= y && y < lines.size() ? lines.get(y) : "";
 					String s = (line.substring(ox) + blankY).substring(0, screenSizeX);
@@ -57,9 +58,12 @@ public class DevMain {
 				return null;
 			})));
 
+			State state0 = State.of(input, IntIntPair.of(0, 0), IntIntPair.of(0, 0));
+			redraw.sink(state0);
+
 			keyboard //
 					.signal() //
-					.map(pair -> pair.map((vk, c) -> {
+					.map(pair -> pair.map((vk, ch) -> {
 						Iterate<State> mutate = state -> state.apply((lines, oc, cc) -> cc.apply((cx, cy) -> {
 							if (vk == VK.LEFT_)
 								return State.of(lines, oc, IntIntPair.of(cx - 1, cy));
@@ -69,22 +73,28 @@ public class DevMain {
 								return State.of(lines, oc, IntIntPair.of(cx, cy - 1));
 							else if (vk == VK.DOWN_)
 								return State.of(lines, oc, IntIntPair.of(cx, cy + 1));
-							else if (c != null) {
-								List<String> lines1 = new ArrayList<>();
-								for (int y = 0; y < lines.size(); y++) {
-									String line = lines.get(y);
-									if (cy != y)
-										lines1.add(line);
-									else
-										lines1.add(line.substring(0, cx) + c + line.substring(cx));
-								}
+							else if (ch != null) {
+								List<String> lines1 = Ints_ //
+										.range(lines.size()) //
+										.map(y -> {
+											String line = lines.get(y);
+											if (cy != y)
+												return line;
+											else {
+												char[] cs_ = Chars_.toArray(cx, i -> i < line.length() ? line.charAt(i) : ' ');
+												String s0 = new String(cs_);
+												String sx = line.substring(cx);
+												return s0 + ch + sx;
+											}
+										}) //
+										.toList();
 								return State.of(lines1, oc, IntIntPair.of(cx + 1, cy));
 							} else
 								return state;
 						}));
 						return mutate;
 					})) //
-					.fold(State.of(input, IntIntPair.of(0, 0), IntIntPair.of(0, 0)), (state, mutate) -> mutate.apply(state)) //
+					.fold(state0, (state, mutate) -> mutate.apply(state)) //
 					.wire(redraw);
 		}
 	}
