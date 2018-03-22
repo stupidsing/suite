9f7d422131224b2951fd10a0453f114d1cdd7c24
stupidsing <stupidsing@gmail.com>
2018-03-22 21:22:08 +0800

    refactor

---
+++ src/main/java/suite/util/Friends.java
@@ -0,0 +1,37 @@
+package suite.util;
+
+public class Friends {
+
+	public static double max(double a, double b) {
+		return Math.max(a, b);
+	}
+
+	public static float max(float a, float b) {
+		return Math.max(a, b);
+	}
+
+	public static int max(int a, int b) {
+		return Math.max(a, b);
+	}
+
+	public static long max(long a, long b) {
+		return Math.max(a, b);
+	}
+
+	public static double min(double a, double b) {
+		return Math.min(a, b);
+	}
+
+	public static float min(float a, float b) {
+		return Math.min(a, b);
+	}
+
+	public static int min(int a, int b) {
+		return Math.min(a, b);
+	}
+
+	public static long min(long a, long b) {
+		return Math.min(a, b);
+	}
+
+}
