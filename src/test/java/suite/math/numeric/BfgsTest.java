e8115e57bb8c2a9c135d391cec76f8df97c6de1e
stupidsing <stupidsing@gmail.com>
2018-03-25 07:27:57 +0800

    refactor

--- src/test/java/suite/math/numeric/BfgsTest.java
+++ src/test/java/suite/math/numeric/BfgsTest.java
@@ -4,17 +4,21 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import suite.math.MathUtil;
+import suite.math.linalg.Vector;
 import suite.primitive.DblPrimitives.Obj_Dbl;
 
 public class BfgsTest {
 
 	private Bfgs bfgs = new Bfgs();
+	private Vector vec = new Vector();
 
 	@Test
 	public void test() {
 		Obj_Dbl<float[]> id = x -> x[0] * x[0];
 		float[] xs = bfgs.bfgs(id, new float[] { 22f, });
 		System.out.println(Arrays.toString(xs));
+		MathUtil.verifyEquals(xs[0], 0f, .05f);
 	}
 
 	// https://en.wikipedia.org/wiki/Rosenbrock_function
@@ -35,6 +39,7 @@ public class BfgsTest {
 		float[] xs = bfgs.bfgs(rosenbrock, new float[] { 0f, 0f, });
 
 		System.out.println(Arrays.toString(xs));
+		vec.verifyEquals(xs, new float[] { (float) a, (float) a, }, .05f);
 	}
 
 }
