780f8fc8fc089f1da61655a4230b3b091ca59e3b
stupidsing <stupidsing@gmail.com>
2018-04-07 09:10:30 +0800

    refactor

---
+++ src/main/java/suite/node/pp/EstimateLength.java
@@ -0,0 +1,57 @@
+package suite.node.pp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import suite.node.Node;
+import suite.node.Tree;
+import suite.node.io.Formatter;
+
+public class EstimateLength {
+
+	private Map<Integer, Integer> lengthByIds;
+	private int defaultLength;
+
+	public EstimateLength(int defaultLength, Node node) {
+		this.defaultLength = defaultLength;
+		lengthByIds = new HashMap<>();
+
+		new Object() {
+			public int estimate(Node node) {
+				var key = getKey(node);
+				var length = lengthByIds.get(key);
+
+				if (length == null) {
+					int len;
+
+					if (node instanceof Tree) {
+						var tree = (Tree) node;
+
+						var op = tree.getOperator();
+						var len0 = estimate(tree.getLeft());
+						var len1 = estimate(tree.getRight());
+						var opLength = op.getName().length();
+
+						len = len0 + len1 + opLength + 2; // rough estimation
+					} else
+						len = Formatter.dump(node).length();
+
+					length = len;
+					lengthByIds.put(key, length);
+				}
+
+				return length;
+			}
+		}.estimate(node);
+	}
+
+	public int getEstimatedLength(Node node) {
+		var length = lengthByIds.get(getKey(node));
+		return length != null ? length : defaultLength; // maximum if not found
+	}
+
+	private int getKey(Node node) {
+		return System.identityHashCode(node);
+	}
+
+}
