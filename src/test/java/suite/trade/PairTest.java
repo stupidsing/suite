97e9b90c9779176e4d712dc70ea9280d368032af
stupidsing <stupidsing@gmail.com>
2018-04-07 12:25:54 +0800

    refactor

--- src/test/java/suite/trade/PairTest.java
+++ src/test/java/suite/trade/PairTest.java
@@ -10,7 +10,6 @@ import suite.primitive.Longs_;
 import suite.primitive.adt.pair.FltObjPair;
 import suite.trade.data.Configuration;
 import suite.trade.data.ConfigurationImpl;
-import suite.trade.data.DataSource;
 
 /**
  * Finds the period of various stocks using FFT.
@@ -24,14 +23,14 @@ public class PairTest {
 
 	@Test
 	public void test() {
-		TimeRange period = TimeRange.of(Time.of(2016, 1, 1), Time.of(2017, 1, 1));
+		var period = TimeRange.of(Time.of(2016, 1, 1), Time.of(2017, 1, 1));
 		// test(period, "0005.HK", "2888.HK");
 		test(period, "0341.HK", "0052.HK");
 	}
 
 	private void test(TimeRange period, String symbol0, String symbol1) {
-		DataSource ds0 = cfg.dataSource(symbol0, period);
-		DataSource ds1 = cfg.dataSource(symbol1, period);
+		var ds0 = cfg.dataSource(symbol0, period);
+		var ds1 = cfg.dataSource(symbol1, period);
 		var ts0 = Longs_.of(ds0.ts);
 		var ts1 = Longs_.of(ds1.ts);
 		var tradeTimes = Longs_.concat(ts0, ts1).distinct().sort().toArray();
