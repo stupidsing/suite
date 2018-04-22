637bd81f5b3781e5d714b09a456c007adc90c106
stupidsing <stupidsing@gmail.com>
2018-04-22 08:15:35 +0800

    refactor

--- src/main/java/suite/math/FiniteDifference.java
+++ src/main/java/suite/math/FiniteDifference.java
@@ -1,11 +1,13 @@
 package suite.math;
 
+import suite.math.linalg.Vector;
 import suite.primitive.DblPrimitives.Obj_Dbl;
 import suite.util.FunUtil.Fun;
 import suite.util.To;
 
 public class FiniteDifference {
 
+	private Vector vec = new Vector();
 	private double step = .001d;
 	private double invStep = 1d / step;
 
@@ -16,6 +18,19 @@ public class FiniteDifference {
 				var x0 = xs[i];
 				xs[i] += step;
 				var gradient = (fun.apply(xs) - ys) * invStep;
+				xs[i] = x0;
+				return gradient;
+			});
+		};
+	}
+
+	public Fun<float[], float[][]> forward(Fun<float[], float[]> fun) {
+		return xs -> {
+			var ys = fun.apply(xs);
+			return To.array(xs.length, float[].class, i -> {
+				var x0 = xs[i];
+				xs[i] += step;
+				var gradient = vec.scale(vec.sub(fun.apply(xs), ys), invStep);
 				xs[i] = x0;
 				return gradient;
 			});
