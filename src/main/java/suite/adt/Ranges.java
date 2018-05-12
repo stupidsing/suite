d769dc1c9081bc3dff4b0370b53871040c169e7f
stupidsing <stupidsing@gmail.com>
2018-05-13 06:48:17 +0800

    refactor

--- src/main/java/suite/adt/Ranges.java
+++ src/main/java/suite/adt/Ranges.java
@@ -2,6 +2,7 @@ package suite.adt;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import suite.util.Object_;
 import suite.util.To;
@@ -10,25 +11,13 @@ public class Ranges<T extends Comparable<? super T>> {
 
 	public final List<Range<T>> ranges;
 
-	private static class Builder<T extends Comparable<? super T>> {
-		private List<Range<T>> list = new ArrayList<>();
-
-		private void add(Range<T> range) {
-			if (!range.isEmpty())
-				list.add(range);
-		}
-
-		private Ranges<T> ranges() {
-			return new Ranges<>(list);
-		}
-	}
-
 	public static <T extends Comparable<? super T>> Ranges<T> intersect(Ranges<T> ranges0, Ranges<T> ranges1) {
 		var source0 = To.source(ranges0.ranges);
 		var source1 = To.source(ranges1.ranges);
 		var range0 = source0.source();
 		var range1 = source1.source();
-		var intersects = new Builder<T>();
+		var intersects = new ArrayList<Range<T>>();
+		var add = add(intersects);
 		T to;
 
 		while (range0 != null && range1 != null) {
@@ -40,10 +29,10 @@ public class Ranges<T extends Comparable<? super T>> {
 				range1 = source1.source();
 			}
 
-			intersects.add(Range.of(Object_.min(range0.from, range0.from), to));
+			add.test(Range.of(Object_.min(range0.from, range0.from), to));
 		}
 
-		return intersects.ranges();
+		return new Ranges<>(intersects);
 	}
 
 	public static <T extends Comparable<? super T>> Ranges<T> minus(Ranges<T> ranges0, Ranges<T> ranges1) {
@@ -63,14 +52,17 @@ public class Ranges<T extends Comparable<? super T>> {
 	}
 
 	public Ranges<T> negate(T min, T max) {
-		var builder = new Builder<T>();
+		var builder = new ArrayList<Range<T>>();
+		var add = add(builder);
 		var t = min;
+
 		for (var range : ranges) {
 			ranges.add(Range.of(t, range.from));
 			t = range.to;
 		}
-		builder.add(Range.of(t, max));
-		return builder.ranges();
+
+		add.test(Range.of(t, max));
+		return new Ranges<>(builder);
 	}
 
 	private static <T extends Comparable<? super T>> int nullMaxCompare(T t0, T t1) {
@@ -78,6 +70,10 @@ public class Ranges<T extends Comparable<? super T>> {
 			return t0 != null ? -1 : 1;
 		else
 			return t0 != null ? t0.compareTo(t1) : 0;
+	}
+
+	private static <T extends Comparable<? super T>> Predicate<Range<T>> add(List<Range<T>> intersects) {
+		return range -> !range.isEmpty() ? intersects.add(range) : false;
 	}
 
 }
