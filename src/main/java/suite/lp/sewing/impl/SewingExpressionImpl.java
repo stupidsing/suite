43ffdc9be7234a659d4f7052c7e87df0cee3eb30
Sing Yiu Wah <yw.sing_contractor@hactl.com>
2018-05-15 14:53:19 +0800

    refactor

--- src/main/java/suite/lp/sewing/impl/SewingExpressionImpl.java
+++ src/main/java/suite/lp/sewing/impl/SewingExpressionImpl.java
@@ -4,10 +4,8 @@ import suite.lp.doer.ClonerFactory;
 import suite.lp.doer.EvaluatorFactory;
 import suite.node.Int;
 import suite.node.Node;
-import suite.node.Tree;
-import suite.node.io.TermOp;
+import suite.node.io.SwitchNode;
 import suite.node.util.TreeUtil;
-import suite.primitive.IntInt_Int;
 
 public class SewingExpressionImpl implements EvaluatorFactory {
 
@@ -18,32 +16,24 @@ public class SewingExpressionImpl implements EvaluatorFactory {
 	}
 
 	public Evaluate_ evaluator(Node node) {
-		var tree = Tree.decompose(node);
-
-		if (tree != null) {
-			var op = tree.getOperator();
-			Evaluate_ lhs, rhs;
-			IntInt_Int fun;
-
-			if (op == TermOp.TUPLE_) {
-				var rightTree = Tree.decompose(tree.getRight());
-				lhs = evaluator(tree.getLeft());
-				rhs = evaluator(rightTree.getRight());
-				fun = TreeUtil.evaluateOp(rightTree.getLeft());
-			} else {
-				lhs = evaluator(tree.getLeft());
-				rhs = evaluator(tree.getRight());
-				fun = TreeUtil.evaluateOp(op);
-			}
-
+		return new SwitchNode<Evaluate_>(node //
+		).match3(".0 .1 .2", (l, op, r) -> {
+			var lhs = evaluator(l);
+			var rhs = evaluator(r);
+			var fun = TreeUtil.evaluateOp(op);
 			return env -> fun.apply(lhs.evaluate(env), rhs.evaluate(env));
-		} else if (node instanceof Int) {
-			var i = ((Int) node).number;
-			return env -> i;
-		} else {
+		}).applyTree((op, l, r) -> {
+			var lhs = evaluator(l);
+			var rhs = evaluator(r);
+			var fun = TreeUtil.evaluateOp(op);
+			return env -> fun.apply(lhs.evaluate(env), rhs.evaluate(env));
+		}).applyIf(Int.class, i -> {
+			var i_ = i.number;
+			return env -> i_;
+		}).applyIf(Node.class, n -> {
 			var f = clonerFactory.cloner(node);
 			return env -> TreeUtil.evaluate(f.apply(env));
-		}
+		}).nonNullResult();
 	}
 
 }
