d3e3327b534cbef8469c5adc221627fa3cb84931
Sing Yiu Wah <yw.sing_contractor@hactl.com>
2018-05-24 09:45:15 +0800

    refactor

--- src/main/java/suite/util/AutoObject.java
+++ src/main/java/suite/util/AutoObject.java
@@ -3,6 +3,7 @@ package suite.util;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import suite.adt.IdentityKey;
@@ -19,29 +20,27 @@ public abstract class AutoObject<T extends AutoObject<T>> implements Cloneable, 
 
 	@Override
 	public AutoObject<T> clone() {
-		var map = new HashMap<IdentityKey<?>, AutoObject<?>>();
-
-		class Clone {
-			private AutoObject<?> clone(AutoObject<?> t0) throws IllegalAccessException {
-				var key = IdentityKey.of(t0);
-				var tx = map.get(key);
-				if (tx == null) {
-					map.put(key, tx = Object_.new_(t0.getClass()));
-					@SuppressWarnings("unchecked")
-					var t1 = (AutoObject<T>) tx;
-					for (var field : t0.fields_()) {
-						var v0 = field.get(t0);
-						var v1 = v0 instanceof AutoObject ? clone((AutoObject<?>) v0) : v0;
-						field.set(t1, v1);
-					}
-				}
-				return tx;
-			}
-		}
-
 		return Rethrow.ex(() -> {
 			@SuppressWarnings("unchecked")
-			var object = (AutoObject<T>) new Clone().clone(this);
+			var object = (AutoObject<T>) new Object() {
+				private Map<IdentityKey<?>, AutoObject<?>> map = new HashMap<>();
+
+				private AutoObject<?> clone(AutoObject<?> t0) throws IllegalAccessException {
+					var key = IdentityKey.of(t0);
+					var tx = map.get(key);
+					if (tx == null) {
+						map.put(key, tx = Object_.new_(t0.getClass()));
+						var t1 = (AutoObject<T>) tx;
+						for (var field : t0.fields_()) {
+							var v0 = field.get(t0);
+							var v1 = v0 instanceof AutoObject ? clone((AutoObject<?>) v0) : v0;
+							field.set(t1, v1);
+						}
+					}
+					return tx;
+				}
+			}.clone(this);
+
 			return object;
 		});
 	}
