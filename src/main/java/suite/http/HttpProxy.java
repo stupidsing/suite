a9dbc9ef54a8450edd442d81e1d969744ac2224b
yw.sing <yw.sing_contractor@hactl.com>
2018-06-08 18:03:06 +0800

    refactor

--- src/main/java/suite/http/HttpProxy.java
+++ src/main/java/suite/http/HttpProxy.java
@@ -1,18 +1,22 @@
 package suite.http;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketException;
 
 import suite.Constants;
 import suite.os.LogUtil;
 import suite.os.SocketUtil;
 import suite.primitive.adt.pair.IntObjPair;
 import suite.streamlet.Read;
-import suite.util.Copy;
 import suite.util.Fail;
 import suite.util.FunUtil.Fun;
 import suite.util.Rethrow;
 import suite.util.String_;
+import suite.util.Th;
 import suite.util.Thread_;
 import suite.util.Util;
 
@@ -44,23 +48,46 @@ public class HttpProxy {
 	}
 
 	private void serve_() throws IOException {
-		new SocketUtil().listenIo(port, (is0, os0) -> {
-			var line = Util.readLine(is0);
+		new SocketUtil().listenIo(port, (is, os) -> {
+			var line = Util.readLine(is);
 			LogUtil.info("PROXY " + line);
 
 			var url = line.split(" ")[1];
 			var pp = String_.split2(url, "://");
 			var path = pp != null ? String_.split2l(pp.t1, "/").t1 : url;
-			var s1 = target.apply(path).map((port1, host1) -> Rethrow.ex(() -> new Socket(host1, port1)));
+			var client = target.apply(path).map((port1, host1) -> Rethrow.ex(() -> new Socket(host1, port1)));
 
-			try (var socket1 = s1; var is1 = socket1.getInputStream(); var os1 = socket1.getOutputStream();) {
-				os1.write(line.getBytes(Constants.charset));
-				os1.write(13);
-				os1.write(10);
-				var threads = Read.each(Copy.streamByThread(is0, os1), Copy.streamByThread(is1, os0));
+			try (var socket1 = client; //
+					var is0 = is; //
+					var os0 = os; //
+					var is1 = socket1.getInputStream(); //
+					var os1 = socket1.getOutputStream();) {
+				os1.write((line + "\r\nConnection: close\r\n").getBytes(Constants.charset));
+				var threads = Read.each(streamByThread(is0, os1, line), streamByThread(is1, os0, line));
 				Thread_.startJoin(threads);
 			}
 		});
+	}
+
+	private static Th streamByThread(InputStream is, OutputStream os, String s) {
+		return Thread_.newThread(() -> {
+			try {
+				stream(is, os);
+			} catch (InterruptedIOException ex) {
+			} catch (SocketException ex) {
+				// if (!String_.equals(ex.getMessage(), "Socket closed"))
+				throw new RuntimeException(s, ex);
+			}
+		});
+	}
+
+	private static void stream(InputStream in, OutputStream out) throws IOException {
+		var buffer = new byte[Constants.bufferSize];
+		int len;
+		while (0 <= (len = in.read(buffer))) {
+			out.write(buffer, 0, len);
+			out.flush();
+		}
 	}
 
 }
