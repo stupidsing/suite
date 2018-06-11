92bc75586896f4c6077b7c09d04744a8c5588ec2
stupidsing <stupidsing@gmail.com>
2018-06-11 20:45:36 +0800

    refactor

--- src/main/java/suite/btree/impl/B_TreeBuilder.java
+++ src/main/java/suite/btree/impl/B_TreeBuilder.java
@@ -35,78 +35,6 @@ public class B_TreeBuilder<Key, Value> {
 	private Serializer<Key> keySerializer;
 	private Serializer<Value> valueSerializer;
 
-	private class B_TreeSuperblockSerializer implements Serializer<B_TreeImpl<Key, Value>.Superblock> {
-		private B_TreeImpl<Key, Value> b_tree;
-
-		private B_TreeSuperblockSerializer(B_TreeImpl<Key, Value> b_tree) {
-			this.b_tree = b_tree;
-		}
-
-		public B_TreeImpl<Key, Value>.Superblock read(DataInput_ dataInput) throws IOException {
-			var superblock = b_tree.new Superblock();
-			superblock.root = serialize.int_.read(dataInput);
-			return superblock;
-		}
-
-		public void write(DataOutput_ dataOutput, B_TreeImpl<Key, Value>.Superblock value) throws IOException {
-			serialize.int_.write(dataOutput, value.root);
-		}
-	}
-
-	private class B_TreePageSerializer implements Serializer<B_TreeImpl<Key, Value>.Page> {
-		private B_TreeImpl<Key, Value> b_tree;
-
-		private B_TreePageSerializer(B_TreeImpl<Key, Value> b_tree) {
-			this.b_tree = b_tree;
-		}
-
-		public B_TreeImpl<Key, Value>.Page read(DataInput_ dataInput) throws IOException {
-			var pointer = dataInput.readInt();
-			var size = dataInput.readInt();
-			var page = b_tree.new Page(pointer);
-
-			for (var i = 0; i < size; i++) {
-				var key = keySerializer.read(dataInput);
-				var nodeType = dataInput.readChar();
-
-				if (nodeType == BRANCH) {
-					var branch = dataInput.readInt();
-					page.add(b_tree.new KeyPointer(key, b_tree.new Branch(branch)));
-				} else if (nodeType == LEAF) {
-					var value = valueSerializer.read(dataInput);
-					page.add(b_tree.new KeyPointer(key, b_tree.new Leaf(value)));
-				} else if (nodeType == PAYLOAD) {
-					var pointer1 = dataInput.readInt();
-					page.add(b_tree.new KeyPointer(key, b_tree.new Payload(pointer1)));
-				} else if (nodeType == TERMINAL)
-					page.add(b_tree.new KeyPointer(key, b_tree.new Terminal()));
-			}
-
-			return page;
-		}
-
-		public void write(DataOutput_ dataOutput, B_TreeImpl<Key, Value>.Page page) throws IOException {
-			dataOutput.writeInt(page.pointer);
-			dataOutput.writeInt(page.size());
-
-			for (var kp : page) {
-				keySerializer.write(dataOutput, kp.key);
-
-				if (kp.pointer instanceof B_TreeImpl.Branch) {
-					dataOutput.writeChar(BRANCH);
-					dataOutput.writeInt(kp.getBranchPointer());
-				} else if (kp.pointer instanceof B_TreeImpl.Leaf) {
-					dataOutput.writeChar(LEAF);
-					valueSerializer.write(dataOutput, kp.getLeafValue());
-				} else if (kp.pointer instanceof B_TreeImpl.Payload) {
-					dataOutput.writeChar(PAYLOAD);
-					dataOutput.writeInt(kp.getPayloadPointer());
-				} else if (kp.pointer instanceof B_TreeImpl.Terminal)
-					dataOutput.writeChar(TERMINAL);
-			}
-		}
-	}
-
 	public static <Key> Pair<B_Tree<Key, Integer>, KeyDataStore<Key>> build( //
 			boolean isNew, //
 			Path path, //
@@ -147,9 +75,9 @@ public class B_TreeBuilder<Key, Value> {
 			var b_tree = new B_TreeImpl<Key, Value>(Object_.nullsFirst(cmp));
 
 			var als = serialize.bytes(pageSize);
-			var sbs = new B_TreeSuperblockSerializer(b_tree);
+			var sbs = superblockSerializer(b_tree);
 			var pys = serialize.bytes(pageSize);
-			var ps = new B_TreePageSerializer(b_tree);
+			var ps = pageSerializer(b_tree);
 
 			var alf = SerializedFileFactory.serialized(alf0, als);
 			var sbf = SerializedFileFactory.serialized(sbf0, sbs);
@@ -163,6 +91,70 @@ public class B_TreeBuilder<Key, Value> {
 			b_tree.setBranchFactor(16);
 			return b_tree;
 		});
+	}
+
+	private Serializer<B_TreeImpl<Key, Value>.Superblock> superblockSerializer(B_TreeImpl<Key, Value> b_tree) {
+		return new Serializer<>() {
+			public B_TreeImpl<Key, Value>.Superblock read(DataInput_ dataInput) throws IOException {
+				var superblock = b_tree.new Superblock();
+				superblock.root = serialize.int_.read(dataInput);
+				return superblock;
+			}
+
+			public void write(DataOutput_ dataOutput, B_TreeImpl<Key, Value>.Superblock value) throws IOException {
+				serialize.int_.write(dataOutput, value.root);
+			}
+		};
+	}
+
+	private Serializer<B_TreeImpl<Key, Value>.Page> pageSerializer(B_TreeImpl<Key, Value> b_tree) {
+		return new Serializer<>() {
+			public B_TreeImpl<Key, Value>.Page read(DataInput_ dataInput) throws IOException {
+				var pointer = dataInput.readInt();
+				var size = dataInput.readInt();
+				var page = b_tree.new Page(pointer);
+
+				for (var i = 0; i < size; i++) {
+					var key = keySerializer.read(dataInput);
+					var nodeType = dataInput.readChar();
+
+					if (nodeType == BRANCH) {
+						var branch = dataInput.readInt();
+						page.add(b_tree.new KeyPointer(key, b_tree.new Branch(branch)));
+					} else if (nodeType == LEAF) {
+						var value = valueSerializer.read(dataInput);
+						page.add(b_tree.new KeyPointer(key, b_tree.new Leaf(value)));
+					} else if (nodeType == PAYLOAD) {
+						var pointer1 = dataInput.readInt();
+						page.add(b_tree.new KeyPointer(key, b_tree.new Payload(pointer1)));
+					} else if (nodeType == TERMINAL)
+						page.add(b_tree.new KeyPointer(key, b_tree.new Terminal()));
+				}
+
+				return page;
+			}
+
+			public void write(DataOutput_ dataOutput, B_TreeImpl<Key, Value>.Page page) throws IOException {
+				dataOutput.writeInt(page.pointer);
+				dataOutput.writeInt(page.size());
+
+				for (var kp : page) {
+					keySerializer.write(dataOutput, kp.key);
+
+					if (kp.pointer instanceof B_TreeImpl.Branch) {
+						dataOutput.writeChar(BRANCH);
+						dataOutput.writeInt(kp.getBranchPointer());
+					} else if (kp.pointer instanceof B_TreeImpl.Leaf) {
+						dataOutput.writeChar(LEAF);
+						valueSerializer.write(dataOutput, kp.getLeafValue());
+					} else if (kp.pointer instanceof B_TreeImpl.Payload) {
+						dataOutput.writeChar(PAYLOAD);
+						dataOutput.writeInt(kp.getPayloadPointer());
+					} else if (kp.pointer instanceof B_TreeImpl.Terminal)
+						dataOutput.writeChar(TERMINAL);
+				}
+			}
+		};
 	}
 
 }
