719c8f4640cb2eef4ebfa08e7c69b829b43596d5
stupidsing <stupidsing@gmail.com>
2018-06-18 10:09:48 +0800

    refactor

--- src/main/java/suite/node/parser/IterativeParser.java
+++ src/main/java/suite/node/parser/IterativeParser.java
@@ -16,6 +16,7 @@ import suite.node.util.Context;
 import suite.node.util.Singleton;
 import suite.text.Preprocess;
 import suite.util.Fail;
+import suite.util.FunUtil.Sink;
 import suite.util.String_;
 
 /**
@@ -37,81 +38,6 @@ public class IterativeParser {
 		terminalParser = new TerminalParser(context);
 	}
 
-	public Node parse(String in0) {
-		var in1 = Preprocess.transform(PreprocessorFactory.create(operators), in0).t0;
-		return new Parse(in1).parse();
-	}
-
-	private class Parse {
-		private String in;
-		private Deque<Section> stack = new ArrayDeque<>();
-
-		private Parse(String in) {
-			this.in = in;
-		}
-
-		private Node parse() {
-			var lex = new Lexer(operators, in);
-			stack.push(new Section(' '));
-			Token token;
-
-			while ((token = lex.lex()) != null) {
-				var operator = token.operator;
-				var data = token.getData();
-				var ch = data.charAt(0);
-
-				if (operator != null) {
-					addOperator(operator);
-					if (operator == TermOp.BRACES)
-						stack.push(new Section('{'));
-				} else if (ch == '(' || ch == '[' || ch == '{')
-					stack.push(new Section(ch));
-				else if (ch == ')' || ch == ']' || ch == '}') {
-					var section = stack.pop();
-
-					if (section.kind == '(' && ch == ')' //
-							|| section.kind == '[' && ch == ']' //
-							|| section.kind == '{' && ch == '}') {
-						var node = section.unwind(null).getRight();
-						if (ch == ']')
-							node = TreeTuple.of(Atom.of("["), node);
-						add(node);
-					} else
-						Fail.t("cannot parse " + in);
-				} else if (ch == '`')
-					if (stack.peek().kind == ch) {
-						var node = stack.pop().unwind(null).getRight();
-						node = TreeTuple.of(Atom.of("`"), node);
-						add(node);
-					} else
-						stack.push(new Section(ch));
-				else if (String_.isNotBlank(data))
-					add(terminalParser.parseTerminal(data));
-			}
-
-			if (stack.size() == 1)
-				return stack.pop().unwind(null).getRight();
-			else
-				return Fail.t("cannot parse " + in);
-		}
-
-		private void add(Node node) {
-			var section = stack.peek();
-			if (!section.isDanglingRight)
-				addOperator(TermOp.TUPLE_);
-			Tree.forceSetRight(section.list.getLast(), node);
-			section.isDanglingRight = false;
-		}
-
-		private void addOperator(Operator operator) {
-			var section = stack.peek();
-			var tree = section.unwind(operator);
-			var tree1 = Tree.of(operator, tree.getRight(), Atom.NIL);
-			Tree.forceSetRight(tree, tree1);
-			section.push(tree1);
-		}
-	}
-
 	private class Section {
 		private char kind;
 		private Deque<Tree> list = new ArrayDeque<>(List.of(Tree.of(null, null, Atom.NIL)));
@@ -125,11 +51,10 @@ public class IterativeParser {
 			var prec0 = operator != null ? operator.precedence() : -1;
 			Operator op;
 			Tree tree;
-
 			while ((op = (tree = list.getLast()).getOperator()) != null) {
 				var prec1 = op.precedence();
 				if (prec0 < prec1 || operator.assoc() == Assoc.LEFT && prec0 == prec1)
-					pop();
+					list.removeLast();
 				else
 					break;
 			}
@@ -140,10 +65,67 @@ public class IterativeParser {
 			list.addLast(tree);
 			isDanglingRight = true;
 		}
+	}
 
-		private Tree pop() {
-			return list.removeLast();
+	public Node parse(String in0) {
+		var in = Preprocess.transform(PreprocessorFactory.create(operators), in0).t0;
+		var stack = new ArrayDeque<Section>();
+
+		Sink<Operator> addOperator = operator -> {
+			var section = stack.peek();
+			var tree = section.unwind(operator);
+			var tree1 = Tree.of(operator, tree.getRight(), Atom.NIL);
+			Tree.forceSetRight(tree, tree1);
+			section.push(tree1);
+		};
+
+		Sink<Node> add = node -> {
+			var section = stack.peek();
+			if (!section.isDanglingRight)
+				addOperator.sink(TermOp.TUPLE_);
+			Tree.forceSetRight(section.list.getLast(), node);
+			section.isDanglingRight = false;
+		};
+
+		var lex = new Lexer(operators, in);
+		stack.push(new Section(' '));
+		Token token;
+
+		while ((token = lex.lex()) != null) {
+			var operator = token.operator;
+			var data = token.getData();
+			var ch = data.charAt(0);
+
+			if (operator != null) {
+				addOperator.sink(operator);
+				if (operator == TermOp.BRACES)
+					stack.push(new Section('{'));
+			} else if (ch == '(' || ch == '[' || ch == '{')
+				stack.push(new Section(ch));
+			else if (ch == ')' || ch == ']' || ch == '}') {
+				var section = stack.pop();
+
+				if (section.kind == '(' && ch == ')' //
+						|| section.kind == '[' && ch == ']' //
+						|| section.kind == '{' && ch == '}') {
+					var node = section.unwind(null).getRight();
+					if (ch == ']')
+						node = TreeTuple.of(Atom.of("["), node);
+					add.sink(node);
+				} else
+					Fail.t("cannot parse " + in);
+			} else if (ch == '`')
+				if (stack.peek().kind == ch) {
+					var node = stack.pop().unwind(null).getRight();
+					node = TreeTuple.of(Atom.of("`"), node);
+					add.sink(node);
+				} else
+					stack.push(new Section(ch));
+			else if (String_.isNotBlank(data))
+				add.sink(terminalParser.parseTerminal(data));
 		}
+
+		return stack.size() == 1 ? stack.pop().unwind(null).getRight() : Fail.t("cannot parse " + in);
 	}
 
 }
