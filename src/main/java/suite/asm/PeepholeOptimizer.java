ade093058ab9c985b33455194e33af4cd8420322
stupidsing <stupidsing@gmail.com>
2018-06-23 06:48:47 +0800

    refactor

--- src/main/java/suite/asm/PeepholeOptimizer.java
+++ src/main/java/suite/asm/PeepholeOptimizer.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import suite.BindArrayUtil.Pattern;
 import suite.Suite;
+import suite.adt.pair.FixieArray;
 import suite.adt.pair.Pair;
 import suite.node.Atom;
 import suite.node.Int;
@@ -25,29 +26,30 @@ public class PeepholeOptimizer {
 			Node node1;
 			Node[] m;
 
-			if ((m = ADDI__.match(node0)) != null) {
-				var m0 = m[0];
-				var i = TreeUtil.evaluate(m[1]);
-				if (i == 1)
-					node1 = Suite.substitute("INC .0", m0);
-				else if (i == -1)
-					node1 = Suite.substitute("DEC .0", m0);
-				else if (0 < i)
-					node1 = Suite.substitute("ADD (.0, .1)", m0, Int.of(i));
-				else if (i < 0)
-					node1 = Suite.substitute("SUB (.0, .1)", m0, Int.of(-i));
-				else
-					node1 = Atom.NIL;
-			} else if ((m = MOV___.match(node0)) != null) {
-				var m0 = m[0];
-				var m1 = m[1];
-				if (m0 == m1)
-					node1 = Atom.NIL;
-				else if (m0 instanceof Atom && m1 instanceof Int && Int.num(m1) == 0)
-					node1 = Suite.substitute("XOR (.0, .0)", m0);
-				else
-					node1 = Suite.substitute("MOV (.0, .1)", m0, m1);
-			} else
+			if ((m = ADDI__.match(node0)) != null)
+				node1 = FixieArray.of(m).map((m0, m1) -> {
+					var i = TreeUtil.evaluate(m1);
+					if (i == 1)
+						return Suite.substitute("INC .0", m0);
+					else if (i == -1)
+						return Suite.substitute("DEC .0", m0);
+					else if (0 < i)
+						return Suite.substitute("ADD (.0, .1)", m0, Int.of(i));
+					else if (i < 0)
+						return Suite.substitute("SUB (.0, .1)", m0, Int.of(-i));
+					else
+						return Atom.NIL;
+				});
+			else if ((m = MOV___.match(node0)) != null)
+				node1 = FixieArray.of(m).map((m0, m1) -> {
+					if (m0 == m1)
+						return Atom.NIL;
+					else if (m0 instanceof Atom && m1 instanceof Int && Int.num(m1) == 0)
+						return Suite.substitute("XOR (.0, .0)", m0);
+					else
+						return Suite.substitute("MOV (.0, .1)", m0, m1);
+				});
+			else
 				node1 = node0;
 
 			lnis1.add(Pair.of(lni0.t0, node1));
