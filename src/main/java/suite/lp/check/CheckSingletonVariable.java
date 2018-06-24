5ca923d894c29e6fed03ef5aeb41181e6729e1b4
stupidsing <stupidsing@gmail.com>
2018-06-24 21:55:49 +0800

    refactor

--- src/main/java/suite/lp/check/CheckSingletonVariable.java
+++ src/main/java/suite/lp/check/CheckSingletonVariable.java
@@ -18,37 +18,35 @@ public class CheckSingletonVariable {
 	public void check(List<Rule> rules) {
 		var rulesByPrototype = Prototype.multimap(rules);
 
-		for (var e : rulesByPrototype.entries()) {
-			var prototype = e.t0;
-			var rule = e.t1;
-
-			var scanner = new Scanner();
-			scanner.scan(rule.head);
-			scanner.scan(rule.tail);
-			scanner.warn(prototype);
-		}
+		for (var e : rulesByPrototype.entries())
+			e.map((prototype, rule) -> {
+				var scanner = new Scanner();
+				scanner.scan(rule.head);
+				scanner.scan(rule.tail);
+				scanner.warn(prototype);
+				return scanner;
+			});
 	}
 
 	private class Scanner {
-		private Map<Atom, Boolean> isSingleton = new HashMap<>();
+		private Map<Node, Boolean> isSingleton = new HashMap<>();
 
 		private void scan(Node node) {
 			while (true) {
 				if (node instanceof Atom) {
-					var atom = (Atom) node;
-					var name = atom.name;
+					var name = Atom.name(node);
 
 					// check all variables starting with alphabets; ignore
 					// computer-generated code
 					if (name.startsWith(ProverConstant.variablePrefix) //
 							&& 1 < name.length() //
 							&& Character.isAlphabetic(name.charAt(1))) {
-						var value = isSingleton.get(atom);
+						var value = isSingleton.get(node);
 						if (value == null)
 							value = Boolean.TRUE;
 						else if (value == Boolean.TRUE)
 							value = Boolean.FALSE;
-						isSingleton.put(atom, value);
+						isSingleton.put(node, value);
 					}
 				} else if (node instanceof Tree) {
 					var tree = (Tree) node;
