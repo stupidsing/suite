3e349a73fc336bf3af3a7b540c1efa0b6b16d18b
stupidsing <stupidsing@gmail.com>
2018-06-27 20:40:00 +0800

    refactor

--- src/test/java/MoliuTest.java
+++ src/test/java/MoliuTest.java
@@ -1,13 +1,9 @@
 import org.junit.Test;
 
-import suite.Suite;
-import suite.asm.Assembler;
-
 public class MoliuTest {
 
 	@Test
-	public void testAssemblePerformance() {
-		new Assembler(32).assemble(Suite.parse(".org = 0, .l CLD (),"));
+	public void test() {
 	}
 
 }
