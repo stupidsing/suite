44a9afcfcfb10184246c66f93223572cb3cbc044
stupidsing <stupidsing@gmail.com>
2018-07-11 06:10:57 +0800

    refactor

--- src/test/java/suite/funp/CrudeScriptTest.java
+++ src/test/java/suite/funp/CrudeScriptTest.java
@@ -13,7 +13,7 @@ public class CrudeScriptTest {
 	public void test() {
 		var isOptimize = false;
 		var f0 = new P0CrudeScript().parse("{ return 1 + 2 * 3; }");
-		var f1 = new P1Inline().inline(f0, isOptimize ? 3 : 0, 1, 1, 1, 1, 1);
+		var f1 = new P1Inline().inline(f0, isOptimize ? 3 : 0, 1, 1, 1, 1);
 		var f2 = new P2InferType().infer(f1);
 		var f3 = new P3Optimize().optimize(f2);
 		var instructions = new P4GenerateCode(true).compile0(f3);
