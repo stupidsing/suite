8de75321711e587daed02db03aa191bbfe4e9c55
yw.sing <yw.sing_contractor@hactl.com>
2018-08-09 15:21:38 +0800

    refactor

--- src/main/java/suite/funp/P4Emit.java
+++ src/main/java/suite/funp/P4Emit.java
@@ -184,7 +184,7 @@ public class P4Emit {
 				if (set.add(label) && b != null) {
 					list.add(amd64.instruction(Insn.LABEL, b.in));
 					list.addAll(b.instructions);
-					var out = b.out;
+						var out = b.out;
 					if (out != null)
 						sink(out, true);
 				} else if (jump)
