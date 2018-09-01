96ccfe439f7d56be14b035d258fbb29b5d765ec8
stupidsing <stupidsing@gmail.com>
2018-09-02 07:52:10 +0800

    refactor

--- src/main/java/suite/net/NetUtil.java
+++ src/main/java/suite/net/NetUtil.java
@@ -14,20 +14,28 @@ import suite.primitive.Bytes;
 public class NetUtil {
 
 	public static int bytesToInt(Bytes bytes) {
+		return bsToInt(bytes.toArray());
+	}
+
+	public static int bsToInt(byte[] bs) {
 		int value = 0, i = 4;
 		do
-			value = value << 8 | bytes.get(--i) & 0xFF;
+			value = value << 8 | bs[--i] & 0xFF;
 		while (0 < i);
 		return value;
 	}
 
 	public static Bytes intToBytes(int value) {
+		return Bytes.of(intToBs(value));
+	}
+
+	public static byte[] intToBs(int value) {
 		var bytes = new byte[4];
 		for (var i = 0; i < 4; i++) {
 			bytes[i] = (byte) (value & 0xFF);
 			value >>>= 8;
 		}
-		return Bytes.of(bytes);
+		return bytes;
 	}
 
 	public static Bytes serialize(Object o) {
