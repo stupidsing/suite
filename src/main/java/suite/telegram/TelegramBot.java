e5c2522d54de00a0c1e969b7840acdd995542063
ywsing <stupidsing@gmail.com>
2018-09-12 20:47:18 +0800

    refactor

--- src/main/java/suite/telegram/TelegramBot.java
+++ src/main/java/suite/telegram/TelegramBot.java
@@ -5,10 +5,10 @@ import static suite.util.Friends.rethrow;
 import java.nio.file.Files;
 
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 import suite.cfg.Defaults;
 import suite.streamlet.FunUtil2.FoldOp;
