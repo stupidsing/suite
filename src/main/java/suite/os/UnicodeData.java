efcef490c843ab592787cb5f5ef914ff73adbdea
stupidsing <stupidsing@gmail.com>
2018-10-28 23:28:26 +0800

    -

--- src/main/java/suite/os/UnicodeData.java
+++ src/main/java/suite/os/UnicodeData.java
@@ -25,6 +25,26 @@ public class UnicodeData {
 		}
 	}
 
+	public char[][] brailles(int[][] pixels) {
+		var brailles = new char[pixels.length / 4][pixels[0].length / 2];
+
+		for (var y = 0; y < pixels.length; y += 4)
+			for (var x = 0; x < pixels[y].length; x += 2) {
+				var i = 0;
+				i = (i << 1) + pixels[y + 3][x + 1];
+				i = (i << 1) + pixels[y + 3][x + 0];
+				i = (i << 1) + pixels[y + 2][x + 1];
+				i = (i << 1) + pixels[y + 1][x + 1];
+				i = (i << 1) + pixels[y + 0][x + 1];
+				i = (i << 1) + pixels[y + 2][x + 0];
+				i = (i << 1) + pixels[y + 1][x + 0];
+				i = (i << 1) + pixels[y + 0][x + 0];
+				brailles[y / 4][x / 2] = (char) (0x2800 + i);
+			}
+
+		return brailles;
+	}
+
 	public Set<Character> getCharsOfClass(String uc) {
 		return classByChars.get(uc);
 	}
