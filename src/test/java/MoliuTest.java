706db77a9aec072c3e86e31653e1953f206414cd
stupidsing <stupidsing@gmail.com>
2018-11-01 21:04:24 +0800

    -

--- src/test/java/MoliuTest.java
+++ src/test/java/MoliuTest.java
@@ -1,9 +1,19 @@
 import org.junit.Test;
 
+import suite.Suite;
+import suite.math.sym.Symbolic;
+
 public class MoliuTest {
 
 	@Test
 	public void test() {
+		Symbolic sym = new Symbolic();
+		// System.out.println(sym.simplify(Suite.parse("neg (neg 1)"),
+		// Suite.parse("x")));
+		System.out.println(sym.simplify(Suite.parse("neg (neg 1 * exp neg x)"), Suite.parse("x")));
+		// System.out.println(
+		// sym.simplify(Suite.parse("neg (neg 1 * exp neg x) * inv (1 + exp neg x) * inv
+		// (1 + exp neg x)"), Suite.parse("x")));
 	}
 
 }
---
+++ src/test/java/suite/math/linalg/SingularValueDecompositionTest.java
@@ -0,0 +1,19 @@
+package suite.math.linalg;
+
+import org.junit.Test;
+
+import suite.util.To;
+
+public class SingularValueDecompositionTest {
+
+	private Matrix mtx = new Matrix();
+	private SingularValueDecomposition svd = new SingularValueDecomposition();
+	private Vector vec = new Vector();
+
+	@Test
+	public void test() {
+		var fixie = svd.svd(mtx.identity(9));
+		vec.verifyEquals(fixie.get0(), To.vector(9, i -> 1d), .01f);
+	}
+
+}
