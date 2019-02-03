b6afe211c278c5038a819603df4630eba6f526c5
stupidsing <stupidsing@gmail.com>
2019-02-03 09:37:51 +0800

    -

--- src/main/java/suite/assembler/Amd64Assemble.java
+++ src/main/java/suite/assembler/Amd64Assemble.java
@@ -408,11 +408,9 @@ public class Amd64Assemble {
 			if (instruction.op0.size == instruction.op1.size)
 				if (instruction.op1 instanceof OpImm) {
 					var op1 = (OpImm) instruction.op1;
-
-					if (instruction.op0 instanceof OpReg) {
-						var op0 = (OpReg) instruction.op0;
-						encode = new InsnCode(op1.size, op1).setByte(0xB0 + (op0.size <= 1 ? 0 : 8) + (op0.reg & 7));
-					} else if (isRm.test(instruction.op0))
+					if (instruction.op0 instanceof OpReg)
+						encode = assembleReg(instruction, 0xB0 + (op1.size <= 1 ? 0 : 8)).imm(op1);
+					else if (isRm.test(instruction.op0))
 						encode = assembleByteFlag(instruction.op0, 0xC6, 0).imm(op1);
 					else
 						encode = invalid;
