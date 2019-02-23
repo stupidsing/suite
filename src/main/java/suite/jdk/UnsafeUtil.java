39f41bd63e632357827575c4fcf27b3f5d0bd70c
ywsing <stupidsing@gmail.com>
2019-02-23 09:49:07 +0800

    -

--- src/main/java/suite/jdk/UnsafeUtil.java
+++ src/main/java/suite/jdk/UnsafeUtil.java
@@ -2,34 +2,36 @@ package suite.jdk;
 
 import static suite.util.Friends.rethrow;
 
-import sun.misc.Unsafe;
+import java.lang.reflect.Method;
 
 public class UnsafeUtil {
-
-	public Class<?> defineClass(byte[] bytes) {
-		return unsafe().defineAnonymousClass(getClass(), bytes, null);
-	}
 
 	public <T> Class<? extends T> defineClass(Class<T> interfaceClazz, String className, byte[] bytes) {
 		return defineClass(interfaceClazz, className, bytes, null);
 	}
 
 	public <T> Class<? extends T> defineClass(Class<T> interfaceClazz, String className, byte[] bytes, Object[] array) {
+		var unsafe = unsafe();
 		@SuppressWarnings("unchecked")
-		var clazz = (Class<? extends T>) unsafe().defineAnonymousClass(interfaceClazz, bytes, array);
+		var clazz = (Class<? extends T>) rethrow(() -> defineAnonClass.invoke(unsafe, interfaceClazz, bytes, array));
 		return clazz;
 	}
 
-	private Unsafe unsafe() {
+	private Object unsafe() {
+		var unsafeClazz = rethrow(() -> Class.forName("sun.misc.Unsafe"));
 		if (unsafe == null)
 			rethrow(() -> {
-				var f = Unsafe.class.getDeclaredField("theUnsafe");
+				var f = unsafeClazz.getDeclaredField("theUnsafe");
 				f.setAccessible(true);
-				return unsafe = (Unsafe) f.get(null);
+				defineAnonClass = (unsafe = f.get(null)) //
+						.getClass() //
+						.getMethod("defineAnonymousClass", Class.class, byte[].class, Object[].class);
+				return unsafe;
 			});
 		return unsafe;
 	}
 
-	private Unsafe unsafe;
+	private Object unsafe;
+	private Method defineAnonClass;
 
 }
