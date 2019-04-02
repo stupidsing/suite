19fc2e52461c7bf18d9cbbfb05c13e9c21d13505
ywsing <ywsing1117@gmail.com>
2019-04-02 18:14:03 +0800

    -

--- src/main/java/suite/lcs/LcsDp.java
+++ src/main/java/suite/lcs/LcsDp.java
@@ -7,8 +7,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
-import suite.streamlet.FunUtil;
-import suite.streamlet.FunUtil.Source;
+import suite.persistent.PerList;
 
 /**
  * Longest common subsequence using dynamic programming.
@@ -17,30 +16,42 @@ import suite.streamlet.FunUtil.Source;
  */
 public class LcsDp<T> {
 
-	private Node emptyNode = new Node(0, FunUtil.<T> nullSource());
-	private Comparator<Node> comparator = (node0, node1) -> node0.length - node1.length;
+	private Node emptyNode = new Node(0, PerList.end());
+	private Comparator<Node> comparator = Comparator.comparingInt(node -> node.length);
 
 	private class Node {
+		private T t;
 		private int length;
-		private Source<T> source;
+		private PerList<Node> list;
 
 		private Node(T t, Node node) {
-			this(node.length + 1, FunUtil.cons(t, node.source));
+			this.t = t;
+			this.length = node.length + 1;
+			this.list = PerList.cons(this, node.list);
 		}
 
-		private Node(int length, Source<T> source) {
+		private Node(int length, PerList<Node> list) {
 			this.length = length;
-			this.source = source;
+			this.list = list;
 		}
 	}
 
 	public List<T> lcs(List<T> l0, List<T> l1) {
 		int size0 = l0.size(), size1 = l1.size();
 
+		// var dp = (Node[][]) Array.newInstance(Node.class, size0, size1);
 		@SuppressWarnings("unchecked")
-		var dp = (Node[][]) Array.newInstance(Node.class, size0, size1);
+		var dp = (Node[][]) Array.newInstance(Node[].class, size0);
+		@SuppressWarnings("unchecked")
+		var unused0 = (Node[]) Array.newInstance(Node.class, size1);
+		@SuppressWarnings("unchecked")
+		var unused1 = (Node[]) Array.newInstance(Node.class, size1);
 
-		for (var i0 = 0; i0 < size0; i0++)
+		for (var i0 = 0; i0 < size0; i0++) {
+			dp[i0] = unused0;
+			unused0 = unused1;
+			unused1 = null;
+
 			for (var i1 = 0; i1 < size1; i1++) {
 				var u = 0 < i0 ? dp[i0 - 1][i1] : emptyNode;
 				var l = 0 < i1 ? dp[i0][i1 - 1] : emptyNode;
@@ -53,12 +64,19 @@ public class LcsDp<T> {
 				dp[i0][i1] = Collections.max(List.of(u, l, lu1), comparator);
 			}
 
-		var node = dp[size0 - 1][size1 - 1];
-		var source = node.source;
-		var result = new ArrayList<T>(Collections.nCopies(node.length, null));
+			if (0 < i0) {
+				unused1 = unused0;
+				unused0 = dp[i0 - 1];
+				dp[i0 - 1] = null;
+			}
+		}
 
-		for (var i = node.length - 1; 0 <= i; i--)
-			result.set(i, source.g());
+		var node = dp[size0 - 1][size1 - 1];
+		var result = new ArrayList<>(Collections.<T> nCopies(node.length, null));
+		var i = node.length;
+
+		for (var node_ : node.list)
+			result.set(--i, node_.t);
 
 		return result;
 	}
