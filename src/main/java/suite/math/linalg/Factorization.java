27873f418f06642a86961ccae8968e338d7ec70a
ywsing <stupidsing@gmail.com>
2019-04-05 08:27:32 +0800

    -

--- src/main/java/suite/math/linalg/Factorization.java
+++ src/main/java/suite/math/linalg/Factorization.java
@@ -10,13 +10,13 @@ public class Factorization {
 	private Matrix mtx = new Matrix();
 	private Random random = new Random();
 
-	public Pair<float[][], float[][]> factorize(float[][] m, int w) {
+	public Pair<float[][], float[][]> factor(float[][] m, int w) { // ALS?
 		var height = mtx.height(m);
 		var width = mtx.width(m);
 		var u = To.matrix(height, w, (i, j) -> random.nextFloat());
 		var v = To.matrix(w, width, (i, j) -> random.nextFloat());
 
-		for (var iter = 0; iter < 20; iter++) {
+		for (var iter = 0; iter < 99; iter++) {
 			// TODO check if error is small enough
 
 			var u0 = u;
@@ -25,22 +25,19 @@ public class Factorization {
 			var error = mtx.sub(m, mtx.mul(u, v));
 			var alpha = .1f;
 
-			var u1 = To.matrix(height, w, (i, q) -> {
+			u = To.matrix(height, w, (i, q) -> {
 				var sum = 0d;
 				for (var j = 0; j < width; j++)
 					sum += error[i][j] * v0[q][j];
 				return (float) (u0[i][q] + alpha * sum);
 			});
 
-			var v1 = To.matrix(w, width, (q, j) -> {
+			v = To.matrix(w, width, (q, j) -> {
 				var sum = 0d;
 				for (var i = 0; i < height; i++)
 					sum += error[i][j] * u0[i][q];
 				return (float) (v0[q][j] + alpha * sum);
 			});
-
-			u = u1;
-			v = v1;
 		}
 
 		return Pair.of(u, v);
---
+++ src/test/java/suite/math/linalg/FactorizationTest.java
@@ -0,0 +1,25 @@
+package suite.math.linalg;
+
+import org.junit.Test;
+
+public class FactorizationTest {
+
+	private Factorization fac = new Factorization();
+	private Matrix mtx = new Matrix();
+
+	@Test
+	public void test() {
+		var m = mtx.identity(3);
+		m[2][2] = 0f;
+
+		var uv = fac.factor(m, 2).map((u, v) -> {
+			System.out.println(mtx.toString(u));
+			System.out.println(mtx.toString(v));
+			return mtx.mul(u, v);
+		});
+
+		System.out.println(mtx.toString(uv));
+		mtx.verifyEquals(m, uv, .01f);
+	}
+
+}
