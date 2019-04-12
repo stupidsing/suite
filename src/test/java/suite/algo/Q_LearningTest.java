b85c9d580b9d272b82e0c600ec443af62aedb958
ywsing <ywsing1117@gmail.com>
2019-04-12 17:41:01 +0800

    -

---
+++ src/test/java/suite/algo/Q_LearningTest.java
@@ -0,0 +1,176 @@
+package suite.algo;
+
+import static suite.util.Friends.fail;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
+
+import org.junit.Test;
+
+import suite.primitive.adt.pair.IntDblPair;
+import suite.primitive.adt.pair.IntIntPair;
+
+// https://towardsdatascience.com/reinforcement-learning-with-python-8ef0242a2fa2
+public class Q_LearningTest {
+
+	private Random random = new Random();
+
+	private List<IntIntPair> locations = List.of( //
+			IntIntPair.of(0, 0), //
+			IntIntPair.of(0, 4), //
+			IntIntPair.of(4, 0), //
+			IntIntPair.of(4, 3));
+
+	private int nStates = 5 * 5 * 5 * 4;
+	private int nActions = 6;
+	private int onTaxi = locations.size();
+
+	private Set<IntIntPair> noLeft = Set.of( //
+			IntIntPair.of(0, 2), //
+			IntIntPair.of(3, 1), //
+			IntIntPair.of(3, 3), //
+			IntIntPair.of(4, 1), //
+			IntIntPair.of(4, 3));
+
+	private Set<IntIntPair> noRight = Set.of( //
+			IntIntPair.of(0, 1), //
+			IntIntPair.of(3, 0), //
+			IntIntPair.of(3, 2), //
+			IntIntPair.of(4, 0), //
+			IntIntPair.of(4, 2));
+
+	private class Result {
+		private State state;
+		private int reward;
+		private boolean done;
+
+		private Result(State state, int reward) {
+			this(state, reward, false);
+		}
+
+		private Result(State state, int reward, boolean done) {
+			this.state = state;
+			this.reward = reward;
+			this.done = done;
+		}
+	}
+
+	private class State {
+		private IntIntPair taxi;
+		private int passenger;
+		private int destination;
+
+		private State(IntIntPair taxi, int passenger, int destination) {
+			this.taxi = taxi;
+			this.passenger = passenger;
+			this.destination = destination;
+		}
+
+		private Result move(int action) {
+			if (action == 0) // up
+				if (taxi.t0 != 0)
+					return new Result(new State(IntIntPair.of(taxi.t0 - 1, taxi.t1), passenger, destination), -1);
+				else
+					return new Result(this, -1);
+			else if (action == 1) // down
+				if (taxi.t0 != 4)
+					return new Result(new State(IntIntPair.of(taxi.t0 + 1, taxi.t1), passenger, destination), -1);
+				else
+					return new Result(this, -1);
+			else if (action == 2) // left
+				if (taxi.t1 != 0 && !noLeft.contains(taxi))
+					return new Result(new State(IntIntPair.of(taxi.t0, taxi.t1 - 1), passenger, destination), -1);
+				else
+					return new Result(this, -1);
+			else if (action == 3) // right
+				if (taxi.t1 != 4 && !noRight.contains(taxi))
+					return new Result(new State(IntIntPair.of(taxi.t0, taxi.t1 + 1), passenger, destination), -1);
+				else
+					return new Result(this, -1);
+			else if (action == 4) // pickup
+				if (passenger != onTaxi && Objects.equals(taxi, locations.get(passenger)))
+					return new Result(new State(taxi, onTaxi, destination), -1);
+				else
+					return new Result(this, -10);
+			else if (action == 5) // drop-off
+				if (passenger == onTaxi && Objects.equals(taxi, locations.get(destination)))
+					return new Result(this, 20, true);
+				else
+					return new Result(this, -10);
+			else
+				return fail();
+		}
+
+		private int encode() {
+			var c = passenger;
+			c = taxi.t0 + 5 * c;
+			c = taxi.t1 + 5 * c;
+			c = destination + 4 * c;
+			return c;
+		}
+	}
+
+	private State decode(int c) {
+		var destination = c % 4;
+		c /= 4;
+		var taxiy = c % 5;
+		c /= 5;
+		var taxix = c % 5;
+		c /= 5;
+		var passenger = c % 5;
+		return new State(IntIntPair.of(taxix, taxiy), passenger, destination);
+	}
+
+	@Test
+	public void test() {
+		var alpha = .1d;
+		var gamma = .6d;
+		var epsilon = .1d;
+
+		var nalpha = 1d - alpha;
+		var q = new float[nStates][nActions];
+		State state;
+		boolean done;
+
+		for (var iter = 0; iter < 1024; iter++) {
+			state = new State(IntIntPair.of(3, 1), 0, 3);
+			done = false;
+
+			while (!done) {
+				var qst = q[state.encode()];
+				IntDblPair actionValue;
+
+				if (random.nextDouble() < epsilon) {
+					var action = random.nextInt(nActions);
+					actionValue = IntDblPair.of(action, qst[action]);
+				} else {
+					actionValue = IntDblPair.of(-1, Integer.MIN_VALUE);
+					for (var action = 0; action < nActions; action++) {
+						var value = qst[action];
+						if (actionValue.t1 < value)
+							actionValue = IntDblPair.of(action, value);
+					}
+				}
+
+				var action = actionValue.t0;
+				var result = state.move(action);
+				var value0 = qst[action];
+				var state1 = result.state;
+				var qst1 = q[state1.encode()];
+				var max = Double.MIN_VALUE;
+
+				for (var value : qst1)
+					max = Math.max(max, value);
+
+				qst[action] = (float) (nalpha * value0 + alpha * (result.reward + gamma * max));
+				state = state1;
+				done = result.done;
+			}
+		}
+
+		decode(0);
+	}
+
+}
