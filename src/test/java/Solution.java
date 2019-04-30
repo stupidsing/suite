0d2f0134ec0f35880f44172da5e99a2ef267d61c
ywsing <ywsing1117@gmail.com>
2019-04-30 17:08:54 +0800

    -

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -7,10 +7,11 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 		int t = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
-		for (int i = 1; i <= t; ++i) {
+		for (int c = 1; c <= t; ++c) {
 			int m = in.nextInt();
 			int n = in.nextInt();
-			System.out.println("Case #" + i + ": " + (m + n) + " " + (m * n));
+			String out = "" + (m + n);
+			System.out.println("Case #" + c + ": " + out);
 		}
 		in.close();
 	}
