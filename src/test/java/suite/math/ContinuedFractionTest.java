bc16167783539b31775316028ad7de58e7ca3529
ywsing <ywsing1117@gmail.com>
2019-05-21 15:24:13 +0800

    -

--- src/test/java/suite/math/ContinuedFractionTest.java
+++ src/test/java/suite/math/ContinuedFractionTest.java
@@ -3,8 +3,11 @@ package suite.math;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
+import suite.primitive.Ints;
 import suite.primitive.Ints.IntsBuilder;
 import suite.primitive.adt.pair.IntIntPair;
 
@@ -12,26 +15,38 @@ public class ContinuedFractionTest {
 
 	@Test
 	public void test() {
-		IntIntPair r = IntIntPair.of(649, 200);
+		var r = IntIntPair.of(649, 200);
+		var ints = toContinuedFraction(r);
+		assertTrue(Arrays.equals(new int[] { 3, 4, 12, 4, }, ints.toArray()));
+		var p = ints.size();
+		var f = toFraction(ints, p);
+		assertEquals(IntIntPair.of(649, 200), f);
+	}
+
+	private Ints toContinuedFraction(IntIntPair r) {
 		var ib = new IntsBuilder();
 
 		// https://en.wikipedia.org/wiki/Continued_fraction#Calculating_continued_fraction_representations
 		while (r.t1 != 0) {
 			var ip = r.t0 / r.t1;
-			var fp = IntIntPair.of(r.t0 - ip * r.t1, r.t1);
-
-			var gcd = gcd(fp.t0, fp.t1);
-			r = IntIntPair.of(fp.t1 / gcd, fp.t0 / gcd);
-
+			r = simplify(IntIntPair.of(r.t1, r.t0 - ip * r.t1));
 			ib.append(ip);
 		}
 
 		var ints = ib.toInts();
-		assertTrue(ints.size() == 4);
-		assertEquals(3, ints.get(0));
-		assertEquals(4, ints.get(1));
-		assertEquals(12, ints.get(2));
-		assertEquals(4, ints.get(3));
+		return ints;
+	}
+
+	private IntIntPair toFraction(Ints ints, int p) {
+		var f = IntIntPair.of(ints.get(--p), 1);
+		while (0 < p)
+			f = simplify(IntIntPair.of(ints.get(--p) * f.t0 + f.t1, f.t0));
+		return f;
+	}
+
+	private IntIntPair simplify(IntIntPair f) {
+		var gcd = gcd(f.t0, f.t1);
+		return IntIntPair.of(f.t0 / gcd, f.t1 / gcd);
 	}
 
 	// a < b
