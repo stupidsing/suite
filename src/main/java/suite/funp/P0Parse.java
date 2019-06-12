32314b73114c15905903dac5167c5ca6f5358b20
ywsing <ywsing1117@gmail.com>
2019-06-12 14:15:22 +0800

    -

--- src/main/java/suite/funp/P0Parse.java
+++ src/main/java/suite/funp/P0Parse.java
@@ -1,7 +1,5 @@
 package suite.funp;
 
-import static suite.util.Friends.forInt;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,8 +40,6 @@ import suite.funp.P0.FunpRepeat;
 import suite.funp.P0.FunpSizeOf;
 import suite.funp.P0.FunpStruct;
 import suite.funp.P0.FunpTag;
-import suite.funp.P0.FunpTagId;
-import suite.funp.P0.FunpTagValue;
 import suite.funp.P0.FunpTree;
 import suite.funp.P0.FunpTypeCheck;
 import suite.funp.P0.FunpVariable;
@@ -62,14 +58,11 @@ import suite.node.util.TreeUtil;
 import suite.persistent.PerMap;
 import suite.persistent.PerSet;
 import suite.primitive.IntMutable;
-import suite.primitive.IntPrimitives.IntObj_Obj;
-import suite.primitive.IntPrimitives.Int_Obj;
 import suite.streamlet.FunUtil.Fun;
 import suite.streamlet.FunUtil.Iterate;
 import suite.streamlet.FunUtil.Source;
 import suite.streamlet.Read;
 import suite.streamlet.Streamlet2;
-import suite.util.Switch;
 import suite.util.To;
 import suite.util.Util;
 
@@ -401,7 +394,7 @@ public class P0Parse {
 			var value = p(b);
 			var then = new Parse(vns_.streamlet().fold(vns, PerSet::add)).p(c);
 			var else_ = p(d);
-			var f0 = new Bind(vns_).bind(be, value, then, else_);
+			var f0 = new P0DBind(vns_).bind(be, value, then, else_);
 			var f1 = FunpTypeCheck.of(be, value, f0);
 			return vns_.streamlet().<Funp> fold(f1, (f, vn) -> FunpDefine.of(vn, FunpDontCare.of(), f, Fdt.L_MONO));
 		}
@@ -416,72 +409,6 @@ public class P0Parse {
 
 		private Parse nv(String vn) {
 			return new Parse(vns.replace(vn));
-		}
-	}
-
-	private class Bind {
-		private PerSet<String> vns;
-
-		private Bind(PerSet<String> vns) {
-			this.vns = vns;
-		}
-
-		private Funp bind(Funp be, Funp value, Funp then, Funp else_) {
-			IntObj_Obj<Int_Obj<Funp>, Funp> bindArray = (size0, fun0) -> {
-				var fun1 = new Switch<Int_Obj<Funp>>(value //
-				).applyIf(FunpArray.class, g -> {
-					var elements = g.elements;
-					return size0 == elements.size() ? elements::get : null;
-				}).applyIf(FunpRepeat.class, g -> g.apply((count, expr) -> {
-					Int_Obj<Funp> fun_ = i -> expr;
-					return size0 == count ? fun_ : null;
-				})).applyIf(Funp.class, g -> {
-					return i -> FunpIndex.of(FunpReference.of(value), FunpNumber.ofNumber(i));
-				}).result();
-
-				return forInt(size0).fold(then, (i, then_) -> bind(fun0.apply(i), fun1.apply(i), then_, else_));
-			};
-
-			if (be instanceof FunpBoolean && value instanceof FunpBoolean)
-				return ((FunpBoolean) be).b == ((FunpBoolean) value).b ? then : else_;
-			else if (be instanceof FunpNumber && value instanceof FunpNumber)
-				return ((FunpNumber) be).i == ((FunpNumber) value).i ? then : else_;
-			else {
-				var result = be.sw( //
-				).applyIf(FunpArray.class, f -> f.apply(elements0 -> {
-					return bindArray.apply(elements0.size(), elements0::get);
-				})).applyIf(FunpDontCare.class, f -> {
-					return then;
-				}).applyIf(FunpReference.class, f -> f.apply(expr -> {
-					return bind(expr, FunpDeref.of(value), then, else_);
-				})).applyIf(FunpRepeat.class, f -> f.apply((size0, expr0) -> {
-					return bindArray.apply(size0, i -> expr0);
-				})).applyIf(FunpStruct.class, f -> f.apply(pairs0 -> {
-					var pairs1 = value.cast(FunpStruct.class, g -> g.pairs);
-					var size0 = pairs0.size();
-
-					Int_Obj<Funp> fun = pairs1 != null && size0 == pairs1.size() //
-							? i -> pairs1.get(i).t1 //
-							: i -> FunpField.of(FunpReference.of(value), pairs0.get(i).t0);
-
-					return forInt(size0).fold(then, (i, then_) -> bind(pairs0.get(i).t1, fun.apply(i), then_, else_));
-				})).applyIf(FunpTag.class, f -> f.apply((id, tag, value_) -> {
-					return new Switch<Funp>(value //
-					).applyIf(FunpTag.class, g -> g.apply((id1, tag1, value1) -> {
-						return id.value() == id1.value() ? bind(value_, value1, then, else_) : else_;
-					})).applyIf(Funp.class, g -> {
-						var ref = FunpReference.of(value);
-
-						// FIXME double else
-						var bind = bind(value_, FunpTagValue.of(ref, tag), then, else_);
-						return FunpIf.of(FunpTree.of(TermOp.EQUAL_, FunpNumber.of(id), FunpTagId.of(ref)), bind, else_);
-					}).result();
-				})).applyIf(FunpVariable.class, f -> f.apply(var -> {
-					return vns.contains(var) ? FunpDoAssignVar.of(f, value, then) : be;
-				})).result();
-
-				return result != null ? result : FunpIf.of(FunpTree.of(TermOp.EQUAL_, be, value), then, else_);
-			}
 		}
 	}
 
