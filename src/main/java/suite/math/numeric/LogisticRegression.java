ce7cb26877c4d98bf98562d664e4a05ab671427c
ywsing <ywsing1117@gmail.com>
2019-06-21 13:58:57 +0800

    add logistic regression

---
+++ src/main/java/suite/math/numeric/LogisticRegression.java
@@ -0,0 +1,46 @@
+package suite.math.numeric;
+
+import suite.math.Sigmoid;
+import suite.math.linalg.Vector;
+import suite.streamlet.FunUtil.Fun;
+
+/**
+ * https://medium.com/@edwinvarghese4442/logistic-regression-with-gradient-descent-tutorial-part-1-theory-529c93866001
+ * 
+ * https://medium.com/@edwinvarghese4442/logistic-regression-with-gradient-descent-tutorial-part-2-code-a4544bb1505
+ *
+ * @author ywsing
+ */
+public class LogisticRegression {
+
+	private Vector vec = new Vector();
+
+	public Fun<float[], Double> train(float[][] xs, float[] ys) {
+		var learningRate = .01d;
+		var nSamples = xs.length;
+		var nParameters = xs[0].length;
+
+		var ms = new float[nParameters];
+		var b = 0d;
+
+		for (var iter = 0; iter < 2048; iter++) {
+			var errorSum = 0d;
+			var errors = new float[nParameters];
+
+			for (var i = 0; i < nSamples; i++) {
+				var output = b + vec.dot(ms, xs[i]);
+				var error = Sigmoid.sigmoid(output) - ys[i];
+				errorSum += error;
+				vec.addOn(errors, vec.scale(xs[i], error));
+			}
+
+			var inv = 1d / nSamples;
+			b -= learningRate * errorSum * inv;
+			vec.subOn(ms, vec.scaleOn(errors, learningRate * inv));
+		}
+
+		var b_ = b;
+		return xs_ -> Sigmoid.sigmoid(vec.dot(ms, xs_) + b_);
+	}
+
+}
---
+++ src/test/java/suite/math/numeric/LogisticRegressionTest.java
@@ -0,0 +1,33 @@
+package suite.math.numeric;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class LogisticRegressionTest {
+
+	private LogisticRegression lr = new LogisticRegression();
+
+	@Test
+	public void test() {
+		var xs = new float[][] { //
+				{ 2f, 1f, }, //
+				{ 4f, 1.5f, }, //
+				{ 3f, 1f, }, //
+				{ 3.5f, .5f, }, //
+				{ 2f, .5f, }, //
+				{ 5.5f, 1f, }, //
+				{ 1f, 1f, }, };
+
+		var ys = new float[] { 0f, 1f, 0f, 1f, 0f, 1f, 0f, };
+
+		var f = lr.train(xs, ys);
+
+		for (var i = 0; i < xs.length; i++) {
+			var actual = ys[i] < .5d;
+			var expected = f.apply(xs[i]) < .5d;
+			assertTrue(expected == actual);
+		}
+	}
+
+}
