915610f43e1e7c42b37efe56b9beb701364ccffc
ywsing <ywsing1117@gmail.com>
2019-07-02 08:32:05 +0800

    -

---
+++ src/main/java/suite/primitive/IntInt_Bool.java
@@ -0,0 +1,19 @@
+package suite.primitive;
+
+import static suite.util.Friends.fail;
+
+public interface IntInt_Bool {
+
+	public boolean apply(int c, int f);
+
+	public default IntInt_Bool rethrow() {
+		return (c, f) -> {
+			try {
+				return apply(c, f);
+			} catch (Exception ex) {
+				return fail("for " + c + ":" + f, ex);
+			}
+		};
+
+	}
+}
