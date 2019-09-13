64c50b01b0118db50a949aaf96fc74e3a7bd48a9
ywsing <ywsing1117@gmail.com>
2019-09-13 14:48:16 +0800

    -

--- src/main/java/suite/funp/p4/P4GenerateCode.java
+++ src/main/java/suite/funp/p4/P4GenerateCode.java
@@ -446,7 +446,7 @@ public class P4GenerateCode {
 			})).applyIf(FunpOperand.class, f -> f.apply(op -> {
 				return returnOp(op.value());
 			})).applyIf(FunpRemark.class, f -> f.apply((remark, expr) -> {
-				em.emit(Insn.NOP, amd64.remark(remark));
+				em.emit(Insn.REMARK, amd64.remark(remark));
 				return compile(expr);
 			})).applyIf(FunpRoutine.class, f -> f.apply((frame, expr, is, os) -> {
 				OpReg _ax = amd64.regs(os)[axReg];
