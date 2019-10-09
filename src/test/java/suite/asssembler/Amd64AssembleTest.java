43b8eae0f9124f34c7f63cde4eb20b6c6d69ab68
ywsing <ywsing1117@gmail.com>
2019-10-09 09:31:09 +0800

    -

--- src/test/java/suite/asssembler/Amd64AssembleTest.java
+++ src/test/java/suite/asssembler/Amd64AssembleTest.java
@@ -20,51 +20,50 @@ public class Amd64AssembleTest {
 
 	@Test
 	public void test() {
-		var push = amd64.instruction(Insn.PUSH, amd64.imm(0l, 1));
-		var bytes = new Amd64Assemble(Amd64Mode.LONG64).assemble(0l, List.of(push), true);
+		var i = amd64.instruction(Insn.PUSH, amd64.imm(0l, 1));
+		var bytes = new Amd64Assemble(Amd64Mode.LONG64).assemble(0l, List.of(i), true);
 		System.out.println(bytes);
 		assertEquals(2, bytes.size());
 	}
 
 	@Test
 	public void test16() {
-		var mov = amd64.instruction(Insn.MOV, amd64.mem(amd64.imm(0, 2), 2), amd64.imm(0x7042, 2));
-		var bytes = new Amd64Assemble(Amd64Mode.REAL16).assemble(0l, List.of(mov), true);
+		var i = amd64.instruction(Insn.MOV, amd64.mem(amd64.imm(0, 2), 2), amd64.imm(0x7042, 2));
+		var bytes = new Amd64Assemble(Amd64Mode.REAL16).assemble(0l, List.of(i), true);
 		System.out.println(bytes);
-
-		var is = new int[] { 0xC7, 0x06, 0x00, 0x00, 0x42, 0x70, };
-		var bs = IntPuller.of(is).map(i -> Bytes.of((byte) i));
-		assertEquals(Bytes.of(bs), bytes);
+		assertEquals(bytes(0xC7, 0x06, 0x00, 0x00, 0x42, 0x70), bytes);
 	}
 
 	@Test
 	public void test64() {
-		var mov = amd64.instruction(Insn.MOV, amd64.mem(amd64.reg("RBX"), 0l, 8), amd64.imm64(1l << 4));
-		var bytes = new Amd64Assemble(Amd64Mode.LONG64).assemble(0l, List.of(mov), true);
+		var i = amd64.instruction(Insn.MOV, amd64.mem(amd64.reg("RBX"), 0l, 8), amd64.imm64(1l << 4));
+		var bytes = new Amd64Assemble(Amd64Mode.LONG64).assemble(0l, List.of(i), true);
 		System.out.println(bytes);
-
-		var is = new int[] { 0x48, 0xC7, 0x03, 0x10, 0x00, 0x00, 0x00, };
-		var bs = IntPuller.of(is).map(i -> Bytes.of((byte) i));
-		assertEquals(Bytes.of(bs), bytes);
+		assertEquals(bytes(0x48, 0xC7, 0x03, 0x10, 0x00, 0x00, 0x00), bytes);
 	}
 
 	@Test
 	public void testFail() {
 		try {
-			var mov = amd64.instruction(Insn.MOV, amd64.reg("BPL"), amd64.reg("AH"));
-			new Amd64Assemble(Amd64Mode.LONG64).assemble(0l, List.of(mov), true);
+			var i = amd64.instruction(Insn.MOV, amd64.reg("BPL"), amd64.reg("AH"));
+			new Amd64Assemble(Amd64Mode.LONG64).assemble(0l, List.of(i), true);
 			assertTrue(false);
 		} catch (RuntimeException e) {
 			assertEquals("bad instruction", e.getCause().getMessage());
 		}
 
 		try {
-			var mov = amd64.instruction(Insn.MOV, amd64.mem(amd64.reg("RBX"), 0l, 8), amd64.imm64(1l << 40));
-			new Amd64Assemble(Amd64Mode.LONG64).assemble(0l, List.of(mov), true);
+			var i = amd64.instruction(Insn.MOV, amd64.mem(amd64.reg("RBX"), 0l, 8), amd64.imm64(1l << 40));
+			new Amd64Assemble(Amd64Mode.LONG64).assemble(0l, List.of(i), true);
 			assertTrue(false);
 		} catch (RuntimeException e) {
 			assertEquals("bad instruction", e.getCause().getMessage());
 		}
+	}
+
+	private Bytes bytes(int... is) {
+		var bs = IntPuller.of(is).map(i -> Bytes.of((byte) i));
+		return Bytes.of(bs);
 	}
 
 }
