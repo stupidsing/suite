22b225f7ccf7589fde2e12c7d822b1f1dcaf8793
ywsing <ywsing1117@gmail.com>
2019-12-06 13:45:48 +0800

    -

--- src/main/sh/tools-path.sh
+++ src/main/sh/tools-path.sh
@@ -2,15 +2,15 @@
 
 source <(curl -sL https://raw.githubusercontent.com/stupidsing/suite/master/src/main/sh/cache.sh)
 
-echo "ECLIPSE_HOME=$(cchs "echo http://ftp.jaist.ac.jp/pub/eclipse/technology/epp/downloads/release/2019-09/R/eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz" "#curl" "#tar-zxf" "#dir")"
-echo "GIT_HD=$(cchs "echo git@github.com:stupidsing/home-data.git" "#git-clone")"
-echo "GIT_PRIMAL=$(cchs "echo git@github.com:stupidsing/primal.git" "#git-clone")"
-echo "GIT_SUITE=$(cchs "echo git@github.com:stupidsing/suite.git" "#git-clone")"
-echo "GOROOT=$(cchs "echo https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz" "#curl" "#tar-zxf" "#dir")"
-echo "GRADLE_HOME=$(cchs "echo https://services.gradle.org/distributions/gradle-6.0.1-bin.zip" "#curl" "#unzip" "#dir")"
-echo "JAVA_HOME=$(cchs "curl -sL https://jdk.java.net/13/" "grep 'https://download.java.net/' | grep -v sha256 | grep linux | grep '\.tar\.gz'" "cut -d'\"' -f2" "#curl" "#tar-zxf" "#dir")"
-echo "M2_HOME=$(cchs "echo http://ftp.cuhk.edu.hk/pub/packages/apache.org/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz" "#curl" "#tar-zxf" "#dir")"
-echo "NODE_HOME=$(cchs "echo https://nodejs.org/dist/v12.13.1/node-v12.13.1-linux-x64.tar.xz" "#curl" "#tar-xf" "#dir")"
+echo ECLIPSE_HOME=$(cchs "echo http://ftp.jaist.ac.jp/pub/eclipse/technology/epp/downloads/release/2019-09/R/eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz" "#curl" "#tar-zxf" "#dir")
+echo GIT_HD=$(cchs "echo git@github.com:stupidsing/home-data.git" "#git-clone")
+echo GIT_PRIMAL=$(cchs "echo git@github.com:stupidsing/primal.git" "#git-clone")
+echo GIT_SUITE=$(cchs "echo git@github.com:stupidsing/suite.git" "#git-clone")
+echo GOROOT=$(cchs "echo https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz" "#curl" "#tar-zxf" "#dir")
+echo GRADLE_HOME=$(cchs "echo https://services.gradle.org/distributions/gradle-6.0.1-bin.zip" "#curl" "#unzip" "#dir")
+echo JAVA_HOME=$(cchs "curl -sL https://jdk.java.net/13/" "grep 'https://download.java.net/' | grep -v sha256 | grep linux | grep '\.tar\.gz'" "cut -d'\"' -f2" "#curl" "#tar-zxf" "#dir")
+echo M2_HOME=$(cchs "echo http://ftp.cuhk.edu.hk/pub/packages/apache.org/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz" "#curl" "#tar-zxf" "#dir")
+echo NODE_HOME=$(cchs "echo https://nodejs.org/dist/v12.13.1/node-v12.13.1-linux-x64.tar.xz" "#curl" "#tar-xf" "#dir")
 
 echo "PATH=${GIT_HD:9}/bin:${GOROOT}/bin:${GRADLE_HOME}/bin:${JAVA_HOME}/bin:${M2_HOME}/bin:${NODE_HOME}/bin:${PATH}"
 
