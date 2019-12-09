3c642e5b3c30e56954f2a20f11e9410dce6000b9
ywsing <ywsing1117@gmail.com>
2019-12-09 14:14:25 +0800

    -

--- src/main/sh/tools-path.sh
+++ src/main/sh/tools-path.sh
@@ -9,11 +9,10 @@ GIT_PRIMAL=$(cchs "echo git@github.com:stupidsing/primal.git" "#git-clone")
 GIT_SUITE=$(cchs "echo git@github.com:stupidsing/suite.git" "#git-clone")
 GOROOT=$(cchs "echo https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz" "#curl" "#tar-zxf" "#dir")
 GRADLE_HOME=$(cchs "echo https://services.gradle.org/distributions/gradle-6.0.1-bin.zip" "#curl" "#unzip" "#dir")
-JAVA_HOME=$(cchs "curl -sL https://jdk.java.net/13/" "grep https://download.java.net/ | grep -v sha256 | grep linux | grep \\.tar\\.gz" "cut -d\" -f2" "#curl" "#tar-zxf" "#dir")
+JAVA_HOME=$(cchs "curl -sL https://jdk.java.net/13/" "grep https://download.java.net/ | grep -v sha256 | grep linux | grep \\.tar\\.gz" "cut -d\\\" -f2" "#curl" "#tar-zxf" "#dir")
 M2_HOME=$(cchs "echo http://ftp.cuhk.edu.hk/pub/packages/apache.org/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz" "#curl" "#tar-zxf" "#dir")
 NODE_HOME=$(cchs "echo https://nodejs.org/dist/v12.13.1/node-v12.13.1-linux-x64.tar.xz" "#curl" "#tar-xf" "#dir")
-
-echo PATH=\${ECLIPSE_HOME}:\${GIT_HD:9}/bin:\${GOROOT}/bin:\${GRADLE_HOME}/bin:\${JAVA_HOME}/bin:\${M2_HOME}/bin:\${NODE_HOME}/bin:\${PATH}'
+PATH=\${ECLIPSE_HOME}:\${GIT_HD:9}/bin:\${GOROOT}/bin:\${GRADLE_HOME}/bin:\${JAVA_HOME}/bin:\${M2_HOME}/bin:\${NODE_HOME}/bin:\${PATH}'
 
 #cchs "echo ${GIT_PRIMAL}" "#git-cd-cmd ${M2_HOME}/bin/mvn install"
 #cchs "echo ${GIT_SUITE}" "#git-cd-cmd ./build.sh"
