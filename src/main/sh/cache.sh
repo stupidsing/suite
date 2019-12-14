a70c421f27db593a8f57daa0840bdf98b713eae2
ywsing <stupidsing@gmail.com>
2019-12-14 21:40:28 +0800

    -

--- src/main/sh/cache.sh
+++ src/main/sh/cache.sh
@@ -15,34 +15,34 @@ cchs() {
 		if [ "${CMD:0:2}" == "{}" ]; then
 			D=$(cat ${F})
 			F=$(cchf "${D}${CMD:2}")
-		elif [ "${CMD:0:3}" == "#cd" ]; then
+		elif [ "${CMD:0:3}" == "@cd" ]; then
 			D=$(cat ${F})
 			F=$(cchf "cd ${D}/; ${CMD:4}")
-		elif [ "${CMD:0:6}" == "#chmod" ]; then
+		elif [ "${CMD:0:6}" == "@chmod" ]; then
 			FILE=$(cat ${F})
 			chmod ${CMD:6} ${FILE}
 			F=$(cchf "printf ${FILE}")
-		elif [ "${CMD}" == "#curl" ]; then
+		elif [ "${CMD}" == "@curl" ]; then
 			URL=$(cat ${F})
-			DF=${DCACHE}/$(urlDir "${URL}")
+			DF=${DCACHE}/$(url-dir "${URL}")
 			[ -f ${DF} ] || do-cmd curl -sL "${URL}" > ${DF}
 			F=$(cchf "printf ${DF}")
-		elif [ "${CMD}" == "#dir" ]; then
+		elif [ "${CMD}" == "@dir" ]; then
 			DIR=$(cat ${F})
 			LINK=$(sh -c "readlink -f ${DIR}/*")
 			F=$(cchf "printf ${LINK}")
-		elif [ "${CMD:0:6}" == "#do-cd" ]; then
+		elif [ "${CMD:0:6}" == "@do-cd" ]; then
 			D=$(cat ${F})
 			F=$(cchf "cd ${D}/; ${CMD:7} 1>&2; echo ${D}")
-		elif [ "${CMD:0:10}" == "#do-git-cd" ]; then
+		elif [ "${CMD:0:10}" == "@do-git-cd" ]; then
 			D=$(cat ${F})
 			F=$(cchf "V=${D:0:8}; cd ${D:9}/; ${CMD:11} 1>&2; echo ${D}")
-		elif [ "${CMD:0:7}" == "#git-cd" ]; then
+		elif [ "${CMD:0:7}" == "@git-cd" ]; then
 			D=$(cat ${F})
 			F=$(cchf "V=${D:0:8}; cd ${D:9}/; ${CMD:8}")
-		elif [ "${CMD}" == "#git-clone" ]; then
+		elif [ "${CMD}" == "@git-clone" ]; then
 			URL=$(cat ${F})
-			DF=${DCACHE}/$(urlDir "${URL}")
+			DF=${DCACHE}/$(url-dir "${URL}")
 			if ! [ -d ${DF} ]; then
 				do-cmd "git clone --depth 1 --single-branch ${URL} ${DF} --quiet"
 				touch ${DF}.pulltime
@@ -55,10 +55,10 @@ cchs() {
 			fi
 			COMMIT=$(cd ${DF}/ && git rev-parse HEAD | cut -c1-8)
 			F=$(cchf "printf ${COMMIT}:${DF}")
-		elif [ "${CMD}" == "#git-clone-branch" ]; then
+		elif [ "${CMD}" == "@git-clone-branch" ]; then
 			URL=$(cat ${F} | cut -d' ' -f1)
 			B=$(cat ${F} | cut -d' ' -f2)
-			DF=${DCACHE}/$(urlDir "${URL}_${B}")
+			DF=${DCACHE}/$(url-dir "${URL}_${B}")
 			if ! [ -d ${DF} ]; then
 				do-cmd "git clone --depth 1 -b ${B} --single-branch ${URL} ${DF} --quiet"
 				touch ${DF}.pulltime
@@ -71,7 +71,7 @@ cchs() {
 			fi
 			COMMIT=$(cd ${DF}/ && git rev-parse HEAD | cut -c1-8)
 			F=$(cchf "printf ${COMMIT}:${DF}")
-		elif [ "${CMD:0:10}" == "#maven-get" ]; then
+		elif [ "${CMD:0:10}" == "@maven-get" ]; then
 			#REPO=https://repo.maven.apache.org/maven2
 			RGAV=$(cat ${F})
 			REPO=$(echo ${RGAV} | cut -d# -f1)
@@ -80,16 +80,16 @@ cchs() {
 			VERSION=$(echo ${RGAV} | cut -d# -f4)
 			P=$(echo ${GROUPID} | sed s#\\.#/#g)
 			URL="${REPO}/${P}/${ARTIFACTID}/${VERSION}/${ARTIFACTID}-${VERSION}.pom"
-			DF=${DCACHE}/$(urlDir "${URL}")
+			DF=${DCACHE}/$(url-dir "${URL}")
 			[ -f ${DF} ] || do-cmd curl -sL "${URL}" > ${DF}
 			F=$(cchf "printf ${DF}")
-		elif [ "${CMD:0:5}" == "#tar-" ]; then
+		elif [ "${CMD:0:5}" == "@tar-" ]; then
 			OPT=${CMD:5}
 			TARF=$(cat ${F})
 			TARDIR=${TARF}.d
 			[ -d ${TARDIR} ] || do-cmd "mkdir -p ${TARDIR} && tar ${OPT} ${TARF} -C ${TARDIR}"
 			F=$(cchf "printf ${TARDIR}")
-		elif [ "${CMD}" == "#unzip" ]; then
+		elif [ "${CMD}" == "@unzip" ]; then
 			ZIPF=$(cat ${F})
 			ZIPDIR=${ZIPF}.d
 			[ -d ${ZIPDIR} ] || do-cmd "mkdir -p ${ZIPDIR} && unzip -d ${ZIPDIR} -q ${ZIPF}"
@@ -101,6 +101,7 @@ cchs() {
 	cat ${F}
 }
 
+# executes a command if not executed before; otherwise, return previous result
 cchf() {
 	CMD="${@}"
 	MD5=$(printf "${CMD}" | md5sum - | cut -d" " -f1)
@@ -121,12 +122,6 @@ cchf() {
 	printf "${VF}"
 }
 
-urlDir() {
-	MD5=$(printf "${1}" | md5sum - | cut -d" " -f1)
-	SHORT=$(printf "${1}" | cch-fn)
-	echo ${MD5:0:8}.${SHORT}
-}
-
 do-cmd() {
 	CMD="${@}"
 	echo "START ${CMD}" >&2
@@ -134,6 +129,12 @@ do-cmd() {
 	echo "END~${?} ${CMD}" >&2
 }
 
-cch-fn() {
+url-dir() {
+	MD5=$(printf "${1}" | md5sum - | cut -d" " -f1)
+	SHORT=$(printf "${1}" | url-dir-name)
+	echo ${MD5:0:8}.${SHORT}
+}
+
+url-dir-name() {
 	tr /:@ _ | tr -dc '[\-.0-9A-Z_a-z]'
 }
--- src/main/sh/tools-path.sh
+++ src/main/sh/tools-path.sh
@@ -4,9 +4,6 @@ curl -sL https://raw.githubusercontent.com/stupidsing/suite/master/src/main/sh/c
 echo '
 ECLIPSE_HOME=$(cchs "echo http://ftp.jaist.ac.jp/pub/eclipse/technology/epp/downloads/release/2019-09/R/eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz" "#curl" "#tar-zxf" "#dir")
 GIT_HD=$(cchs "echo git@github.com:stupidsing/home-data.git" "#git-clone")
-GIT_PIECES=$(cchs "echo git@github.com:stupidsing/pieces.git" "#git-clone")
-GIT_PRIMAL=$(cchs "echo git@github.com:stupidsing/primal.git" "#git-clone")
-GIT_SUITE=$(cchs "echo git@github.com:stupidsing/suite.git" "#git-clone")
 GOROOT=$(cchs "echo https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz" "#curl" "#tar-zxf" "#dir")
 GRADLE_HOME=$(cchs "echo https://services.gradle.org/distributions/gradle-6.0.1-bin.zip" "#curl" "#unzip" "#dir")
 JAVA_HOME=$(cchs "curl -sL https://jdk.java.net/13/" "grep https://download.java.net/ | grep -v sha256 | grep linux | grep \\.tar\\.gz" "cut -d\\\" -f2" "#curl" "#tar-zxf" "#dir")
@@ -14,6 +11,9 @@ M2_HOME=$(cchs "echo http://ftp.cuhk.edu.hk/pub/packages/apache.org/maven/maven-
 NODE_HOME=$(cchs "echo https://nodejs.org/dist/v12.13.1/node-v12.13.1-linux-x64.tar.xz" "#curl" "#tar-xf" "#dir")
 PATH=${ECLIPSE_HOME}:${GIT_HD:9}/bin:${GOROOT}/bin:${GRADLE_HOME}/bin:${JAVA_HOME}/bin:${M2_HOME}/bin:${NODE_HOME}/bin:${PATH}'
 
+#GIT_PIECES=$(cchs "echo git@github.com:stupidsing/pieces.git" "#git-clone")
+#GIT_PRIMAL=$(cchs "echo git@github.com:stupidsing/primal.git" "#git-clone")
+#GIT_SUITE=$(cchs "echo git@github.com:stupidsing/suite.git" "#git-clone")
 #cchs "echo ${GIT_PRIMAL}" "#do-git-cd ${M2_HOME}/bin/mvn install"
 #cchs "echo ${GIT_SUITE}" "#do-git-cd ./build.sh"
 #cchs "echo / dump yes \#" "#git-cd ./run.sh"
