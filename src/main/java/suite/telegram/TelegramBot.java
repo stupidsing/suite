51bff8d230231cfaebe64829adf7267049df2b96
ywsing <ywsing1117@gmail.com>
2019-12-16 12:08:34 +0800

    -

--- src/main/java/suite/telegram/TelegramBot.java
+++ src/main/java/suite/telegram/TelegramBot.java
@@ -4,6 +4,9 @@ import static primal.statics.Rethrow.ex;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.function.Predicate;
 
@@ -11,37 +14,48 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.generics.BotSession;
 
 import primal.Verbs.Sleep;
 import primal.fp.Funs.Source;
 import primal.fp.Funs2.FoldOp;
+import primal.os.Log_;
 
 public class TelegramBot {
 
+	private String name = getClass().getSimpleName();
+
 	private String botUsername;
 	private Path tokenPath;
-	private Predicate<String> verifyToken;
+	private Predicate<String> login;
 	private Source<String> alert;
 	private FoldOp<Long, String> do_;
+
+	private Deque<String> alerts = new ArrayDeque<>();
 
 	public TelegramBot( //
 			String botUsername, //
 			Path tokenPath, //
-			Predicate<String> verifyToken, //
+			Predicate<String> login, //
 			Source<String> alert, //
 			FoldOp<Long, String> do_) {
 		this.botUsername = botUsername;
 		this.tokenPath = tokenPath;
-		this.verifyToken = verifyToken;
+		this.login = login;
 		this.alert = alert;
 		this.do_ = do_;
 	}
 
-	public void bot() {
-		var loggedInChatIds = new HashSet<Long>();
-		var subscribedChatIds = new HashSet<Long>();
+	public boolean bot() {
+		String interval = System.getenv("INTERVAL");
+		long intervalMs = (interval != null ? Integer.valueOf(interval) : 15) * 1000l;
 
-		ex(() -> {
+		var loggedInChatIds = Collections.synchronizedSet(new HashSet<Long>());
+		var subscribedChatIds = Collections.synchronizedSet(new HashSet<Long>());
+
+		return ex(() -> {
+			TelegramBotsApi tba = new TelegramBotsApi();
+
 			var bot = new TelegramLongPollingBot() {
 				public String getBotUsername() {
 					return botUsername;
@@ -54,13 +68,15 @@ public class TelegramBot {
 				public void onUpdateReceived(Update update) {
 					if (update.hasMessage()) {
 						var message = update.getMessage();
-						var chatId = message.getChat().getId();
 						var messageText = message.getText();
 						var messageTextLower = messageText.toLowerCase();
+						var chatId = message.getChat().getId();
+
+						Log_.info("[" + chatId + " => " + name + "] " + messageTextLower);
 
 						if (messageTextLower.startsWith("/"))
 							if (messageTextLower.startsWith("/login"))
-								if (verifyToken.test(messageText.substring(7))) {
+								if (login.test(7 <= messageText.length() ? messageText.substring(7) : "")) {
 									loggedInChatIds.add(chatId);
 									send(chatId, "FINE");
 								} else
@@ -73,13 +89,13 @@ public class TelegramBot {
 								if (messageTextLower.startsWith("/do"))
 									send(chatId, do_.apply(chatId, messageText.substring(4)));
 								else if (messageTextLower.startsWith("/status"))
-									sendYouAreSubscribed(chatId);
+									sendYouAreSubscribing(chatId);
 								else if (messageTextLower.startsWith("/subscribe")) {
 									subscribedChatIds.add(chatId);
-									sendYouAreSubscribed(chatId);
+									sendYouAreSubscribing(chatId);
 								} else if (messageTextLower.startsWith("/unsubscribe")) {
 									subscribedChatIds.remove(chatId);
-									sendYouAreSubscribed(chatId);
+									sendYouAreSubscribing(chatId);
 								} else
 									send(chatId, "UNKNOWN COMMAND");
 							else
@@ -88,27 +104,38 @@ public class TelegramBot {
 					}
 				}
 
-				private void sendYouAreSubscribed(Long chatId) {
-					var isSubscribed = subscribedChatIds.contains(chatId);
-					send(chatId, "YOU ARE " + (isSubscribed ? "" : " NOT") + " SUBSCRIBED");
+				private void sendYouAreSubscribing(Long chatId) {
+					var isSubscribing = subscribedChatIds.contains(chatId);
+					send(chatId, "YOU ARE " + (isSubscribing ? "" : " NOT") + " SUBSCRIBING");
 				}
 
-				private void send(long chatId, String text) {
+				private void send(long chatId, String messageText) {
+					Log_.info("[" + name + " => " + chatId + "]\n" + messageText);
 					var sendMessage = new SendMessage();
 					sendMessage.setChatId(Long.toString(chatId));
-					sendMessage.setText(text);
+					sendMessage.setText(messageText);
 					ex(() -> sendApiMethod(sendMessage));
 				}
 			};
 
-			new TelegramBotsApi().registerBot(bot);
+			BotSession botSession = tba.registerBot(bot);
 
-			while (true) {
-				Sleep.quietly(10000l);
+			while (botSession.isRunning()) {
+				Sleep.quietly(intervalMs);
+				String alert_ = alert.g();
 
-				for (var chatId : subscribedChatIds)
-					bot.send(chatId, "LATEST:\n" + alert.g());
+				if (!alert_.isEmpty()) {
+					for (var chatId : subscribedChatIds)
+						bot.send(chatId, "LATEST:\n" + alert_);
+
+					alerts.addLast(alert_);
+
+					while (8 < alerts.size())
+						alerts.removeFirst();
+				}
 			}
+
+			return true;
 		});
 	}
 
