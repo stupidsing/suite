4d1e057376033556e01d5d1b9cce009122c37fba
ywsing <stupidsing@gmail.com>
2019-12-18 04:25:19 +0800

    -

--- src/main/java/suite/exchange/Exchange.java
+++ src/main/java/suite/exchange/Exchange.java
@@ -13,48 +13,87 @@ import suite.trade.Trade;
 
 public class Exchange {
 
+	private boolean isLeveraged = true;
+	private int leverage = 100;
+	private double invLeverage = 1d / leverage;
+
 	private Map<String, Participant> participantById = new ConcurrentHashMap<>();
 	private Map<String, LimitOrderBook<String>> lobBySymbol = new ConcurrentHashMap<>();
 	private Map<String, MarketData> marketDataBySymbol = new ConcurrentHashMap<>();
 
 	private class Participant {
-		private Map<String, LimitOrderBook<String>.Order> orderById = new HashMap<>();
-		private Map<String, Integer> positionBySymbol = new HashMap<>();
+		private Map<String, LimitOrderBook<String>.Order> orderByOrderId = new HashMap<>();
+		private Map<String, Position> positionByPositionId = new HashMap<>();
 		private List<Trade> trades = new ArrayList<>();
 		private float balance;
+		private float marginUsed;
 
-		private synchronized boolean record(int buySell, String symbol, float price) {
-			balance -= buySell * price;
+		private synchronized boolean record(int buySell, String symbolPositionId, float price) {
+			return sp(symbolPositionId).map((symbol, positionId) -> {
+				var position = positionByPositionId.get(positionId);
 
-			positionBySymbol.compute(symbol, (s, balance0) -> {
-				var balancex = (balance0 != null ? balance0 : 0) + buySell;
-				return balancex != 0 ? balancex : null;
+				if (!isLeveraged)
+					balance -= buySell * price;
+				else
+					marginUsed += Math.abs(buySell) * price * invLeverage;
+
+				position.buySell += buySell;
+
+				if (position.buySell == 0)
+					positionByPositionId.remove(symbolPositionId);
+
+				return trades.add(Trade.of(buySell, symbol, price));
 			});
-
-			return trades.add(Trade.of(buySell, symbol, price));
 		}
 
-		private synchronized LimitOrderBook<String>.Order put(String orderId, LimitOrderBook<String>.Order order) {
-			return orderById.put(orderId, order);
+		private synchronized void put( //
+				String orderId, LimitOrderBook<String>.Order order, //
+				String positionId, Position position) {
+			orderByOrderId.put(orderId, order);
+			positionByPositionId.put(positionId, position);
 		}
 
 		private synchronized LimitOrderBook<String>.Order remove(String orderId) {
-			return orderById.remove(orderId);
+			return orderByOrderId.remove(orderId);
 		}
 	}
 
-	public synchronized void submit(String participantId, int buySell, String symbol, float price) {
-		var orderId = "O#" + Get.temp();
+	private class Position {
+		private int buySell;
+	}
+
+	public String orderNew(String participantId, int buySell, String symbol, float price) {
+		var positionId = !isLeveraged ? "" : "P" + Get.temp();
+		return submitOrder(participantId, positionId, buySell, symbol, price);
+	}
+
+	public String positionClose(String participantId, String symbolPositionId, float price) {
+		var buySell = sp(symbolPositionId).map((symbol, positionId) -> {
+			return participantById.get(participantId).positionByPositionId.get(positionId).buySell;
+		});
+		return positionClosePartially(participantId, symbolPositionId, -buySell, price);
+	}
+
+	public String positionClosePartially(String participantId, String symbolPositionId, int buySell, float price) {
+		return sp(symbolPositionId).map((symbol, positionId) -> {
+			return submitOrder(participantId, positionId, buySell, symbol, price);
+		});
+	}
+
+	private String submitOrder(String participantId, String positionId, int buySell, String symbol, float price) {
+		var orderId = "O" + Get.temp();
+		var symbolPositionId = symbol + "#" + positionId;
 		var lob = lob(symbol);
 
 		var order = lob.new Order();
-		order.id = participantId + ":" + symbol + ":" + orderId;
+		order.id = participantId + ":" + symbolPositionId + ":" + orderId;
 		order.isMarket = Float.isNaN(price);
 		order.price = !order.isMarket ? price : 0 < buySell ? Float.MAX_VALUE : Float.MIN_VALUE;
 		order.buySell = buySell;
 
 		lob.update(null, order);
-		participantById.get(participantId).put(orderId, order);
+		participantById.get(participantId).put(orderId, order, positionId, new Position());
+		return symbolPositionId;
 	}
 
 	private LimitOrderBook<String> lob(String symbol) {
@@ -62,13 +101,13 @@ public class Exchange {
 
 		return lobBySymbol.computeIfAbsent(symbol, s -> new LimitOrderBook<>(new LobListener<>() {
 			public void handleOrderFulfilled(LimitOrderBook<String>.Order order, float price, int buySell) {
-				pso(order).map((participantId, symbol, orderId) -> participantById //
+				ppo(order).map((participantId, symbolPositionId, orderId) -> participantById //
 						.get(participantId) //
-						.record(buySell, symbol, price));
+						.record(buySell, symbolPositionId, price));
 			}
 
 			public void handleOrderDisposed(LimitOrderBook<String>.Order order) {
-				pso(order).map((participantId, symbol, orderId) -> participantById //
+				ppo(order).map((participantId, symbolPositionId, orderId) -> participantById //
 						.get(participantId) //
 						.remove(orderId));
 			}
@@ -79,8 +118,12 @@ public class Exchange {
 		}));
 	}
 
-	private FixieArray<String> pso(LimitOrderBook<String>.Order order) {
+	private FixieArray<String> ppo(LimitOrderBook<String>.Order order) {
 		return FixieArray.of(order.id.split(":"));
+	}
+
+	private FixieArray<String> sp(String symbolPositionId) {
+		return FixieArray.of(symbolPositionId.split("#"));
 	}
 
 }
