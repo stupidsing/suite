f70c6e277de8ca4bb6202e6db025f9ed64e049d0
ywsing <ywsing1117@gmail.com>
2020-01-29 14:55:48 +0800

    -

--- src/main/html/eval-script.js
+++ src/main/html/eval-script.js
@@ -1,21 +1,28 @@
 'use strict';
 
 let evalscript = (() => {
-	let fetchbase = { cache: 'default', credentials: 'omit', redirect: 'follow', referrer: 'no-referrer', };
 	let cache = {};
 
-	return url => {
-		let r = cache[url];
+	return (url, expr) => {
+		let e = expr != null ? ';' + expr : '';
+		let key = url + '::' + e;
+		let r = cache[key];
+
 		return r != null
 			? Promise.resolve(r)
 			: fetch(url, {
-				...fetchbase,
-				mode: 'no-cors',
+				cache: 'default',
+				credentials: 'omit',
+				mode: 'cors',
 				method: 'GET',
+				redirect: 'follow',
+				referrer: 'no-referrer',
 			})
 			.then(response => {
-				if (response.ok) return eval(response.text()); else throw response.statusText;
+				if (response.ok) return response.text(); else throw response.statusText;
 			})
-			.catch(error => console.error('evalscript()', error));
+			.then(text => eval(text + e))
+			.then(result => cache[key] = result)
+			.catch(error => console.error('evalscript()', url, e, error));
 	};
 })();
