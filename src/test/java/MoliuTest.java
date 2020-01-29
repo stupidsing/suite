9124be06c49662e44668da7762df657a1a4e1c54
ywsing <stupidsing@gmail.com>
2020-01-29 18:50:16 +0800

    -

--- src/test/java/MoliuTest.java
+++ src/test/java/MoliuTest.java
@@ -4,19 +4,6 @@ public class MoliuTest {
 
 	@Test
 	public void test() {
-		for (int x = -19; x <= 19; x++) {
-			for (int y = -19; y <= 19; y++) {
-				int x_ = Math.abs(x);
-				int y_ = Math.abs(y);
-				int diag = Math.min(x_, y_);
-				int d = diag * 3 + (x_ - diag) * 2 + (y_ - diag) * 2;
-				if (37 < d)
-					System.out.print("OO");
-				else
-					System.out.print("__");
-			}
-			System.out.println();
-		}
 	}
 
 }
