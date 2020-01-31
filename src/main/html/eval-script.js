4dd5b25941f4744909d4b4c7fccfee4032924e81
ywsing <ywsing1117@gmail.com>
2020-01-31 09:43:05 +0800

    use resolveLocalFileSystemURL for cordova

--- src/main/html/eval-script.js
+++ src/main/html/eval-script.js
@@ -10,23 +10,42 @@ let evalscript = (() => {
 		let e = expr != null ? ';' + expr : '';
 		let key = url + '::' + e;
 		let r = cache[key];
+		
+		if (r != null)
+		return Promise.resolve(r);
+		else if (document.URL.startsWith('file://'))
+		url = 'https://raw.githubusercontent.com/stupidsing/suite/master/src/main/html/' + url;
+		else {
+			let loader;
 
-		return r != null
-			? Promise.resolve(r)
-			: fetch(url, {
-				cache: 'default',
-				credentials: 'omit',
-				mode: 'cors',
-				method: 'GET',
-				redirect: 'follow',
-				referrer: 'no-referrer',
-			})
-			.then(response => {
-				if (response.ok) return response.text(); else throw response.statusText;
-			})
+			if (window.hasOwnProperty('resolveLocalFileSystemURL')) // for cordova
+				loader = new Promise((resolve, reject) => {					
+					window.resolveLocalFileSystemURL(
+						url,
+						file => {
+							let fr = new FileReader();
+							fr.onloadend = e => resolve(this.result);
+						fr.readAsText(file);
+						}, reject);
+				});
+			else
+				loader = fetch(url, {
+					cache: 'default',
+					credentials: 'omit',
+					method: 'GET',
+					mode: 'cors',
+					redirect: 'follow',
+					referrer: 'no-referrer',
+				})
+				.then(response => {
+					if (response.ok) return response.text(); else throw response.statusText;
+				});
+
+			return loader
 			.then(text => eval(text + e))
 			.then(result => cache[key] = result)
 			.catch(error => console.error('evalscript()', url, e, error));
+		}
 	};
 })();
 
