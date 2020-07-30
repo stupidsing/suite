d05f121e68750695c2fd20bb27c1fcddf7c0871b
ywsing <stupidsing@gmail.com>
2020-07-30 11:39:55 +0800

    -

--- src/main/sh/tools-path.sh
+++ src/main/sh/tools-path.sh
@@ -63,7 +63,7 @@ tp_geckodriver() {
 
 tp_gh() {
 	# https://github.com/cli/cli/releases/latest
-	$(cchs "echo https://github.com/cli/cli/releases/download/v0.11.0/gh_0.11.0_linux_amd64.tar.gz" @curl @tar-zxf @dir)/bin/gh $@
+	$(cchs "echo https://github.com/cli/cli/releases/download/v0.11.1/gh_0.11.1_linux_amd64.tar.gz" @curl @tar-zxf @dir)/bin/gh $@
 }
 
 tp_google_java_format() {
@@ -76,11 +76,6 @@ tp_gradle() {
 
 tp_gradle4() {
 	D=$(cchs "echo https://downloads.gradle-dn.com/distributions/gradle-4.10.3-bin.zip" @curl @unzip @dir)
-	GRADLE_HOME=${D} ${D}/bin/gradle $@
-}
-
-tp_gradle5() {
-	D=$(cchs "echo https://downloads.gradle-dn.com/distributions/gradle-5.6.4-bin.zip" @curl @unzip @dir)
 	GRADLE_HOME=${D} ${D}/bin/gradle $@
 }
 
