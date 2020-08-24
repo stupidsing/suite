2bca55d2358b23e9b716a6cb3daca2b086b54741
ywsing <ywsing1117@gmail.com>
2020-08-24 13:49:51 +0800

    -

--- src/main/sh/install-service.sh
+++ src/main/sh/install-service.sh
@@ -1,14 +1,27 @@
-source <(curl -sL https://raw.githubusercontent.com/stupidsing/suite/master/src/main/sh/cache.sh)
+################################################################################
+source <(curl -sL https://raw.githubusercontent.com/stupidsing/suite/master/src/main/sh/tools-path.sh | bash -)
 
-BASE=$(cchs "echo git@github.com:stupidsing/suite.git" @git-clone "@git-cd pwd")
+PRIMAL=$(cchs "echo git@github.com:stupidsing/primal.git" @git-clone "@git-cd pwd")
+SUITE=$(cchs "echo git@github.com:stupidsing/suite.git" @git-clone "@git-cd pwd")
 SERVE=${HOME}/serve
 
-${BASE}/build.sh
+(cd ${PRIMAL} && mvn install) &&
+${SUITE}/build.sh
 
+################################################################################
+printf "
+ProxyPass /ywsing http://127.0.0.1:8051/
+ProxyPassReverse /ywsing http://127.0.0.1:8051/
+ProxyPreserveHost On
+" | sudo tee /etc/apache2/sites-enabled/000-suite.conf > /dev/null
+
+sudo systemctl restart apache2
+
+################################################################################
 printf "[Service]
-ExecStart=${BASE}/service.sh
+ExecStart=${SUITE}/service.sh
 User=root
-WorkingDirectory=${BASE}
+WorkingDirectory=${SUITE}
 
 SuccessExitStatus=143
 TimeoutStopSec=10
@@ -20,19 +33,17 @@ WantedBy=multi-user.target
 " | sudo tee /etc/systemd/system/suite.service > /dev/null
 
 echo -e "#\x21/bin/sh
-HOME=${SERVE} ${JAVA_HOME}/bin/java -Dsuite.dir=${BASE} -cp \$(cat ${BASE}/target/classpath):${BASE}/target/suite-1.0.jar suite.ServerMain
-" > ${BASE}/service.sh
+HOME=${SERVE} ${JAVA_HOME}/bin/java -Dsuite.dir=${SUITE} -cp \$(cat ${SUITE}/target/classpath):${SUITE}/target/suite-1.0.jar suite.ServerMain
+" > ${SUITE}/service.sh
 
-chmod 755 ${BASE}/service.sh
+chmod 755 ${SUITE}/service.sh
 
 sudo systemctl daemon-reload
 
 sudo systemctl enable suite.service
-sudo systemctl start suite
+sudo systemctl restart suite
 
+################################################################################
 sudo systemctl status suite --no-pager
 sudo journalctl -f -u suite
-#curl http://localhost:8051/sse
-
-#sudo systemctl stop suite
-#sudo systemctl disable suite.service
+#curl https://pointless.online/ywsing/sse
