e3ce29b877e3fcf2de949e3e78687ee5e5fa476c
ywsing <stupidsing@gmail.com>
2020-09-19 20:28:58 +0800

    -

--- src/test/java/suite/fp/SuiteIntrinsicsTest.java
+++ src/test/java/suite/fp/SuiteIntrinsicsTest.java
@@ -11,23 +11,25 @@ public class SuiteIntrinsicsTest {
 
 	@Test
 	public void testMatch() {
-		var fp0 = "" //
-				+ "use SUITE ~ chars:\"1 + 2\" \n" //
-				+ "| suite-parse \n" //
-				+ "| suite-match_{chars:\".0 + .1\"} \n" //
-				+ "| (`$n0; _;` => n0) \n" //
-				+ "| suite-to-chars \n" //
-				+ "| cs-to-string";
+		var fp0 = """
+				use SUITE ~ chars:"1 + 2"
+				| suite-parse
+				| suite-match_{chars:".0 + .1"}
+				| (`$n0; _;` => n0)
+				| suite-to-chars
+				| cs-to-string
+				""";
 		assertEquals("49;", Suite.evaluateFun(fp0, true).toString());
 	}
 
 	@Test
 	public void testSubstitute() {
-		var fp0 = "" //
-				+ "use SUITE ~ chars:\"1 + 2\" \n" //
-				+ "| suite-parse \n" //
-				+ "| (n => suite-substitute_{chars:\"fn_{.0}\"}_{n;}) \n" //
-				+ "| suite-to-chars";
+		var fp0 = """
+				use SUITE ~ chars:"1 + 2"
+				| suite-parse
+				| (n => suite-substitute_{chars:"fn_{.0}"}_{n;})
+				| suite-to-chars
+				""";
 		var n = Suite.evaluateFun(fp0, true);
 		assertEquals("fn_{1 + 2}", Data.get(n).toString());
 	}
