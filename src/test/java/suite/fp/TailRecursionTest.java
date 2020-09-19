b94059e611a09aa477ef733ddedfae6efc0dd27f
ywsing <stupidsing@gmail.com>
2020-09-19 20:29:49 +0800

    -

--- src/test/java/suite/fp/TailRecursionTest.java
+++ src/test/java/suite/fp/TailRecursionTest.java
@@ -12,27 +12,30 @@ public class TailRecursionTest {
 	@Test
 	public void testTailRecursion() {
 		Suite.noLibraries(() -> {
-			var fp0 = "" //
-					+ "define dec := n => if (1 < n) then (dec_{n - 1}) else 0 \n" //
-					+ "~ \n" //
-					+ "dec_{65536}";
+			var fp0 = """
+					define dec := n => if (1 < n) then (dec_{n - 1}) else 0
+					~
+					dec_{65536}
+					""";
 			assertEquals(Int.of(0), Suite.evaluateFun(fp0, false));
 
-			var fp1 = "" //
-					+ "define sum := n => s => if (0 < n) then (sum_{n - 1}_{s + n}) else s ~ \n" //
-					+ "sum_{16384}_{0}";
+			var fp1 = """
+					define sum := n => s => if (0 < n) then (sum_{n - 1}_{s + n}) else s ~
+					sum_{16384}_{0}
+					""";
 			assertEquals(Int.of((1 + 16384) * 16384 / 2), Suite.evaluateFun(fp1, false));
 
-			var fp2 = "" //
-					+ "define or := \n" //
-					+ "  x => y => if x then true else y \n" //
-					+ "~ \n" //
-					+ "define fold-left := fun => init => \n" //
-					+ "  case \n" //
-					+ "  || `$h; $t` => fold-left_{fun}_{fun_{init}_{h}}_{t} \n" //
-					+ "  || anything => init \n" //
-					+ "~ \n" //
-					+ "fold-left_{`+`}_{0}_{0; 1; 2; 3; 4; 5; 6; 7; 8; 9;}";
+			var fp2 = """
+					define or :=
+					  x => y => if x then true else y
+					~
+					define fold-left := fun => init =>
+					  case
+					  || `$h; $t` => fold-left_{fun}_{fun_{init}_{h}}_{t}
+					  || anything => init
+					~
+					fold-left_{`+`}_{0}_{0; 1; 2; 3; 4; 5; 6; 7; 8; 9;}
+					""";
 			assertEquals(Int.of(45), Suite.evaluateFun(fp2, false));
 			return true;
 		});
