262e34479d95ab67de38b8722e0f2437a5d0c9e1
ywsing <stupidsing@gmail.com>
2020-09-19 20:24:22 +0800

    -

--- src/test/java/suite/fp/InterpretFunLazy0Test.java
+++ src/test/java/suite/fp/InterpretFunLazy0Test.java
@@ -14,11 +14,12 @@ public class InterpretFunLazy0Test {
 		var expr0 = "define fib := (n => if (1 < n) then (fib_{n - 1} + fib_{n - 2}) else n) ~ fib_{12}";
 		assertEquals(Int.of(144), new InterpretFunLazy0().lazy(Suite.parse(expr0)).get());
 
-		var expr1 = "" //
-				+ "define take := (i => l => if (0 < i) then (take_{i - 1}_{snd_{l}}) else (fst_{l})) ~ " //
-				+ "define zip-add := (l0 => l1 => (fst_{l0} + fst_{l1}, zip-add_{snd_{l0}}_{snd_{l1}})) ~ " //
-				+ "define fib := 0, 1, zip-add_{fib}_{snd_{fib}} ~ " //
-				+ "take_{12}_{fib}";
+		var expr1 = """
+				define take := (i => l => if (0 < i) then (take_{i - 1}_{snd_{l}}) else (fst_{l})) ~
+				define zip-add := (l0 => l1 => (fst_{l0} + fst_{l1}, zip-add_{snd_{l0}}_{snd_{l1}})) ~
+				define fib := 0, 1, zip-add_{fib}_{snd_{fib}} ~
+				take_{12}_{fib}
+				""";
 		assertEquals(Int.of(144), new InterpretFunLazy0().lazy(Suite.parse(expr1)).get());
 	}
 
