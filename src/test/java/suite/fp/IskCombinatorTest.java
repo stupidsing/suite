ee822c827951b280f5453551128c92f4c6e9a4f9
ywsing <stupidsing@gmail.com>
2020-09-19 20:25:54 +0800

    -

--- src/test/java/suite/fp/IskCombinatorTest.java
+++ src/test/java/suite/fp/IskCombinatorTest.java
@@ -9,26 +9,27 @@ import suite.node.Node;
 
 public class IskCombinatorTest {
 
-	private String isk = "" //
-			+ "define i := x => x ~ " //
-			+ "define k := x => y => x ~ " //
-			+ "define s := x => y => z => x_{z}_{y_{z}} ~ ";
+	private String isk = """
+			define i := x => x ~
+			define k := x => y => x ~
+			define s := x => y => z => x_{z}_{y_{z}} ~
+			""";
 
 	@Test
 	public void testSksk() {
 		var sksk = "s_{k}_{s}_{k}";
-		assertEquals(Suite.parse("1"), eval(isk //
-				+ "(" + sksk + ")_{1}_{2}"));
+		assertEquals(Suite.parse("1"), eval(isk + "(" + sksk + ")_{1}_{2}"));
 	}
 
 	@Test
 	public void testTf() {
-		var tf = "" //
-				+ "define t := k ~ " //
-				+ "define f := k_{i} ~ " //
-				+ "define not_ := f_{t} ~ " //
-				+ "define or_ := k ~ " //
-				+ "define and_ := f ~ ";
+		var tf = """
+				define t := k ~
+				define f := k_{i} ~
+				define not_ := f_{t} ~
+				define or_ := k ~
+				define and_ := f ~
+				""";
 
 		assertEquals(Suite.parse("1"), eval(isk + tf + "t_{1}_{2}"));
 		assertEquals(Suite.parse("2"), eval(isk + tf + "f_{1}_{2}"));
