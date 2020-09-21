86a17c0b9fe58d608adf839c6952fcc5061cef07
ywsing <ywsing1117@gmail.com>
2020-09-21 08:23:00 +0800

    -

--- src/main/sh/tools-path.sh
+++ src/main/sh/tools-path.sh
@@ -5,11 +5,11 @@ curl -sL https://raw.githubusercontent.com/stupidsing/suite/master/src/main/sh/c
 echo '
 GH_HOME=$(cchs "echo https://github.com/cli/cli/releases/download/v1.0.0/gh_1.0.0_linux_amd64.tar.gz" @curl @tar-zxf @dir)
 GIT_HD=$(cchs "echo git@github.com:stupidsing/home-data.git" @git-clone)
-GOROOT=$(cchs "echo https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz" @curl @tar-zxf @dir)
-GRADLE_HOME=$(cchs "echo https://services.gradle.org/distributions/gradle-6.5.1-bin.zip" @curl @unzip @dir)
+GOROOT=$(cchs "echo https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz" @curl @tar-zxf @dir)
+GRADLE_HOME=$(cchs "echo https://services.gradle.org/distributions/gradle-6.6.1-bin.zip" @curl @unzip @dir)
 JAVA_HOME=$(cchs "curl -sL https://jdk.java.net/15/" "grep https://download.java.net/ | grep -v sha256 | grep linux-x64 | grep \\.tar\\.gz" "cut -d\\\" -f2" @curl @tar-zxf @dir)
 M2_HOME=$(cchs "echo http://ftp.cuhk.edu.hk/pub/packages/apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz" @curl @tar-zxf @dir)
-NODE_HOME=$(cchs "echo https://nodejs.org/dist/v12.18.3/node-v12.18.3-linux-x64.tar.xz" @curl @tar-xf @dir)
+NODE_HOME=$(cchs "echo https://nodejs.org/dist/v12.18.4/node-v12.18.4-linux-x64.tar.xz" @curl @tar-xf @dir)
 PATH=${GH_HOME}/bin:${GIT_HD:9}/bin:${GOROOT}/bin:${GRADLE_HOME}/bin:${JAVA_HOME}/bin:${M2_HOME}/bin:${NODE_HOME}/bin:${PATH}
 
 tp_android_avdmanager() {
