8139214c6fbef3c9ce77c104b2a4453d99d9f7e6
ywsing <stupidsing@gmail.com>
2020-11-24 07:26:31 +0800

    -

--- src/main/java/suite/consensus/Pbft.java
+++ src/main/java/suite/consensus/Pbft.java
@@ -26,6 +26,7 @@ public class Pbft {
 
 	private Source<String> decisionf;
 	private List<General> generals;
+	private General client = null;
 
 	public interface General {
 		public Map<General, Message> round(int epoch, Map<General, Message> recvs);
@@ -46,11 +47,11 @@ public class Pbft {
 	}
 
 	public enum Mt {
-		PREP, PREPARE, COMMIT, REPLY, VIEWCHANGE, NEWVIEW,
+		REQUEST, PREP, PREPARE, COMMIT, REPLY, VIEWCHANGE, NEWVIEW,
 	}
 
 	public void play() {
-		var messages = new HashMap<General, Map<General, Message>>();
+		var messages = Map.of(generals.get(0), Map.of(client, new Message(Mt.REQUEST, ".")));
 
 		for (var round = 0; true; round++) {
 			var messages1 = new HashMap<General, Map<General, Message>>();
@@ -99,10 +100,13 @@ public class Pbft {
 						.toMap();
 			} else if (state == State.PRE____)
 				if (this == leader)
-					return Read //
-							.from(generals) //
-							.map2(r -> r, r -> new Message(Mt.PREP, decisionf.g())) //
-							.toMap();
+					if (recvs.get(client).type == Mt.REQUEST)
+						return Read //
+								.from(generals) //
+								.map2(r -> r, r -> new Message(Mt.PREP, decisionf.g())) //
+								.toMap();
+					else
+						return Map.ofEntries();
 				else if (fromLeader.type == Mt.PREP) {
 					state = State.PREPARE;
 					decision = fromLeader.decision;
@@ -126,7 +130,7 @@ public class Pbft {
 				if (nGenerals - maxTraitors <= count.f(m -> m.isDecision(Mt.COMMIT, decision))) {
 					Log_.info("EXECUTE " + decision);
 					state = State.PRE____;
-					return Map.ofEntries(entry(null, new Message(Mt.REPLY, decision)));
+					return Map.ofEntries(entry(client, new Message(Mt.REPLY, decision)));
 				} else
 					return Map.ofEntries();
 			else if (state == State.VIEWCHG)
