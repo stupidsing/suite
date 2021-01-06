e17c4cdca47b873a529e95f0c7d36b67e63c4bba
ywsing <stupidsing@gmail.com>
2021-01-06 20:36:17 +0800

    -

--- src/main/sh/cache.sh
+++ src/main/sh/cache.sh
@@ -28,15 +28,6 @@ cchs() {
 			local D=$(cat ${F})
 			local LINK=$(sh -c "readlink -f ${D}/*")
 			local F=$(cchf "printf ${LINK}")
-		elif [ "${CMD:0:3}" == "@do" ]; then
-			local D=$(cat ${F})
-			local MD5=$(printf "${D}:${CMD}" | md5sum - | cut -d" " -f1)
-			local F=$(cchf "cd ${D}/; ${CMD:4} 1>&2; echo ${D}")
-			local O=${CCACHE}/${MD5}.o U=${CCACHE}/${MD5}.u
-			mkdir -p ${U}/ ${O}/
-			choverlay_ ${D}/ ${U}/ ${O}/
-			local F=$(cchf "cd ${O}/; ${CMD:7} 1>&2; echo ${O}")
-			#choverlayx
 		elif [ "${CMD:0:6}" == "@do-cd" ]; then
 			local D=$(cat ${F})
 			local MD5=$(printf "${D}:${CMD}" | md5sum - | cut -d" " -f1)
@@ -48,6 +39,15 @@ cchs() {
 		elif [ "${CMD:0:10}" == "@do-git-cd" ]; then
 			local D=$(cat ${F})
 			local F=$(cchf "V=${D:0:8}; cd ${D:9}/; ${CMD:11} 1>&2; echo ${D}")
+		elif [ "${CMD:0:5}" == "@exec" ]; then
+			local D=$(cat ${F})
+			local MD5=$(printf "${D}:${CMD}" | md5sum - | cut -d" " -f1)
+			local F=$(cchf "cd ${D}/; ${CMD:6} 1>&2; echo ${D}")
+			local O=${CCACHE}/${MD5}.o U=${CCACHE}/${MD5}.u
+			mkdir -p ${U}/ ${O}/
+			choverlay_ ${D}/ ${U}/ ${O}/
+			local F=$(cchf "cd ${O}/; ${CMD:7} 1>&2; echo ${O}")
+			#choverlayx
 		elif [ "${CMD}" == "@docker-build" ]; then
 			local DOCKERNAME=${CMD:13:}-$(cat "${F}" | md5sum - | cut -d" " -f1)
 			local F=$(cchf "cat ${F} | docker build -q -t cchs/${DOCKERNAME} -")
