868eb73b3b02cecf543e0bcd370cc7eb637aa1ac
ywsing <stupidsing@gmail.com>
2021-01-07 20:15:31 +0800

    -

--- src/main/sh/cache.sh
+++ src/main/sh/cache.sh
@@ -42,9 +42,9 @@ cchs() {
 		elif [ "${CMD:0:5}" == "@exec" ]; then
 			local D=$(cat ${F})
 			local MD5=$(printf "${D}:${CMD}" | md5sum - | cut -d" " -f1)
-			local O=${CCACHE}/${MD5}.o U=${CCACHE}/${MD5}.u
+			local O=${CCACHE}/${MD5}.o U=${CCACHE}/${MD5}.u W=${CCACHE}/${MD5}.w
 			mkdir -p ${U}/ ${O}/
-			mountpoint -q ${O}/ || choverlay_ ${D}/ ${U}/ ${O}/
+			mountpoint -q ${O}/ || WORKDIR=${W} choverlay_ ${D}/ ${U}/ ${O}/
 			local F=$(cchf "cd ${O}/; ${CMD:6} 1>&2; echo ${O}")
 			#choverlayx
 		elif [ "${CMD}" == "@docker-build" ]; then
@@ -75,9 +75,9 @@ cchs() {
 			local G=$(cat ${F})
 			local D=${G:9}
 			local MD5=$(printf "${G}:${CMD}" | md5sum - | cut -d" " -f1)
-			local O=${CCACHE}/${MD5}.o U=${CCACHE}/${MD5}.u
+			local O=${CCACHE}/${MD5}.o U=${CCACHE}/${MD5}.u W=${CCACHE}/${MD5}.w
 			mkdir -p ${U}/ ${O}/
-			choverlay_ ${D}/ ${U}/ ${O}/
+			mountpoint -q ${O}/ || WORKDIR=${W} choverlay_ ${D}/ ${U}/ ${O}/
 			local F=$(cchf "V=${G:0:8}; cd ${D}/; ${CMD:10} 1>&2; echo ${G}")
 			#choverlayx
 		elif [ "${CMD:0:10}" == "@maven-get" ]; then
