b9ec564efe4526b90ab1cfb5972c3cf43b514737
ywsing <stupidsing@gmail.com>
2021-04-01 13:48:30 +0800

    -

--- src/test/java/suite/funp/FunpTest.java
+++ src/test/java/suite/funp/FunpTest.java
@@ -32,6 +32,7 @@ public class FunpTest {
 	public void testBind() {
 		test(1, "define a := [0, 1,] ~ if (`[0, v,]` = a) then v else 0");
 		test(0, "define a := [0, 1,] ~ if (`[1, v,]` = a) then v else 0");
+		test(2, "define s := { a: 1, b: 2, c: 3, } ~ if (`{ b: v, }` = s) then v else 0");
 		test(2, "define s := { a: 1, b: 2, c: 3, } ~ if (`{ a, b: v, c, }` = s) then v else 0");
 		test(2, "define s := { a: 1, b: 2, c: 3, } ~ if (`{ c, a, b: v, }` = s) then v else 0");
 		test(2, "define s := { a: 1, b: 2, c: 3, } ~ if (`address-of { a, b: v, c, }` = address-of s) then v else 0");
