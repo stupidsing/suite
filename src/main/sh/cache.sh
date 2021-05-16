fad3269a91f5a121636ff04c5eb1a4b67bd484f4
ywsing <stupidsing@gmail.com>
2021-05-16 11:04:04 +0800

    -

--- src/main/sh/cache.sh
+++ src/main/sh/cache.sh
@@ -14,47 +14,47 @@ cchs() {
 		shift
 		if [ "${CMD:0:2}" == "{}" ]; then
 			local DIR=$(cat ${STATE})
-			local STATE=$(exec-memoized "${DIR}${CMD:2}")
+			STATE=$(exec-memoized "${DIR}${CMD:2}")
 		elif [ "${CMD:0:3}" == "@cd" ]; then
 			local DIR=$(cat ${STATE})
-			local STATE=$(exec-memoized "cd ${DIR}/; ${CMD:4}")
+			STATE=$(exec-memoized "cd ${DIR}/; ${CMD:4}")
 		elif [ "${CMD}" == "@curl" ]; then
 			local URL=$(cat ${STATE})
 			local FILE=${DCACHE}/$(url-dir "${URL}")
 			local FILEI=${FILE}.inprogress
 			[ -f ${FILE} ] || exec-logged "curl -sL '${URL}' > ${FILEI} && mv ${FILEI} ${FILE}"
-			local STATE=$(exec-memoized "printf ${FILE}")
+			STATE=$(exec-memoized "printf ${FILE}")
 		elif [ "${CMD}" == "@dir" ]; then
 			local DIR=$(cat ${STATE})
 			local LINK=$(sh -c "readlink -f ${DIR}/*")
-			local STATE=$(exec-memoized "printf ${LINK}")
+			STATE=$(exec-memoized "printf ${LINK}")
 		elif [ "${CMD:0:6}" == "@do-cd" ]; then
 			local DIR=$(cat ${STATE})
-			local STATE=$(exec-memoized "cd ${DIR}/; ${CMD:7} 1>&2; echo ${DIR}")
+			STATE=$(exec-memoized "cd ${DIR}/; ${CMD:7} 1>&2 && echo ${DIR}")
 		elif [ "${CMD:0:9}" == "@do-chmod" ]; then
 			local FILE=$(cat ${STATE})
 			chmod ${CMD:10} ${FILE}
-			local STATE=$(exec-memoized "printf ${FILE}")
+			STATE=$(exec-memoized "printf ${FILE}")
 		elif [ "${CMD:0:10}" == "@do-git-cd" ]; then
 			local GIT=$(cat ${STATE})
 			local DIR=${GIT:9}
-			local STATE=$(exec-memoized "V=${GIT:0:8}; cd ${DIR}/; ${CMD:11} 1>&2; echo ${GIT}")
+			STATE=$(exec-memoized "V=${GIT:0:8}; cd ${DIR}/; ${CMD:11} 1>&2 && echo ${GIT}")
 		elif [ "${CMD:0:5}" == "@exec" ]; then
 			local DIR=$(cat ${STATE})
 			local PREFIX=$(md5-dir "${DIR}:${CMD}")
 			local O=${PREFIX}.o U=${PREFIX}.u W=${PREFIX}.w
 			mkdir -p ${U}/ ${O}/ ${W}/
 			mountpoint -q ${O}/ || WORKDIR=${W}/ choverlay_ ${DIR}/ ${U}/ ${O}/
-			local STATE=$(exec-memoized "cd ${O}/; ${CMD:6} 1>&2; echo ${O}")
+			STATE=$(exec-memoized "cd ${O}; ${CMD:6} 1>&2 && echo ${O}")
 			#choverlayx
 		elif [ "${CMD}" == "@docker-build" ]; then
 			local FILE=${STATE}
 			local IMAGE=${CMD:13:}-$(cat ${FILE} | md5sum - | cut -d" " -f1)
-			local STATE=$(exec-memoized "cat ${FILE} | docker build -q -t cchs/${IMAGE} -")
+			STATE=$(exec-memoized "cat ${FILE} | docker build -q -t cchs/${IMAGE} -")
 		elif [ "${CMD:0:7}" == "@git-cd" ]; then
 			local GIT=$(cat ${STATE})
 			local DIR=${GIT:9}
-			local STATE=$(exec-memoized "V=${GIT:0:8}; cd ${DIR}/; ${CMD:8}")
+			STATE=$(exec-memoized "V=${GIT:0:8}; cd ${DIR}/; ${CMD:8}")
 		elif [ "${CMD:0:10}" == "@git-clone" ]; then
 			local URL=$(cat ${STATE})
 			local BRANCH=${CMD:11}
@@ -71,7 +71,7 @@ cchs() {
 				touch ${DF}.pulltime
 			fi
 			local COMMIT=$(cd ${DF}/ && git rev-parse HEAD | cut -c1-8)
-			local STATE=$(exec-memoized "printf ${COMMIT}:${DF}")
+			STATE=$(exec-memoized "printf ${COMMIT}:${DF}")
 		elif [ "${CMD:0:9}" == "@git-exec" ]; then
 			local GIT=$(cat ${STATE})
 			local DIR=${GIT:9}
@@ -79,7 +79,7 @@ cchs() {
 			local O=${PREFIX}.o U=${PREFIX}.u W=${PREFIX}.w
 			mkdir -p ${U}/ ${O}/ ${W}/
 			mountpoint -q ${O}/ || WORKDIR=${W}/ choverlay_ ${DIR}/ ${U}/ ${O}/
-			local STATE=$(exec-memoized "V=${GIT:0:8}; cd ${DIR}/; ${CMD:10} 1>&2; echo ${GIT}")
+			STATE=$(exec-memoized "V=${GIT:0:8}; cd ${DIR}/; ${CMD:10} 1>&2 && echo ${GIT}")
 			#choverlayx
 		elif [ "${CMD:0:10}" == "@maven-get" ]; then
 			#local REPO=https://repo.maven.apache.org/maven2
@@ -93,31 +93,31 @@ cchs() {
 			local FILE=${DCACHE}/$(url-dir "${URL}")
 			local FILEI=${FILE}.inprogress
 			[ -f ${FILE} ] || exec-logged curl -sL "${URL}" > ${FILEI} && mv ${FILEI} ${FILE}
-			local STATE=$(exec-memoized "printf ${DF}")
+			STATE=$(exec-memoized "printf ${DF}")
 		elif [ "${CMD:0:6}" == "@mkdir" ]; then
 			local NAME=$(cat ${STATE})
 			local DIR=${DCACHE}/$(url-dir "${NAME}")
 			mkdir -p ${DIR}
-			local STATE=$(exec-memoized "printf ${DIR}")
+			STATE=$(exec-memoized "printf ${DIR}")
 		elif [ "${CMD:0:5}" == "@tar-" ]; then
 			local OPT=${CMD:5}
 			local FILE=$(cat ${STATE})
 			local DIR=${FILE}.d
 			local DIRI=${DIR}.inprogress
 			[ -d ${DIR} ] || exec-logged "mkdir -p ${DIRI} && tar ${OPT} ${FILE} -C ${DIRI} && mv ${DIRI} ${DIR}"
-			local STATE=$(exec-memoized "printf ${DIR}")
+			STATE=$(exec-memoized "printf ${DIR}")
 		elif [ "${CMD:0:6}" == "@unzip" ]; then
 			local FILE=$(cat ${STATE})
 			local DIR=${FILE}.d
 			local DIRI=${DIR}.inprogress
 			local TARGET=${DIRI}/${CMD:7}
 			[ -d ${DIR} ] || exec-logged "mkdir -p ${TARGET} && unzip -d ${TARGET} -q ${FILE} && mv ${DIRI} ${DIR}"
-			local STATE=$(exec-memoized "printf ${DIR}")
+			STATE=$(exec-memoized "printf ${DIR}")
 		else
-			local STATE=$(exec-memoized "cat ${STATE} | ${CMD}")
+			STATE=$(exec-memoized "cat ${STATE} | ${CMD}")
 		fi
 		if [ ${?} != 0 ]; then
-			echo FAIL >2
+			echo FAIL >&2
 			return 1
 		fi
 	done
