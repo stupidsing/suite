3e207476201505013f2220835eb49fb86499cb1a
ywsing <stupidsing@gmail.com>
2021-06-30 06:48:37 +0800

    -

--- src/main/js/parse.js
+++ src/main/js/parse.js
@@ -903,8 +903,8 @@ let typeJSON = typeStructCompletedOf({
 
 let typeObject = typeStructCompletedOf({
 	'.assign': typeLambdaOf(newRef(), newRef()),
-	'.entries': typeLambdaOf(newRef(), typeArrayOf(typeTupleOf(typeString, newRef()))),
-	'.fromEntries': typeLambdaOf(typeArrayOf(typeTupleOf(typeString, newRef())), newRef()),
+	'.entries': typeLambdaOf(typeStructOf({}), typeArrayOf(typeTupleOf(typeString, newRef()))),
+	'.fromEntries': typeLambdaOf(typeArrayOf(typeTupleOf(typeString, newRef())), typeStructOf({})),
 	'.keys': typeLambdaOf(typeStructOf({}), typeArrayOf(typeString)),
 });
 
