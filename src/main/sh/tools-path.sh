6afd1852fb6db54b8abfc8b6ec136fffd9d3208a
stupidsing <stupidsing@gmail.com>
2022-05-05 00:43:14 +0000

    node

--- src/main/sh/tools-path.sh
+++ src/main/sh/tools-path.sh
@@ -5,7 +5,7 @@ curl -sL https://raw.githubusercontent.com/stupidsing/suite/master/src/main/sh/c
 ECLIPSE_V=2022-03
 GH_V=2.9.0
 MAVEN_V=3.8.5
-NODE_V=16.14.2
+NODE_V=16.15.0
 
 echo '
 AWS_DIST=$(cchs "echo https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" @curl @unzip "@exec ./aws/install -i . -b .")/aws/dist
