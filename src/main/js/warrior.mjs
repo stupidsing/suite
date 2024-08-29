cf07db6bf498529368a65eef903f97bc9a94470d
stupidsing <stupidsing@gmail.com>
2024-08-29 05:18:58 +0000

    -

--- src/main/js/warrior.mjs
+++ src/main/js/warrior.mjs
@@ -23,6 +23,38 @@ let subnetClass = () => {
 	// let findIdByName = name => `aws ec2 describe-subnets --filter Name:${name} | jq -r .Subnets[0].SubnetId`;
 	let findIdByName = name => `cat ${stateFilename(name)}.json | jq -r .SubnetId`;
 
+	let update = (resource, state) => {
+		let { class_, name, attributes } = resource;
+		let commands = [];
+		let attributes0;
+
+		if (state != null) {
+			attributes0 = state;
+		} else {
+			commands.push(...create(resource));
+			attributes0 = { CidrBlock: attributes['CidrBlock'] };
+		}
+
+		let { attributes: attributes1 } = resource ?? { class_, name, attributes: {} };
+		let SubnetId = '$(' + findIdByName(name) + ')';
+
+		{
+			let key = 'CidrBlock';
+			if (attributes0[key] !== attributes1[key]) {
+				return [...delete_(name, SubnetId), ...update(resource, null)];
+			}
+		}
+
+		{
+			let key = 'VpcId';
+			if (attributes0[key] !== attributes1[key]) {
+				return [...delete_(name, SubnetId), ...update(resource, null)];
+			}
+		}
+
+		return commands;
+	};
+
 	return {
 		create,
 		delete_,
@@ -32,37 +64,7 @@ let subnetClass = () => {
 		refresh: (name, id) => [
 			`aws ec2 describe-subnets --subnet-ids ${id} | jq .Subnets[0] > ${stateFilename(name)}.json`,
 		],
-		update: (resource, state) => {
-			let { class_, name, attributes } = resource;
-			let commands = [];
-			let attributes0;
-
-			if (state != null) {
-				attributes0 = state;
-			} else {
-				commands.push(...create(resource));
-				attributes0 = { CidrBlock: attributes['CidrBlock'] };
-			}
-
-			let { attributes: attributes1 } = resource ?? { class_, name, attributes: {} };
-			let SubnetId = '$(' + findIdByName(name) + ')';
-
-			{
-				let key = 'CidrBlock';
-				if (attributes0[key] !== attributes1[key]) {
-					return [...delete_(name, SubnetId), ...create(name)];
-				}
-			}
-
-			{
-				let key = 'VpcId';
-				if (attributes0[key] !== attributes1[key]) {
-					return [...delete_(name, SubnetId), ...create(name)];
-				}
-			}
-
-			return commands;
-		},
+		update,
 	};
 };
 
@@ -79,6 +81,60 @@ let vpcClass = () => {
 
 	// let findIdByName = name => `aws ec2 describe-vpcs --filter Name:${name} | jq -r .Vpcs[0].VpcId`;
 	let findIdByName = name => `cat ${stateFilename(name)}.json | jq -r .VpcId`;
+
+	let update = (resource, state) => {
+		let { class_, name, attributes } = resource;
+		let commands = [];
+		let attributes0;
+
+		if (state != null) {
+			attributes0 = state;
+		} else {
+			commands.push(...create(resource));
+			attributes0 = { CidrBlockAssociationSet: [{ CidrBlock: attributes['CidrBlockAssociationSet'][0]['CidrBlock'] }] };
+		}
+
+		let { attributes: attributes1 } = resource ?? { class_, name, attributes: {} };
+		let VpcId = '$(' + findIdByName(name) + ')';
+
+		{
+			let key = 'CidrBlockAssociationSet';
+			let map0 = Object.fromEntries(attributes0[key].map(({ CidrBlock, AssociationId }) => [CidrBlock, AssociationId]));
+			let map1 = Object.fromEntries(attributes1[key].map(({ CidrBlock, AssociationId }) => [CidrBlock, AssociationId]));
+			for (let [CidrBlock, AssociationId] of Object.entries(map0)) {
+				if (!map1.hasOwnProperty(CidrBlock)) {
+					commands.push(`aws ec2 disassociate-vpc-cidr-block --vpc-id ${VpcId} --association-id ${AssociationId}`);
+				}
+			}
+			for (let [CidrBlock, AssociationId] of Object.entries(map1)) {
+				if (!map0.hasOwnProperty(CidrBlock)) {
+					commands.push(`aws ec2 associate-vpc-cidr-block --vpc-id ${VpcId} --cidr-block ${CidrBlock}`);
+				}
+			}
+		}
+		{
+			let key = 'EnableDnsHostnames';
+			if (attributes0[key] !== attributes1[key]) {
+				commands.push(
+					`aws ec2 modify-vpc-attribute --vpc-id ${VpcId} ${attributes1[key] ? `--` : `--no-`}enable-dns-hostnames`,
+					`echo ${attributes1[key]} > ${stateFilename(name)}.EnableDnsHostnames.json`);
+			}
+		}
+		{
+			let key = 'EnableDnsSupport';
+			if (attributes0[key] !== attributes1[key]) {
+				commands.push(
+					`aws ec2 modify-vpc-attribute --vpc-id ${VpcId} ${attributes1[key] ? `--` : `--no-`}enable-dns-support`,
+					`echo ${attributes1[key]} > ${stateFilename(name)}.EnableDnsSupport.json`);
+			}
+		}
+
+		if (resource == null) {
+			commands.push(...delete_(name, VpcId));
+		}
+
+		return commands;
+	};
 
 	return {
 		create,
@@ -99,59 +155,7 @@ let vpcClass = () => {
 			`aws ec2 describe-vpc-attribute --vpc-id ${id} --attribute enableDnsHostnames | jq -r .EnableDnsHostnames.Value > ${stateFilename(name)}.EnableDnsHostnames.json`,
 			`aws ec2 describe-vpc-attribute --vpc-id ${id} --attribute enableDnsSupport | jq -r .EnableDnsSupport.Value > ${stateFilename(name)}.EnableDnsSupport.json`,
 		],
-		update: (resource, state) => {
-			let { class_, name, attributes } = resource;
-			let commands = [];
-			let attributes0;
-
-			if (state != null) {
-				attributes0 = state;
-			} else {
-				commands.push(...create(resource));
-				attributes0 = { CidrBlockAssociationSet: [{ CidrBlock: attributes['CidrBlockAssociationSet'][0]['CidrBlock'] }] };
-			}
-
-			let { attributes: attributes1 } = resource ?? { class_, name, attributes: {} };
-			let VpcId = '$(' + findIdByName(name) + ')';
-
-			{
-				let key = 'CidrBlockAssociationSet';
-				let map0 = Object.fromEntries(attributes0[key].map(({ CidrBlock, AssociationId }) => [CidrBlock, AssociationId]));
-				let map1 = Object.fromEntries(attributes1[key].map(({ CidrBlock, AssociationId }) => [CidrBlock, AssociationId]));
-				for (let [CidrBlock, AssociationId] of Object.entries(map0)) {
-					if (!map1.hasOwnProperty(CidrBlock)) {
-						commands.push(`aws ec2 disassociate-vpc-cidr-block --vpc-id ${VpcId} --association-id ${AssociationId}`);
-					}
-				}
-				for (let [CidrBlock, AssociationId] of Object.entries(map1)) {
-					if (!map0.hasOwnProperty(CidrBlock)) {
-						commands.push(`aws ec2 associate-vpc-cidr-block --vpc-id ${VpcId} --cidr-block ${CidrBlock}`);
-					}
-				}
-			}
-			{
-				let key = 'EnableDnsHostnames';
-				if (attributes0[key] !== attributes1[key]) {
-					commands.push(
-						`aws ec2 modify-vpc-attribute --vpc-id ${VpcId} ${attributes1[key] ? `--` : `--no-`}enable-dns-hostnames`,
-						`echo ${attributes1[key]} > ${stateFilename(name)}.EnableDnsHostnames.json`);
-				}
-			}
-			{
-				let key = 'EnableDnsSupport';
-				if (attributes0[key] !== attributes1[key]) {
-					commands.push(
-						`aws ec2 modify-vpc-attribute --vpc-id ${VpcId} ${attributes1[key] ? `--` : `--no-`}enable-dns-support`,
-						`echo ${attributes1[key]} > ${stateFilename(name)}.EnableDnsSupport.json`);
-				}
-			}
-
-			if (resource == null) {
-				commands.push(...delete_(name, VpcId));
-			}
-
-			return commands;
-		},
+		update,
 	};
 };
 
