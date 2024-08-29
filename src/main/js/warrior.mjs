23ac60fc904cb1a803a5691f43908a637dfe4259
stupidsing <stupidsing@gmail.com>
2024-08-29 08:17:51 +0000

    -

--- src/main/js/warrior.mjs
+++ src/main/js/warrior.mjs
@@ -11,7 +11,7 @@ let stateFilename = name => `${stateDir}/${name}`;
 
 let subnetClass = () => {
 	let create = ({ name, attributes: { VpcId } }) => [
-		`aws ec2 create-subnet --cidr-block ${CidrBlock} --tag-specifications '${JSON.stringify([
+		`aws ec2 create-subnet --tag-specifications '${JSON.stringify([
 			{ ResourceType: 'vpc', Tags: [{ Key: 'Name', Value: name }] }
 		])} --vpc-id ${VpcId}' | jq .Vpc > ${stateFilename(name)}.json`,
 	];
@@ -20,34 +20,26 @@ let subnetClass = () => {
 		`aws ec2 delete-vpc --subnet-id ${state.SubnetId}`, `rm -f ${stateFilename(name)}.json`,
 	];
 
-	// let findIdByName = name => `aws ec2 describe-subnets --filter Name:${name} | jq -r .Subnets[0].SubnetId`;
-	let findIdByName = name => `cat ${stateFilename(name)}.json | jq -r .SubnetId`;
-
-	let update = (resource, state) => {
-		let { class_, name, attributes } = resource;
+	let update = (name, state, resource) => {
+		let { attributes } = resource;
 		let commands = [];
-		let attributes0;
 
-		if (state != null) {
-			attributes0 = state;
-		} else {
+		if (state == null) {
 			commands.push(...create(resource));
-			attributes0 = { CidrBlock: attributes['CidrBlock'], VpcId: attributes['VpcId'] };
+			state = { CidrBlock: attributes['CidrBlock'], VpcId: attributes['VpcId'] };
 		}
-
-		let { attributes: attributes1 } = resource ?? { class_, name, attributes: {} };
 
 		{
 			let key = 'CidrBlock';
-			if (attributes0[key] !== attributes1[key]) {
-				return [...delete_(name, state), ...update(resource, null)];
+			if (state[key] !== attributes[key]) {
+				return [...delete_(name, state), ...update(name, null, resource)];
 			}
 		}
 
 		{
 			let key = 'VpcId';
-			if (attributes0[key] !== attributes1[key]) {
-				return [...delete_(name, state), ...update(resource, null)];
+			if (state[key] !== attributes[key]) {
+				return [...delete_(name, state), ...update(name, null, resource)];
 			}
 		}
 
@@ -57,7 +49,6 @@ let subnetClass = () => {
 	return {
 		create,
 		delete_,
-		findIdByName,
 		getKey: resource => [resource.name, resource.CidrBlock, resource.VpcId],
 		getState: ({ name }) => readJsonIfExists(`${stateFilename(name)}.json`),
 		refresh: (name, id) => [
@@ -78,28 +69,22 @@ let vpcClass = () => {
 		`aws ec2 delete-vpc --vpc-id ${state.VpcId}`, `rm -f ${stateFilename(name)}.json`,
 	];
 
-	// let findIdByName = name => `aws ec2 describe-vpcs --filter Name:${name} | jq -r .Vpcs[0].VpcId`;
-	let findIdByName = name => `cat ${stateFilename(name)}.json | jq -r .VpcId`;
-
-	let update = (resource, state) => {
-		let { class_, name, attributes } = resource;
+	let update = (name, state, resource) => {
+		let { attributes } = resource;
 		let commands = [];
-		let attributes0;
 
-		if (state != null) {
-			attributes0 = state;
-		} else {
+		if (state == null) {
 			commands.push(...create(resource));
-			attributes0 = { CidrBlockAssociationSet: [{ CidrBlock: attributes['CidrBlockAssociationSet'][0]['CidrBlock'] }] };
+			state = { CidrBlockAssociationSet: [{ CidrBlock: attributes['CidrBlockAssociationSet'][0]['CidrBlock'] }] };
 		}
 
-		let { attributes: attributes1 } = resource ?? { class_, name, attributes: {} };
-		let VpcId = '$(' + findIdByName(name) + ')';
+		// let VpcId = `$(aws ec2 describe-vpcs --filter Name:${name} | jq -r .Vpcs[0].VpcId)`;
+		let VpcId = `$(cat ${stateFilename(name)}.json | jq -r .VpcId)`;
 
 		{
 			let key = 'CidrBlockAssociationSet';
-			let map0 = Object.fromEntries(attributes0[key].map(({ CidrBlock, AssociationId }) => [CidrBlock, AssociationId]));
-			let map1 = Object.fromEntries(attributes1[key].map(({ CidrBlock, AssociationId }) => [CidrBlock, AssociationId]));
+			let map0 = Object.fromEntries(state[key].map(({ CidrBlock, AssociationId }) => [CidrBlock, AssociationId]));
+			let map1 = Object.fromEntries(attributes[key].map(({ CidrBlock, AssociationId }) => [CidrBlock, AssociationId]));
 			for (let [CidrBlock, AssociationId] of Object.entries(map0)) {
 				if (!map1.hasOwnProperty(CidrBlock)) {
 					commands.push(`aws ec2 disassociate-vpc-cidr-block --vpc-id ${VpcId} --association-id ${AssociationId}`);
@@ -113,18 +98,18 @@ let vpcClass = () => {
 		}
 		{
 			let key = 'EnableDnsHostnames';
-			if (attributes0[key] !== attributes1[key]) {
+			if (state[key] !== attributes[key]) {
 				commands.push(
-					`aws ec2 modify-vpc-attribute --vpc-id ${VpcId} ${attributes1[key] ? `--` : `--no-`}enable-dns-hostnames`,
-					`echo ${attributes1[key]} > ${stateFilename(name)}.EnableDnsHostnames.json`);
+					`aws ec2 modify-vpc-attribute --vpc-id ${VpcId} ${attributes[key] ? `--` : `--no-`}enable-dns-hostnames`,
+					`echo ${attributes[key]} > ${stateFilename(name)}.EnableDnsHostnames.json`);
 			}
 		}
 		{
 			let key = 'EnableDnsSupport';
-			if (attributes0[key] !== attributes1[key]) {
+			if (state[key] !== attributes[key]) {
 				commands.push(
-					`aws ec2 modify-vpc-attribute --vpc-id ${VpcId} ${attributes1[key] ? `--` : `--no-`}enable-dns-support`,
-					`echo ${attributes1[key]} > ${stateFilename(name)}.EnableDnsSupport.json`);
+					`aws ec2 modify-vpc-attribute --vpc-id ${VpcId} ${attributes[key] ? `--` : `--no-`}enable-dns-support`,
+					`echo ${attributes[key]} > ${stateFilename(name)}.EnableDnsSupport.json`);
 			}
 		}
 
@@ -138,7 +123,6 @@ let vpcClass = () => {
 	return {
 		create,
 		delete_,
-		findIdByName,
 		getKey: resource => [resource.name],
 		getState: ({ name }) => {
 			let stateFilename_ = stateFilename(name);
@@ -158,7 +142,12 @@ let vpcClass = () => {
 	};
 };
 
-let resource = {
+let objectByClass = {
+	subnet: subnetClass(),
+	vpc: vpcClass(),
+};
+
+let vpc = {
 	class_: 'vpc',
 	name: 'npt-cloud-vpc',
 	attributes: {
@@ -168,11 +157,16 @@ let resource = {
 	},
 };
 
-let objectByClass = {
-	subnet: subnetClass(),
-	vpc: vpcClass(),
+let subnet = {
+	class_: 'subnet',
+	name: 'npt-cloud-subnet',
+	attributes: {
+		VpcId: vpc.VpcId,
+	},
 };
 
-let object = objectByClass[resource.class_];
-
-console.log(object.update(resource, object.getState(resource)).join('\n'));
+for (let resource of [vpc, subnet]) {
+	let { getState, update } = objectByClass[resource.class_];
+	let state = getState(resource);
+	console.log(update(resource.name, state, resource).join('\n'));
+}
